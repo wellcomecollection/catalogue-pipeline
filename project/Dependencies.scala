01b6107e75e13f7ef75ceb87c3f7720c4fe5bc28
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-08 16:23:30 +0000

    Port the display library from the catalogue repo

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,17 +16,49 @@ object WellcomeDependencies {
   )
 }
 
-object Dependencies {
+object ExternalDependencies {
   lazy val versions = new {
-    val scalaCsv = "1.3.5"
-    val scalatest = "3.0.1"
+    val finatra             = "18.11.0"
+    val guice               = "4.2.0"
+    val scalacheck          = "1.13.4"
+    val scalacheckShapeless = "1.1.6"
+    val scalacsv            = "1.3.5"
+    val scalatest           = "3.0.1"
   }
 
-  val sharedDependencies = Seq(
+  val guiceDependencies = Seq(
+    "com.google.inject" % "guice" % versions.guice,
+    "com.google.inject.extensions" % "guice-testlib" % versions.guice % "test"
+  )
+
+  val scalacheckDependencies = Seq(
+    "org.scalacheck" %% "scalacheck" % versions.scalacheck % "test",
+    "com.github.alexarchambault" %% "scalacheck-shapeless_1.13" % versions.scalacheckShapeless % "test"
+  )
+
+  val scalacsvDependencies = Seq(
+    "com.github.tototoshi" %% "scala-csv" % versions.scalacsv
+  )
+
+  val scalatestDependencies = Seq(
     "org.scalatest" %% "scalatest" % versions.scalatest % "test"
   )
 
-  val internalModelDependencies = Seq(
-    "com.github.tototoshi" %% "scala-csv" % versions.scalaCsv
-  ) ++ WellcomeDependencies.jsonLibrary
+  val swaggerDependencies = Seq(
+    "com.jakehschwartz" %% "finatra-swagger" % versions.finatra
+  )
+}
+
+object CatalogueDependencies {
+  val sharedDependencies =
+    ExternalDependencies.scalatestDependencies
+
+  val internalModelDependencies =
+    ExternalDependencies.scalacsvDependencies ++
+    WellcomeDependencies.jsonLibrary
+
+  val displayModelDependencies =
+    ExternalDependencies.swaggerDependencies ++
+    ExternalDependencies.guiceDependencies ++
+    ExternalDependencies.scalacheckDependencies
 }
