09eff951dee12e68c067e7da2457e8a51d7acd1c
Alex Chan <a.chan@wellcome.ac.uk>
2019-02-12 13:48:51 +0000

    Fix the Travis logic around Makefiles

--- .travis/test_run_job.py
+++ .travis/test_run_job.py
@@ -12,11 +12,13 @@ def repo():
     yield Repository(".sbt_metadata")
 
 
-@pytest.mark.parametrize('project_name, path, should_run_project', [
-    ("id_minter", "snapshots/Makefile", False),
-    ("id_minter", "pipeline/Makefile", True),
+@pytest.mark.parametrize('project_name, changed_paths, should_run_project', [
+    ("id_minter", ["snapshots/Makefile"], False),
+    ("id_minter", ["pipeline/Makefile"], True),
+    ("id_minter", ["common/Makefile", "pipeline/Makefile"], True),
+    ("elasticsearch", ["common/Makefile"], True),
+    ("elasticsearch", ["common/Makefile", "pipeline/Makefile"], True),
 ])
-def test_should_run_sbt_project(repo, project_name, path, should_run_project):
-    project = repo.get_project(project_name)
-    result = should_run_sbt_project(project, changed_paths=[path])
+def test_should_run_sbt_project(repo, project_name, changed_paths, should_run_project):
+    result = should_run_sbt_project(repo, project_name, changed_paths=changed_paths)
     assert result == should_run_project
