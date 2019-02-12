015edf6ac46060387edd0df1b5773014ab0a5bf0
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-02-12 14:27:31 +0000

    Apply auto-formatting rules

--- .travis/test_run_job.py
+++ .travis/test_run_job.py
@@ -12,10 +12,13 @@ def repo():
     yield Repository(".sbt_metadata")
 
 
-@pytest.mark.parametrize('project_name, path, should_run_project', [
-    ("id_minter", "snapshots/Makefile", False),
-    ("id_minter", "pipeline/Makefile", True),
-])
+@pytest.mark.parametrize(
+    "project_name, path, should_run_project",
+    [
+        ("id_minter", "snapshots/Makefile", False),
+        ("id_minter", "pipeline/Makefile", True),
+    ],
+)
 def test_should_run_sbt_project(repo, project_name, path, should_run_project):
     project = repo.get_project(project_name)
     result = should_run_sbt_project(project, changed_paths=[path])
