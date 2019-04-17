ba78bdcb5f4ea6f33e3d82c5ae95600b3cc322d9
mklander <6085323+mklander@users.noreply.github.com>
2019-04-17 15:27:46 +0100

    Release secondary-miro-merges  (#37)
    * release secondary-miro-merges to production api
    
    * typo in comment.
    
    * Apply auto-formatting rules

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,7 +1,7 @@
-module "catalogue_pipeline_20190205" {
+module "catalogue_pipeline_20190416" {
   source = "stack"
 
-  namespace = "catalogue-20190205"
+  namespace = "catalogue-20190416"
 
   release_label = "latest"
 
@@ -18,26 +18,19 @@ module "catalogue_pipeline_20190205" {
   # reindexer topic names.
 
   sierra_adapter_topic_names = [
-    # "${local.sierra_reindexer_topic_name}",
+    "${local.sierra_reindexer_topic_name}",
     "${local.sierra_merged_bibs_topic_name}",
-
     "${local.sierra_merged_items_topic_name}",
   ]
   miro_adapter_topic_names = [
-    # "${local.miro_reindexer_topic_name}",
+    "${local.miro_reindexer_topic_name}",
     "${local.miro_updates_topic_name}",
   ]
-
   # Elasticsearch
-
-  es_works_index = "v2-2019-02-05-extra-subject-labels"
-
+  es_works_index = "v2-2019-04-16-secondary-miro-merge-candidates"
   # RDS
-
   rds_ids_access_security_group_id = "${local.rds_access_security_group_id}"
-
   # Adapter VHS
-
   vhs_sierra_read_policy = "${local.vhs_sierra_read_policy}"
   vhs_miro_read_policy   = "${local.vhs_miro_read_policy}"
 }
