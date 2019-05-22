87f1a3f79b376c6bd1b7fff84d69135ab15892c0
Alex Chan <alex@alexwlchan.net>
2019-05-22 09:20:39 +0100

    Get the ingestor tests passing

--- pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/services/IngestorWorkerService.scala
+++ pipeline/ingestor/src/main/scala/uk/ac/wellcome/platform/ingestor/services/IngestorWorkerService.scala
@@ -5,7 +5,6 @@ import com.amazonaws.services.sqs.model.Message
 import com.sksamuel.elastic4s.http.ElasticClient
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.elasticsearch.{ElasticsearchIndexCreator, WorksIndex}
-import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.message.MessageStream
 import uk.ac.wellcome.models.work.internal.IdentifiedBaseWork
 import uk.ac.wellcome.platform.ingestor.config.models.IngestorConfig
