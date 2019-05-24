80aff814645cf0a0898a6dc64d76376dcc01fabc
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-24 10:13:46 +0100

    add reporting access to topics

--- infrastructure/critical/vhs_miro_migration.tf
+++ infrastructure/critical/vhs_miro_migration.tf
@@ -2,4 +2,6 @@ module "vhs_miro_migration" {
   source     = "./vhs"
   name       = "miro-migration"
   account_id = "${data.aws_caller_identity.current.account_id}"
+
+  s3_cross_account_access_accounts = ["269807742353"]
 }
--- infrastructure/critical/vhs_sierra_items.tf
+++ infrastructure/critical/vhs_sierra_items.tf
@@ -2,4 +2,6 @@ module "vhs_sierra_items" {
   source     = "./vhs"
   name       = "sourcedata-sierra-items"
   account_id = "${data.aws_caller_identity.current.account_id}"
+
+  s3_cross_account_access_accounts = ["269807742353"]
 }
