88a81de9305c1fa11dcba0444e55e87aeb8ddea0
James A Gorrie <j.gorrie@wellcome.ac.uk>
2019-05-30 11:47:36 +0100

    add table driven tests for InstantRange

---
+++ common/internal_model/src/test/scala/uk/ac/wellcome/models/work/generators/InstantRangeGenerators.scala
@@ -0,0 +1,19 @@
+package uk.ac.wellcome.models.work.generators
+
+import java.time.{LocalDateTime, ZoneOffset}
+
+import uk.ac.wellcome.models.work.internal._
+
+trait InstantRangeGenerators {
+  // This is used to make the tests more readable.
+  def createInstantRangeWith(label: String,
+                             from: String,
+                             to: String,
+                             inferred: Boolean) =
+    InstantRange(
+      label,
+      from = LocalDateTime.parse(from).toInstant(ZoneOffset.UTC),
+      to = LocalDateTime.parse(to).toInstant(ZoneOffset.UTC),
+      inferred = inferred
+    )
+}
--- common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/InstantRangeTest.scala
+++ common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/InstantRangeTest.scala
@@ -1,44 +1,44 @@
 package uk.ac.wellcome.models.work.internal
 
-import java.time.{LocalDateTime, ZoneOffset}
-
+import org.scalatest.prop.TableDrivenPropertyChecks._
 import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.models.work.generators.InstantRangeGenerators
 
-class InstantRangeTest extends FunSpec with Matchers {
+class InstantRangeTest
+    extends FunSpec
+    with Matchers
+    with InstantRangeGenerators {
 
-  describe("parse") {
-    it(
-      "parses a year like 1909 into a range of the beginning and end of that year") {
-      val label = "1909"
-      val expected = InstantRange(
-        label = "1909",
-        from = LocalDateTime.parse("1909-01-01T00:00").toInstant(ZoneOffset.UTC),
-        to = LocalDateTime
-          .parse("1909-12-31T23:59:59.999999999")
-          .toInstant(ZoneOffset.UTC),
-        inferred = false
-      )
+  it("parses valid date labels") {
+    val instantRange = createInstantRangeWith(
+      "1909",
+      "1909-01-01T00:00",
+      "1909-12-31T23:59:59.999999999",
+      inferred = false)
 
-      InstantRange.parse(label) shouldBe Some(expected)
+    val inferredInstantRange = createInstantRangeWith(
+      "[2123]",
+      "2123-01-01T00:00",
+      "2123-12-31T23:59:59.999999999",
+      inferred = true)
+
+    forAll(
+      Table(
+        ("label", "parsedInstantRange"),
+        ("1909", instantRange),
+        ("[2123]", inferredInstantRange),
+      )) { (label, parsedInstantRange) =>
+      InstantRange.parse(label) shouldBe Some(parsedInstantRange)
     }
+  }
 
-    it(
-      "parses a year like [1918] into a range of the beginning and end of that year and marks it as inferred") {
-      val label = "[1918]"
-      val expected = InstantRange(
-        label = "[1918]",
-        from = LocalDateTime.parse("1918-01-01T00:00").toInstant(ZoneOffset.UTC),
-        to = LocalDateTime
-          .parse("1918-12-31T23:59:59.999999999")
-          .toInstant(ZoneOffset.UTC),
-        inferred = true
-      )
-
-      InstantRange.parse(label) shouldBe Some(expected)
-    }
-
-    it("Should return None if we don't understand the label") {
-      val label = "nineteen sixty what, nineteen sixty who"
+  it("does not parse invalid date labels") {
+    forAll(
+      Table(
+        "label",
+        "nineteen sixty what, nineteen sixty who",
+        "[21233]",
+      )) { label =>
       InstantRange.parse(label) shouldBe None
     }
   }
