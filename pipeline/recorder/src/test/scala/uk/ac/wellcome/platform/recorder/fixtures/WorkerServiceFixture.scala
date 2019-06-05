f51f31ea3aa4194d93000212db4c8c8b7f88fc73
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-05 11:09:48 +0100

    First attempt at a working recorder

--- pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/fixtures/WorkerServiceFixture.scala
+++ pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/fixtures/WorkerServiceFixture.scala
@@ -3,44 +3,51 @@ package uk.ac.wellcome.platform.recorder.fixtures
 import uk.ac.wellcome.fixtures.TestWith
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.fixtures.Messaging
-import uk.ac.wellcome.messaging.fixtures.SNS.Topic
 import uk.ac.wellcome.messaging.fixtures.SQS.Queue
+import uk.ac.wellcome.messaging.memory.MemoryMessageSender
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.recorder.services.RecorderWorkerService
-import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
-import uk.ac.wellcome.storage.fixtures.LocalVersionedHybridStore
-import uk.ac.wellcome.storage.fixtures.S3.Bucket
-import uk.ac.wellcome.storage.vhs.EmptyMetadata
+import uk.ac.wellcome.storage.memory.{MemoryObjectStore, MemoryVersionedDao}
+import uk.ac.wellcome.storage.vhs.{EmptyMetadata, Entry, VersionedHybridStore}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait WorkerServiceFixture extends LocalVersionedHybridStore with Messaging {
+trait WorkerServiceFixture extends Messaging {
+  type RecorderEntry = Entry[String, EmptyMetadata]
+
+  type RecorderDao = MemoryVersionedDao[String, RecorderEntry]
+  type RecorderStore = MemoryObjectStore[TransformedBaseWork]
+  type RecorderVhs = VersionedHybridStore[String, TransformedBaseWork, EmptyMetadata]
+
+  def createDao: RecorderDao =
+    MemoryVersionedDao[String, RecorderEntry]()
+
+  def createStore: RecorderStore = new RecorderStore()
+
+  def createVhs(dao: RecorderDao, store: RecorderStore): RecorderVhs =
+    new RecorderVhs {
+      override protected val versionedDao: RecorderDao = dao
+      override protected val objectStore: RecorderStore = store
+    }
+
   def withWorkerService[R](
-    table: Table,
-    storageBucket: Bucket,
-    topic: Topic,
-    queue: Queue)(testWith: TestWith[RecorderWorkerService, R]): R =
+    dao: RecorderDao,
+    store: RecorderStore,
+    messageSender: MemoryMessageSender,
+    queue: Queue)(testWith: TestWith[RecorderWorkerService[String], R]): R =
     withActorSystem { implicit actorSystem =>
-      withMetricsSender() { metricsSender =>
-        withSNSWriter(topic) { snsWriter =>
-          withTypeVHS[TransformedBaseWork, EmptyMetadata, R](
-            bucket = storageBucket,
-            table = table) { versionedHybridStore =>
-            withMessageStream[TransformedBaseWork, R](
-              queue = queue,
-              metricsSender = metricsSender) { messageStream =>
-              val workerService = new RecorderWorkerService(
-                versionedHybridStore = versionedHybridStore,
-                messageStream = messageStream,
-                snsWriter = snsWriter
-              )
+      val vhs = createVhs(dao, store)
 
-              workerService.run()
+      withMessageStream[TransformedBaseWork, R](queue) { messageStream =>
+        val workerService = new RecorderWorkerService(
+          versionedHybridStore = vhs,
+          messageStream = messageStream,
+          messageSender = messageSender
+        )
 
-              testWith(workerService)
-            }
-          }
-        }
+        workerService.run()
+
+        testWith(workerService)
       }
     }
 }
