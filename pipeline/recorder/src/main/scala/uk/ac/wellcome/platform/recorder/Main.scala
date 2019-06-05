fa1b94384ba16dfe2b8d44c7244d6786be92bcbd
Alex Chan <a.chan@wellcome.ac.uk>
2019-06-05 11:09:48 +0100

    First attempt at a working recorder

--- pipeline/recorder/src/main/scala/uk/ac/wellcome/platform/recorder/Main.scala
+++ pipeline/recorder/src/main/scala/uk/ac/wellcome/platform/recorder/Main.scala
@@ -4,31 +4,28 @@ import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import com.typesafe.config.Config
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.messaging.typesafe.{MessagingBuilder, SNSBuilder}
+import uk.ac.wellcome.messaging.typesafe.{BigMessagingBuilder, SNSBuilder}
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.recorder.services.RecorderWorkerService
+import uk.ac.wellcome.storage.streaming.CodecInstances._
 import uk.ac.wellcome.storage.typesafe.VHSBuilder
 import uk.ac.wellcome.storage.vhs.EmptyMetadata
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
 
-import scala.concurrent.ExecutionContext
-
 object Main extends WellcomeTypesafeApp {
   runWithConfig { config: Config =>
     implicit val actorSystem: ActorSystem =
       AkkaBuilder.buildActorSystem()
-    implicit val executionContext: ExecutionContext =
-      AkkaBuilder.buildExecutionContext()
     implicit val materializer: ActorMaterializer =
       AkkaBuilder.buildActorMaterializer()
 
     new RecorderWorkerService(
       versionedHybridStore =
-        VHSBuilder.buildVHS[TransformedBaseWork, EmptyMetadata](config),
+        VHSBuilder.buildVHS[String, TransformedBaseWork, EmptyMetadata](config),
       messageStream =
-        MessagingBuilder.buildMessageStream[TransformedBaseWork](config),
-      snsWriter = SNSBuilder.buildSNSWriter(config)
+        BigMessagingBuilder.buildMessageStream[TransformedBaseWork](config),
+      messageSender = SNSBuilder.buildSNSMessageSender(config, subject = "Sent from the recorder")
     )
   }
 }
