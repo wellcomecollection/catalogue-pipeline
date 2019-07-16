408f2327b42ec4b1facab606358238018407733d
Nick Ward <ward.nickjames@gmail.com>
2019-07-16 16:31:44 +0100

    Parser trait
    This introduces a new trait for parsing some input into some type T using the fastparse library

---
+++ common/internal_model/src/main/scala/uk/ac/wellcome/models/parse/Parser.scala
@@ -0,0 +1,28 @@
+package uk.ac.wellcome.models.parse
+
+import fastparse._
+
+import uk.ac.wellcome.models.work.internal.InstantRange
+
+/**
+ *  Trait for parsing some input into T with the FastParse library
+ */
+trait Parser[T] {
+
+  /**
+   *  The FastParse parser combinator applied to the input
+   */
+  def parser[_ : P] : P[T]
+
+  /**
+   *  Parse some input
+   *
+   *  @param input the input string
+   *  @return Some(output) if parse was successful, None otherwise
+   */
+  def apply(input: String) : Option[T] =
+    parse(input, parser(_)) match {
+      case Parsed.Success(value, _) => Some(value)
+      case Parsed.Failure(_, _, _) => None
+    }
+}
