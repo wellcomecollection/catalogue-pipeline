4187c4df258b429ae9362edc32fd4b775e90da42
James Gorrie <j.gorrie@wellcome.ac.uk>
2019-07-18 14:05:18 +0100

    use SimpleStringQuery for boosted / msm queries

--- api/api/src/main/scala/uk/ac/wellcome/platform/api/models/WorkQuery.scala
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/models/WorkQuery.scala
@@ -5,10 +5,6 @@ import com.sksamuel.elastic4s.requests.searches.queries.{
   Query,
   SimpleStringQuery
 }
-import com.sksamuel.elastic4s.requests.searches.queries.matches.{
-  MultiMatchQuery,
-  MultiMatchQueryBuilderType
-}
 
 sealed trait WorkQuery {
   val queryString: String
@@ -16,6 +12,15 @@ sealed trait WorkQuery {
 }
 
 object WorkQuery {
+  val defaultMSM = "60%"
+  val defaultBoostedFields = Seq(
+    ("*", Some(1.0)),
+    ("title", Some(9.0)),
+    ("subjects*", Some(8.0)),
+    ("genres*", Some(8.0)),
+    ("description*", Some(5.0)),
+    ("contributors*", Some(2.0))
+  )
 
   case class SimpleQuery(queryString: String) extends WorkQuery {
     override def query(): SimpleStringQuery = {
@@ -24,40 +29,32 @@ object WorkQuery {
   }
 
   case class MSMQuery(queryString: String) extends WorkQuery {
-    override def query(): MultiMatchQuery = {
-      multiMatchQuery(queryString)
-        .fields("*")
-        .matchType(MultiMatchQueryBuilderType.CROSS_FIELDS)
-        .minimumShouldMatch("60%")
+    override def query(): SimpleStringQuery = {
+      SimpleStringQuery(
+        queryString,
+        fields = Seq(("*", Some(1.0))),
+        minimumShouldMatch = Some(defaultMSM))
     }
   }
 
   case class BoostQuery(queryString: String) extends WorkQuery {
-    override def query(): MultiMatchQuery = {
-      multiMatchQuery(queryString)
-        .fields(
-          "*",
-          "title^9",
-          "subjects*^8",
-          "genres*^8",
-          "description*^5",
-          "contributors*^2")
-        .matchType(MultiMatchQueryBuilderType.CROSS_FIELDS)
+    override def query(): SimpleStringQuery = {
+      SimpleStringQuery(
+        queryString,
+        fields = defaultBoostedFields,
+        lenient = Some(true)
+      )
     }
   }
 
   case class MSMBoostQuery(queryString: String) extends WorkQuery {
-    override def query(): MultiMatchQuery = {
-      multiMatchQuery(queryString)
-        .fields(
-          "*",
-          "title^9",
-          "subjects*^8",
-          "genres*^8",
-          "description*^5",
-          "contributors*^2")
-        .matchType(MultiMatchQueryBuilderType.CROSS_FIELDS)
-        .minimumShouldMatch("60%")
+    override def query(): SimpleStringQuery = {
+      SimpleStringQuery(
+        queryString,
+        fields = defaultBoostedFields,
+        lenient = Some(true),
+        minimumShouldMatch = Some(defaultMSM)
+      )
     }
   }
 }
