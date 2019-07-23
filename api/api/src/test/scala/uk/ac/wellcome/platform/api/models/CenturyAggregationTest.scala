3b48918be208e482c7b47ffa7c7efad27ba2d48b
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-07-23 10:42:39 +0000

    Apply auto-formatting rules

--- api/api/src/test/scala/uk/ac/wellcome/platform/api/models/CenturyAggregationTest.scala
+++ api/api/src/test/scala/uk/ac/wellcome/platform/api/models/CenturyAggregationTest.scala
@@ -5,12 +5,11 @@ import java.time.LocalDate
 import org.scalatest.FunSpec
 
 class CenturyAggregationTest extends FunSpec {
-  it ("maps across centuries") {
+  it("maps across centuries") {
     CenturyAggregation(
       LocalDate.of(1600, 1, 1),
       LocalDate.of(2000, 1, 1)
     ) map { century =>
-
-    }
+      }
   }
 }
