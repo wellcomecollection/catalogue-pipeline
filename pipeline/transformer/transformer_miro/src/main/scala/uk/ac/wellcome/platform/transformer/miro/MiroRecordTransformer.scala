5e179e8624477fd88094a25e9df9068ff7fa2b7e
Nick Ward <ward.nickjames@gmail.com>
2019-07-29 15:45:14 +0100

    Fix test

--- pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/MiroRecordTransformer.scala
+++ pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/MiroRecordTransformer.scala
@@ -72,6 +72,7 @@ class MiroRecordTransformer
         otherIdentifiers = getOtherIdentifiers(miroRecord),
         mergeCandidates = List(),
         title = title,
+        uniformTitle = None,
         workType = getWorkType,
         description = description,
         physicalDescription = None,
