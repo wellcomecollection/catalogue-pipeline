3ac52d833ed2789daba05d48f0beaa464b37be7c
Nick Ward <ward.nickjames@gmail.com>
2019-07-29 12:54:32 +0100

    Tests for uniform title transformation

---
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraUniformTitleTest.scala
@@ -0,0 +1,57 @@
+package uk.ac.wellcome.platform.transformer.sierra.transformers
+
+import org.scalatest.{FunSpec, Matchers}
+import uk.ac.wellcome.platform.transformer.sierra.source.{
+  MarcSubfield,
+  VarField
+}
+import uk.ac.wellcome.platform.transformer.sierra.generators.{
+  MarcGenerators,
+  SierraDataGenerators
+}
+
+class SierraUniformTitleTest
+  extends FunSpec
+  with Matchers 
+  with MarcGenerators
+  with SierraDataGenerators {
+
+  val title = "Shepherd of Salisbury-Plain."
+
+  val altTitle = "Basic documents."
+
+  it("should extract uniform title when there is 240") {
+    val varFields =  createVarField(title) :: Nil
+    uniformTitle(varFields) shouldBe Some(title)
+  }
+
+  it("should ignore uniform title when there no 240") {
+    val varFields =  createVarField(title, tag = "253") :: Nil
+    uniformTitle(varFields) shouldBe None
+  }
+
+  it("should ignore uniform title when there no 'a' subfield") {
+    val varFields =  createVarField(title, contentTag = "b") :: Nil
+    uniformTitle(varFields) shouldBe None
+  }
+
+  it("should use first content varfield when multiple 240s defined") {
+    val varFields =  List(createVarField(title), createVarField("x"))
+    uniformTitle(varFields) shouldBe Some(title)
+  }
+
+  val transformer = new SierraUniformTitle {}
+
+  private def uniformTitle(varFields: List[VarField]) =
+    transformer getUniformTitle (createSierraBibDataWith(varFields = varFields))
+
+  private def createVarField(
+    content: String,
+    tag: String = "240",
+    contentTag: String = "a"
+  ) =
+    createVarFieldWith(
+      tag,
+      "1",
+      MarcSubfield(tag = contentTag, content = content) :: Nil)
+}
