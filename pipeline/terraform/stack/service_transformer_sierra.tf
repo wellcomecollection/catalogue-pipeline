56dc786045d0909a74e77cb3271625726ccf04da
James Gorrie <j.gorrie@wellcome.ac.uk>
2019-07-31 17:32:38 +0100

    manual labour

--- pipeline/terraform/stack/service_transformer_sierra.tf
+++ pipeline/terraform/stack/service_transformer_sierra.tf
@@ -26,7 +26,7 @@ module "sierra_transformer" {
 
   service_name = "${var.namespace}_sierra_transformer"
 
-  container_image = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/transformer_sierra:6ce8da1ec2c47aa883895da8b1e95f57b44dd87e"
+  container_image = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/transformer_sierra:8f0e2f6b610808cc1284b10700b4e345f4437277"
 
   security_group_ids = [
     "${module.egress_security_group.sg_id}",
