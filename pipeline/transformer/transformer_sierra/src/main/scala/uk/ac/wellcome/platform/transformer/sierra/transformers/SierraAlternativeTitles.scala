ca9f822e56ecaf72441560de8b1127ae42469547
Nick Ward <ward.nickjames@gmail.com>
2019-08-01 11:53:26 +0100

    Add comment about how alternativeTitles is generated

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraAlternativeTitles.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraAlternativeTitles.scala
@@ -5,6 +5,15 @@ import uk.ac.wellcome.platform.transformer.sierra.source.VarField
 
 trait SierraAlternativeTitles extends MarcUtils {
 
+  // Populate work:alternativeTitles
+  //
+  // The following fields are used as possible alternative titles:
+  // * 240 $a https://www.loc.gov/marc/bibliographic/bd240.html
+  // * 130 $a http://www.loc.gov/marc/bibliographic/bd130.html
+  // * 246 $a https://www.loc.gov/marc/bibliographic/bd246.html
+  //
+  // 246 is only used when indicator2 is not equal to 6, as this is used for
+  // the work:lettering field
   def getAlternativeTitles(bibData: SierraBibData): List[String] =
     getAlternativeTitleFields(bibData).flatMap {
       getSubfieldContents(_, Some("a"))
