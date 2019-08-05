36d5931856191bfd563304dc5082b8e8f64f9f21
James Gorrie <j.gorrie@wellcome.ac.uk>
2019-08-05 13:36:19 +0100

    add new experiemnt logger container

--- pipeline/terraform/stack/locals.tf
+++ pipeline/terraform/stack/locals.tf
@@ -1,4 +1,4 @@
 locals {
   logstash_transit_service_name = "${var.namespace}-logstash-transit"
-  logstash_transit_image        = "wellcome/logstash_transit:experiment1641290719"
+  logstash_transit_image        = "wellcome/logtstash_transit:experiment1332"
 }
