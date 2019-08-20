fc9dba3e2a2751361f9461a180f095f1da4a15c4
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-08-20 12:09:31 +0100

    add new stack and index for alternativeTitles

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,9 +1,9 @@
-module "catalogue_pipeline_20190808" {
+module "catalogue_pipeline_20190816" {
   source = "stack"
 
-  namespace = "catalogue-20190808"
+  namespace = "catalogue-20190816"
 
-  release_label = "prod"
+  release_label = "latest"
 
   account_id      = "${data.aws_caller_identity.current.account_id}"
   aws_region      = "${local.aws_region}"
@@ -19,20 +19,19 @@ module "catalogue_pipeline_20190808" {
   # reindexer topic names.
 
   sierra_adapter_topic_names = [
-    # "${local.sierra_reindexer_topic_name}",
+    "${local.sierra_reindexer_topic_name}",
     "${local.sierra_merged_bibs_topic_name}",
     "${local.sierra_merged_items_topic_name}",
   ]
   miro_adapter_topic_names = [
-    # "${local.miro_reindexer_topic_name}",
+    "${local.miro_reindexer_topic_name}",
     "${local.miro_updates_topic_name}",
   ]
   # Elasticsearch
-  es_works_index = "v2-2019-04-26-contributors-label-from-multiple-subfields"
+  es_works_index = "v2-20190816-alternative-titles"
   # RDS
   rds_ids_access_security_group_id = "${local.rds_access_security_group_id}"
   # Adapter VHS
   vhs_sierra_read_policy = "${local.vhs_sierra_read_policy}"
   vhs_miro_read_policy   = "${local.vhs_miro_read_policy}"
 }
-
