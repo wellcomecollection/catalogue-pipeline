2576cfcd2c9274750648884cb0cefd4d4937892e
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-08-20 12:14:37 +0000

    Apply auto-formatting rules

--- pipeline/transformer/transformer_miro/src/test/scala/uk/ac/wellcome/platform/transformer/miro/fixtures/MiroVHSRecordReceiverFixture.scala
+++ pipeline/transformer/transformer_miro/src/test/scala/uk/ac/wellcome/platform/transformer/miro/fixtures/MiroVHSRecordReceiverFixture.scala
@@ -7,7 +7,10 @@ import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.transformer.miro.generators.MiroRecordGenerators
 import uk.ac.wellcome.platform.transformer.miro.models.MiroMetadata
-import uk.ac.wellcome.platform.transformer.miro.services.{MiroVHSRecordReceiver, HybridRecord}
+import uk.ac.wellcome.platform.transformer.miro.services.{
+  HybridRecord,
+  MiroVHSRecordReceiver
+}
 import uk.ac.wellcome.platform.transformer.miro.source.MiroRecord
 import uk.ac.wellcome.fixtures.TestWith
 
@@ -18,25 +21,28 @@ import uk.ac.wellcome.bigmessaging.fixtures.BigMessagingFixture
 import uk.ac.wellcome.messaging.sns.{NotificationMessage, SNSConfig}
 
 import uk.ac.wellcome.storage.{ObjectLocation, Version}
-import uk.ac.wellcome.storage.store.{Store, HybridStoreEntry}
+import uk.ac.wellcome.storage.store.{HybridStoreEntry, Store}
 import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
 import uk.ac.wellcome.storage.store.memory.MemoryStore
 import uk.ac.wellcome.storage.streaming.Codec._
 
-trait MiroVHSRecordReceiverFixture extends BigMessagingFixture with MiroRecordGenerators {
-  
+trait MiroVHSRecordReceiverFixture
+    extends BigMessagingFixture
+    with MiroRecordGenerators {
+
   type MiroStore = Store[
     Version[String, Int],
     HybridStoreEntry[MiroRecord, MiroMetadata]
   ]
 
-  private val store : MiroStore = new MemoryStore(Map.empty)
+  private val store: MiroStore = new MemoryStore(Map.empty)
 
   def withMiroVHSRecordReceiver[R](topic: Topic, bucket: Bucket)(
     testWith: TestWith[MiroVHSRecordReceiver[SNSConfig], R]): R = {
-    withSqsBigMessageSender[TransformedBaseWork, R](bucket, topic, snsClient) { msgSender =>
-      val recordReceiver = new MiroVHSRecordReceiver(msgSender, store)
-      testWith(recordReceiver)
+    withSqsBigMessageSender[TransformedBaseWork, R](bucket, topic, snsClient) {
+      msgSender =>
+        val recordReceiver = new MiroVHSRecordReceiver(msgSender, store)
+        testWith(recordReceiver)
     }
   }
 
