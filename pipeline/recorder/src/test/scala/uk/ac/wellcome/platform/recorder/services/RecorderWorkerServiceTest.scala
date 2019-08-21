42fdef65968e45a4a8957a95f549d935c9fae1cf
Nick Ward <ward.nickjames@gmail.com>
2019-08-21 17:53:16 +0100

    Tests

--- pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/services/RecorderWorkerServiceTest.scala
+++ pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/services/RecorderWorkerServiceTest.scala
@@ -1,51 +1,49 @@
-/*
 package uk.ac.wellcome.platform.recorder.services
 
-import com.gu.scanamo.Scanamo
-import io.circe.Decoder
-import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+//import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
+import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
+
 import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.models.Implicits._
-import uk.ac.wellcome.messaging.fixtures.SNS.Topic
-import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
-import uk.ac.wellcome.messaging.fixtures.{Messaging, SQS}
 import uk.ac.wellcome.models.work.generators.WorksGenerators
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
 import uk.ac.wellcome.platform.recorder.fixtures.WorkerServiceFixture
-import uk.ac.wellcome.storage.fixtures.LocalDynamoDb.Table
-import uk.ac.wellcome.storage.fixtures.LocalVersionedHybridStore
-import uk.ac.wellcome.storage.fixtures.S3.Bucket
-import uk.ac.wellcome.storage.vhs.HybridRecord
+import uk.ac.wellcome.platform.recorder.EmptyMetadata
+
+import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
+import uk.ac.wellcome.storage.{Version, Identified}
+import uk.ac.wellcome.storage.store.HybridStoreEntry
+
+import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
+import uk.ac.wellcome.messaging.fixtures.SQS
+import uk.ac.wellcome.bigmessaging.fixtures.BigMessagingFixture
 
 class RecorderWorkerServiceTest
     extends FunSpec
     with Matchers
     with MockitoSugar
     with Akka
-    with LocalVersionedHybridStore
     with SQS
     with ScalaFutures
-    with Messaging
+    with BigMessagingFixture
     with MetricsSenderFixture
-    with IntegrationPatience
+    //with IntegrationPatience
     with WorkerServiceFixture
     with WorksGenerators {
 
   it("records an UnidentifiedWork") {
-    withLocalDynamoDbTable { table =>
-      withLocalSnsTopic { topic =>
-        withLocalS3Bucket { storageBucket =>
-          withLocalSqsQueue { queue =>
-            withLocalSnsTopic { topic =>
-              val work = createUnidentifiedWork
-              sendMessage[TransformedBaseWork](queue = queue, obj = work)
-              withWorkerService(table, storageBucket, topic, queue) { _ =>
-                eventually {
-                  assertStoredSingleWork(topic, table, work)
-                }
+    withLocalSnsTopic { topic =>
+      withLocalS3Bucket { storageBucket =>
+        withLocalSqsQueue { queue =>
+          withLocalSnsTopic { topic =>
+            val work = createUnidentifiedWork
+            sendMessage[TransformedBaseWork](queue = queue, obj = work)
+            withWorkerService(storageBucket, topic, queue) { case  (service, vhs) =>
+              eventually {
+                assertWorkStored(vhs, work)
               }
             }
           }
@@ -55,17 +53,15 @@ class RecorderWorkerServiceTest
   }
 
   it("stores UnidentifiedInvisibleWorks") {
-    withLocalDynamoDbTable { table =>
-      withLocalSnsTopic { topic =>
-        withLocalS3Bucket { storageBucket =>
-          withLocalSqsQueue { queue =>
-            withLocalSnsTopic { topic =>
-              withWorkerService(table, storageBucket, topic, queue) { service =>
-                val invisibleWork = createUnidentifiedInvisibleWork
-                sendMessage[TransformedBaseWork](queue = queue, invisibleWork)
-                eventually {
-                  assertStoredSingleWork(topic, table, invisibleWork)
-                }
+    withLocalSnsTopic { topic =>
+      withLocalS3Bucket { storageBucket =>
+        withLocalSqsQueue { queue =>
+          withLocalSnsTopic { topic =>
+            withWorkerService(storageBucket, topic, queue) { case (service, vhs) =>
+              val invisibleWork = createUnidentifiedInvisibleWork
+              sendMessage[TransformedBaseWork](queue = queue, invisibleWork)
+              eventually {
+                assertWorkStored(vhs, invisibleWork)
               }
             }
           }
@@ -77,22 +73,19 @@ class RecorderWorkerServiceTest
   it("doesn't overwrite a newer work with an older work") {
     val olderWork = createUnidentifiedWork
     val newerWork = olderWork.copy(version = 10, title = "A nice new thing")
-
-    withLocalDynamoDbTable { table =>
-      withLocalSnsTopic { topic =>
-        withLocalS3Bucket { storageBucket =>
-          withLocalSqsQueue { queue =>
-            withLocalSnsTopic { topic =>
-              withWorkerService(table, storageBucket, topic, queue) { _ =>
-                sendMessage[TransformedBaseWork](queue = queue, newerWork)
+    withLocalSnsTopic { topic =>
+      withLocalS3Bucket { storageBucket =>
+        withLocalSqsQueue { queue =>
+          withLocalSnsTopic { topic =>
+            withWorkerService(storageBucket, topic, queue) { case (service, vhs) =>
+              sendMessage[TransformedBaseWork](queue = queue, newerWork)
+              eventually {
+                assertWorkStored(vhs, newerWork)
+                sendMessage[TransformedBaseWork](
+                  queue = queue,
+                  obj = olderWork)
                 eventually {
-                  assertStoredSingleWork(topic, table, newerWork)
-                  sendMessage[TransformedBaseWork](
-                    queue = queue,
-                    obj = olderWork)
-                  eventually {
-                    assertStoredSingleWork(topic, table, newerWork)
-                  }
+                  assertWorkStored(vhs, newerWork)
                 }
               }
             }
@@ -105,23 +98,19 @@ class RecorderWorkerServiceTest
   it("overwrites an older work with an newer work") {
     val olderWork = createUnidentifiedWork
     val newerWork = olderWork.copy(version = 10, title = "A nice new thing")
-
-    withLocalDynamoDbTable { table =>
-      withLocalS3Bucket { storageBucket =>
-        withLocalSqsQueue { queue =>
-          withLocalSnsTopic { topic =>
-            withWorkerService(table, storageBucket, topic, queue) { _ =>
-              sendMessage[TransformedBaseWork](queue = queue, obj = olderWork)
+    withLocalS3Bucket { storageBucket =>
+      withLocalSqsQueue { queue =>
+        withLocalSnsTopic { topic =>
+          withWorkerService(storageBucket, topic, queue) { case (service, vhs) =>
+            sendMessage[TransformedBaseWork](queue = queue, obj = olderWork)
+            eventually {
+              assertWorkStored(vhs, olderWork)
+              sendMessage[TransformedBaseWork](queue = queue, obj = newerWork)
               eventually {
-                assertStoredSingleWork(topic, table, olderWork)
-                sendMessage[TransformedBaseWork](queue = queue, obj = newerWork)
-                eventually {
-                  assertStoredSingleWork(
-                    topic,
-                    table,
-                    newerWork,
-                    expectedVhsVersion = 2)
-                }
+                assertWorkStored(
+                  vhs,
+                  newerWork,
+                  expectedVhsVersion = 1)
               }
             }
           }
@@ -130,21 +119,20 @@ class RecorderWorkerServiceTest
     }
   }
 
+  /*
   it("fails if saving to S3 fails") {
-    withLocalDynamoDbTable { table =>
-      withLocalSnsTopic { topic =>
-        val badBucket = Bucket(name = "bad-bukkit")
-        withLocalSqsQueueAndDlq {
-          case QueuePair(queue, dlq) =>
-            withWorkerService(table, badBucket, topic, queue) { _ =>
-              val work = createUnidentifiedWork
-              sendMessage[TransformedBaseWork](queue = queue, obj = work)
-              eventually {
-                assertQueueEmpty(queue)
-                assertQueueHasSize(dlq, 1)
-              }
+    withLocalSnsTopic { topic =>
+      val badBucket = Bucket(name = "bad-bukkit")
+      withLocalSqsQueueAndDlq {
+        case QueuePair(queue, dlq) =>
+          withWorkerService(badBucket, topic, queue) { case (service, vhs) =>
+            val work = createUnidentifiedWork
+            sendMessage[TransformedBaseWork](queue = queue, obj = work)
+            eventually {
+              assertQueueEmpty(queue)
+              assertQueueHasSize(dlq, 1)
             }
-        }
+          }
       }
     }
   }
@@ -167,27 +155,17 @@ class RecorderWorkerServiceTest
       }
     }
   }
+  */
 
-  private def assertStoredSingleWork[T <: TransformedBaseWork](
-    topic: Topic,
-    table: Table,
-    expectedWork: T,
-    expectedVhsVersion: Int = 1)(implicit decoder: Decoder[T]) = {
-    val actualRecords: List[HybridRecord] =
-      Scanamo
-        .scan[HybridRecord](dynamoDbClient)(table.name)
-        .map(_.right.get)
+  private def assertWorkStored[T <: TransformedBaseWork](
+    vhs: RecorderVhs,
+    work: T,
+    expectedVhsVersion: Int = 0) = {
 
-    actualRecords.size shouldBe 1
-
-    val hybridRecord: HybridRecord = actualRecords.head
-    hybridRecord.id shouldBe expectedWork.sourceIdentifier.toString
-    hybridRecord.version shouldBe expectedVhsVersion
-
-    val actualEntry = getObjectFromS3[T](hybridRecord.location)
-
-    actualEntry shouldBe expectedWork
-    getMessages[T](topic) should contain(expectedWork)
+    val id = work.sourceIdentifier.toString
+    vhs.getLatest(id) shouldBe
+      Right(Identified(
+        Version(id, expectedVhsVersion),
+        HybridStoreEntry(work, EmptyMetadata())))
   }
 }
-*/
