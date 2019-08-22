1e88791cf8bfce2e70953069ae83c951a9697890
Nick Ward <ward.nickjames@gmail.com>
2019-08-22 15:27:31 +0100

    Integration test

--- pipeline/recorder/src/main/scala/uk/ac/wellcome/platform/recorder/RecorderVhs.scala
+++ pipeline/recorder/src/main/scala/uk/ac/wellcome/platform/recorder/RecorderVhs.scala
@@ -4,13 +4,15 @@ import java.util.UUID
 import scala.util.{Failure, Success, Try}
 import org.scanamo.DynamoFormat
 import com.typesafe.config.Config
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.json.JsonUtil._
 
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
-import uk.ac.wellcome.storage.dynamo.DynamoHashEntry
+import uk.ac.wellcome.storage.dynamo.{DynamoHashEntry, DynamoConfig}
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.store.{
   HybridIndexedStoreEntry,
@@ -76,34 +78,40 @@ class RecorderHybridStore(
 
 object RecorderVhs {
 
-  type IndexEntry = HybridIndexedStoreEntry[ObjectLocation, EmptyMetadata]
-
-  type HashEntry = DynamoHashEntry[String, Int, IndexEntry]
-
   def build(config: Config): RecorderVhs = {
     // TODO: from where do we get the correct values for this?
     val objectLocationPrefix = ObjectLocationPrefix("namespace", "path")
-    val dynamoConfig =
-      DynamoBuilder.buildDynamoConfig(config, namespace = "namespace")
+    val dynamoConfig = DynamoBuilder.buildDynamoConfig(config, namespace = "namespace")
+    implicit val s3Client = S3Builder.buildS3Client(config)
+    implicit val dynamoClient = DynamoBuilder.buildDynamoClient(config)
+    RecorderVhs.build(objectLocationPrefix, dynamoConfig, dynamoClient, s3Client)
+  }
 
-    // For some reason these dont get derived with scanamo auto derivation
-    implicit def indexEntryFormat: DynamoFormat[IndexEntry] =
-      DynamoFormat[IndexEntry]
-    implicit def hashEntryFormat: DynamoFormat[HashEntry] =
-      DynamoFormat[HashEntry]
-
-    implicit val s3Client =
-      S3Builder.buildS3Client(config)
-    implicit val dynamoClient =
-      DynamoBuilder.buildDynamoClient(config)
-
-    val s3Store =
-      S3TypedStore[TransformedBaseWork]
-    val dynamoIndexStore =
-      new DynamoHashStore[String, Int, IndexEntry](dynamoConfig)
-
+  def build(
+    objectLocationPrefix: ObjectLocationPrefix,
+    dynamoConfig: DynamoConfig,
+    dynamoClient: AmazonDynamoDB,
+    s3Client: AmazonS3): RecorderVhs = {
     new RecorderVhs(
-      new RecorderHybridStore(objectLocationPrefix, dynamoIndexStore, s3Store)
+      new RecorderHybridStore(
+        objectLocationPrefix,
+        RecorderVhs.buildIndexStore(dynamoClient, dynamoConfig),
+        RecorderVhs.buildTypedStore(s3Client)
+      )
     )
+  }
+
+  def buildIndexStore(dynamoClient: AmazonDynamoDB, dynamoConfig: DynamoConfig) = {
+    type IndexEntry = HybridIndexedStoreEntry[ObjectLocation, EmptyMetadata]
+    type HashEntry = DynamoHashEntry[String, Int, IndexEntry]
+    implicit val client = dynamoClient
+    implicit def idxFormat: DynamoFormat[IndexEntry] = DynamoFormat[IndexEntry]
+    implicit def hashFormat: DynamoFormat[HashEntry] = DynamoFormat[HashEntry]
+    new DynamoHashStore[String, Int, IndexEntry](dynamoConfig)
+  }
+
+  def buildTypedStore(s3Client: AmazonS3) = {
+    implicit val client = s3Client
+    S3TypedStore[TransformedBaseWork]
   }
 }
--- pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/RecorderFeatureTest.scala
+++ pipeline/recorder/src/test/scala/uk/ac/wellcome/platform/recorder/RecorderFeatureTest.scala
@@ -1,5 +1,6 @@
-/*
 package uk.ac.wellcome.platform.recorder
+
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType
 
 import org.scalatest.concurrent.IntegrationPatience
 import org.scalatest.{FunSpec, Matchers}
@@ -8,32 +9,44 @@ import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.models.work.generators.WorksGenerators
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.recorder.fixtures.WorkerServiceFixture
-import uk.ac.wellcome.storage.{Version, Identified}
-import uk.ac.wellcome.storage.store.HybridStoreEntry
 
 import uk.ac.wellcome.bigmessaging.fixtures.BigMessagingFixture
+import uk.ac.wellcome.storage.fixtures.DynamoFixtures
+import uk.ac.wellcome.storage.dynamo.DynamoConfig
+import uk.ac.wellcome.storage.ObjectLocationPrefix
 
-class RecorderFeatureTest
+class RecorderIntegrationTest
     extends FunSpec
     with Matchers
     with IntegrationPatience
+    with DynamoFixtures
     with BigMessagingFixture
     with WorkerServiceFixture
     with WorksGenerators {
 
+  override def createTable(table: DynamoFixtures.Table): DynamoFixtures.Table = {
+    createTableWithHashKey(
+      table,
+      keyName = "id",
+      keyType = ScalarAttributeType.S
+    )
+  }
+
   it("receives a transformed Work, and saves it to the VHS") {
     val work = createUnidentifiedWork
-
     withLocalSqsQueue { queue =>
-      withMemoryMessageSender { msgSender =>
-        withRecorderVhs { vhs => 
-          withWorkerService(queue, vhs, msgSender) { service =>
-            sendMessage[TransformedBaseWork](queue = queue, obj = work)
-
-            eventually {
-              val key = Version(work.sourceIdentifier.toString, 0)
-              vhs.get(key) shouldBe
-                Right(Identified(key, HybridStoreEntry(work, EmptyMetadata())))
+      withLocalS3Bucket { bucket =>
+        withLocalDynamoDbTable { table =>
+          withMemoryMessageSender { msgSender =>
+            val vhs = RecorderVhs.build(
+              ObjectLocationPrefix("test", "root"),
+              DynamoConfig(table.name, table.index),
+              dynamoClient,
+              s3Client,
+            )
+            withWorkerService(queue, vhs, msgSender) { service =>
+              sendMessage[TransformedBaseWork](queue = queue, obj = work)
+              eventually {}
             }
           }
         }
@@ -41,4 +54,3 @@ class RecorderFeatureTest
     }
   }
 }
-*/
