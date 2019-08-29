db425f1584db144f97482220a39b4e5b9ccd37fc
Nick Ward <ward.nickjames@gmail.com>
2019-08-29 17:15:07 +0100

    Update HybridRecordReceiverFixture

--- pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/fixtures/HybridRecordReceiverFixture.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/fixtures/HybridRecordReceiverFixture.scala
@@ -1,35 +1,28 @@
 package uk.ac.wellcome.platform.transformer.sierra.fixtures
 
-import com.amazonaws.services.sns.AmazonSNS
-import uk.ac.wellcome.messaging.fixtures.SNS.Topic
-import uk.ac.wellcome.messaging.fixtures.{Messaging, SNS}
+import scala.concurrent.ExecutionContext.Implicits.global
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.transformer.sierra.services.HybridRecordReceiver
-import uk.ac.wellcome.storage.ObjectStore
-import uk.ac.wellcome.storage.fixtures.S3.Bucket
 import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.models.transformable.SierraTransformable
 import uk.ac.wellcome.models.Implicits._
-//import uk.ac.wellcome.json.JsonUtil._
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import uk.ac.wellcome.models.transformable.SierraTransformable
 
-trait HybridRecordReceiverFixture extends Messaging with SNS {
+import uk.ac.wellcome.bigmessaging.fixtures.VHSFixture
+import uk.ac.wellcome.messaging.fixtures.SNS.Topic
+import uk.ac.wellcome.messaging.sns.SNSConfig
+
+import uk.ac.wellcome.storage.fixtures.S3Fixtures.Bucket
+
+trait HybridRecordReceiverFixture extends VHSFixture[SierraTransformable] {
+
   def withHybridRecordReceiver[R](
+    vhs: VHS,
     topic: Topic,
-    bucket: Bucket,
-    snsClient: AmazonSNS = snsClient
-  )(testWith: TestWith[HybridRecordReceiver, R])(
-    implicit objectStore: ObjectStore[SierraTransformable]): R =
-    withMessageWriter[TransformedBaseWork, R](
-      bucket,
-      topic,
-      writerSnsClient = snsClient) { messageWriter =>
-      val recordReceiver = new HybridRecordReceiver(
-        messageWriter = messageWriter,
-        objectStore = objectStore
-      )
-
-      testWith(recordReceiver)
-    }
+    bucket: Bucket)(
+    testWith: TestWith[HybridRecordReceiver[SNSConfig], R]): R =
+    withSqsBigMessageSender[TransformedBaseWork, R](bucket, topic) { msgSender =>
+      val recorderReciver = new HybridRecordReceiver(msgSender, vhs)
+      testWith(recorderReciver)
+  }
 }
