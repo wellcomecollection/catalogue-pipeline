4f6c5de27f6235dd643a678694906048eda84838
Nick Ward <ward.nickjames@gmail.com>
2019-09-02 17:08:49 +0100

    MergerWorkerServiceTest updates

--- pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerServiceTest.scala
+++ pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerServiceTest.scala
@@ -1,34 +1,34 @@
 package uk.ac.wellcome.platform.merger.services
 
-import org.mockito.Matchers.endsWith
-import org.mockito.Mockito.{atLeastOnce, times, verify}
+// import org.mockito.Matchers.endsWith
+// import org.mockito.Mockito.{atLeastOnce, times, verify}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.messaging.fixtures.SNS.Topic
-import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
-import uk.ac.wellcome.messaging.fixtures.Messaging
+
 import uk.ac.wellcome.models.matcher.{MatchedIdentifiers, MatcherResult}
 import uk.ac.wellcome.models.work.generators.WorksGenerators
 import uk.ac.wellcome.models.work.internal._
-import uk.ac.wellcome.monitoring.MetricsSender
-import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
+// import uk.ac.wellcome.monitoring.MetricsSender
+// import uk.ac.wellcome.monitoring.fixtures.MetricsSenderFixture
 import uk.ac.wellcome.platform.merger.fixtures.{
-  LocalWorksVhs,
   MatcherResultFixture,
   WorkerServiceFixture
 }
 import uk.ac.wellcome.fixtures.TestWith
 import uk.ac.wellcome.models.Implicits._
 
+import uk.ac.wellcome.bigmessaging.fixtures.BigMessagingFixture
+import uk.ac.wellcome.messaging.fixtures.SNS.Topic
+import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
+
 class MergerWorkerServiceTest
     extends FunSpec
     with ScalaFutures
     with IntegrationPatience
-    with MetricsSenderFixture
-    with Messaging
+    with BigMessagingFixture
+    // with MetricsSenderFixture
     with WorksGenerators
-    with LocalWorksVhs
     with MatcherResultFixture
     with Matchers
     with MockitoSugar
@@ -37,7 +37,7 @@ class MergerWorkerServiceTest
   it(
     "reads matcher result messages, retrieves the works from vhs and sends them to sns") {
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, metricsSender) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         val work1 = createUnidentifiedWork
         val work2 = createUnidentifiedWork
         val work3 = createUnidentifiedWork
@@ -59,15 +59,15 @@ class MergerWorkerServiceTest
           val worksSent = getMessages[BaseWork](topic)
           worksSent should contain only (work1, work2, work3)
 
-          verify(metricsSender, atLeastOnce)
-            .incrementCount(endsWith("_success"))
+          // verify(metricsSender, atLeastOnce)
+          //   .incrementCount(endsWith("_success"))
         }
     }
   }
 
   it("sends InvisibleWorks unmerged") {
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, metricsSender) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         val work = createUnidentifiedInvisibleWork
 
         val matcherResult = matcherResultWith(Set(Set(work)))
@@ -86,15 +86,15 @@ class MergerWorkerServiceTest
           val worksSent = getMessages[BaseWork](topic)
           worksSent should contain only work
 
-          verify(metricsSender, times(1))
-            .incrementCount(endsWith("_success"))
+          // verify(metricsSender, times(1))
+          //   .incrementCount(endsWith("_success"))
         }
     }
   }
 
   it("fails if the work is not in vhs") {
     withMergerWorkerServiceFixtures {
-      case (_, QueuePair(queue, dlq), topic, metricsSender) =>
+      case (_, QueuePair(queue, dlq), topic) =>
         val work = createUnidentifiedWork
 
         val matcherResult = matcherResultWith(Set(Set(work)))
@@ -109,15 +109,15 @@ class MergerWorkerServiceTest
           assertQueueHasSize(dlq, 1)
           listMessagesReceivedFromSNS(topic) shouldBe empty
 
-          verify(metricsSender, times(3))
-            .incrementCount(endsWith("_failure"))
+          // verify(metricsSender, times(3))
+          //   .incrementCount(endsWith("_failure"))
         }
     }
   }
 
   it("discards works with newer versions in vhs, sends along the others") {
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, _) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         val work = createUnidentifiedWork
         val olderWork = createUnidentifiedWork
         val newerWork = olderWork.copy(version = 2)
@@ -142,7 +142,7 @@ class MergerWorkerServiceTest
 
   it("discards works with version 0 and sends along the others") {
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, metricsSender) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         val versionZeroWork = createUnidentifiedWorkWith(version = 0)
         val work = versionZeroWork
           .copy(version = 1)
@@ -163,8 +163,8 @@ class MergerWorkerServiceTest
           val worksSent = getMessages[BaseWork](topic)
           worksSent should contain only work
 
-          verify(metricsSender, times(1))
-            .incrementCount(endsWith("_success"))
+          // verify(metricsSender, times(1))
+          //   .incrementCount(endsWith("_success"))
         }
     }
   }
@@ -176,7 +176,7 @@ class MergerWorkerServiceTest
     val works = List(physicalWork, digitalWork)
 
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, _) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         givenStoredInVhs(vhs, works: _*)
 
         val matcherResult = MatcherResult(
@@ -218,7 +218,7 @@ class MergerWorkerServiceTest
     val works = workPair1 ++ workPair2
 
     withMergerWorkerServiceFixtures {
-      case (vhs, QueuePair(queue, dlq), topic, _) =>
+      case (vhs, QueuePair(queue, dlq), topic) =>
         givenStoredInVhs(vhs, works: _*)
 
         val matcherResult = MatcherResult(
@@ -251,36 +251,30 @@ class MergerWorkerServiceTest
 
   it("fails if the message sent is not a matcher result") {
     withMergerWorkerServiceFixtures {
-      case (_, QueuePair(queue, dlq), _, metricsSender) =>
+      case (_, QueuePair(queue, dlq), _) =>
         sendInvalidJSONto(queue)
 
         eventually {
           assertQueueEmpty(queue)
           assertQueueHasSize(dlq, 1)
-          verify(metricsSender, times(3))
-            .incrementCount(endsWith("_recognisedFailure"))
+          // verify(metricsSender, times(3))
+          //   .incrementCount(endsWith("_recognisedFailure"))
         }
     }
   }
 
   def withMergerWorkerServiceFixtures[R](
-    testWith: TestWith[
-      (TransformedBaseWorkVHS, QueuePair, Topic, MetricsSender),
-      R]): R =
-    withTransformedBaseWorkVHS { vhs =>
-      withLocalSqsQueueAndDlq {
-        case queuePair @ QueuePair(queue, _) =>
-          withLocalSnsTopic { topic =>
-            withMockMetricsSender { mockMetricsSender =>
-              withWorkerService(
-                vhs = vhs,
-                topic = topic,
-                queue = queue,
-                metricsSender = mockMetricsSender) { _ =>
-                testWith((vhs, queuePair, topic, mockMetricsSender))
-              }
-            }
+    testWith: TestWith[(VHS, QueuePair, Topic), R]): R =
+    withVHS { vhs =>
+      withLocalSqsQueueAndDlq { case QueuePair(queue, dlq) =>
+        withLocalSnsTopic { topic =>
+          withWorkerService(
+            vhs = vhs,
+            topic = topic,
+            queue = queue) { _ =>
+            testWith((vhs, QueuePair(queue, dlq), topic))
           }
+        }
       }
     }
 }
--- pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/RecorderPlaybackServiceTest.scala
+++ pipeline/merger/src/test/scala/uk/ac/wellcome/platform/merger/services/RecorderPlaybackServiceTest.scala
@@ -2,13 +2,10 @@ package uk.ac.wellcome.platform.merger.services
 
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.fixtures._
 import uk.ac.wellcome.models.matcher.WorkIdentifier
 import uk.ac.wellcome.models.work.generators.WorksGenerators
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
 import uk.ac.wellcome.platform.merger.fixtures.LocalWorksVhs
-import uk.ac.wellcome.storage.vhs.EmptyMetadata
-import uk.ac.wellcome.models.Implicits._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -23,7 +20,7 @@ class RecorderPlaybackServiceTest
   it("fetches a single Work") {
     val work = createUnidentifiedWork
 
-    withRecorderVHS { vhs =>
+    withVHS { vhs =>
       givenStoredInVhs(vhs, work)
 
       whenReady(fetchAllWorks(vhs = vhs, work)) { result =>
@@ -35,7 +32,7 @@ class RecorderPlaybackServiceTest
   it("throws an error if asked to fetch a missing entry") {
     val work = createUnidentifiedWork
 
-    withRecorderVHS { vhs =>
+    withVHS { vhs =>
       whenReady(fetchAllWorks(vhs = vhs, work).failed) { result =>
         result shouldBe a[NoSuchElementException]
         result.getMessage shouldBe s"Work ${work.sourceIdentifier} is not in VHS!"
@@ -46,7 +43,7 @@ class RecorderPlaybackServiceTest
   it("returns None if asked to fetch a Work with version 0") {
     val work = createUnidentifiedWorkWith(version = 0)
 
-    withRecorderVHS { vhs =>
+    withVHS { vhs =>
       whenReady(fetchAllWorks(vhs = vhs, work)) { result =>
         result shouldBe Seq(None)
       }
@@ -61,7 +58,7 @@ class RecorderPlaybackServiceTest
       version = work.version + 1
     )
 
-    withRecorderVHS { vhs =>
+    withVHS { vhs =>
       givenStoredInVhs(vhs, workToStore)
 
       whenReady(fetchAllWorks(vhs = vhs, work)) { result =>
@@ -87,7 +84,7 @@ class RecorderPlaybackServiceTest
     val lookupWorks = (unchangedWorks ++ outdatedWorks ++ zeroWorks).toList
     val storedWorks = (unchangedWorks ++ updatedWorks ++ zeroWorks).toList
 
-    withRecorderVHS { vhs =>
+    withVHS { vhs =>
       givenStoredInVhs(vhs, storedWorks: _*)
 
       whenReady(fetchAllWorks(vhs = vhs, lookupWorks: _*)) { result =>
@@ -98,9 +95,7 @@ class RecorderPlaybackServiceTest
     }
   }
 
-  private def fetchAllWorks(
-    vhs: TransformedBaseWorkVHS,
-    works: TransformedBaseWork*): Future[Seq[Option[TransformedBaseWork]]] = {
+  private def fetchAllWorks(vhs: VHS, works: TransformedBaseWork*): Future[Seq[Option[TransformedBaseWork]]] = {
     val service = new RecorderPlaybackService(vhs)
 
     val workIdentifiers = works
@@ -109,17 +104,5 @@ class RecorderPlaybackServiceTest
       }
 
     service.fetchAllWorks(workIdentifiers)
-  }
-
-  private def withRecorderVHS[R](
-    testWith: TestWith[TransformedBaseWorkVHS, R]): R = {
-    withLocalS3Bucket { bucket =>
-      withLocalDynamoDbTable { table =>
-        withTypeVHS[TransformedBaseWork, EmptyMetadata, R](bucket, table) {
-          vhs =>
-            testWith(vhs)
-        }
-      }
-    }
   }
 }
