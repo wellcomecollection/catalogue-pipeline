24c756e9781873b329114cc7fead83c7851f8f1c
Nick Ward <ward.nickjames@gmail.com>
2019-09-05 14:35:35 +0100

    Use VHSBuilder in miro main

--- pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
+++ pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
@@ -4,9 +4,7 @@ import scala.concurrent.ExecutionContext
 
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-
-import org.scanamo.DynamoFormat
-
+import org.scanamo.auto._
 import com.typesafe.config.Config
 
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
@@ -21,38 +19,19 @@ import uk.ac.wellcome.platform.transformer.miro.models.MiroMetadata
 import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.platform.transformer.miro.Implicits._
 
-import uk.ac.wellcome.bigmessaging.typesafe.BigMessagingBuilder
+import uk.ac.wellcome.bigmessaging.typesafe.{BigMessagingBuilder, VHSBuilder}
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.typesafe.SQSBuilder
 import uk.ac.wellcome.messaging.sns.SNSConfig
 
-import uk.ac.wellcome.storage.store.dynamo.{DynamoHashStore, DynamoHybridStore}
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
-import uk.ac.wellcome.storage.store.HybridIndexedStoreEntry
-import uk.ac.wellcome.storage.{ObjectLocation, ObjectLocationPrefix}
-import uk.ac.wellcome.storage.typesafe.{DynamoBuilder, S3Builder}
-import uk.ac.wellcome.storage.dynamo.DynamoHashEntry
+import uk.ac.wellcome.storage.typesafe.S3Builder
 import uk.ac.wellcome.storage.streaming.Codec._
 
 
 object Main extends WellcomeTypesafeApp {
 
-  type IndexEntry = HybridIndexedStoreEntry[ObjectLocation, MiroMetadata]
-
-  type HashEntry = DynamoHashEntry[String, Int, IndexEntry]
-
   runWithConfig { config: Config =>
-    // TODO: from where do we get the correct values for this?
-    val objectLocationPrefix = ObjectLocationPrefix("namespace", "path")
-    val dynamoConfig =
-      DynamoBuilder.buildDynamoConfig(config, namespace = "namespace")
-
-    // For some reason these dont get derived with scanamo auto derivation
-    implicit def indexEntryFormat: DynamoFormat[IndexEntry] =
-      DynamoFormat[IndexEntry]
-    implicit def hashEntryFormat: DynamoFormat[HashEntry] =
-      DynamoFormat[HashEntry]
-
     implicit val actorSystem: ActorSystem =
       AkkaBuilder.buildActorSystem()
     implicit val executionContext: ExecutionContext =
@@ -61,20 +40,14 @@ object Main extends WellcomeTypesafeApp {
       AkkaBuilder.buildActorMaterializer()
     implicit val s3Client =
       S3Builder.buildS3Client(config)
-    implicit val dynamoClient =
-      DynamoBuilder.buildDynamoClient(config)
-    implicit val transformedStore =
+    implicit val msgStore =
       S3TypedStore[TransformedBaseWork]
-    implicit val mirmoRecordStore =
-      S3TypedStore[MiroRecord]
-    implicit val dynamoIndexStore =
-      new DynamoHashStore[String, Int, IndexEntry](dynamoConfig)
 
     val vhsRecordReceiver = new MiroVHSRecordReceiver[SNSConfig](
-      msgSender =
-        BigMessagingBuilder.buildBigMessageSender[TransformedBaseWork](config),
-      store =
-        new DynamoHybridStore[MiroRecord, MiroMetadata](objectLocationPrefix)
+      msgSender = BigMessagingBuilder
+        .buildBigMessageSender[TransformedBaseWork](config),
+      store = VHSBuilder
+        .buildWithMetadata[MiroRecord, MiroMetadata](config)
     )
 
     new MiroTransformerWorkerService(
