6d09173108d0cb3d23f8a6c5be29abfee203df2a
Nick Ward <ward.nickjames@gmail.com>
2019-09-24 12:28:36 +0100

    Tests for getting physical description fro 563

--- pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraPhysicalDescriptionTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraPhysicalDescriptionTest.scala
@@ -1,7 +1,11 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
 import org.scalatest.{FunSpec, Matchers}
-import uk.ac.wellcome.platform.transformer.sierra.source.MarcSubfield
+import uk.ac.wellcome.platform.transformer.sierra.source.{
+  MarcSubfield,
+  SierraBibData,
+  VarField
+}
 import uk.ac.wellcome.platform.transformer.sierra.generators.{
   MarcGenerators,
   SierraDataGenerators
@@ -12,72 +16,69 @@ class SierraPhysicalDescriptionTest
     with Matchers
     with MarcGenerators
     with SierraDataGenerators {
-
+  
   it(
     "gets no physical description if there is no MARC field 300 with subfield $b") {
-    val bibData = createSierraBibDataWith(varFields = List())
-    SierraPhysicalDescription(createSierraBibNumber, bibData) shouldBe None
+    val field = varField(
+      "500",
+      MarcSubfield("b", "The edifying extent of early emus")
+    )
+    SierraPhysicalDescription(bibId, bibData(field)) shouldBe None
   }
 
   it("extracts physical description from MARC field 300 subfield $b") {
-    val expectedDescription = "Queuing quokkas quarrel about Quirinus Quirrell"
-
-    val varFields = List(
-      createVarFieldWith(
-        marcTag = "300",
-        subfields = List(
-          MarcSubfield(
-            tag = "a",
-            content = "The edifying extent of early emus"
-          ),
-          MarcSubfield(
-            tag = "b",
-            content = expectedDescription
-          )
-        )
-      )
+    val description = "Queuing quokkas quarrel about Quirinus Quirrell"
+    val field = varField(
+      "300",
+      MarcSubfield("a", "The edifying extent of early emus"),
+      MarcSubfield("b", description)
     )
-
-    val bibData = createSierraBibDataWith(varFields = varFields)
-    val bibId = createSierraBibNumber
-    SierraPhysicalDescription(bibId, bibData) shouldBe Some(expectedDescription)
+    SierraPhysicalDescription(bibId, bibData(field)) shouldBe Some(description)
   }
 
   it(
     "extracts a physical description where there are multiple MARC field 300 $b") {
-    val physicalDescription1 = "The queer quolls quits and quarrels"
-    val physicalDescription2 = "A quintessential quadraped is quick"
-
-    val expectedDescription =
-      s"$physicalDescription1\n\n$physicalDescription2"
-
-    val varFields = List(
-      createVarFieldWith(
-        marcTag = "300",
-        subfields = List(
-          MarcSubfield(
-            tag = "b",
-            content = physicalDescription1
-          )
-        )
+    val descriptionA = "The queer quolls quits and quarrels"
+    val descriptionB = "A quintessential quadraped is quick"
+    val expectedDescription = s"$descriptionA\n\n$descriptionB"
+    val data = bibData(
+      varField("300", MarcSubfield("b", descriptionA)),
+      varField(
+        "300",
+        MarcSubfield("a", "Egad!  An early eagle is eating the earwig."),
+        MarcSubfield("b", descriptionB)
       ),
-      createVarFieldWith(
-        marcTag = "300",
-        subfields = List(
-          MarcSubfield(
-            tag = "a",
-            content = "Egad!  An early eagle is eating the earwig."
-          ),
-          MarcSubfield(
-            tag = "b",
-            content = physicalDescription2
-          )
-        )
-      )
     )
-
-    val bibData = createSierraBibDataWith(varFields = varFields)
-    val bibId = createSierraBibNumber
-    SierraPhysicalDescription(bibId, bibData) shouldBe Some(expectedDescription)
+    SierraPhysicalDescription(bibId, data) shouldBe Some(expectedDescription)
   }
+
+  it(f"extracts physical description from MARC field 563 subfield $$a") {
+    val description = "Queuing quokkas quarrel about Quirinus Quirrell"
+    val field = varField(
+      "563",
+      MarcSubfield("b", "The edifying extent of early emus"),
+      MarcSubfield("a", description)
+    )
+    SierraPhysicalDescription(bibId, bibData(field)) shouldBe Some(description)
+  }
+
+  it(
+    "extracts a physical description where there both MARC field 300 and 563") {
+    val descriptionA = "The queer quolls quits and quarrels"
+    val descriptionB = "A quintessential quadraped is quick"
+    val expectedDescription = s"$descriptionA\n\n$descriptionB"
+    val data = bibData(
+      varField("563", MarcSubfield("a", descriptionB)),
+      varField("300", MarcSubfield("b", descriptionA)),
+    )
+    SierraPhysicalDescription(bibId, data) shouldBe Some(expectedDescription)
+  }
+
+  def bibId = createSierraBibNumber
+
+  def bibData(varFields: VarField*): SierraBibData =
+    createSierraBibDataWith(varFields = varFields.toList)
+
+  def varField(tag: String, subfields: MarcSubfield*): VarField =
+    createVarFieldWith(marcTag = tag, subfields = subfields.toList)
 }
