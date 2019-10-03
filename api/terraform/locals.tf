00af9850ff3fd4b3567d09a0c57dc75e9eb54c66
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-10-03 16:03:35 +0100

    deploy fields for elastic to query

--- api/terraform/locals.tf
+++ api/terraform/locals.tf
@@ -8,16 +8,16 @@ locals {
 
   # API pins
 
-  production_api     = "romulus"
+  production_api     = "remus"
   pinned_nginx       = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/nginx_api-gw:bad0dbfa548874938d16496e313b05adb71268b7"
-  pinned_remus_api   = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/api:cf75732d311bbc6fb09377f0263c0cb80901673d"
+  pinned_remus_api   = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/api:282e9d374ec2b17cf9c325691c293b4a46c39b5e"
   pinned_romulus_api = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/api:bef198ff0bc2bcb665b013760616b002be0201f1"
   romulus_es_config = {
     index_v2 = "v2-20190916-raw-labels"
     doc_type = "work"
   }
   remus_es_config = {
-    index_v2 = "v2-20190816-alternative-titles"
+    index_v2 = "v2-20190916-raw-labels"
     doc_type = "work"
   }
 
