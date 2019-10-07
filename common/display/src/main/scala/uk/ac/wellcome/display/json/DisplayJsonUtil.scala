de1f8a2a1f02bdf6241cf7150a0249be24f74e7e
Nick Ward <ward.nickjames@gmail.com>
2019-10-07 12:15:14 +0100

    Fix tests

--- common/display/src/main/scala/uk/ac/wellcome/display/json/DisplayJsonUtil.scala
+++ common/display/src/main/scala/uk/ac/wellcome/display/json/DisplayJsonUtil.scala
@@ -3,6 +3,8 @@ package uk.ac.wellcome.display.json
 import io.circe.generic.extras.{AutoDerivation, Configuration}
 import io.circe.{Encoder, Printer}
 import io.circe.syntax._
+import uk.ac.wellcome.display.models.DisplayWork
+import uk.ac.wellcome.display.models.v2._
 
 /** Format JSON objects as suitable for display.
   *
@@ -25,6 +27,42 @@ trait DisplayJsonUtil extends AutoDerivation {
   def toJson[T](value: T)(implicit encoder: Encoder[T]): String = {
     assert(encoder != null)
     printer.pretty(value.asJson)
+  }
+
+  // Circe wants to add a type discriminator, and we don't want it to!  Doing so
+  // would expose internal names like "DisplayDigitalLocationV1" in the public JSON.
+  // So instead we have to do the slightly less nice thing of encoding all the subclasses
+  // here by hand.  Annoying, it is.
+  //
+  // It is the "recommended: approach in the Circe docs:
+  // https://circe.github.io/circe/codecs/adt.html
+
+  implicit val abstractAgentEncoder: Encoder[DisplayAbstractAgentV2] = {
+    case agent: DisplayAgentV2               => agent.asJson
+    case person: DisplayPersonV2             => person.asJson
+    case organisation: DisplayOrganisationV2 => organisation.asJson
+    case meeting: DisplayMeetingV2           => meeting.asJson
+  }
+
+  implicit val abstractConceptEncoder: Encoder[DisplayAbstractConcept] = {
+    case concept: DisplayConcept => concept.asJson
+    case place: DisplayPlace     => place.asJson
+    case period: DisplayPeriod   => period.asJson
+  }
+
+  implicit val abstractRootConceptEncoder
+    : Encoder[DisplayAbstractRootConcept] = {
+    case agent: DisplayAbstractAgentV2   => agent.asJson
+    case concept: DisplayAbstractConcept => concept.asJson
+  }
+
+  implicit val locationV2Encoder: Encoder[DisplayLocationV2] = {
+    case digitalLocation: DisplayDigitalLocationV2   => digitalLocation.asJson
+    case physicalLocation: DisplayPhysicalLocationV2 => physicalLocation.asJson
+  }
+
+  implicit val displayWorkEncoder: Encoder[DisplayWork] = {
+    case work: DisplayWorkV2 => work.asJson
   }
 }
 
--- common/display/src/main/scala/uk/ac/wellcome/display/models/Implicits.scala
+++ common/display/src/main/scala/uk/ac/wellcome/display/models/Implicits.scala
@@ -4,45 +4,8 @@ import io.circe.generic.extras.semiauto._
 import io.circe.{Decoder, Encoder}
 import uk.ac.wellcome.display.json.DisplayJsonUtil
 import uk.ac.wellcome.display.models.v2._
-import io.circe.syntax._
 
 object Implicits extends DisplayJsonUtil {
-
-  // Circe wants to add a type discriminator, and we don't want it to!  Doing so
-  // would expose internal names like "DisplayDigitalLocationV1" in the public JSON.
-  // So instead we have to do the slightly less nice thing of encoding all the subclasses
-  // here by hand.  Annoying, it is.
-  //
-  // It is the "recommended: approach in the Circe docs:
-  // https://circe.github.io/circe/codecs/adt.html
-
-  implicit val abstractAgentEncoder: Encoder[DisplayAbstractAgentV2] = {
-    case agent: DisplayAgentV2               => agent.asJson
-    case person: DisplayPersonV2             => person.asJson
-    case organisation: DisplayOrganisationV2 => organisation.asJson
-    case meeting: DisplayMeetingV2           => meeting.asJson
-  }
-
-  implicit val abstractConceptEncoder: Encoder[DisplayAbstractConcept] = {
-    case concept: DisplayConcept => concept.asJson
-    case place: DisplayPlace     => place.asJson
-    case period: DisplayPeriod   => period.asJson
-  }
-
-  implicit val abstractRootConceptEncoder
-    : Encoder[DisplayAbstractRootConcept] = {
-    case agent: DisplayAbstractAgentV2   => agent.asJson
-    case concept: DisplayAbstractConcept => concept.asJson
-  }
-
-  implicit val locationV2Encoder: Encoder[DisplayLocationV2] = {
-    case digitalLocation: DisplayDigitalLocationV2   => digitalLocation.asJson
-    case physicalLocation: DisplayPhysicalLocationV2 => physicalLocation.asJson
-  }
-
-  implicit val displayWorkEncoder: Encoder[DisplayWork] = {
-    case work: DisplayWorkV2 => work.asJson
-  }
 
   // Cache these here to improve compilation times (otherwise they are
   // re-derived every time they are required).
