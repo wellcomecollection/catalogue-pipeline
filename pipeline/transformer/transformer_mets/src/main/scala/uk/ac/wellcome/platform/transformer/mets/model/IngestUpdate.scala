b80ecea1a84a2bc6d70bd590bf9736ddfcd503f3
alicefuzier <alicefuzier@gmail.com>
2019-10-22 16:15:26 +0100

    Add basic Bag info to IngestUpdate

--- pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/model/IngestUpdate.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/model/IngestUpdate.scala
@@ -1,3 +1,7 @@
 package uk.ac.wellcome.platform.transformer.mets.model
 
-case class IngestUpdate()
+case class IngestUpdate(space: Space, bag: Bag)
+
+case class Space(id: String)
+case class Bag(info: BagInfo)
+case class BagInfo(externalIdentifier: String)
