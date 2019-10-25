ee2748ce5ff7e1592709ed2aee13881eeec40328
alicefuzier <alicefuzier@gmail.com>
2019-10-25 16:28:28 +0100

    Handle bag not existing

--- pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/service/BagsRetriever.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/service/BagsRetriever.scala
@@ -5,10 +5,16 @@ import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.platform.transformer.mets.model.Bag
 
 class BagsRetriever(url: String) {
-  def getBag(space: String, bagId: String): Bag = {
+  def getBag(space: String, bagId: String): Option[Bag] = {
+
     val response = Http(s"$url/$space/$bagId").asString
 
-    fromJson[Bag](response.body).get
+    response.code match {
+      case 200 => Some(fromJson[Bag](response.body).get)
+      case _ => None
+    }
+
+
   }
 
 }
--- pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/service/BagsRetrieverTest.scala
+++ pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/service/BagsRetrieverTest.scala
@@ -5,17 +5,26 @@ import org.scalatest.{FunSpec, Inside, Matchers}
 import uk.ac.wellcome.platform.transformer.mets.model.{Bag, BagFile, BagLocation, BagManifest}
 
 class BagsRetrieverTest extends FunSpec with Matchers with BagsWiremock with Inside{
+
   it("gets a bag from the storage service"){
+
     withBagsService(8089, "localhost") {
       val bagsRetriever = new BagsRetriever("http://localhost:8089/storage/v1/bags")
-      inside(bagsRetriever.getBag("digitised","b30246039")) { case Bag(_, BagManifest(files), BagLocation(bucket, path)) =>
+      inside(bagsRetriever.getBag("digitised","b30246039")) { case Some(Bag(_, BagManifest(files), BagLocation(bucket, path))) =>
         verify(getRequestedFor(urlEqualTo("/storage/v1/bags/digitised/b30246039")))
         files.head shouldBe BagFile("data/b30246039.xml", "v1/data/b30246039.xml")
         bucket shouldBe "wellcomecollection-storage"
-          path shouldBe "digitised/b30246039"
+        path shouldBe "digitised/b30246039"
       }
     }
 
+  }
+
+  it("returns a none if the bag does not exist in the storage service") {
+    withBagsService(8089, "localhost") {
+      val bagsRetriever = new BagsRetriever("http://localhost:8089/storage/v1/bags")
+      bagsRetriever.getBag("digitised", "not-existing") shouldBe None
+    }
   }
 
 }
