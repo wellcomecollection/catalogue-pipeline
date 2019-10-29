a4656df80f7338b36ed26b73b802a2012ce68bb8
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-10-29 17:44:06 +0000

    Apply auto-formatting rules

--- api/live_testing/main.py
+++ api/live_testing/main.py
@@ -18,22 +18,27 @@ routes = [
     ("a22au6yn", {"include": "items,genres,identifiers"}),
 ]
 
+
 class ApiDiffer:
 
-    prod = 'api.wellcomecollection.org'
-    stage = 'api-stage.wellcomecollection.org'
+    prod = "api.wellcomecollection.org"
+    stage = "api-stage.wellcomecollection.org"
 
-    def  __init__(self, work_id=None, params=None, show_colour=True):
-        suffix = f'/{work_id}' if work_id else ''
-        self.path = f'/catalogue/v2/works{suffix}'
+    def __init__(self, work_id=None, params=None, show_colour=True):
+        suffix = f"/{work_id}" if work_id else ""
+        self.path = f"/catalogue/v2/works{suffix}"
         self.params = params or {}
         self.show_colour = show_colour
 
     def display_diff(self):
-        click.echo(f"================================================================================")
+        click.echo(
+            f"================================================================================"
+        )
         click.echo(f"Performing diff on {self.path} with params:")
         click.echo(self.params)
-        click.echo(f"================================================================================")
+        click.echo(
+            f"================================================================================"
+        )
         click.echo()
         click.echo("* Calling prod API")
         (prod_status, prod_json) = self.call_api(self.prod)
@@ -49,25 +54,25 @@ class ApiDiffer:
                 "stage:",
                 f"{json.dumps(stage_json, indent=2)}",
             ]
-            msg = '\n'.join(lines)
+            msg = "\n".join(lines)
             if self.show_colour:
-                msg = click.style(msg, fg='red')
+                msg = click.style(msg, fg="red")
             click.echo(msg)
         else:
             click.echo(f"* Recived {prod_status} status from both APIs")
             click.echo("* Generating diff")
             click.echo()
-            differ = ObjDiffer(prod_json, stage_json, 'prod', 'stage', self.show_colour)
+            differ = ObjDiffer(prod_json, stage_json, "prod", "stage", self.show_colour)
             differ.display_diff()
         click.echo()
 
     def call_api(self, api_base):
-        url = f'https://{api_base}{self.path}'
+        url = f"https://{api_base}{self.path}"
         response = requests.get(url, params=self.params)
         return (response.status_code, response.json())
 
-class ObjDiffer:
 
+class ObjDiffer:
     def __init__(self, obj_a, obj_b, name_a, name_b, show_colour=True):
         self.obj_a = dict(self.flatten(obj_a))
         self.obj_b = dict(self.flatten(obj_b))
@@ -88,18 +93,18 @@ class ObjDiffer:
             if obj is None:
                 return "null"
             return str(obj)
+
         if a == b:
-            msg = f'unchanged between {self.name_a} and {self.name_b}'
-            col = 'green'
+            msg = f"unchanged between {self.name_a} and {self.name_b}"
+            col = "green"
         else:
-            msg = f'changed from {self.name_b}={fmt(a)} to {self.name_b}={fmt(b)}'
-            col = 'red'
+            msg = f"changed from {self.name_b}={fmt(a)} to {self.name_b}={fmt(b)}"
+            col = "red"
         if self.show_colour:
             msg = click.style(msg, fg=col)
-        click.echo(f'{key}: {msg}')
+        click.echo(f"{key}: {msg}")
 
     def flatten(self, obj):
-
         def flatten_tupled(obj):
             nested = [join_subitems(key, self.flatten(value)) for key, value in obj]
             flattened = sum(nested, [])
@@ -114,17 +119,19 @@ class ObjDiffer:
             return flatten_tupled(obj.items())
         return [(Key(), obj)]
 
-class Key:
 
+class Key:
     def __init__(self, parts=None):
         self.parts = parts or ()
 
     def prepend(self, prefix):
-        prefix = prefix.parts if isinstance(prefix, Key) else (prefix, )
+        prefix = prefix.parts if isinstance(prefix, Key) else (prefix,)
         return Key(prefix + self.parts)
 
     def __str__(self):
-        return ".".join(f"[{part}]" if isinstance(part, int) else part for part in self.parts)
+        return ".".join(
+            f"[{part}]" if isinstance(part, int) else part for part in self.parts
+        )
 
     def __repr__(self):
         return str(self)
@@ -138,11 +145,13 @@ class Key:
     def __eq__(self, other):
         return self.parts == other.parts
 
+
 @click.command()
 def main():
     for work_id, params in routes:
         differ = ApiDiffer(work_id, params)
         differ.display_diff()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
