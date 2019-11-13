ff8e82760b66afe5480d30789b464a48d02ac50b
alicefuzier <alicefuzier@gmail.com>
2019-11-13 10:16:15 +0000

    delete goobi adapter

--- goobi_adapter/terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  vpc_id          = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_id}"
-  private_subnets = "${data.terraform_remote_state.shared_infra.catalogue_vpc_delta_private_subnets}"
-
-  vhs_goobi_full_access_policy = "${data.terraform_remote_state.catalogue_infra_critical.vhs_goobi_full_access_policy}"
-  vhs_goobi_table_name         = "${data.terraform_remote_state.catalogue_infra_critical.vhs_goobi_table_name}"
-  vhs_goobi_bucket_name        = "${data.terraform_remote_state.catalogue_infra_critical.vhs_goobi_bucket_name}"
-}
