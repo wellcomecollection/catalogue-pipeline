ed8a163173126366090aac77c9c2c1dd0d1e71b0
jamie <j.parkinson@wellcome.ac.uk>
2019-11-15 10:33:46 +0000

    Explain the empty stage name in the api gw deployment and make the egress group tag name more useful

--- api/terraform/catalogue_api/gateway_stage.tf
+++ api/terraform/catalogue_api/gateway_stage.tf
@@ -6,8 +6,13 @@ locals {
 
 resource "aws_api_gateway_deployment" "stage" {
   rest_api_id = "${var.api_id}"
-  stage_name  = ""
-  variables   = "${local.variables}"
+
+  # If we specify the stage name here then API Gateway tries to create it even
+  # if it already exists (from below). Setting it to an empty string prevents this.
+  # See https://github.com/terraform-providers/terraform-provider-aws/issues/2918#issuecomment-356684239
+  stage_name = ""
+
+  variables = "${local.variables}"
 
   # This forces a new deployment (which is necessary) when Gateway config changes
   stage_description = "${filemd5("gateway.tf")}"
--- api/terraform/catalogue_api/main.tf
+++ api/terraform/catalogue_api/main.tf
@@ -43,6 +43,6 @@ resource "aws_security_group" "service_egress_security_group" {
   }
 
   tags {
-    Name = "${var.namespace}-${var.environment}-egress"
+    Name = "${var.namespace}"
   }
 }
