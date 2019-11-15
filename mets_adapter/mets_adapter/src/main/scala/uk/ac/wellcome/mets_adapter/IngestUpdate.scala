a8a15adb271a07b3368bb38783474a47794da96f
Nick Ward <ward.nickjames@gmail.com>
2019-11-15 16:57:58 +0000

    Parse version from bag

--- mets_adapter/mets_adapter/src/main/scala/uk/ac/wellcome/mets_adapter/IngestUpdate.scala
+++ mets_adapter/mets_adapter/src/main/scala/uk/ac/wellcome/mets_adapter/IngestUpdate.scala
@@ -4,14 +4,22 @@ case class IngestUpdate(space: String, bagId: String)
 
 case class MetsData(path: String, version: Int)
 
-case class Bag(info: BagInfo, manifest: BagManifest, location: BagLocation) {
+case class Bag(info: BagInfo, manifest: BagManifest, location: BagLocation, version: String) {
 
   // Storage-service only stores a list of files, so we need to search for a
   // XML file in data directory named with some b-number.
   private val metsRegex = "^data/b[0-9]{7}[0-9x].xml$".r
 
+  private val versionRegex = "^v([0-9]+)".r
+
   def metsData: Either[Throwable, MetsData] =
-    Left(new Exception("Not found"))
+    metsPath
+      .map { path =>
+        parsedVersion
+          .map(version =>  Right(MetsData(path, version)))
+          .getOrElse(Left(new Exception("Couldn't parse version")))
+      }
+      .getOrElse(Left(new Exception("Couldn't parse METS path")))
 
   def metsPath: Option[String] =
     manifest.files
@@ -19,6 +27,12 @@ case class Bag(info: BagInfo, manifest: BagManifest, location: BagLocation) {
         case file if metsRegex.findFirstIn(file.name).nonEmpty =>
           s"${location.path}/${file.path}"
       }
+
+  def parsedVersion: Option[Int] =
+    version match {
+      case versionRegex(num) => Some(num.toInt)
+      case _ => None
+    }
 }
 
 case class BagInfo(externalIdentifier: String)
