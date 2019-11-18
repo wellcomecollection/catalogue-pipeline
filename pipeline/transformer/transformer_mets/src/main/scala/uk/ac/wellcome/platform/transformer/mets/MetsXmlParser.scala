7c9e4f76f2e462070b70460f8f6ad28457d569b8
alicefuzier <alicefuzier@gmail.com>
2019-11-18 11:51:15 +0000

    Make MetsXmlParser a bit more robust

--- pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/MetsXmlParser.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/MetsXmlParser.scala
@@ -1,27 +1,39 @@
 package uk.ac.wellcome.platform.transformer.mets
 
+import scala.util.{Failure, Success, Try}
 import scala.xml.{Elem, XML}
 
 object MetsXmlParser {
 
-  def apply(str: String): Option[MetsData] =
+  def apply(str: String): Try[MetsData] =
     MetsXmlParser(XML.loadString(str))
 
-  def apply(root: Elem): Option[MetsData] =
-    recordIdentifier(root).map { id =>
-      MetsData(
+  def apply(root: Elem): Try[MetsData] =
+    {
+      for {
+        id <- recordIdentifier(root)
+        maybeAccessCondition <- accessCondition(root)
+      } yield MetsData(
         recordIdentifier = id,
-        accessCondition = accessCondition(root),
+        accessCondition = maybeAccessCondition,
       )
     }
 
-  private def recordIdentifier(root: Elem): Option[String] =
-    (root \\ "recordIdentifier").headOption
-      .map(_.text)
+  private def recordIdentifier(root: Elem): Try[String] = {
+    val identifierNodes = (root \\ "dmdSec" \ "mdWrap" \\ "recordInfo" \ "recordIdentifier").toList
+    identifierNodes match {
+      case List(identifierNode) => Success(identifierNode.text)
+      case _ => Failure(new Exception("Could not parse recordIdentifier from METS XML"))
+    }
+  }
 
-  private def accessCondition(root: Elem): Option[String] =
-    (root \\ "accessCondition")
-      .filter(_ \@ "type" == "dz")
-      .headOption
-      .map(_.text)
+  private def accessCondition(root: Elem): Try[Option[String]] = {
+    val licenseNodes = (root \\ "dmdSec" \ "mdWrap" \\ "accessCondition")
+      .filter(_ \@ "type" == "dz").toList
+    licenseNodes match {
+      case Nil => Success(None)
+      case List(licenseNode) => Success(Some(licenseNode.text))
+      case _ => Failure(new Exception("Found multiple accessCondtions in METS XML"))
+    }
+  }
 }
--- pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/MetsXmlParserTest.scala
+++ pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/MetsXmlParserTest.scala
@@ -3,7 +3,34 @@ package uk.ac.wellcome.platform.transformer.mets
 import scala.io.Source
 import org.scalatest.{FunSpec, Matchers}
 
+import scala.util.Failure
+
 class MetsXmlParserTest extends FunSpec with Matchers {
+
+  it("parses recordIdentifier from XML") {
+    MetsXmlParser(xml).get.recordIdentifier shouldBe "b30246039"
+  }
+
+  it("does not parse a mets if recordIdentifier is outside of dmdSec element"){
+    MetsXmlParser(xmlNodmdSec) shouldBe a [Failure[_]]
+  }
+
+  it("does not parse if there is more than one recordIdentifier"){
+    MetsXmlParser(xmlMultipleIds) shouldBe a [Failure[_]]
+  }
+
+  it("parses accessCondition from XML") {
+    MetsXmlParser(xml).get.accessCondition shouldBe Some("CC-BY-NC")
+  }
+
+  it("parses a METS with no access condition") {
+    MetsXmlParser(xmlNoLicense).get.accessCondition shouldBe None
+  }
+
+  it("does not parse a METS with no multiple licenses") {
+    MetsXmlParser(xmlMultipleLicense) shouldBe a [Failure[_]]
+  }
+
 
   val xml =
     Source
@@ -11,11 +38,64 @@ class MetsXmlParserTest extends FunSpec with Matchers {
       .getLines
       .mkString
 
-  it("parses recordIdentifier from XML") {
-    MetsXmlParser(xml).get.recordIdentifier shouldBe "b30246039"
-  }
+  val xmlNodmdSec =
+    s"""
+       |<mets:mets xmlns:mets="http://www.loc.gov/METS/" xmlns:mods="http://www.loc.gov/mods/v3">
+       |  <mods:recordIdentifier source="gbv-ppn">b30246039</mods:recordIdentifier>
+       |</mets:mets>
+       |""".stripMargin
 
-  it("parses accessCondition from XML") {
-    MetsXmlParser(xml).get.accessCondition shouldBe Some("CC-BY-NC")
-  }
+  val xmlMultipleIds =
+    s"""
+       |<mets:mets xmlns:mets="http://www.loc.gov/METS/" xmlns:mods="http://www.loc.gov/mods/v3">
+       |<mets:dmdSec ID="DMDLOG_0000">
+       |    <mets:mdWrap MDTYPE="MODS">
+       |      <mets:xmlData>
+       |        <mods:mods>
+       |        <mods:recordInfo>
+       |  <mods:recordIdentifier source="gbv-ppn">b30246039</mods:recordIdentifier>
+       |  <mods:recordIdentifier source="gbv-ppn">b3024346567</mods:recordIdentifier>
+       |  </mods:recordInfo>
+       |  </mods:mods>
+       |  </mets:xmlData>
+       |  </mets:mdWrap>
+       |</mets:dmdSec>
+       |</mets:mets>
+       |""".stripMargin
+
+  val xmlNoLicense =
+    s"""
+       |<mets:mets xmlns:mets="http://www.loc.gov/METS/" xmlns:mods="http://www.loc.gov/mods/v3">
+       |<mets:dmdSec ID="DMDLOG_0000">
+       |    <mets:mdWrap MDTYPE="MODS">
+       |      <mets:xmlData>
+       |        <mods:mods>
+       |                <mods:recordInfo>
+       |  <mods:recordIdentifier source="gbv-ppn">b30246039</mods:recordIdentifier>
+       |  </mods:recordInfo>
+       |  </mods:mods>
+       |  </mets:xmlData>
+       |  </mets:mdWrap>
+       |</mets:dmdSec>
+       |</mets:mets>
+       |""".stripMargin
+
+  val xmlMultipleLicense =
+    s"""
+       |<mets:mets xmlns:mets="http://www.loc.gov/METS/" xmlns:mods="http://www.loc.gov/mods/v3">
+       |<mets:dmdSec ID="DMDLOG_0000">
+       |    <mets:mdWrap MDTYPE="MODS">
+       |      <mets:xmlData>
+       |        <mods:mods>
+       |                <mods:recordInfo>
+       |  <mods:recordIdentifier source="gbv-ppn">b30246039</mods:recordIdentifier>
+       |  </mods:recordInfo>
+       |  <mods:accessCondition type="dz">CC-BY-NC</mods:accessCondition>
+       |  <mods:accessCondition type="dz">CC-BY</mods:accessCondition>
+       |  </mods:mods>
+       |  </mets:xmlData>
+       |  </mets:mdWrap>
+       |</mets:dmdSec>
+       |</mets:mets>
+       |""".stripMargin
 }
