268f043d2e8177ddaf499df5ff7677eb816f027b
alicefuzier <alicefuzier@gmail.com>
2019-11-25 14:00:10 +0000

    Make matcher read NotificationMessage

--- pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/services/MatcherWorkerService.scala
+++ pipeline/matcher/src/main/scala/uk/ac/wellcome/platform/matcher/services/MatcherWorkerService.scala
@@ -3,19 +3,17 @@ package uk.ac.wellcome.platform.matcher.services
 import akka.Done
 import akka.actor.ActorSystem
 import grizzled.slf4j.Logging
+import uk.ac.wellcome.bigmessaging.EmptyMetadata
+import uk.ac.wellcome.messaging.MessageSender
+import uk.ac.wellcome.messaging.sqs.NotificationStream
+import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.models.work.internal.TransformedBaseWork
+import uk.ac.wellcome.platform.matcher.exceptions.MatcherException
 import uk.ac.wellcome.platform.matcher.matcher.WorkMatcher
 import uk.ac.wellcome.platform.matcher.models.VersionExpectedConflictException
-import uk.ac.wellcome.platform.matcher.exceptions.MatcherException
-import uk.ac.wellcome.typesafe.Runnable
-import uk.ac.wellcome.models.Implicits._
-import uk.ac.wellcome.json.JsonUtil._
-
 import uk.ac.wellcome.storage.store.{HybridStoreEntry, VersionedStore}
 import uk.ac.wellcome.storage.{Identified, Version}
-import uk.ac.wellcome.messaging.sqs.SQSStream
-import uk.ac.wellcome.messaging.MessageSender
-import uk.ac.wellcome.bigmessaging.EmptyMetadata
+import uk.ac.wellcome.typesafe.Runnable
 
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -23,15 +21,14 @@ class MatcherWorkerService[MsgDestination](
   store: VersionedStore[String,
                         Int,
                         HybridStoreEntry[TransformedBaseWork, EmptyMetadata]],
-  msgStream: SQSStream[Version[String, Int]],
+  msgStream: NotificationStream[Version[String, Int]],
   msgSender: MessageSender[MsgDestination],
   workMatcher: WorkMatcher)(implicit val actorSystem: ActorSystem,
                             ec: ExecutionContext)
     extends Logging
     with Runnable {
 
-  def run(): Future[Done] =
-    msgStream.foreach(this.getClass.getSimpleName, processMessage)
+  def run(): Future[Done] = msgStream.run(processMessage)
 
   def processMessage(key: Version[String, Int]): Future[Unit] = {
     (for {
