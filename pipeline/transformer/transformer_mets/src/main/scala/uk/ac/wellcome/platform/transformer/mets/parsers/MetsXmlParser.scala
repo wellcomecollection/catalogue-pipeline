46c1dfa80f10cd6beef5b78da83556831255c025
Nick Ward <ward.nickjames@gmail.com>
2019-12-03 12:25:36 +0000

    Add comments for METS thumbnail parsing

--- pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/parsers/MetsXmlParser.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/parsers/MetsXmlParser.scala
@@ -51,6 +51,10 @@ object MetsXmlParser {
     }
   }
 
+  /** Here we use the first defined item in the physicalStructMap to lookup a
+    *  file ID the fileObjects mapping, and use the files location as the
+    *  thumbnail image.
+    */
   private def thumbnailLocation(root: Elem): Option[String] =
     physicalStructMap(root).headOption
       .flatMap {
@@ -59,6 +63,25 @@ object MetsXmlParser {
       }
       .map(_.stripPrefix("objects/"))
 
+  /** The METS XML contains locations of associated files, contained in a
+    *  mapping with the following format:
+    *
+    *  <mets:fileSec>
+    *    <mets:fileGrp USE="OBJECTS">
+    *      <mets:file ID="FILE_0001_OBJECTS" MIMETYPE="image/jp2">
+    *        <mets:FLocat LOCTYPE="URL" xlink:href="objects/b30246039_0001.jp2" />
+    *      </mets:file>
+    *      <mets:file ID="FILE_0002_OBJECTS" MIMETYPE="image/jp2">
+    *        <mets:FLocat LOCTYPE="URL" xlink:href="objects/b30246039_0002.jp2" />
+    *      </mets:file>
+    *    </mets:fileGrp>
+    *  </mets:fileSec>
+    *
+    *  For this input we would expect the following output:
+    *
+    *  Map("FILE_0001_OBJECTS" -> "objects/b30246039_0001.jp2",
+    *      "FILE_0002_OBJECTS" -> "objects/b30246039_0002.jp2")
+    */
   private def fileObjects(root: Elem): Map[String, String] =
     (root \ "fileSec" \ "fileGrp")
       .filterByAttribute("USE", "OBJECTS")
@@ -69,6 +92,28 @@ object MetsXmlParser {
         valueAttrib = "{http://www.w3.org/1999/xlink}href"
       )
 
+  /** Valid METS documents should contain a physicalStructMap section, with the
+    *  bottom most divs each representing a physical page, and linking to files
+    *  in the corresponding fileSec structures:
+    *
+    *  <mets:structMap TYPE="PHYSICAL">
+    *    <mets:div DMDID="DMDPHYS_0000" ID="PHYS_0000" TYPE="physSequence">
+    *      <mets:div ADMID="AMD_0001" ID="PHYS_0001" ORDER="1" TYPE="page">
+    *        <mets:fptr FILEID="FILE_0001_OBJECTS" />
+    *        <mets:fptr FILEID="FILE_0001_ALTO" />
+    *      </mets:div>
+    *      <mets:div ADMID="AMD_0002" ID="PHYS_0002" ORDER="2" TYPE="page">
+    *        <mets:fptr FILEID="FILE_0002_OBJECTS" />
+    *        <mets:fptr FILEID="FILE_0002_ALTO" />
+    *      </mets:div>
+    *    </mets:div>
+    *  </mets:structMap>
+    *
+    *  For this input we would expect the following output:
+    *
+    *  Map("PHYS_0001" -> "FILE_0001_OBJECTS",
+    *      "PHYS_0002" -> "FILE_0002_OBJECTS")
+    */
   private def physicalStructMap(root: Elem): ListMap[String, String] =
     (root \ "structMap")
       .filterByAttribute("TYPE", "PHYSICAL")
