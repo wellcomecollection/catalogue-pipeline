ae76bcd4d0b746371c07c648784c0226adb3eac6
alicefuzier <alicefuzier@gmail.com>
2019-12-04 11:43:46 +0000

    Comment

--- pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/rules/physicaldigital/SierraPhysicalDigitalPartitioner.scala
+++ pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/rules/physicaldigital/SierraPhysicalDigitalPartitioner.scala
@@ -16,21 +16,29 @@ trait SierraPhysicalDigitalPartitioner extends WorkTagPartitioner {
     work.sourceIdentifier.identifierType == IdentifierType(
       "sierra-system-number")
 
+  private def isSierraDigitalWork(work: UnidentifiedWork): Boolean =
+    isSierraWork(work) && isDigitalWork(work)
+
+  private def isSierraPhysicalWork(work: UnidentifiedWork): Boolean =
+    isSierraWork(work) && isPhysicalWork(work)
+
+  /***
+    * A Sierra digitised work will have a single item with a single [[DigitalLocation]]
+    */
   private def isDigitalWork(work: UnidentifiedWork): Boolean =
     work.data.items match {
       case List(Unidentifiable(Item(List(_:DigitalLocation), _))) => true
       case _ => false
     }
 
+  /***
+    * A Sierra physical work will have a single item with one or more locations
+    * of which at least one is a [[PhysicalLocation]]. It might have additional
+    * [[DigitalLocation]]s if the Sierra record contains a dlnk
+    */
   private def isPhysicalWork(work: UnidentifiedWork): Boolean =
     work.data.items match {
-      case List(Identifiable(Item(List(_:PhysicalLocation), _), _, _,_)) => true
+      case List(Identifiable(Item(locations, _), _, _,_)) if locations.exists(l => l.isInstanceOf[PhysicalLocation]) => true
       case _ => false
     }
-
-  private def isSierraDigitalWork(work: UnidentifiedWork): Boolean =
-    isSierraWork(work) && isDigitalWork(work)
-
-  private def isSierraPhysicalWork(work: UnidentifiedWork): Boolean =
-    isSierraWork(work) && isPhysicalWork(work)
 }
