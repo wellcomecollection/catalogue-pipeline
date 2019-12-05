e7e1e72af116aa6228a60313d6542a22367ebde2
Travis CI on behalf of Wellcome <wellcomedigitalplatform@wellcome.ac.uk>
2019-12-05 10:37:50 +0000

    s/scoringTiers/ScoringTiers

--- api/api/src/main/scala/uk/ac/wellcome/platform/api/QueryParams.scala
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/QueryParams.scala
@@ -175,7 +175,7 @@ object MultipleWorksParams extends QueryParamsUtils {
   implicit val _queryTypeDecoder: Decoder[SearchQueryType] =
     decodeOneWithDefaultOf(
       SearchQueryType.default,
-      "scoringTiers" -> SearchQueryType.ScoringTiers
+      "ScoringTiers" -> SearchQueryType.ScoringTiers
     )
 }
 
