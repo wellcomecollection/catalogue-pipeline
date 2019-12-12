bb32300f1c0f7b2f04350ded9ea4cdaa3340f349
alicefuzier <alicefuzier@gmail.com>
2019-12-12 10:47:42 +0000

    Split test and fix flakyness

--- pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/client/AssumeRoleClientProviderTest.scala
+++ pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/client/AssumeRoleClientProviderTest.scala
@@ -26,58 +26,68 @@ class AssumeRoleClientProviderTest
     with MockitoSugar {
 
   it("return a client using temporary credentials") {
-    withActorSystem { implicit actorSystem =>
-      withMockClientFactory(testS3ClientBuilder) {
-        case (mockClientFactory, invocationArguments) =>
-          withAssumeRoleClientProvider(
-            "arn:aws:iam::123456789012:role/new_role",
-            1 minute)(mockClientFactory) { assumeRoleClientProvider =>
-            val s3Client1 = assumeRoleClientProvider.getClient
+      withActorSystem { implicit actorSystem =>
+        withMockClientFactory(testS3ClientBuilder) {
+          case (mockClientFactory, invocationArguments) =>
+            withAssumeRoleClientProvider(
+              "arn:aws:iam::123456789012:role/new_role",
+              1 minute)(mockClientFactory) { assumeRoleClientProvider =>
+              val s3Client = assumeRoleClientProvider.getClient
 
-            s3Client1 shouldBe a[Right[_, _]]
-            s3Client1.right.get shouldBe a[AmazonS3]
-            val s3Client2 = assumeRoleClientProvider.getClient
-            Thread.sleep((15 milliseconds).toMillis)
-            val s3Client3 = assumeRoleClientProvider.getClient
-
-            s3Client2.right.get should be theSameInstanceAs (s3Client3.right.get)
-
-            invocationArguments.length should be >= 1
-            invocationArguments.length should be <= 2
-          }
+              s3Client shouldBe a[Right[_, _]]
+              s3Client.right.get shouldBe a[AmazonS3]
+            }
+        }
       }
-    }
   }
 
-  it("refreshes the credentials with the interval specified") {
-    withActorSystem { implicit actorSystem =>
-      withMockClientFactory(testS3ClientBuilder) {
-        case (mockClientFactory, credentials) =>
-          withAssumeRoleClientProvider(
-            "arn:aws:iam::123456789012:role/new_role",
-            10 milliseconds)(mockClientFactory) { assumeRoleClientProvider =>
-            Thread.sleep((30 milliseconds).toMillis)
-            val s3Client1 = assumeRoleClientProvider.getClient
-            Thread.sleep((30 milliseconds).toMillis)
-            val s3Client2 = assumeRoleClientProvider.getClient
+  it("refreshes the client when the interval specified expires") {
+      withActorSystem { implicit actorSystem =>
+        withMockClientFactory(testS3ClientBuilder) {
+          case (mockClientFactory, credentials) =>
+            withAssumeRoleClientProvider(
+              "arn:aws:iam::123456789012:role/new_role",
+              10 milliseconds)(mockClientFactory) { assumeRoleClientProvider =>
+              val s3Client1 = assumeRoleClientProvider.getClient
+              Thread.sleep((200 milliseconds).toMillis)
+              val s3Client2 = assumeRoleClientProvider.getClient
 
-            s3Client1.right.get shouldNot be theSameInstanceAs (s3Client2.right.get)
+              s3Client1.right.get shouldNot be theSameInstanceAs (s3Client2.right.get)
 
-            credentials.length should be >= 2
-            credentials.head.getSessionToken shouldNot be(
-              credentials.tail.head.getSessionToken)
-          }
+              credentials.length should be >= 2
+            }
+        }
       }
-    }
+  }
+
+  it("does not refresh the client if the interval has not expired") {
+      withActorSystem { implicit actorSystem =>
+        withMockClientFactory(testS3ClientBuilder) {
+          case (mockClientFactory, credentials) =>
+            withAssumeRoleClientProvider(
+              "arn:aws:iam::123456789012:role/new_role",
+              1 minute)(mockClientFactory) { assumeRoleClientProvider =>
+              // wait to make sure the client is initialised
+              Thread.sleep((100 milliseconds).toMillis)
+              val s3Client1 = assumeRoleClientProvider.getClient
+              Thread.sleep((30 milliseconds).toMillis)
+              val s3Client2 = assumeRoleClientProvider.getClient
+
+              s3Client1.right.get should be theSameInstanceAs (s3Client2.right.get)
+
+              credentials.length should be <= 2
+            }
+        }
+      }
   }
 
   def withMockClientFactory[T, R](clientFactory: ClientFactory[T])(
     testWith: TestWith[(ClientFactory[T],
-                        mutable.MutableList[BasicSessionCredentials]),
+                        mutable.Queue[BasicSessionCredentials]),
                        R]) = {
     val mockClientFactory = mock[ClientFactory[T]]
     val credentialsAccumulator =
-      new mutable.MutableList[BasicSessionCredentials]
+      new mutable.Queue[BasicSessionCredentials]
     Mockito
       .when(
         mockClientFactory.buildClient(
