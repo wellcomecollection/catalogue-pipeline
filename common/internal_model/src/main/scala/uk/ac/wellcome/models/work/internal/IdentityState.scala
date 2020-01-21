b99fe48d13451b5f92dc083b6b7f835ddfa15da2
Nick Ward <ward.nickjames@gmail.com>
2020-01-21 12:34:31 +0000

    Add doc strings to IdentityState

--- common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/IdentityState.scala
+++ common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/IdentityState.scala
@@ -1,14 +1,19 @@
 package uk.ac.wellcome.models.work.internal
 
+/** The root trait for all possible ID types. */
 sealed trait IdState {
   def maybeCanonicalId: Option[String]
   def allSourceIdentifiers: List[SourceIdentifier]
 }
 
+/** Parent trait for an ID of an object that is pre minter. */
 sealed trait Unminted extends IdState
 
+/** Parent trait for an ID of an object that is post minter. */
 sealed trait Minted extends IdState
 
+/** Represents an ID that has been succesfully mintedo, and thus has a
+ *  canonicalId assigned. */
 case class Identified(
   canonicalId: String,
   sourceIdentifier: SourceIdentifier,
@@ -19,6 +24,8 @@ case class Identified(
   def allSourceIdentifiers = sourceIdentifier +: otherIdentifiers
 }
 
+/** Represents an ID that has not yet been minted, but will have a canonicalId
+ *  assigned later in the pipeline. */
 case class Identifiable(
   sourceIdentifier: SourceIdentifier,
   otherIdentifiers: List[SourceIdentifier] = Nil,
@@ -29,11 +36,15 @@ case class Identifiable(
   def allSourceIdentifiers = sourceIdentifier +: otherIdentifiers
 }
 
+/** Represents an ID that has no sourceIdentifier and thus impossible to have a
+ *  canonicalId assigned. Note that it is possible for this ID to be either pre
+ *  or post minter. */
 case object Unidentifiable extends IdState with Unminted with Minted {
   def maybeCanonicalId = None
   def allSourceIdentifiers = Nil
 }
 
+/** A trait assigned to all objects that contain some ID value. */
 trait HasIdState[+Id] {
   val id: Id
 }
