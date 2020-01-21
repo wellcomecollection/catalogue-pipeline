15f06366b411dbeae358df8fd27d6a7edd5b5a19
jamie <j.parkinson@wellcome.ac.uk>
2020-01-21 11:21:56 +0000

    Decode and map aggregations from ES using optics

--- api/api/src/main/scala/uk/ac/wellcome/platform/api/models/Aggregation.scala
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/models/Aggregation.scala
@@ -1,11 +1,10 @@
 package uk.ac.wellcome.platform.api.models
 
-import scala.util.{Failure, Try}
+import scala.util.{Failure, Success, Try}
 import io.circe.Decoder
 import java.time.{Instant, LocalDateTime, ZoneOffset}
 
-import com.sksamuel.elastic4s.AggReader
-import com.sksamuel.elastic4s.requests.searches.{SearchResponse, Transformable}
+import com.sksamuel.elastic4s.requests.searches.SearchResponse
 import grizzled.slf4j.Logging
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.json.JsonUtil._
@@ -26,24 +25,14 @@ object Aggregations extends Logging {
     if (e4sAggregations.data.nonEmpty) {
       Some(
         Aggregations(
-          workType = e4sAggregations
-            .getAgg("workType")
-            .flatMap(_.toAgg[WorkType]),
-          genres = e4sAggregations
-            .getAgg("genres")
-            .flatMap(_.toAgg[Genre[Minted[AbstractConcept]]]),
-          productionDates = e4sAggregations
-            .getAgg("productionDates")
-            .flatMap(_.toAgg[Period]),
-          language = e4sAggregations
-            .getAgg("language")
-            .flatMap(_.toAgg[Language]),
+          workType = e4sAggregations.decodeAgg[WorkType]("workType"),
+          genres =
+            e4sAggregations.decodeAgg[Genre[Minted[AbstractConcept]]]("genres"),
+          productionDates = e4sAggregations.decodeAgg[Period]("productionDates"),
+          language = e4sAggregations.decodeAgg[Language]("language"),
           subjects = e4sAggregations
-            .getAgg("subjects")
-            .flatMap(_.toAgg[Subject[Minted[AbstractRootConcept]]]),
-          license = e4sAggregations
-            .getAgg("license")
-            .flatMap(_.toAgg[License])
+            .decodeAgg[Subject[Minted[AbstractRootConcept]]]("subjects"),
+          license = e4sAggregations.decodeAgg[License]("license")
         ))
     } else {
       None
@@ -69,74 +58,84 @@ object Aggregations extends Logging {
         .map(err => err.getMessage)
     }
 
-  implicit class EnhancedTransformable(transformable: Transformable) {
-    def toAgg[T: Decoder]: Option[Aggregation[T]] = {
-      transformable
-        .safeTo[EsAggregation[T]]
-        .recoverWith {
-          case err =>
-            warn("Failed to parse aggregation from ES", err)
-            Failure(err)
-        }
-        .toOption
-        .map(
-          agg =>
-            Aggregation(
-              agg.buckets
-                .map(getFilteredBucket)
-                .filter(_.count != 0)))
+  implicit val decodeWorkType: Decoder[WorkType] =
+    Decoder.decodeString.emap { str =>
+      WorkType.fromCode(str) match {
+        case Some(workType) => Right(workType)
+        case None           => Left(s"couldn't find workType for code '$str'")
+      }
     }
 
-    /** If the `filtered` sub-aggregation is present (see [[uk.ac.wellcome.platform.api.services.FiltersAndAggregationsBuilder]])
-      * then use the count from it */
-    private def getFilteredBucket[T](
-      esAggBucket: EsAggregationBucket[T]): AggregationBucket[T] =
-      AggregationBucket(
-        esAggBucket.key,
-        esAggBucket.filtered
-          .map(_.doc_count)
-          .getOrElse(esAggBucket.doc_count))
+  implicit val decodeGenreFromLabel: Decoder[Genre[Minted[AbstractConcept]]] =
+    Decoder.decodeString.map { str =>
+      Genre(label = str)
+    }
 
-    implicit def fromJsonAggReader[T: Decoder]: AggReader[T] =
-      (json: String) => fromJson[T](json)
+  implicit val decodeSubjectFromLabel
+    : Decoder[Subject[Minted[AbstractRootConcept]]] =
+    Decoder.decodeString.map { str =>
+      Subject(label = str)
+    }
+
+  implicit class EnhancedEsAggregations(
+    aggregations: com.sksamuel.elastic4s.requests.searches.Aggregations) {
+
+    def decodeAgg[T: Decoder](name: String): Option[Aggregation[T]] = {
+      aggregations
+        .getAgg(name)
+        .flatMap(
+          _.safeTo[Aggregation[T]]((json: String) =>
+            AggregationMapping.aggregationParser(name, json)).recoverWith {
+            case err =>
+              warn("Failed to parse aggregation from ES", err)
+              Failure(err)
+          }.toOption
+        )
+    }
   }
+}
+
+object AggregationMapping extends Logging {
+  import io.circe.parser._
+  import io.circe.optics.JsonPath._
+
+  private val buckets = root.buckets.arr
+  private val bucketFilteredCount = root.filtered.doc_count.int
+  private val bucketRootCount = root.doc_count.int
+  private val bucketKey = root.key.json
+  private val bucketSampleDoc =
+    root.sample_doc.hits.hits.index(0)._source.data.json
+
+  def aggregationParser[T: Decoder](name: String,
+                                    json: String): Try[Aggregation[T]] = {
+    val parsedJson = parse(json) match {
+      case Right(json) => json
+      case Left(err) =>
+        return Failure(err)
+    }
+    val bucketMaybes = for {
+      bucket <- buckets.getOption(parsedJson).getOrElse(List())
+      bucketDoc <- bucketSampleDoc
+        .getOption(bucket)
+        .flatMap(_.hcursor.downField(name).focus)
+        .orElse(bucketKey.getOption(bucket))
+        .map(_.as[T])
+      bucketCount <- bucketFilteredCount
+        .getOption(bucket)
+        .orElse(bucketRootCount.getOption(bucket))
+    } yield {
+      (bucketCount, bucketDoc) match {
+        case (0, _)           => None
+        case (n, Right(data)) => Some(AggregationBucket(data, n))
+        case (_, Left(err)) =>
+          warn("Failed to parse aggregation from ES", err)
+          return Failure(err)
+      }
+    }
+    Success(Aggregation(bucketMaybes.toList.flatten))
+  }
+
 }
 
 case class Aggregation[T](buckets: List[AggregationBucket[T]])
 case class AggregationBucket[T](data: T, count: Int)
-
-/**
-  * We use these to convert the JSON into Elasticsearch case classes (not supplied via elastic4s)
-  * and then convert them into our representations of aggregations.
-  * This is to get around things like having property names like `doc_count` and `key`
-  * The general format here is:
-  *
-  * {
-  *   "buckets": {
-  *     "key": {
-  *       "this": "is",
-  *       "structured": "content"
-  *     },
-  *     "doc_count" 1009
-  *   }
-  * }
-  *
-  * And we convert them to:
-  * {
-  *   "buckets": {
-  *     "data": {
-  *       "this": "is",
-  *       "structured": "content"
-  *     },
-  *     "count" 1009
-  *   }
-  * }
-  */
-case class EsAggregation[T](buckets: List[EsAggregationBucket[T]])
-
-/** The `filtered` key is introduced by the nested aggregations requested by
-  * [[uk.ac.wellcome.platform.api.services.FiltersAndAggregationsBuilder]] */
-case class EsAggregationBucket[T](key: T,
-                                  doc_count: Int,
-                                  filtered: Option[EsAggregationSubAgg])
-case class EsAggregationSubAgg(doc_count: Int)
