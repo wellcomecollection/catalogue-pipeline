4fe1943a720a6560d3682a01ef4ce5bd65e07a6c
Alex Chan <a.chan@wellcome.ac.uk>
2020-01-22 15:22:24 +0000

    Fix an output from the scaling_worker module

--- sierra_adapter/terraform/modules/scaling_worker/outputs.tf
+++ sierra_adapter/terraform/modules/scaling_worker/outputs.tf
@@ -1,9 +1,9 @@
 output "task_role_name" {
-  value = module.worker.task_role_name
+  value = module.task_definition.task_role_name
 }
 
 output "task_role_arn" {
-  value = module.worker.task_role_arn
+  value = module.task_definition.task_role_arn
 }
 
 output "scale_up_arn" {
