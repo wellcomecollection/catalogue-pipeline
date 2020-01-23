a373ae333fd4ea824e869c5dbc0fdc98d46742f7
Nick Ward <ward.nickjames@gmail.com>
2020-01-23 15:52:26 +0000

    Basic terraform setup for CALM adapter

---
+++ calm_adapter/terraform/terraform.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = ">= 0.9"
+
+  backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+
+    bucket         = "wellcomecollection-platform-infra"
+    key            = "terraform/calm_adapter.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
+  }
+}
+
+data "terraform_remote_state" "shared_infra" {
+  backend = "s3"
+
+  config {
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
+
+    bucket = "wellcomecollection-platform-infra"
+    key    = "terraform/shared_infra.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+data "aws_caller_identity" "current" {}
