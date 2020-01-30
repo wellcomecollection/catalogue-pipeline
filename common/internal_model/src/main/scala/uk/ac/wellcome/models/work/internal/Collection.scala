d85e889ee2218c59ccadefa4ea1ba46ac5ce5c15
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-01-30 16:44:42 +0000

    remove paths and depth from Collection

--- common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Collection.scala
+++ common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/Collection.scala
@@ -3,8 +3,4 @@ package uk.ac.wellcome.models.work.internal
 case class Collection(
   label: Option[String],
   path: String,
-) {
-  val paths: List[String] =
-    path.split("/").inits.toList.reverse.drop(0).map(_.mkString("/"))
-  val depth: Int = paths.size
-}
+)
