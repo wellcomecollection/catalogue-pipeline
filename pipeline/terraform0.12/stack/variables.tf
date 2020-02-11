b38122b3ff7ec0c313d2e2badf51655b9cd5ced6
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-11 08:06:07 +0000

    add stack main and ecs

---
+++ pipeline/terraform0.12/stack/variables.tf
@@ -0,0 +1,52 @@
+variable "namespace" {}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "vpc_id" {}
+variable "aws_region" {}
+
+variable "account_id" {}
+
+variable "dlq_alarm_arn" {}
+
+variable "es_works_index" {}
+
+variable "rds_ids_access_security_group_id" {}
+
+variable "release_label" {}
+
+variable "miro_adapter_topic_names" {
+  type = list(string)
+}
+
+variable "miro_adapter_topic_count" {}
+
+variable "mets_adapter_topic_names" {
+  type = list(string)
+}
+
+variable "mets_adapter_topic_count" {}
+
+variable "sierra_adapter_topic_names" {
+  type = list(string)
+}
+
+variable "sierra_adapter_topic_count" {}
+
+variable "vhs_miro_read_policy" {}
+
+variable "vhs_sierra_read_policy" {}
+variable "vhs_sierra_sourcedata_bucket_name" {}
+variable "vhs_sierra_sourcedata_table_name" {}
+
+variable "mets_adapter_read_policy" {}
+
+variable "mets_adapter_table_name" {}
+
+variable "private_subnets" {
+  type = list(string)
+}
+
+variable "read_storage_s3_role_arn" {}
