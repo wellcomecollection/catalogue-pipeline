8044ff714501c7614810ea4c7dcead1462a7d9cc
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-12 13:39:42 +0000

    add work result

--- api/api/src/main/scala/uk/ac/wellcome/platform/api/models/ResultList.scala
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/models/ResultList.scala
@@ -2,8 +2,10 @@ package uk.ac.wellcome.platform.api.models
 
 import uk.ac.wellcome.models.work.internal.IdentifiedWork
 
+case class WorkResult(work: IdentifiedWork, score: Int, queryName: String)
+
 case class ResultList(
-  results: List[IdentifiedWork],
+  results: List[WorkResult],
   totalResults: Int,
   aggregations: Option[Aggregations]
 )
