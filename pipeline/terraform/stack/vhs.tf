809adcb73fdec2270f2366a083d79aa8deae9319
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 11:53:05 +0000

    add prefix to recorder VHS

--- pipeline/terraform/stack/vhs.tf
+++ pipeline/terraform/stack/vhs.tf
@@ -1,5 +1,6 @@
 module "vhs_recorder" {
-  source = "git::github.com/wellcomecollection/terraform-aws-vhs.git?ref=v1.0.0"
-
-  name = "${local.namespace_hyphen}-recorder"
+  source             = "git::github.com/wellcomecollection/terraform-aws-vhs.git?ref=v1.0.0"
+  bucket_name_prefix = "wellcomecollection-vhs-"
+  table_name_prefix  = "vhs-"
+  name               = "${local.namespace_hyphen}-recorder"
 }
