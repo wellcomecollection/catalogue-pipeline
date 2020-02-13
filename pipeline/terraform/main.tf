be671c42f16d8c616383f2ba94a1b5b4d09a1980
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 09:17:31 +0000

    pipeline tf0.12 upgrade: main

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,50 +1,51 @@
-module "catalogue_pipeline_20200131" {
+module "catalogue_pipeline_20200211" {
   source = "./stack"
 
-  namespace = "catalogue-20200131"
+  namespace = "catalogue-20200211"
 
   release_label = "prod"
 
-  account_id      = "${data.aws_caller_identity.current.account_id}"
-  aws_region      = "${local.aws_region}"
-  vpc_id          = "${local.vpc_id}"
-  subnets         = ["${local.private_subnets}"]
-  private_subnets = ["${local.private_subnets}"]
+  account_id      = data.aws_caller_identity.current.account_id
+  aws_region      = local.aws_region
+  vpc_id          = local.vpc_id
+  subnets         = local.private_subnets
+  private_subnets = local.private_subnets
 
-  dlq_alarm_arn = "${local.dlq_alarm_arn}"
+  dlq_alarm_arn = local.dlq_alarm_arn
 
   # Transformer config
   #
   # If this pipeline is meant to be reindexed, remember to uncomment the
   # reindexer topic names.
 
-  sierra_adapter_topic_count = 2
-  sierra_adapter_topic_names = [
-    //    "${local.sierra_reindexer_topic_name}",
-    "${local.sierra_merged_bibs_topic_name}",
+  sierra_adapter_topic_arns = [
+    local.sierra_reindexer_topic_arn,
+    local.sierra_merged_bibs_topic_arn,
+    local.sierra_merged_items_topic_arn,
+  ]
 
-    "${local.sierra_merged_items_topic_name}",
+  miro_adapter_topic_arns = [
+    local.miro_reindexer_topic_arn,
+    local.miro_updates_topic_arn,
   ]
-  miro_adapter_topic_count = 1
-  miro_adapter_topic_names = [
-    //    "${local.miro_reindexer_topic_name}",
-    "${local.miro_updates_topic_name}",
+
+  mets_adapter_topic_arns = [
+    local.mets_reindexer_topic_arn,
+    local.mets_adapter_topic_arn,
   ]
-  mets_adapter_topic_count = 1
-  mets_adapter_topic_names = [
-    //    "${local.mets_reindexer_topic_name}",
-    "${local.mets_adapter_topic_name}",
-  ]
+
   # Elasticsearch
-  es_works_index = "v2-20200131"
+  es_works_index = "v2-20200211"
+
   # RDS
-  rds_ids_access_security_group_id = "${local.rds_access_security_group_id}"
+  rds_ids_access_security_group_id = local.rds_access_security_group_id
+
   # Adapter VHS
-  vhs_sierra_read_policy            = "${local.vhs_sierra_read_policy}"
-  vhs_miro_read_policy              = "${local.vhs_miro_read_policy}"
-  vhs_sierra_sourcedata_bucket_name = "${local.vhs_sierra_sourcedata_bucket_name}"
-  vhs_sierra_sourcedata_table_name  = "${local.vhs_sierra_sourcedata_table_name}"
-  mets_adapter_read_policy          = "${local.mets_adapter_read_policy}"
-  mets_adapter_table_name           = "${local.mets_adapter_table_name}"
-  read_storage_s3_role_arn          = "${aws_iam_role.read_storage_s3.arn}"
+  vhs_sierra_read_policy            = local.vhs_sierra_read_policy
+  vhs_miro_read_policy              = local.vhs_miro_read_policy
+  vhs_sierra_sourcedata_bucket_name = local.vhs_sierra_sourcedata_bucket_name
+  vhs_sierra_sourcedata_table_name  = local.vhs_sierra_sourcedata_table_name
+  mets_adapter_read_policy          = local.mets_adapter_read_policy
+  mets_adapter_table_name           = local.mets_adapter_table_name
+  read_storage_s3_role_arn          = aws_iam_role.read_storage_s3.arn
 }
