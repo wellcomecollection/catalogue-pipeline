42abcdfe1162b1602fed3eaaa7903005e383e797
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 10:12:28 +0000

    reference VHS module correctly

--- pipeline/terraform/stack/vhs.tf
+++ pipeline/terraform/stack/vhs.tf
@@ -1,5 +1,5 @@
 module "vhs_recorder" {
-  source = "git::https://github.com/wellcomecollection/terraform-aws-vhs.git?ref=v19.7.2"
+  source = "git::github.com/wellcomecollection/terraform-aws-vhs.git?ref=v1.0.0"
 
   name = "${local.namespace_hyphen}-recorder"
 }
