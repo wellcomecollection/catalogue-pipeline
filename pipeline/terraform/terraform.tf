0dea5742d0babd41ce4522a1d820b1095e5584c3
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 09:21:02 +0000

    update path to tfstate to be repo/project

--- pipeline/terraform/terraform.tf
+++ pipeline/terraform/terraform.tf
@@ -12,7 +12,7 @@ terraform {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
 
     bucket         = "wellcomecollection-platform-infra"
-    key            = "terraform/catalogue_pipeline_tf_0.12.tfstate"
+    key            = "terraform/catalogue/pipeline.tfstate"
     dynamodb_table = "terraform-locktable"
     region         = "eu-west-1"
   }
