4bd598ee6a01f5fba9f431c1bc684b4a76260e1b
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-13 09:46:38 +0000

    pipeline tf0.12 upgrade: stack/s3+security_groups

--- pipeline/terraform/stack/security_groups.tf
+++ pipeline/terraform/stack/security_groups.tf
@@ -1,16 +1,7 @@
-module "egress_security_group" {
-  source = "github.com/wellcometrust/terraform//network/prebuilt/vpc/egress_security_group?ref=v19.5.0"
-
-  name = "${local.namespace_hyphen}_pipeline_services"
-
-  vpc_id     = "${var.vpc_id}"
-  subnet_ids = "${var.subnets}"
-}
-
 resource "aws_security_group" "service_egress" {
   name        = "${local.namespace_hyphen}_service_egress"
-  description = "Allow traffic between services"
-  vpc_id      = "${var.vpc_id}"
+  description = "Allow egress traffic to service"
+  vpc_id      = var.vpc_id
 
   egress {
     from_port = 0
@@ -21,25 +12,17 @@ resource "aws_security_group" "service_egress" {
       "0.0.0.0/0",
     ]
   }
-
-  tags {
-    Name = "${local.namespace_hyphen}-egress"
-  }
 }
 
 resource "aws_security_group" "interservice" {
   name        = "${local.namespace_hyphen}_interservice"
   description = "Allow traffic between services"
-  vpc_id      = "${var.vpc_id}"
+  vpc_id      = var.vpc_id
 
   ingress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     self      = true
-  }
-
-  tags {
-    Name = "${local.namespace_hyphen}-interservice"
   }
 }
