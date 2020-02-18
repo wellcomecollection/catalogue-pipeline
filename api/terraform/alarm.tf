068aafbe9356ba4855d4d2207308359c1f023721
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-18 17:53:18 +0000

    remove terraform upgrade todos

--- api/terraform/alarm.tf
+++ api/terraform/alarm.tf
@@ -1,10 +1,4 @@
-# TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to begin a
-# resource name with a number, but it is no longer possible in Terraform v0.12.
-#
-# Rename the resource and run `terraform state mv` to apply the rename in the
-# state. Detailed information on the `state move` command can be found in the
-# documentation online: https://www.terraform.io/docs/commands/state/mv.html
-resource "aws_cloudwatch_metric_alarm" "5xx_alarm" {
+resource "aws_cloudwatch_metric_alarm" "alarm_5xx" {
   alarm_name          = "catalogue-api-${local.prod_name}-5xx-alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "1"
@@ -19,14 +13,6 @@ resource "aws_cloudwatch_metric_alarm" "5xx_alarm" {
     Stage   = local.prod_name
   }
 
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   alarm_actions = [local.gateway_server_error_alarm_arn]
 }
 
--- api/terraform/domains.tf
+++ api/terraform/domains.tf
@@ -1,13 +1,5 @@
 resource "aws_acm_certificate" "catalogue_api" {
   domain_name = local.prod_domain_name
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   subject_alternative_names = [local.staging_domain_name]
   validation_method         = "DNS"
 
@@ -26,14 +18,6 @@ resource "aws_route53_record" "cert_validation" {
   name     = local.validation_opts[count.index]["resource_record_name"]
   type     = local.validation_opts[count.index]["resource_record_type"]
   zone_id  = local.routemaster_router53_zone_id
-  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
-  # force an interpolation expression to be interpreted as a list by wrapping it
-  # in an extra set of list brackets. That form was supported for compatibility in
-  # v0.11, but is no longer supported in Terraform v0.12.
-  #
-  # If the expression in the following list itself returns a list, remove the
-  # brackets to avoid interpretation as a list of lists. If the expression
-  # returns a single list item then leave it as-is and remove this TODO comment.
   records = [local.validation_opts[count.index]["resource_record_value"]]
   ttl     = 60
 }
