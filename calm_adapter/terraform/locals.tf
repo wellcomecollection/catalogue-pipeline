20eeb5ecac434161960df2b282aaf8249b9a3c70
Nick Ward <ward.nickjames@gmail.com>
2020-02-18 15:23:59 +0000

    Calm adapter terraform

--- calm_adapter/terraform/locals.tf
+++ calm_adapter/terraform/locals.tf
@@ -9,4 +9,15 @@ locals {
   dlq_alarm_arn   = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
   vpc_id          = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_id
   private_subnets = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_private_subnets
+
+  env_vars = {
+    calm_sqs_url = module.calm_windows_queue.url
+    calm_sns_topic = aws_sns_topic.calm_adapter_topic.name
+    vhs_dynamo_table_name = module.vhs.table_name
+    vhs_bucket_name = module.vhs.bucket_name
+  }
+  secret_env_vars = {
+    calm_api_username = "calm_adapter/calm_api/username"
+    calm_api_password = "calm_adapter/calm_api/password"
+  }
 }
---
+++ calm_adapter/terraform/service.tf
@@ -0,0 +1,36 @@
+module "service" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//service?ref=v1.2.0"
+  service_name = local.namespace
+  cluster_arn = aws_ecs_cluster.cluster.arn
+  task_definition_arn = module.task_definition.arn
+  subnets = local.private_subnets
+  namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
+  security_group_ids = []
+  launch_type = "FARGATE"
+  desired_task_count = 1
+}
+
+module "task_definition" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//task_definition/single_container?ref=v1.2.0"
+  task_name = local.namespace
+  container_image = local.calm_adapter_image
+  cpu    = "256"
+  memory = "512"
+  env_vars        = local.env_vars
+  secret_env_vars = local.secret_env_vars
+  launch_type = "FARGATE"
+  aws_region = local.aws_region
+  use_awslogs = true
+}
+
+module "scaling" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//autoscaling?ref=v1.2.0"
+
+  name = local.namespace
+
+  cluster_name = local.namespace
+  service_name = local.namespace
+
+  min_capacity = 1
+  max_capacity = 2
+}
