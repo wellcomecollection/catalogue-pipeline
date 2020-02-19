35101aad0c8c06de0c17c60488ecd7247193f80c
Nick Ward <ward.nickjames@gmail.com>
2020-02-19 16:45:03 +0000

    Configure Calm VHS and CalmRetriever in adapter

--- calm_adapter/calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/Main.scala
+++ calm_adapter/calm_adapter/src/main/scala/uk/ac/wellcome/calm_adapter/Main.scala
@@ -1,14 +1,18 @@
 package uk.ac.wellcome.calm_adapter
 
 import scala.concurrent.ExecutionContext
-import akka.NotUsed
+import com.typesafe.config.Config
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl._
 
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
+import uk.ac.wellcome.storage.store.VersionedStore
+import uk.ac.wellcome.bigmessaging.VHSWrapper
+import uk.ac.wellcome.bigmessaging.typesafe.VHSBuilder
+import uk.ac.wellcome.json.JsonUtil._
 
 object Main extends WellcomeTypesafeApp {
 
@@ -19,15 +23,33 @@ object Main extends WellcomeTypesafeApp {
       AkkaBuilder.buildActorSystem()
     implicit val materializer: ActorMaterializer =
       AkkaBuilder.buildActorMaterializer()
+    implicit val httpClient: CalmHttpClient =
+      new CalmAkkaHttpClient()
 
     new CalmAdapterWorkerService(
       SQSBuilder.buildSQSStream(config),
       SNSBuilder.buildSNSMessageSender(config, subject = "CALM adapter"),
-      new CalmRetriever {
-        def apply(query: CalmQuery): Source[CalmRecord, NotUsed] =
-          ???
-      },
-      new CalmStore(???)
+      calmRetriever(config),
+      calmStore(config),
     )
   }
+
+  def calmRetriever(config: Config)(implicit
+                                    ec: ExecutionContext,
+                                    materializer: ActorMaterializer,
+                                    httpClient: CalmHttpClient) =
+    new HttpCalmRetriever(
+      url = config.required[String]("calm.api.url"),
+      username = config.required[String]("calm.api.username"),
+      password = config.required[String]("calm.api.password"),
+    )
+
+  def calmStore(config: Config) =
+    new CalmStore(
+      new VersionedStore(
+        new VHSWrapper(
+          VHSBuilder.build[CalmRecord](config)
+        )
+      )
+    )
 }
