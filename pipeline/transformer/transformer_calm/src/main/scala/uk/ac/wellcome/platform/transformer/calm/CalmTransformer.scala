7902a9382529d9d20215bd675b66d17219ac9ce1
Nick Ward <ward.nickjames@gmail.com>
2020-02-24 14:02:15 +0000

    WIP

--- pipeline/transformer/transformer_calm/src/main/scala/uk/ac/wellcome/platform/transformer/calm/CalmTransformer.scala
+++ pipeline/transformer/transformer_calm/src/main/scala/uk/ac/wellcome/platform/transformer/calm/CalmTransformer.scala
@@ -1,29 +1,24 @@
 package uk.ac.wellcome.platform.transformer.calm
 
-import uk.ac.wellcome.models.work.internal.{TransformedBaseWork, UnidentifiedWork, WorkData}
+import uk.ac.wellcome.models.work.internal.{TransformedBaseWork, UnidentifiedWork, WorkData, SourceIdentifier}
 import uk.ac.wellcome.platform.transformer.calm.models.CalmRecord
-import uk.ac.wellcome.platform.transformer.calm.transformers.CalmToSourceIdentifier
 
 trait CalmTransformerError extends Throwable
 case object SourceIdentifierMissingError extends CalmTransformerError
 case object MultipleSourceIdentifiersFoundError extends CalmTransformerError
 
 object CalmTransformer extends Transformer[CalmRecord, TransformedBaseWork] {
-  def transform(calmRecord: CalmRecord)
-  : Either[CalmTransformerError, TransformedBaseWork] = {
-    val sourceIdentifier = CalmToSourceIdentifier.transform(calmRecord)
+  def transform(record: CalmRecord)
+  : Either[CalmTransformerError, TransformedBaseWork] =
 
-    sourceIdentifier.map {
-      case head :: Nil =>
-        Right(
-          UnidentifiedWork(
-            version = 1,
-            sourceIdentifier = head,
-            data = WorkData()))
-
-      case _ => Left(MultipleSourceIdentifiersFoundError)
-    } getOrElse Left(SourceIdentifierMissingError)
-
-  }
-
+    Right(
+      UnidentifiedWork(
+        sourceIdentifier = SourceIdentifier(
+          value = record.id,
+          identifierType = CalmIdentifierTypes.recordId,
+          ontologyType = "IdentifierType"),
+        version = 1,
+        data = WorkData()
+      )
+  )
 }
