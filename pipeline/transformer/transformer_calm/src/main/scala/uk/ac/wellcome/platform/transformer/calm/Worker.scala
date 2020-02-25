bca467359f3c8ad95d315575b89bba249184e447
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-02-25 10:04:26 +0000

    add Worker with AKKA flows

--- pipeline/transformer/transformer_calm/src/main/scala/uk/ac/wellcome/platform/transformer/calm/Worker.scala
+++ pipeline/transformer/transformer_calm/src/main/scala/uk/ac/wellcome/platform/transformer/calm/Worker.scala
@@ -1,38 +1,56 @@
 package uk.ac.wellcome.platform.transformer.calm
 
+import akka.{Done, NotUsed}
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.amazonaws.services.sqs.model.Message
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.platform.transformer.calm.service.{
-  CalmTransformerWorkerServiceState,
-  MessageReadError
-}
-import uk.ac.wellcome.storage.Version
+import uk.ac.wellcome.typesafe.Runnable
+
+import scala.concurrent.Future
 
 trait Worker[Store, StoreKey, SourceData, SinkData] extends Runnable {
   type Result[T] = Either[Throwable, T]
+  type SourceOut = (Message, NotificationMessage)
+  val errorSink: Sink[Any, Future[Done]] = Sink.ignore
 
   def runStream(stream: SQSStream[NotificationMessage]) =
-    stream.foreach(this.getClass.getSimpleName, processMessage)
+    stream.runStream(streamName = "StreamName", processMessage)
 
-  def processMessage(message: NotificationMessage) =
-    for {
-      key <- readMessage(message)
-      storeRecord <- get(key)
-      sinkData <- doWork(storeRecord)
-      sent <- done(sinkData)
-    } yield sent
+  def processMessage(source: Source[SourceOut, NotUsed]) =
+    source.via(getNotificationFlow)
 
-  def get(storeKey: StoreKey): Result[SourceData]
-
-  private def readMessage(message: NotificationMessage): Result[StoreKey] =
-    fromJson[StoreKey](message.body).toEither match {
-      case Left(err) =>
-        Left(new Exception(s"Worker: Cannot read message, ${err}"))
-      case _ => _
+  def divertEither[I, O](flow: Flow[I, Result[O], NotUsed],
+                         to: Sink[Any, Future[Done]]): Flow[I, O, NotUsed] =
+    flow.divertTo(Sink.ignore, _.isLeft).collect {
+      case Right(out) => out
     }
 
-  def doWork(sourceData: SourceData): Result[SinkData]
+  val getNotificationFlow: Flow[SourceOut, NotificationMessage, NotUsed] =
+    Flow.fromFunction(_._2)
+
+  val decodeKeyFlow: Flow[NotificationMessage, StoreKey, NotUsed] =
+    divertEither(Flow.fromFunction(decodeKey), to = errorSink)
+
+  val getSourceDataFlow: Flow[StoreKey, SourceData, NotUsed] =
+    divertEither(Flow.fromFunction(getSourceData), to = errorSink)
+
+  val workFlow: Flow[SourceData, SinkData, NotUsed] =
+    divertEither(Flow.fromFunction(work), to = errorSink)
+
+  val doneFlow: Flow[SinkData, Unit, NotUsed] =
+    divertEither(Flow.fromFunction(done), to = errorSink)
+
+  def decodeKey(notification: NotificationMessage): Result[StoreKey] =
+    fromJson[StoreKey](notification.body) toEither match {
+      case Left(err)  => Left(err)
+      case Right(key) => Right(key)
+    }
+
+  def getSourceData(key: StoreKey): Result[SourceData]
+
+  def work(sourceData: SourceData): Result[SinkData]
 
   def done(sinkData: SinkData): Result[Unit]
 }
