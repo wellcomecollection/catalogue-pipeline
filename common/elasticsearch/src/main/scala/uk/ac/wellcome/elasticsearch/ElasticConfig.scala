11e07a5a13027fdda0c90196cb88b03e98c44686
jamie <j.parkinson@wellcome.ac.uk>
2020-03-02 10:40:12 +0000

    Point API at new index

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
@@ -9,5 +9,5 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  def apply(): ElasticConfig = ElasticConfig(index = Index("v2-20200131"))
+  def apply(): ElasticConfig = ElasticConfig(index = Index("v2-20200228"))
 }
