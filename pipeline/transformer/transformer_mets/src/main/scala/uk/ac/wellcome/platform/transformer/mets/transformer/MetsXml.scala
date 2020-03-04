20d7274bfea2630ddef65f742f201a16076c1271
alicefuzier <alicefuzier@gmail.com>
2020-03-04 10:27:38 +0000

    Remove useless regex

--- pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/transformer/MetsXml.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/uk/ac/wellcome/platform/transformer/mets/transformer/MetsXml.scala
@@ -162,17 +162,11 @@ case class MetsXml(root: Elem) {
     *  - prepend the bnumber followed by an underscore if it's not already present (uppercase or lowercase)
     */
   private def normaliseLocation(
-    bnumber: String): FileReference => FileReference = {
-    val filePrefixRegex = s"""objects/(?i:($bnumber)_)?(.*)""".r
+                                 bnumber: String): FileReference => FileReference = {
     fileReference: FileReference =>
-      fileReference.copy(location = fileReference.location match {
-        case filePrefixRegex(caseInsensitiveBnumber, postFix) =>
-          Option(caseInsensitiveBnumber) match {
-            case Some(caseInsensitiveBnumber) =>
-              s"${caseInsensitiveBnumber}_$postFix"
-            case _ => s"${bnumber}_$postFix"
-          }
-        case _ => fileReference.location
+      fileReference.copy(location = fileReference.location.replaceFirst("objects/", "") match {
+        case fileName if fileName.toLowerCase.startsWith(bnumber.toLowerCase) => fileName
+        case fileName => s"${bnumber}_$fileName"
       })
   }
 
