d5d6302df6bc9dcc4e95c1df1609f45d925177b4
Alex Chan <a.chan@wellcome.ac.uk>
2020-03-06 13:35:30 +0000

    Use a terraform 0.12-updated SNS module

--- sierra_adapter/terraform/items_to_dynamo/topic.tf
+++ sierra_adapter/terraform/items_to_dynamo/topic.tf
@@ -1,4 +1,4 @@
 module "sierra_to_dynamo_updates_topic" {
-  source = "git::https://github.com/wellcometrust/terraform.git//sns?ref=v1.0.0"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
   name   = "sierra_items_to_dynamo_updates"
 }
