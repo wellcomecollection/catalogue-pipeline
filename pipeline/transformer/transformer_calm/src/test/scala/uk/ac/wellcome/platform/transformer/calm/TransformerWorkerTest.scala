fae83e8fdb55fa7ad3041bc6d4a74f1f7627fee6
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-03-09 16:37:16 +0000

    add transformer tests to transformer worker

--- pipeline/transformer/transformer_calm/src/test/scala/uk/ac/wellcome/platform/transformer/calm/TransformerWorkerTest.scala
+++ pipeline/transformer/transformer_calm/src/test/scala/uk/ac/wellcome/platform/transformer/calm/TransformerWorkerTest.scala
@@ -27,27 +27,35 @@ import uk.ac.wellcome.storage.maxima.memory.MemoryMaxima
 import uk.ac.wellcome.storage.store.VersionedStore
 import uk.ac.wellcome.storage.store.memory.{MemoryStore, MemoryVersionedStore}
 
-case class TestDataIn(data: List[String])
+trait TestData
+case object ValidTestData extends TestData
+case object InvalidTestData extends TestData
 
-object TestTransformer extends Transformer[TestDataIn] {
-  def apply(data: TestDataIn, version: Int) =
-    Right(
-      UnidentifiedWork(
-        sourceIdentifier = SourceIdentifier(
-          value = "id",
-          identifierType = CalmIdentifierTypes.recordId,
-          ontologyType = "IdentifierType"),
-        version = version,
-        data = WorkData()
-      )
-    )
+object TestTransformer extends Transformer[TestData] {
+  def apply(data: TestData, version: Int) =
+    data match {
+      case ValidTestData =>
+        Right(
+          UnidentifiedWork(
+            sourceIdentifier = SourceIdentifier(
+              value = "id",
+              identifierType = CalmIdentifierTypes.recordId,
+              ontologyType = "IdentifierType"),
+            version = version,
+            data = WorkData()
+          )
+        )
+
+      case InvalidTestData => Left(new Exception("No No No"))
+    }
+
 }
 
 class TestTransformerWorker(
   val stream: SQSStream[NotificationMessage],
   val sender: MemoryBigMessageSender[TransformedBaseWork],
-  val store: VersionedStore[String, Int, TestDataIn]
-) extends TransformerWorker[TestDataIn, String] {
+  val store: VersionedStore[String, Int, TestData]
+) extends TransformerWorker[TestData, String] {
   val transformer = TestTransformer
 }
 
@@ -66,13 +74,13 @@ class TransformerWorkerTest
     with SNS
     with TestJsonCodecs {
 
-  it("If the stream fails, should eventually put the message on the DLQ") {
+  it("Should have an empty queue and DLQ if it can process all messages") {
 
     withTransformerWorker(
       records = Map(
-        Version("A", 1) -> TestDataIn(List("Hats", "Cats")),
-        Version("B", 2) -> TestDataIn(List("Things", "Thangs")),
-        Version("C", 3) -> TestDataIn(List("Pots", "Pans"))
+        Version("A", 1) -> ValidTestData,
+        Version("B", 2) -> ValidTestData,
+        Version("C", 3) -> ValidTestData
       )
     ) {
       case (transformerWorker, QueuePair(queue, dlq), metrics) =>
@@ -84,16 +92,55 @@ class TransformerWorkerTest
 
         assertQueueEmpty(dlq)
         assertQueueEmpty(queue)
-        metrics.incrementedCounts should be(
-          List(
-            "TestTransformerWorker_ProcessMessage_success",
-            "TestTransformerWorker_ProcessMessage_success",
-            "TestTransformerWorker_ProcessMessage_success"))
-
     }
   }
 
-  def withTransformerWorker[R](records: Map[Version[String, Int], TestDataIn])(
+  it(
+    "Send failed messages to the DLQ and send to metrics if it can't read from store") {
+
+    withTransformerWorker(
+      records = Map(
+        Version("A", 1) -> ValidTestData,
+        Version("B", 2) -> ValidTestData,
+        Version("C", 3) -> ValidTestData
+      )
+    ) {
+      case (transformerWorker, QueuePair(queue, dlq), metrics) =>
+        sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
+        sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
+        sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
+        sendNotificationToSQS[Version[String, Int]](queue, Version("D", 4))
+
+        Thread.sleep(2000)
+
+        assertQueueHasSize(dlq, 1)
+        assertQueueEmpty(queue)
+    }
+  }
+
+  it(
+    "Send failed messages to the DLQ if it receives an error from the transformer") {
+
+    withTransformerWorker(
+      records = Map(
+        Version("A", 1) -> ValidTestData,
+        Version("B", 2) -> ValidTestData,
+        Version("C", 3) -> InvalidTestData
+      )
+    ) {
+      case (transformerWorker, QueuePair(queue, dlq), metrics) =>
+        sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
+        sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
+        sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
+
+        Thread.sleep(2000)
+
+        assertQueueHasSize(dlq, 1)
+        assertQueueEmpty(queue)
+    }
+  }
+
+  def withTransformerWorker[R](records: Map[Version[String, Int], TestData])(
     testWith: TestWith[(TestTransformerWorker,
                         SQS.QueuePair,
                         MemoryMetrics[StandardUnit]),
@@ -104,10 +151,10 @@ class TransformerWorkerTest
           val metrics = new MemoryMetrics[StandardUnit]()
           withSQSStream[NotificationMessage, R](queue, metrics) { stream =>
             val sender = new MemoryBigMessageSender[TransformedBaseWork]()
-            val data: MemoryStore[Version[String, Int], TestDataIn]
+            val data: MemoryStore[Version[String, Int], TestData]
               with Maxima[String, Int] =
-              new MemoryStore(records) with MemoryMaxima[String, TestDataIn]
-            val store = new MemoryVersionedStore[String, TestDataIn](data)
+              new MemoryStore(records) with MemoryMaxima[String, TestData]
+            val store = new MemoryVersionedStore[String, TestData](data)
 
             val transformerWorker =
               new TestTransformerWorker(stream, sender, store)
