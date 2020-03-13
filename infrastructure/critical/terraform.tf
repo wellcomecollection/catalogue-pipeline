3caa27225f964c14a88629a12343fee3613fd57a
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-03-13 13:12:53 +0000

    make tf roles read_only

--- infrastructure/critical/terraform.tf
+++ infrastructure/critical/terraform.tf
@@ -13,7 +13,7 @@ data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
   config = {
-    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
     bucket   = "wellcomecollection-platform-infra"
     key      = "terraform/platform-infrastructure/shared.tfstate"
     region   = "eu-west-1"
