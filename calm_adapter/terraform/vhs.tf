398b5f998d306a924098d8da439b41c30cef5e32
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-03-13 09:49:17 +0000

    Revert "rename calm sourcedata VHS to be consistant"
    This reverts commit c84c89db7ec6b29bb106d47edc55476c8a57bbf8.

--- calm_adapter/terraform/vhs.tf
+++ calm_adapter/terraform/vhs.tf
@@ -2,7 +2,7 @@ module "vhs" {
   source             = "git::github.com/wellcomecollection/terraform-aws-vhs.git//hash-store?ref=f62e0544687a7361810a14ef78a5e198cfc5d365"
   bucket_name_prefix = "wellcomecollection-vhs-"
   table_name_prefix  = "vhs-"
-  name               = "sourcedata-calm"
+  name               = local.namespace
 }
 
 resource "aws_iam_role_policy" "vhs_readwrite" {
--- reindexer/start_reindex.py
+++ reindexer/start_reindex.py
@@ -17,7 +17,6 @@ SOURCES = {
     "sierra": "vhs-sourcedata-sierra",
     "sierra-items": "vhs-sourcedata-sierra-items",
     "mets": "mets-adapter-store",
-    "mets": "mets-adapter-store",
 }
 
 DESTINATIONS = ["catalogue", "reporting"]
