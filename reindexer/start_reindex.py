8c109c3eedd549cbdee3ca727b4586a8c8fd1f57
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-03-13 09:57:08 +0000

    add calm VHS name to reindexer

--- reindexer/start_reindex.py
+++ reindexer/start_reindex.py
@@ -17,6 +17,7 @@ SOURCES = {
     "sierra": "vhs-sourcedata-sierra",
     "sierra-items": "vhs-sourcedata-sierra-items",
     "mets": "mets-adapter-store",
+    "calm": "vhs-sourcedata-calm",
 }
 
 DESTINATIONS = ["catalogue", "reporting"]
