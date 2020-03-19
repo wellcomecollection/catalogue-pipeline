7eeaef8dc90f1277f81ef0971123661a224bb688
Nick Ward <ward.nickjames@gmail.com>
2020-03-19 15:49:21 +0000

    CollectionService

---
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/services/CollectionService.scala
@@ -0,0 +1,66 @@
+package uk.ac.wellcome.platform.api.services
+
+import scala.concurrent.{ExecutionContext, Future}
+import com.sksamuel.elastic4s.ElasticDsl._
+import com.sksamuel.elastic4s.{ElasticClient, ElasticError, Index}
+import com.sksamuel.elastic4s.requests.searches.{SearchHit, SearchResponse}
+
+import uk.ac.wellcome.models.work.internal._
+import uk.ac.wellcome.models.work.internal.result._
+import uk.ac.wellcome.models.work.internal.CollectionTree
+import uk.ac.wellcome.models.Implicits._
+import uk.ac.wellcome.json.JsonUtil.fromJson
+import uk.ac.wellcome.platform.api.Tracing
+
+/**
+  * Retrieves a CollectionTree given some paths to expand.
+  *
+  * For example, if asked to expand the path "A/B", it will return a tree
+  * consisting of the node A, the child node B, and any children of either of
+  * those nodes.
+  *
+  * A
+  * ├── B
+  * │   ├── ?
+  * │   ├── ?
+  * │   └── ?
+  * │
+  * ├── ?
+  * └── ?
+  *
+  * More specifically, it will return the requested node, any direct children it
+  * has, all ancestors it has, and any direct children of those ancestors.
+  *
+  * Note that this tree might not contain the complete tree data for some
+  * hierarchical collection that exists in the index: it is intended to allow
+  * incremental exploration of the data (akin to exploring a filesystem), due to
+  * the fact the whole tree may be rather large.
+  */
+class CollectionService(elasticClient: ElasticClient, index: Index)(
+  implicit ec: ExecutionContext)
+    extends Tracing {
+
+  def retrieveTree(
+    expandedPaths: List[String]): Future[Result[CollectionTree]] =
+    makeEsRequest(expandedPaths)
+      .map { result =>
+        result.left
+          .map(_.asException)
+          .flatMap { searchResponse =>
+            searchResponse.hits.hits.toList.map(toWork).toResult
+          }
+          .flatMap(CollectionTree(_))
+      }
+
+  private def makeEsRequest(
+    paths: List[String]): Future[Either[ElasticError, SearchResponse]] =
+    withActiveTrace(elasticClient.execute {
+      CollectionRequestBuilder(index, paths).request
+    }).map {
+      case resp if resp.isError => Left(resp.error)
+      case resp                 => Right(resp.result)
+    }
+
+  private def toWork(hit: SearchHit): Result[IdentifiedWork] =
+    fromJson[IdentifiedWork](hit.sourceAsString).toEither
+}
---
+++ api/api/src/test/scala/uk/ac/wellcome/platform/api/services/CollectionServiceTest.scala
@@ -0,0 +1,154 @@
+package uk.ac.wellcome.platform.api.services
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.concurrent.ScalaFutures
+import com.sksamuel.elastic4s.Index
+
+import uk.ac.wellcome.models.work.internal._
+import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
+import uk.ac.wellcome.models.work.generators.{
+  IdentifiersGenerators,
+  WorksGenerators
+}
+
+class CollectionServiceTest
+    extends FunSpec
+    with Matchers
+    with ScalaFutures
+    with ElasticsearchFixtures
+    with IdentifiersGenerators
+    with WorksGenerators {
+
+  def collectionService(index: Index) =
+    new CollectionService(elasticClient, index)
+
+  def work(path: String) =
+    createIdentifiedWorkWith(
+      collection = Some(Collection(path = path)),
+      title = Some(path),
+      sourceIdentifier = createSourceIdentifierWith(value = path)
+    )
+
+  def storeWorks(index: Index, works: List[IdentifiedWork] = works) =
+    insertIntoElasticsearch(index, works: _*)
+
+  val workA = work("a")
+  val workB = work("a/b")
+  val workC = work("a/b/c")
+  val workD = work("a/b/d")
+  val workE = work("a/e")
+  val workF = work("a/e/f")
+  val workG = work("a/e/f/g")
+  val workX = work("x")
+  val workY = work("x/y")
+  val workZ = work("x/oops/z")
+
+  val works =
+    List(workA, workB, workC, workD, workE, workF, workG, workX, workY, workZ)
+
+  it("Retrieves a tree with the given path and all ancestors expanded") {
+    withLocalWorksIndex { index =>
+      storeWorks(index)
+      val service = collectionService(index)
+      whenReady(service.retrieveTree(List("a/b"))) { result =>
+        result shouldBe Right(
+          CollectionTree(
+            path = "a",
+            work = workA,
+            children = List(
+              CollectionTree(
+                path = "a/b",
+                work = workB,
+                children = List(
+                  CollectionTree(path = "a/b/c", work = workC),
+                  CollectionTree(path = "a/b/d", work = workD),
+                )
+              ),
+              CollectionTree(path = "a/e", work = workE),
+            )
+          )
+        )
+      }
+    }
+  }
+
+  it("Retrieves a tree with multiple paths and their ancestors expanded") {
+    withLocalWorksIndex { index =>
+      storeWorks(index)
+      val service = collectionService(index)
+      whenReady(service.retrieveTree(List("a/b", "a/e/f"))) { result =>
+        result shouldBe Right(
+          CollectionTree(
+            path = "a",
+            work = workA,
+            children = List(
+              CollectionTree(
+                path = "a/b",
+                work = workB,
+                children = List(
+                  CollectionTree(path = "a/b/c", work = workC),
+                  CollectionTree(path = "a/b/d", work = workD),
+                )
+              ),
+              CollectionTree(
+                path = "a/e",
+                work = workE,
+                children = List(
+                  CollectionTree(
+                    path = "a/e/f",
+                    work = workF,
+                    children = List(
+                      CollectionTree(path = "a/e/f/g", work = workG)
+                    )
+                  ),
+                )
+              ),
+            )
+          )
+        )
+      }
+    }
+  }
+
+  it("Only expands by a single depth beyond the given path") {
+    withLocalWorksIndex { index =>
+      storeWorks(index)
+      val service = collectionService(index)
+      whenReady(service.retrieveTree(List("a"))) { result =>
+        result shouldBe Right(
+          CollectionTree(
+            path = "a",
+            work = workA,
+            children = List(
+              CollectionTree(path = "a/b", work = workB),
+              CollectionTree(path = "a/e", work = workE),
+            )
+          )
+        )
+      }
+    }
+  }
+
+  it("Fails creating a tree when incomplete data") {
+    withLocalWorksIndex { index =>
+      storeWorks(index)
+      val service = collectionService(index)
+      whenReady(service.retrieveTree(List("x/oops"))) { result =>
+        result shouldBe a[Left[_, _]]
+        result.left.get.getMessage shouldBe "Not all works in collection are connected to root 'x': x/oops/z"
+      }
+    }
+  }
+
+  it("Fails creating a tree when duplicate paths") {
+    withLocalWorksIndex { index =>
+      storeWorks(index, work("a/e/f/g") :: works)
+      val service = collectionService(index)
+      whenReady(service.retrieveTree(List("a/e/f"))) { result =>
+        result shouldBe a[Left[_, _]]
+        result.left.get.getMessage shouldBe "Tree contains duplicate paths: a/e/f/g"
+      }
+    }
+  }
+}
