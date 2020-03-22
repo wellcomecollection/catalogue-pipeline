bce2ab6c27685e100a506e4352ac3983f3c9f634
Alex Chan <alex@alexwlchan.net>
2020-03-22 21:33:10 +0000

    Use the terraform 0.12-optimised SNS topic module
    This fixes a deprecated type hint.

--- mets_adapter/terraform/topics.tf
+++ mets_adapter/terraform/topics.tf
@@ -1,4 +1,4 @@
 module "mets_adapter_topic" {
-  source                         = "git::https://github.com/wellcometrust/terraform.git//sns?ref=v19.13.2"
-  name                           = "mets_adapter_topic"
+  source = "github.com/wellcomecollection/terraform-aws-sns-topic.git?ref=v1.0.0"
+  name   = "mets_adapter_topic"
 }
