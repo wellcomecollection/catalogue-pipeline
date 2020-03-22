3441c54120d08a89ff6dd0fcbf75c0a1fda46fd6
Alex Chan <alex@alexwlchan.net>
2020-03-22 21:22:07 +0000

    Use the terraform 0.12-compatible ECS modules

--- mets_adapter/terraform/iam.tf
+++ mets_adapter/terraform/iam.tf
@@ -1,20 +1,20 @@
 resource "aws_iam_role_policy" "read_from_q" {
-  role = "${module.service.task_role_name}"
+  role   = module.task.task_role_name
   policy = "${module.queue.read_policy}"
 }
 
 resource "aws_iam_role_policy" "publish_to_topic" {
-  role = "${module.service.task_role_name}"
+  role   = module.task.task_role_name
   policy = "${module.mets_adapter_topic.publish_policy}"
 }
 
 resource "aws_iam_role_policy" "mets_adapter_dynamo_readwrite" {
-  role   = "${module.service.task_role_name}"
+  role   = module.task.task_role_name
   policy = "${local.mets_full_access_policy }"
 }
 
 resource "aws_iam_role_policy" "cloudwatch_push_metrics" {
-  role   = "${module.service.task_role_name}"
+  role   = module.task.task_role_name
   policy = "${data.aws_iam_policy_document.allow_cloudwatch_push_metrics.json}"
 }
 
