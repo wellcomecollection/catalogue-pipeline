4a6c7118d1992e8fa0b8eea516d496f4d73097fc
Alex Chan <alex@alexwlchan.net>
2020-03-22 21:21:01 +0000

    Bump to a version of the terraform provider that's 0.12-compatible

--- mets_adapter/terraform/provider.tf
+++ mets_adapter/terraform/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region  = "${local.aws_region}"
-  version = "1.60.0"
+  region  = local.aws_region
+  version = "~> 2.7"
 
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
