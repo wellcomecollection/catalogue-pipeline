77c2c9598ebd6e7639b662c184473a7f2cc07bad
Alex Chan <alex@alexwlchan.net>
2020-03-24 07:29:06 +0000

    Remove an unwanted set of IAM roles

--- api/terraform/update_api_docs/iam.tf
+++
@@ -1,4 +0,0 @@
-module "ecs_update_api_docs_iam" {
-  source    = "github.com/wellcomecollection/terraform-aws-ecs-service.git//task_definition/modules/iam_role?ref=v1.5.0"
-  task_name = "update_api_docs"
-}
