b0a90edfa2655cb85b13f0ffc2799542ec3b7658
Alex Chan <alex@alexwlchan.net>
2020-03-24 13:06:59 +0000

    Fix-ups in terraform.tf

--- api/terraform/shared/terraform.tf
+++ api/terraform/shared/terraform.tf
@@ -12,8 +12,8 @@ terraform {
 data "terraform_remote_state" "shared_infra" {
   backend = "s3"
 
-  config {
-    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+  config = {
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
 
     bucket = "wellcomecollection-platform-infra"
     key    = "terraform/platform-infrastructure/shared.tfstate"
