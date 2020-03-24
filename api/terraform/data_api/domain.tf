7bb85934e4ce48cfcd5aa13187ed07db9b1b1bf0
Alex Chan <alex@alexwlchan.net>
2020-03-24 11:52:42 +0000

    The Route53 Zone ID lookup has broken in tf 0.12

--- api/terraform/data_api/domain.tf
+++ api/terraform/data_api/domain.tf
@@ -1,7 +1,11 @@
-data "aws_route53_zone" "dotorg" {
+/*data "aws_route53_zone" "dotorg" {
   provider = "aws.routemaster"
 
   name = "wellcomecollection.org."
+}*/
+
+locals {
+  route53_zone_id = "Z3THRVQ5VDYDMC"
 }
 
 resource "aws_acm_certificate" "data_page" {
@@ -18,7 +22,7 @@ resource "aws_route53_record" "cert_validation" {
   provider = "aws.routemaster"
   name     = "${aws_acm_certificate.data_page.domain_validation_options.0.resource_record_name}"
   type     = "${aws_acm_certificate.data_page.domain_validation_options.0.resource_record_type}"
-  zone_id  = "${data.aws_route53_zone.dotorg.id}"
+  zone_id  = "${local.route53_zone_id}"
   records  = ["${aws_acm_certificate.data_page.domain_validation_options.0.resource_record_value}"]
   ttl      = 60
 }
@@ -31,7 +35,7 @@ resource "aws_acm_certificate_validation" "catalogue_api_validation" {
 
 resource "aws_route53_record" "data_page" {
   provider = "aws.routemaster"
-  zone_id  = "${data.aws_route53_zone.dotorg.id}"
+  zone_id  = "${local.route53_zone_id}"
   name     = "${var.data_page_url}"
   type     = "A"
 
