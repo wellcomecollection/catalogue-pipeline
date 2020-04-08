dd88cbe491760afea65c9680393531621e431c76
jamie <j.parkinson@wellcome.ac.uk>
2020-04-08 11:12:05 +0100

    Allow logger to fall back to stdout

--- pipeline/inferrer/feature_inferrer/app/src/logging.py
+++ pipeline/inferrer/feature_inferrer/app/src/logging.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 
 import logstash
 
@@ -7,8 +8,12 @@ import logstash
 def get_logstash_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    logstash_handler = logstash.LogstashHandler(
-        os.environ["LOGSTASH_HOST"], int(os.environ["LOGSTASH_PORT"]), version=1
-    )
-    logger.addHandler(logstash_handler)
+    try:
+        logstash_handler = logstash.LogstashHandler(
+            os.environ["LOGSTASH_HOST"], int(os.environ["LOGSTASH_PORT"]), version=1
+        )
+        logger.addHandler(logstash_handler)
+    except KeyError:
+        stream_handler = logging.StreamHandler(sys.stdout)
+        logger.addHandler(stream_handler)
     return logger
