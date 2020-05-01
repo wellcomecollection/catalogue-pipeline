6244fdea764e2a4adf94323fa677dbf3e8d7b7d2
Nick Ward <ward.nickjames@gmail.com>
2020-05-01 15:11:55 +0100

    Implement logging RFC for Calm adapter

--- calm_adapter/terraform/locals.tf
+++ calm_adapter/terraform/locals.tf
@@ -1,14 +1,13 @@
 locals {
-  namespace                     = "calm-adapter"
-  logstash_transit_service_name = "${local.namespace}_logstash_transit"
-  logstash_host                 = "${local.logstash_transit_service_name}.${local.namespace}"
+  namespace = "calm-adapter"
 
-  infra_bucket    = data.terraform_remote_state.shared_infra.outputs.infra_bucket
-  account_id      = data.aws_caller_identity.current.account_id
-  aws_region      = "eu-west-1"
-  dlq_alarm_arn   = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
-  vpc_id          = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_id
-  private_subnets = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_private_subnets
+  infra_bucket           = data.terraform_remote_state.shared_infra.outputs.infra_bucket
+  account_id             = data.aws_caller_identity.current.account_id
+  aws_region             = "eu-west-1"
+  dlq_alarm_arn          = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
+  vpc_id                 = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_id
+  private_subnets        = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_private_subnets
+  shared_secrets_logging = data.terraform_remote_state.shared_infra.outputs.shared_secrets_logging
 
   env_vars = {
     calm_api_url          = "https://wt-calm.wellcome.ac.uk/CalmAPI/ContentService.asmx"
--- calm_adapter/terraform/service.tf
+++ calm_adapter/terraform/service.tf
@@ -1,29 +1,17 @@
 module "service" {
-  source              = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//service?ref=v1.2.0"
-  service_name        = local.namespace
-  cluster_arn         = aws_ecs_cluster.cluster.arn
-  task_definition_arn = module.task_definition.arn
-  subnets             = local.private_subnets
-  namespace_id        = aws_service_discovery_private_dns_namespace.namespace.id
-  security_group_ids  = []
-  launch_type         = "FARGATE"
-  desired_task_count  = 1
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/service?ref=v2.4.1"
+
+  task_definition_arn            = module.task_definition.arn
+  service_name                   = local.namespace
+  cluster_arn                    = aws_ecs_cluster.cluster.arn
+  subnets                        = local.private_subnets
+  service_discovery_namespace_id = aws_service_discovery_private_dns_namespace.namespace.id
+  launch_type                    = "FARGATE"
+  desired_task_count             = 1
 }
 
-module "task_definition" {
-  source          = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//task_definition/single_container?ref=v1.2.0"
-  task_name       = local.namespace
-  container_image = local.calm_adapter_image
-  cpu             = "256"
-  memory          = "512"
-  env_vars        = local.env_vars
-  secret_env_vars = local.secret_env_vars
-  launch_type     = "FARGATE"
-  aws_region      = local.aws_region
-}
-
-module "scaling" {
-  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//autoscaling?ref=v1.2.0"
+module "autoscaling" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/autoscaling?ref=v2.4.1"
 
   name = local.namespace
 
@@ -32,4 +20,48 @@ module "scaling" {
 
   min_capacity = 1
   max_capacity = 2
+}
+
+module "task_definition" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/task_definition?ref=v2.4.1"
+
+  cpu    = 256
+  memory = 512
+
+  launch_types = ["FARGATE"]
+  task_name    = local.namespace
+
+  container_definitions = [
+    module.log_router_container.container_definition,
+    module.app_container.container_definition
+  ]
+}
+
+module "app_container" {
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/container_definition?ref=v2.4.1"
+
+  name  = local.namespace
+  image = local.calm_adapter_image
+
+  environment = local.env_vars
+  secrets     = local.secret_env_vars
+
+  log_configuration = module.log_router_container.container_log_configuration
+}
+
+module "app_permissions" {
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v2.4.1"
+  secrets   = local.secret_env_vars
+  role_name = module.task_definition.task_execution_role_name
+}
+
+module "log_router_container" {
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/firelens?ref=v2.4.1"
+  namespace = local.namespace
+}
+
+module "log_router_permissions" {
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v2.4.1"
+  secrets   = local.shared_secrets_logging
+  role_name = module.task_definition.task_execution_role_name
 }
