2f6f4bfb46478b4e5feebd87f8275d90625052d3
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-05-12 18:12:43 +0100

    remove SuppressedInvisibilityReason

--- common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/InvisibilityReason.scala
+++ common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/InvisibilityReason.scala
@@ -2,12 +2,10 @@ package uk.ac.wellcome.models.work.internal
 
 sealed trait InvisibilityReason
 
-sealed trait SuppressedInvisibilityReason extends InvisibilityReason
 object InvisibilityReason {
   case class DeletedFromSource(info: String) extends InvisibilityReason
   case class SuppressedFromSource(info: String) extends InvisibilityReason
-  case class CopyrightNotCleared(info: String)
-      extends SuppressedInvisibilityReason
+  case class CopyrightNotCleared(info: String) extends InvisibilityReason
   case class SourceFieldMissing(info: String) extends InvisibilityReason
   case class InvalidValueInSourceField(info: String) extends InvisibilityReason
 }
