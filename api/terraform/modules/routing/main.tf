360cec1374a61d1034798150f2bb42cb6a97b6f1
Nick Ward <ward.nickjames@gmail.com>
2020-05-19 11:51:55 +0100

    Pull out API gateway / route53 config to new routing module

--- api/terraform/modules/routing/main.tf
+++ api/terraform/modules/routing/main.tf
@@ -9,6 +9,10 @@ locals {
   # We can't look this up programatically because the role we use doesn't have
   # the right permissions in that account.
   route53_zone_id = "Z3THRVQ5VDYDMC"
+
+  variables = {
+    port = var.listener_port
+  }
 }
 
 resource "aws_api_gateway_domain_name" "catalogue_api" {
@@ -32,4 +36,35 @@ resource "aws_route53_record" "catalogue_api" {
     zone_id                = aws_api_gateway_domain_name.catalogue_api.regional_zone_id
     evaluate_target_health = false
   }
+}
+
+resource "aws_api_gateway_base_path_mapping" "catalogue_api" {
+  api_id      = var.api_id
+  stage_name  = var.environment
+  domain_name = aws_api_gateway_domain_name.catalogue_api.domain_name
+  base_path   = "catalogue"
+}
+
+resource "aws_api_gateway_deployment" "stage" {
+  rest_api_id = var.api_id
+
+  # If we specify the stage name here then API Gateway tries to create it even
+  # if it already exists (from below). Setting it to an empty string prevents this.
+  # See https://github.com/terraform-providers/terraform-provider-aws/issues/2918#issuecomment-356684239
+  stage_name = ""
+
+  variables = local.variables
+
+  stage_description = filemd5("${path.module}/../../shared/gateway.tf")
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = var.environment
+  rest_api_id   = var.api_id
+  deployment_id = aws_api_gateway_deployment.stage.id
+  variables     = local.variables
 }
---
+++ api/terraform/modules/routing/variables.tf
@@ -0,0 +1,9 @@
+variable "environment" {}
+
+variable "api_id" {}
+
+variable "listener_port" {}
+
+variable "domain_name" {}
+
+variable "certificate_arn" {}
