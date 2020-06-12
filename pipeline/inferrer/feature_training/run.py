40dba8fa82b27d14029a98436861d27b11e4c88e
jamie <j.parkinson@wellcome.ac.uk>
2020-06-12 16:55:03 +0100

    Add script for starting the training task

---
+++ pipeline/inferrer/feature_training/run.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+import boto3
+import click
+
+VPC_NAME = "catalogue-172-31-0-0-16"
+session = boto3.Session(profile_name="platform-developer")
+
+
+# Gets the first private subnet of the catalogue VPC
+def get_subnet():
+    ec2 = session.client("ec2")
+    vpcs = ec2.describe_vpcs(
+        Filters=[
+            {
+                "Name": "tag:Name",
+                "Values": [VPC_NAME]
+            }
+        ]
+    )
+    catalogue_vpc_id = vpcs["Vpcs"][0]["VpcId"]
+    subnets = ec2.describe_subnets(
+        Filters=[
+            {
+                "Name": "vpc-id",
+                "Values": [catalogue_vpc_id]
+            },
+            {
+                "Name": "tag:Availability",
+                "Values": ["private"]
+            }
+        ]
+    )
+    return subnets["Subnets"][0]["SubnetId"]
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--pipeline-name", help="The name of the pipeline (eg catalogue-19700101)", required=True
+)
+def train_new_model(pipeline_name):
+    print("Starting model training task...")
+    ecs_client = session.client("ecs")
+    res = ecs_client.run_task(
+        taskDefinition=f"{pipeline_name}_image_training",
+        cluster=pipeline_name,
+        count=1,
+        launchType="FARGATE",
+        networkConfiguration={
+            "awsvpcConfiguration": {
+                "subnets": [get_subnet()],
+                "assignPublicIp": "ENABLED"
+            }
+        }
+    )
+    print(f"Successfully started training task [{res['tasks'][0]['taskArn']}]")
+
+
+@cli.command()
+def deploy_model():
+    pass
+
+
+if __name__ == "__main__":
+    cli()
