c4e620b9cc35c59b783fed4aa121a48477cd4617
alicefuzier <alicefuzier@gmail.com>
2020-06-15 16:15:59 +0100

    Fix reindex and transformer permissions

--- reindexer/terraform/terraform.tf
+++ reindexer/terraform/terraform.tf
@@ -45,3 +45,14 @@ data "terraform_remote_state" "calm_adapter" {
     region   = "eu-west-1"
   }
 }
+
+data "terraform_remote_state" "sierra_adapter" {
+  backend = "s3"
+
+  config = {
+    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
+    bucket   = "wellcomecollection-platform-infra"
+    key      = "terraform/sierra_adapter.tfstate"
+    region   = "eu-west-1"
+  }
+}
