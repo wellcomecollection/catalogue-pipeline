8d2c261a444e89c7e31e1ee2f4e7ac5a8429990a
jamie <j.parkinson@wellcome.ac.uk>
2020-06-26 16:57:02 +0100

    Add tests for similarity search

--- api/api/src/test/scala/uk/ac/wellcome/platform/api/services/ImagesServiceTest.scala
+++ api/api/src/test/scala/uk/ac/wellcome/platform/api/services/ImagesServiceTest.scala
@@ -48,4 +48,39 @@ class ImagesServiceTest
         .map { _.left.value shouldBe a[ElasticError] }
     }
   }
+
+  describe("retrieveSimilarImages") {
+    it("gets visually similarImages") {
+      withLocalImagesIndex { index =>
+        val images = createVisuallySimilarImages(6)
+        insertImagesIntoElasticsearch(index, images: _*)
+
+        imagesService
+          .retrieveSimilarImages(index, images.head)
+          .map { results =>
+            results should not be empty
+            results should contain theSameElementsAs images.tail
+          }
+      }
+    }
+
+    it("returns Nil when no visually similar images can be found") {
+      withLocalImagesIndex { index =>
+        val image = createAugmentedImage()
+        insertImagesIntoElasticsearch(index, image)
+
+        imagesService.retrieveSimilarImages(index, image).map { results =>
+          results shouldBe empty
+        }
+      }
+    }
+
+    it("returns Nil when Elasticsearch returns an error") {
+      imagesService
+        .retrieveSimilarImages(Index("doesn't exist"), createAugmentedImage())
+        .map { results =>
+          results shouldBe empty
+        }
+    }
+  }
 }
