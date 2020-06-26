eda08fbf456b6b2087accc26980c7ebc7a3b9242
Nick Ward <ward.nickjames@gmail.com>
2020-06-26 15:13:43 +0100

    Allow reporting to fetch data from calm and sierra VHS

--- sierra_adapter/terraform/stack/vhs_sierra_sourcedata.tf
+++ sierra_adapter/terraform/stack/vhs_sierra_sourcedata.tf
@@ -1,6 +1,8 @@
 module "vhs_sierra" {
-  source             = "git::github.com/wellcomecollection/terraform-aws-vhs.git//hash-store?ref=v2.0.0"
+  source             = "git::github.com/wellcomecollection/terraform-aws-vhs.git//hash-store?ref=v3.4.3"
   bucket_name_prefix = "wellcomecollection-vhs-"
   table_name_prefix  = "vhs-"
   name               = "sierra-${local.namespace_hyphen}"
+  tags               = {}
+  read_principals    = ["arn:aws:iam::269807742353:root"]
 }
