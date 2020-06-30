042723f4cf723d2ca1eacb956acaef12579f0e79
alicefuzier <alicefuzier@gmail.com>
2020-06-30 13:38:56 +0100

    Put back ids on ImageSource

--- common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/ImageSource.scala
+++ common/internal_model/src/main/scala/uk/ac/wellcome/models/work/internal/ImageSource.scala
@@ -1,11 +1,17 @@
 package uk.ac.wellcome.models.work.internal
 
-sealed trait ImageSource[Id <: WithSourceIdentifier, DataId <: IdState]
+sealed trait ImageSource[Id <: WithSourceIdentifier, DataId <: IdState]{
+  val id : Id
+  val ontologyType: String
+}
 
 case class SourceWorks[Id <: WithSourceIdentifier, DataId <: IdState](
                                                   canonicalWork: SourceWork[Id, DataId]
                                                  , redirectedWork: Option[SourceWork[Id, DataId]]
-) extends ImageSource[Id, DataId]
+) extends ImageSource[Id, DataId]{
+  override val id = canonicalWork.id
+  override val ontologyType: String = canonicalWork.ontologyType
+}
 
 case class SourceWork[Id <: WithSourceIdentifier, DataId <: IdState](
                                                  id: Id,
