e07003d37fb9ca2999827851b69e9540e133095d
jamie <j.parkinson@wellcome.ac.uk>
2020-07-10 15:53:41 +0100

    Disable sierra progress reporter

--- sierra_adapter/terraform/stack/progress_reporter.tf
+++ sierra_adapter/terraform/stack/progress_reporter.tf
@@ -1,13 +1,16 @@
-module "progress_reporter" {
-  source = "./../sierra_progress_reporter"
+// The progress reporter currently times out (even at the 15min limit)
+// TODO: fix it
 
-  trigger_interval_minutes = 240
-  s3_adapter_bucket_name   = aws_s3_bucket.sierra_adapter.id
-  slack_access_token       = local.critical_slack_webhook
-
-  infra_bucket = var.infra_bucket
-
-  lambda_error_alarm_arn = var.lambda_error_alarm_arn
-
-  namespace = local.namespace_hyphen
-}
+//module "progress_reporter" {
+//  source = "./../sierra_progress_reporter"
+//
+//  trigger_interval_minutes = 240
+//  s3_adapter_bucket_name   = aws_s3_bucket.sierra_adapter.id
+//  slack_access_token       = local.critical_slack_webhook
+//
+//  infra_bucket = var.infra_bucket
+//
+//  lambda_error_alarm_arn = var.lambda_error_alarm_arn
+//
+//  namespace = local.namespace_hyphen
+//}
