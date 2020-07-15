1512ff651c252c0a42623262cee94afb046f827d
James Gorrie <j.gorrie@wellcome.ac.uk>
2020-07-15 07:56:16 +0100

    add title index mapping

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/WorksIndexConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/WorksIndexConfig.scala
@@ -10,10 +10,14 @@ import com.sksamuel.elastic4s.ElasticDsl.{
   textField,
   tokenCountField
 }
+import com.sksamuel.elastic4s.json.{XContentBuilder, XContentFactory}
 import com.sksamuel.elastic4s.requests.analysis.{
   Analysis,
   CustomAnalyzer,
-  PathHierarchyTokenizer
+  PathHierarchyTokenizer,
+  ShingleTokenFilter,
+  StemmerTokenFilter,
+  TokenFilter
 }
 import com.sksamuel.elastic4s.requests.mappings.{
   FieldDefinition,
@@ -22,17 +26,87 @@ import com.sksamuel.elastic4s.requests.mappings.{
 }
 import com.sksamuel.elastic4s.requests.mappings.dynamictemplate.DynamicMapping
 
+// TODO: Patch back to elastic4s
+case class AsciiFoldingTokenFilter(override val name: String,
+                                   preserveOriginal: Option[Boolean] = None)
+    extends TokenFilter {
+
+  override def build: XContentBuilder = {
+    val b = XContentFactory.jsonBuilder()
+    b.field("type", "asciifolding")
+    preserveOriginal.foreach(b.field("preserve_original", _))
+    b
+  }
+}
+
 case object WorksIndexConfig extends IndexConfig {
   // Analysis
   val pathTokenizer = PathHierarchyTokenizer("path_hierarchy_tokenizer")
+
   val pathAnalyzer =
     CustomAnalyzer("path_hierarchy_analyzer", pathTokenizer.name, Nil, Nil)
+
+  val asciiFoldingTokenFilter = AsciiFoldingTokenFilter(
+    "asciifolding_token_filter",
+    preserveOriginal = Some(true)
+  )
+
+  val shingleTokenFilter = ShingleTokenFilter(
+    "shingle_token_filter",
+    minShingleSize = Some(2),
+    maxShingleSize = Some(4))
+
+  val englishStemmerTokenFilter =
+    StemmerTokenFilter("english_token_filter", lang = "english")
+
+  val englishPossessiveStemmerTokenFilter =
+    StemmerTokenFilter(
+      "english_possessive_token_filter",
+      lang = "possessive_english")
+
+  val asciifoldingAnalyzer = CustomAnalyzer(
+    "shingle_asciifolding_analyzer",
+    tokenizer = "standard",
+    tokenFilters =
+      List("lowercase", shingleTokenFilter.name, asciiFoldingTokenFilter.name),
+    charFilters = Nil
+  )
+
+  val englishAnalyzer = CustomAnalyzer(
+    "english_analyzer",
+    tokenizer = "standard",
+    tokenFilters = List(
+      "lowercase",
+      asciiFoldingTokenFilter.name,
+      englishStemmerTokenFilter.name,
+      englishPossessiveStemmerTokenFilter.name),
+    charFilters = Nil
+  )
+
+  val shingleAsciifoldingAnalyzer = CustomAnalyzer(
+    "shingle_asciifolding_analyzer",
+    tokenizer = "standard",
+    tokenFilters =
+      List("lowercase", shingleTokenFilter.name, asciiFoldingTokenFilter.name),
+    charFilters = Nil
+  )
+
   val analysis = Analysis(
     analyzers = List(
-      pathAnalyzer
+      pathAnalyzer,
+      asciifoldingAnalyzer,
+      shingleAsciifoldingAnalyzer,
+      englishAnalyzer
     ),
-    tokenizers = List(pathTokenizer))
+    tokenFilters = List(
+      asciiFoldingTokenFilter,
+      shingleTokenFilter,
+      englishStemmerTokenFilter,
+      englishPossessiveStemmerTokenFilter),
+    tokenizers = List(pathTokenizer)
+  )
 
+  // Fields
   val sourceIdentifier = objectField("sourceIdentifier")
     .fields(sourceIdentifierFields)
 
@@ -44,6 +118,14 @@ case object WorksIndexConfig extends IndexConfig {
       label,
       keywordField("ontologyType"),
       keywordField("id")
+    )
+
+  val title = textField("title")
+    .analyzer(asciifoldingAnalyzer.name)
+    .fields(
+      keywordField("keyword"),
+      textField("english").analyzer(englishAnalyzer.name),
+      textField("shingles").analyzer(shingleAsciifoldingAnalyzer.name)
     )
 
   val notes = objectField("notes")
@@ -162,7 +244,7 @@ case object WorksIndexConfig extends IndexConfig {
       otherIdentifiers,
       mergeCandidates,
       workType,
-      englishTextField("title"),
+      title,
       englishTextField("alternativeTitles"),
       englishTextField("description"),
       englishTextField("physicalDescription"),
