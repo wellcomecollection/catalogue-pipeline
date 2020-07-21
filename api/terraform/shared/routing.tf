bbff75a6db941100a9aac1878dbd08ccecc42407
Robert Kenny <kenoir@gmail.com>
2020-07-21 16:03:25 +0100

    [ci skip] renew staging stack

--- api/terraform/shared/routing.tf
+++ api/terraform/shared/routing.tf
@@ -2,7 +2,7 @@ module "staging_routing" {
   source          = "../modules/routing"
   environment     = "staging"
   api_id          = aws_api_gateway_rest_api.catalogue_api.id
-  listener_port   = 1234
+  listener_port   = 1235
   domain_name     = "catalogue.api-stage.wellcomecollection.org"
   certificate_arn = aws_acm_certificate_validation.catalogue_api_validation.certificate_arn
   aws_region      = "eu-west-1"
