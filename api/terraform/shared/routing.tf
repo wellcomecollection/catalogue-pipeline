aa08ca82698cae303dc90035762cb9a9758ca955
Robert Kenny <kenoir@gmail.com>
2020-07-21 15:05:26 +0100

    [ci skip] move to new prod stack

--- api/terraform/shared/routing.tf
+++ api/terraform/shared/routing.tf
@@ -16,7 +16,7 @@ module "prod_routing" {
   source          = "../modules/routing"
   environment     = "prod"
   api_id          = aws_api_gateway_rest_api.catalogue_api.id
-  listener_port   = 8080
+  listener_port   = 8081
   domain_name     = "catalogue.api.wellcomecollection.org"
   certificate_arn = aws_acm_certificate_validation.catalogue_api_validation.certificate_arn
   aws_region      = "eu-west-1"
