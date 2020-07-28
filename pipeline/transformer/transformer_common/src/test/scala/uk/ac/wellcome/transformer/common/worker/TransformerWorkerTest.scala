718b6a1b1e63c8ed8ea0a73a906bdd9f279fac19
Alex Chan <alex@alexwlchan.net>
2020-07-28 09:39:07 +0100

    Simplify the transformer-common tests

--- pipeline/transformer/transformer_common/src/test/scala/uk/ac/wellcome/transformer/common/worker/TransformerWorkerTest.scala
+++ pipeline/transformer/transformer_common/src/test/scala/uk/ac/wellcome/transformer/common/worker/TransformerWorkerTest.scala
@@ -1,55 +1,33 @@
 package uk.ac.wellcome.transformer.common.worker
 
-import io.circe.{Decoder, Encoder}
-import io.circe.generic.semiauto._
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funspec.AnyFunSpec
-import uk.ac.wellcome.akka.fixtures.Akka
-import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
-import uk.ac.wellcome.messaging.fixtures.{SNS, SQS}
 import org.scalatest.matchers.should.Matchers
-import software.amazon.awssdk.services.cloudwatch.model.StandardUnit
+import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.bigmessaging.memory.MemoryBigMessageSender
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.json.JsonUtil._
+import uk.ac.wellcome.messaging.fixtures.SQS.{Queue, QueuePair}
+import uk.ac.wellcome.messaging.fixtures.{SNS, SQS}
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
 import uk.ac.wellcome.models.Implicits._
-import uk.ac.wellcome.models.work.internal.{
-  IdentifierType,
-  SourceIdentifier,
-  TransformedBaseWork,
-  UnidentifiedWork,
-  WorkData
-}
-import uk.ac.wellcome.monitoring.memory.MemoryMetrics
+import uk.ac.wellcome.models.work.generators.WorksGenerators
+import uk.ac.wellcome.models.work.internal.{TransformedBaseWork, UnidentifiedWork}
 import uk.ac.wellcome.storage.Version
-import uk.ac.wellcome.storage.maxima.Maxima
-import uk.ac.wellcome.storage.maxima.memory.MemoryMaxima
 import uk.ac.wellcome.storage.store.VersionedStore
-import uk.ac.wellcome.storage.store.memory.{MemoryStore, MemoryVersionedStore}
+import uk.ac.wellcome.storage.store.memory.MemoryVersionedStore
 
 trait TestData
 case object ValidTestData extends TestData
 case object InvalidTestData extends TestData
 
-object TestTransformer extends Transformer[TestData] {
-  def apply(data: TestData, version: Int) =
+object TestTransformer extends Transformer[TestData] with WorksGenerators {
+  def apply(data: TestData, version: Int): Either[Exception, UnidentifiedWork] =
     data match {
-      case ValidTestData =>
-        Right(
-          UnidentifiedWork(
-            sourceIdentifier = SourceIdentifier(
-              value = "id",
-              identifierType = IdentifierType("miro-image-number"),
-              ontologyType = "IdentifierType"),
-            version = version,
-            data = WorkData()
-          )
-        )
-
+      case ValidTestData   => Right(createUnidentifiedWork)
       case InvalidTestData => Left(new Exception("No No No"))
     }
-
 }
 
 class TestTransformerWorker(
@@ -57,13 +35,7 @@ class TestTransformerWorker(
   val sender: MemoryBigMessageSender[TransformedBaseWork],
   val store: VersionedStore[String, Int, TestData]
 ) extends TransformerWorker[TestData, String] {
-  val transformer = TestTransformer
-}
-
-trait TestJsonCodecs {
-  implicit val versionEnc: Encoder[Version[String, Int]] = deriveEncoder
-  implicit val notificationMessageEnc: Decoder[NotificationMessage] =
-    deriveDecoder
+  val transformer: Transformer[TestData] = TestTransformer
 }
 
 class TransformerWorkerTest
@@ -72,19 +44,17 @@ class TransformerWorkerTest
     with Matchers
     with Akka
     with SQS
-    with SNS
-    with TestJsonCodecs {
+    with SNS {
 
-  it("Should have an empty queue and DLQ if it can process all messages") {
+  it("empties the queue if it can process everything") {
+    val records = Map(
+      Version("A", 1) -> ValidTestData,
+      Version("B", 2) -> ValidTestData,
+      Version("C", 3) -> ValidTestData
+    )
 
-    withTransformerWorker(
-      records = Map(
-        Version("A", 1) -> ValidTestData,
-        Version("B", 2) -> ValidTestData,
-        Version("C", 3) -> ValidTestData
-      )
-    ) {
-      case (transformerWorker, QueuePair(queue, dlq), metrics) =>
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withWorker(queue, records = records) { _ =>
         sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
         sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
         sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
@@ -93,78 +63,59 @@ class TransformerWorkerTest
 
         assertQueueEmpty(dlq)
         assertQueueEmpty(queue)
+      }
     }
   }
 
-  it(
-    "Send failed messages to the DLQ and send to metrics if it can't read from store") {
-
-    withTransformerWorker(
-      records = Map(
-        Version("A", 1) -> ValidTestData,
-        Version("B", 2) -> ValidTestData,
-        Version("C", 3) -> ValidTestData
-      )
-    ) {
-      case (transformerWorker, QueuePair(queue, dlq), metrics) =>
+  it("sends failed messages to the DLQ if it can't read from store") {
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withWorker(queue, records = Map.empty) { _ =>
         sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
-        sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
-        sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
-        sendNotificationToSQS[Version[String, Int]](queue, Version("D", 4))
 
         Thread.sleep(2000)
 
-        assertQueueHasSize(dlq, 1)
+        assertQueueHasSize(dlq, size = 1)
         assertQueueEmpty(queue)
+      }
     }
   }
 
-  it(
-    "Send failed messages to the DLQ if it receives an error from the transformer") {
+  it("sends failed messages to the DLQ if the transformer errors") {
+    val records = Map(
+      Version("A", 1) -> ValidTestData,
+      Version("B", 2) -> ValidTestData,
+      Version("C", 3) -> InvalidTestData
+    )
 
-    withTransformerWorker(
-      records = Map(
-        Version("A", 1) -> ValidTestData,
-        Version("B", 2) -> ValidTestData,
-        Version("C", 3) -> InvalidTestData
-      )
-    ) {
-      case (transformerWorker, QueuePair(queue, dlq), metrics) =>
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withWorker(queue, records = records) { _ =>
         sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
         sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
         sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
 
         Thread.sleep(2000)
 
-        assertQueueHasSize(dlq, 1)
+        assertQueueHasSize(dlq, size = 1)
         assertQueueEmpty(queue)
+      }
     }
   }
 
-  def withTransformerWorker[R](records: Map[Version[String, Int], TestData])(
-    testWith: TestWith[(TestTransformerWorker,
-                        SQS.QueuePair,
-                        MemoryMetrics[StandardUnit]),
-                       R]) =
+  def withWorker[R](queue: Queue, records: Map[Version[String, Int], TestData])(
+    testWith: TestWith[Unit, R]
+  ): R =
     withActorSystem { implicit actorSystem =>
-      withLocalSqsQueuePair() {
-        case QueuePair(queue, dlq) =>
-          val metrics = new MemoryMetrics[StandardUnit]()
-          withSQSStream[NotificationMessage, R](queue, metrics) { stream =>
-            val sender = new MemoryBigMessageSender[TransformedBaseWork]()
-            val data
-              : MemoryStore[Version[String, Int], TestData] with Maxima[String,
-                                                                        Int] =
-              new MemoryStore(records) with MemoryMaxima[String, TestData]
-            val store = new MemoryVersionedStore[String, TestData](data)
+      withSQSStream[NotificationMessage, R](queue) { stream =>
+        val store = MemoryVersionedStore[String, TestData](records)
 
-            val transformerWorker =
-              new TestTransformerWorker(stream, sender, store)
+        val worker = new TestTransformerWorker(
+          stream = stream,
+          sender = new MemoryBigMessageSender[TransformedBaseWork](),
+          store = store
+        )
 
-            transformerWorker.run()
-            testWith((transformerWorker, QueuePair(queue, dlq), metrics))
-          }
-
+        worker.run()
+        testWith(())
       }
     }
 }
