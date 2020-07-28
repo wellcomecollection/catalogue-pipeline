9b64d11bbc9e4e879395ea67df41d42766faab8b
Alex Chan <alex@alexwlchan.net>
2020-07-28 12:39:06 +0100

    Use the new memory-only fixtures in the inference manager

--- pipeline/inferrer/inference_manager/src/test/scala/uk/ac/wellcome/platform/inference_manager/integration/ManagerInferrerIntegrationTest.scala
+++ pipeline/inferrer/inference_manager/src/test/scala/uk/ac/wellcome/platform/inference_manager/integration/ManagerInferrerIntegrationTest.scala
@@ -6,8 +6,8 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.{Inside, Inspectors, OptionValues}
 import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.messaging.fixtures.SNS.Topic
 import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
+import uk.ac.wellcome.messaging.memory.MemoryMessageSender
 import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.models.work.generators.ImageGenerators
 import uk.ac.wellcome.models.work.internal.{
@@ -38,7 +38,7 @@ class ManagerInferrerIntegrationTest
 
   it("augments images with feature vectors") {
     withWorkerServiceFixtures {
-      case (QueuePair(queue, dlq), topic) =>
+      case (QueuePair(queue, dlq), messageSender) =>
         // This is (more than) enough time for the inferrer to have
         // done its prestart work and be ready to use
         eventually(Timeout(scaled(90 seconds))) {
@@ -54,7 +54,9 @@ class ManagerInferrerIntegrationTest
         eventually {
           assertQueueEmpty(queue)
           assertQueueEmpty(dlq)
-          val augmentedImage = getMessages[AugmentedImage](topic).head
+
+          val augmentedImage = messageSender.getMessages[AugmentedImage].head
+
           inside(augmentedImage) {
             case AugmentedImage(id, _, _, _, inferredData) =>
               id should be(image.id)
@@ -74,25 +76,24 @@ class ManagerInferrerIntegrationTest
 
   def inferrerIsHealthy: Boolean = {
     val source =
-      Source.fromURL(s"http://localhost:${localInferrerPort}/healthcheck")
+      Source.fromURL(s"http://localhost:$localInferrerPort/healthcheck")
     try source.mkString.nonEmpty
     catch { case _: Exception => false } finally source.close()
   }
 
   def withWorkerServiceFixtures[R](
-    testWith: TestWith[(QueuePair, Topic), R]): R =
+    testWith: TestWith[(QueuePair, MemoryMessageSender), R]): R =
     // We would like a timeout longer than 1s here because the inferrer
     // may need to warm up.
     withLocalSqsQueuePair(visibilityTimeout = 5) { queuePair =>
-      withLocalSnsTopic { topic =>
-        withWorkerService(
-          queuePair.queue,
-          topic,
-          FeatureVectorInferrerAdapter,
-          localInferrerPort) { _ =>
-          testWith((queuePair, topic))
-        }
+      val messageSender = new MemoryMessageSender()
+
+      withWorkerService(
+        queuePair.queue,
+        messageSender,
+        FeatureVectorInferrerAdapter,
+        localInferrerPort) { _ =>
+        testWith((queuePair, messageSender))
       }
     }
-
 }
