dd0e544a9a2bcd831334f6bb72386f25fb843377
Alex Chan <alex@alexwlchan.net>
2020-07-29 12:22:25 +0100

    Update sierra-reader to use the new Location models

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/services/SierraReaderWorkerService.scala
@@ -27,8 +27,8 @@ import uk.ac.wellcome.sierra_adapter.model.{
   SierraBibRecord,
   SierraItemRecord
 }
-import uk.ac.wellcome.storage.{Identified, ObjectLocation}
-import uk.ac.wellcome.storage.s3.S3Config
+import uk.ac.wellcome.storage.Identified
+import uk.ac.wellcome.storage.s3.{S3Config, S3ObjectLocation}
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.typesafe.Runnable
 
@@ -68,7 +68,7 @@ class SierraReaderWorkerService(
 
   private def runSierraStream(
     window: String,
-    windowStatus: WindowStatus): Future[Identified[ObjectLocation, String]] = {
+    windowStatus: WindowStatus): Future[Identified[S3ObjectLocation, String]] = {
 
     info(s"Running the stream with window=$window and status=$windowStatus")
 
@@ -110,7 +110,7 @@ class SierraReaderWorkerService(
 
       Future.fromTry(
         S3TypedStore[String]
-          .put(ObjectLocation(s3Config.bucketName, key))("")
+          .put(S3ObjectLocation(bucket = s3Config.bucketName, key = key))("")
           .left
           .map { _.e }
           .toTry)
