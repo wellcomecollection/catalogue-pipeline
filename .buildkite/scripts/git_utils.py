06f61182852340a225f72cedd11e06c48c298574
Robert Kenny <kenoir@gmail.com>
2020-08-10 15:06:24 +0100

    reinstate

--- .buildkite/scripts/git_utils.py
+++ .buildkite/scripts/git_utils.py
@@ -56,4 +56,4 @@ def is_default_branch():
     current_branch_name = current_branch()
     default_branch_name = remote_default_branch()
 
-    return True  # current_branch_name == default_branch_name
+    return current_branch_name == default_branch_name
