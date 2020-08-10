99fc868ce0cffc9a3d377d95dcb1c5ece6ad38fe
Robert Kenny <kenoir@users.noreply.github.com>
2020-08-10 15:08:28 +0100

    Buildkite (#732)
    Remove garbage commits

---
+++ .buildkite/scripts/complete_build.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8
+
+import os
+
+import git_utils as git
+
+
+if __name__ == "__main__":
+    # Get git metadata
+    branch_name = git.current_branch()
+
+    if git.is_default_branch():
+        print(f"Successful completion on {branch_name}, tagging latest.")
+        git.git("tag", "latest", "--force")
+        git.git("push", "origin", "latest", "--force")
+        print("Done.")
+    else:
+        print(f"Successful completion on {branch_name}, nothing to do.")
---
+++ .buildkite/scripts/git_utils.py
@@ -0,0 +1,59 @@
+# -*- encoding: utf-8
+
+import os
+import subprocess
+import sys
+
+
+def git(*args):
+    """Run a Git command and return its output."""
+    cmd = ["git"] + list(args)
+    try:
+        return subprocess.check_output(cmd).decode("utf8").strip()
+    except subprocess.CalledProcessError as err:
+        sys.exit(err.returncode)
+
+
+def get_changed_paths(*args, globs=None):
+    """
+    Returns a set of changed paths in a given commit range.
+
+    :param args: Arguments to pass to ``git diff``.
+    :param globs: List of file globs to include in changed paths.
+    """
+    if globs:
+        args = list(args) + ["--", *globs]
+    diff_output = git("diff", "--name-only", *args)
+
+    return set([line.strip() for line in diff_output.splitlines()])
+
+
+def remote_default_branch():
+    """Inspect refs to discover default branch @ remote origin."""
+    return git("symbolic-ref", "refs/remotes/origin/HEAD").split("/")[-1]
+
+
+def remote_default_head():
+    """Inspect refs to discover default branch HEAD @ remote origin."""
+    return git("show-ref", f"refs/remotes/origin/{remote_default_branch()}", "-s")
+
+
+def local_current_head():
+    """Use rev-parse to discover hash for current commit AKA HEAD (from .git/HEAD)."""
+    return git("rev-parse", "HEAD")
+
+
+def get_sha1_for_tag(tag):
+    git("fetch")
+    return git("show-ref", "-s", tag)
+
+
+def current_branch():
+    return os.environ["BUILDKITE_BRANCH"]
+
+
+def is_default_branch():
+    current_branch_name = current_branch()
+    default_branch_name = remote_default_branch()
+
+    return current_branch_name == default_branch_name
