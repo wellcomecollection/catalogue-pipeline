78009431c6675af4c731fd8f2724077b8e8b1f6d
jamie <j.parkinson@wellcome.ac.uk>
2020-08-12 15:16:53 +0100

    Add ability to fetch local files to feature inferrer

--- pipeline/inferrer/feature_inferrer/app/src/image.py
+++ pipeline/inferrer/feature_inferrer/app/src/image.py
@@ -1,5 +1,6 @@
+from aiofile import AIOFile
 from io import BytesIO
-from urllib.parse import unquote_plus
+from urllib.parse import unquote_plus, urlparse
 from PIL import Image
 from piffle.iiif import IIIFImageClient, ParseError
 
@@ -7,20 +8,32 @@ from .feature_extraction import input_size as model_input_size
 from .http import fetch_url_bytes
 
 
+async def get_image_from_url(image_url):
+    image_url = unquote_plus(image_url)
+    parsed_url = urlparse(image_url)
+    if parsed_url.scheme == "file":
+        image_pointer = await get_local_image(parsed_url.path)
+    else:
+        image_pointer = await get_remote_image(image_url)
+    return Image.open(BytesIO(image_pointer))
+
+
+async def get_local_image(path):
+    async with AIOFile(path, "rb") as afp:
+        return await afp.read()
+
+
 def is_valid_image(response):
     image_formats = ["image/png", "image/jpeg", "image/jpg", "image/jp2"]
     return (response.status == 200) and (response.content_type in image_formats)
 
 
-async def get_image_from_url(image_url):
-    image_url = unquote_plus(image_url)
-    response = await fetch_url_bytes(image_url)
+async def get_remote_image(url):
+    response = await fetch_url_bytes(url)
     if is_valid_image(response["object"]):
-        image_bytes = BytesIO(response["bytes"])
-        image = Image.open(image_bytes)
-        return image
+        return response["bytes"]
     else:
-        raise ValueError(f"{image_url} is not a valid image URL")
+        raise ValueError(f"{url} is not a valid image URL")
 
 
 def get_image_url_from_iiif_url(iiif_url):
