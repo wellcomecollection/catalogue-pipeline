3974f19f71e1d2dd9eda537f3432d1c55fa3b37c
jamie <j.parkinson@wellcome.ac.uk>
2020-08-13 10:19:18 +0100

    Create 20200813 pipeline

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -62,15 +62,13 @@ module "catalogue_pipeline_20200811" {
   inferrer_model_data_bucket_name = aws_s3_bucket.inferrer_model_core_data.id
 }
 
-// DO NOT DEPLOY TO PROD
-// DO NOT REMOVE
-// This is a test pipeline for more aggressive merging of Miro/Sierra works
-module "catalogue_pipeline_20200727" {
+
+module "catalogue_pipeline_20200813" {
   source = "./stack"
 
-  namespace = "catalogue-20200727"
+  namespace = "catalogue-20200813"
 
-  release_label = "miro_merging_test"
+  release_label = "latest"
 
   account_id      = data.aws_caller_identity.current.account_id
   aws_region      = local.aws_region
@@ -86,29 +84,29 @@ module "catalogue_pipeline_20200727" {
   # reindexer topic names.
 
   sierra_adapter_topic_arns = [
-    //    local.sierra_reindexer_topic_arn,
+    local.sierra_reindexer_topic_arn,
     local.sierra_merged_bibs_topic_arn,
     local.sierra_merged_items_topic_arn,
   ]
 
   miro_adapter_topic_arns = [
-    //    local.miro_reindexer_topic_arn,
+    local.miro_reindexer_topic_arn,
     local.miro_updates_topic_arn,
   ]
 
   mets_adapter_topic_arns = [
-    //    local.mets_reindexer_topic_arn,
+    local.mets_reindexer_topic_arn,
     local.mets_adapter_topic_arn,
   ]
 
   calm_adapter_topic_arns = [
-    //    local.calm_reindexer_topic_arn,
+    local.calm_reindexer_topic_arn,
     local.calm_adapter_topic_arn,
   ]
 
   # Elasticsearch
-  es_works_index  = "works-20200727"
-  es_images_index = "images-20200727"
+  es_works_index  = "works-20200813"
+  es_images_index = "images-20200813"
 
   # RDS
   rds_ids_access_security_group_id = local.rds_access_security_group_id
