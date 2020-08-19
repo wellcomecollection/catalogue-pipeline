44d5a9cb07affbcd015fc7f9a01614d933dd9bdb
jamie <j.parkinson@wellcome.ac.uk>
2020-08-19 12:08:06 +0100

    Handle local files in palette inferrer

--- pipeline/inferrer/palette_inferrer/app/src/image.py
+++ pipeline/inferrer/palette_inferrer/app/src/image.py
@@ -1,9 +1,25 @@
+from aiofile import AIOFile
 from io import BytesIO
-from urllib.parse import unquote_plus
+from urllib.parse import unquote_plus, urlparse
 from PIL import Image
 from piffle.iiif import IIIFImageClient, ParseError
 
 from .http import fetch_url_bytes
+
+
+async def get_image_from_url(image_url):
+    image_url = unquote_plus(image_url)
+    parsed_url = urlparse(image_url)
+    if parsed_url.scheme == "file":
+        image_pointer = await get_local_image(parsed_url.path)
+    else:
+        image_pointer = await get_remote_image(image_url)
+    return Image.open(BytesIO(image_pointer))
+
+
+async def get_local_image(path):
+    async with AIOFile(path, "rb") as afp:
+        return await afp.read()
 
 
 def is_valid_image(response):
@@ -11,15 +27,12 @@ def is_valid_image(response):
     return (response.status == 200) and (response.content_type in image_formats)
 
 
-async def get_image_from_url(image_url):
-    image_url = unquote_plus(image_url)
-    response = await fetch_url_bytes(image_url)
+async def get_remote_image(url):
+    response = await fetch_url_bytes(url)
     if is_valid_image(response["object"]):
-        image_bytes = BytesIO(response["bytes"])
-        image = Image.open(image_bytes)
-        return image
+        return response["bytes"]
     else:
-        raise ValueError(f"{image_url} is not a valid image URL")
+        raise ValueError(f"{url} is not a valid image URL")
 
 
 def get_image_url_from_iiif_url(iiif_url):
@@ -28,14 +41,4 @@ def get_image_url_from_iiif_url(iiif_url):
     except ParseError:
         raise ValueError(f"{iiif_url} is not a valid iiif URL")
 
-    if "dlcs" in image.api_endpoint:
-        # DLCS provides a thumbnails service which only serves certain sizes of image.
-        # Requests for these don't touch the image server and so, as we're performing
-        # lots of requests, we use 400x400 thumbnails and resize them ourselves later on.
-        image.api_endpoint = image.api_endpoint.replace("iiif-img", "thumbs")
-        # `exact=True` equates to a `/!400,400/` image request
-        # https://iiif.io/api/image/1.0/#4-2-size
-        image_url = str(image.size(width=400, height=400, exact=True))
-
-    image_url = str(image.size(width=400, height=400, exact=False))
-    return image_url
+    return str(image.size(width=400, height=400, exact=False))
