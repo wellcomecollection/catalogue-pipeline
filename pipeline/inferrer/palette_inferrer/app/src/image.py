a9d5d1a7343424c38dd4c01d68f099486fcb1300
Harrison Pim <harrison.stroma@gmail.com>
2020-08-24 11:24:17 +0100

    Forgotten palette improvements (#809)
    * update encoder to extract better palettes
    
    * Apply auto-formatting rules
    
    * sacrifice whitespace to the linter gods
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- pipeline/inferrer/palette_inferrer/app/src/image.py
+++ pipeline/inferrer/palette_inferrer/app/src/image.py
@@ -1,9 +1,31 @@
+from aiofile import AIOFile
 from io import BytesIO
-from urllib.parse import unquote_plus
+from urllib.parse import unquote_plus, urlparse
 from PIL import Image
 from piffle.iiif import IIIFImageClient, ParseError
 
 from .http import fetch_url_bytes
+
+
+async def get_image_from_url(image_url, size=None):
+    image_url = unquote_plus(image_url)
+    parsed_url = urlparse(image_url)
+    if parsed_url.scheme == "file":
+        image_pointer = await get_local_image(parsed_url.path)
+    else:
+        image_pointer = await get_remote_image(image_url)
+    image = Image.open(BytesIO(image_pointer))
+    if size:
+        image = image.resize((size, size), resample=Image.BILINEAR)
+    return image
+
+
+async def get_local_image(path):
+    try:
+        async with AIOFile(path, "rb") as afp:
+            return await afp.read()
+    except FileNotFoundError:
+        return ValueError(f"{path} does not exist")
 
 
 def is_valid_image(response):
@@ -11,18 +33,15 @@ def is_valid_image(response):
     return (response.status == 200) and (response.content_type in image_formats)
 
 
-async def get_image_from_url(image_url):
-    image_url = unquote_plus(image_url)
-    response = await fetch_url_bytes(image_url)
+async def get_remote_image(url):
+    response = await fetch_url_bytes(url)
     if is_valid_image(response["object"]):
-        image_bytes = BytesIO(response["bytes"])
-        image = Image.open(image_bytes)
-        return image
+        return response["bytes"]
     else:
-        raise ValueError(f"{image_url} is not a valid image URL")
+        raise ValueError(f"{url} is not a valid image URL")
 
 
-def get_image_url_from_iiif_url(iiif_url):
+def get_image_url_from_iiif_url(iiif_url, input_size=224):
     try:
         image = IIIFImageClient.init_from_url(iiif_url)
     except ParseError:
@@ -35,7 +54,6 @@ def get_image_url_from_iiif_url(iiif_url):
         image.api_endpoint = image.api_endpoint.replace("iiif-img", "thumbs")
         # `exact=True` equates to a `/!400,400/` image request
         # https://iiif.io/api/image/1.0/#4-2-size
-        image_url = str(image.size(width=400, height=400, exact=True))
+        return str(image.size(width=400, height=400, exact=True))
 
-    image_url = str(image.size(width=400, height=400, exact=False))
-    return image_url
+    return str(image.size(width=input_size, height=input_size, exact=False))
