d583f88c0ffba4d693dcefd4bb8c16fbb08927ed
Robert Kenny <kenoir@users.noreply.github.com>
2020-08-25 17:09:46 +0100

    Remove Travis scripts (#816)
    * Remove Travis scripts
    
    * de-travis

--- shared_conftest.py
+++ shared_conftest.py
@@ -16,7 +16,6 @@ def pytest_runtest_setup(item):
     # Set a default region before we start running tests.
     #
     # Without this line, boto3 complains about not having a region defined
-    # (despite one being passed in the Travis env variables/local config).
     # TODO: Investigate this properly.
     boto3.setup_default_session(region_name="eu-west-1")
 
