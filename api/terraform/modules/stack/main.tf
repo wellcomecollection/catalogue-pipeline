bdd771b2d788d96d4d558b149cd9373aecb3f6f2
Robert Kenny <kenoir@users.noreply.github.com>
2020-09-07 15:35:12 +0100

    Add alarm for api 500s (#844)

--- api/terraform/modules/stack/main.tf
+++ api/terraform/modules/stack/main.tf
@@ -49,3 +49,21 @@ module "service" {
     apm_secret     = "catalogue/api/apm_secret"
   }
 }
+
+resource "aws_cloudwatch_metric_alarm" "server_error" {
+  alarm_name                = "catalogue-api-${var.environment}-500x"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = "5XXError"
+  namespace                 = "AWS/ApiGateway"
+  period                    = "60"
+  statistic                 = "Sum"
+  threshold                 = "10"
+  alarm_description         = "This metric monitors 500s from the Catalogue API (${var.environment})"
+  alarm_actions             = [aws_sns_topic.server_error_alarm.arn]
+  insufficient_data_actions = []
+}
+
+resource "aws_sns_topic" "server_error_alarm" {
+  name = "catalogue-api-${var.environment}-500x"
+}
