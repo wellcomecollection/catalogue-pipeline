5f4481ab659bccd309b863fcbec1fc5941281b00
Nick Ward <ward.nickjames@gmail.com>
2020-09-16 10:00:22 +0100

    Point to new index

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
@@ -12,7 +12,7 @@ object ElasticConfig {
   // i.e. The API and the snapshot generator.
   def apply(): ElasticConfig =
     ElasticConfig(
-      worksIndex = Index("works-20200908"),
-      imagesIndex = Index("images-20200908")
+      worksIndex = Index("works-20200915"),
+      imagesIndex = Index("images-20200915")
     )
 }
