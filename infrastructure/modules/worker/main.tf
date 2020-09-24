4e47c3e5654e76d8c71c10c61f18e72e410d0582
Robert Kenny <kenoir@users.noreply.github.com>
2020-09-24 14:07:37 +0100

    Use latest TF modules to add deployment label to services (#883)
    * Use latest TF modules to add deployment label to services
    
    * Apply auto-formatting rules
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- infrastructure/modules/worker/main.tf
+++ infrastructure/modules/worker/main.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 module "service" {
-  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/service?ref=v3.1.0"
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/service?ref=v3.2.0"
 
   task_definition_arn            = module.task_definition.arn
   service_name                   = var.name
@@ -20,14 +20,13 @@ module "service" {
 
   propagate_tags = "SERVICE"
 
-  tags = {
-    "deployment:service" = local.deployment_service_name
-    "deployment:env"     = var.deployment_service_env
-  }
+  deployment_service = local.deployment_service_name
+  deployment_env     = var.deployment_service_env
+  deployment_label   = "initial"
 }
 
 module "autoscaling" {
-  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/autoscaling?ref=v3.1.0"
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/autoscaling?ref=v3.2.0"
 
   name = var.name
 
@@ -39,7 +38,7 @@ module "autoscaling" {
 }
 
 module "task_definition" {
-  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/task_definition?ref=v3.1.0"
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/task_definition?ref=v3.2.0"
 
   cpu    = var.cpu
   memory = var.memory
@@ -54,7 +53,7 @@ module "task_definition" {
 }
 
 module "app_container" {
-  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/container_definition?ref=v3.1.0"
+  source = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/container_definition?ref=v3.2.0"
 
   name  = var.name
   image = var.image
@@ -66,18 +65,18 @@ module "app_container" {
 }
 
 module "app_permissions" {
-  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v3.1.0"
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v3.2.0"
   secrets   = var.secret_env_vars
   role_name = module.task_definition.task_execution_role_name
 }
 
 module "log_router_container" {
-  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/firelens?ref=v3.1.0"
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/firelens?ref=v3.2.0"
   namespace = var.name
 }
 
 module "log_router_permissions" {
-  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v3.1.0"
+  source    = "git::github.com/wellcomecollection/terraform-aws-ecs-service.git//modules/secrets?ref=v3.2.0"
   secrets   = data.terraform_remote_state.shared_infra.outputs.shared_secrets_logging
   role_name = module.task_definition.task_execution_role_name
 }
