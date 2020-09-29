8ca80fa779a238c0a3d5682cf8528da007b96c87
Alex Chan <alex@alexwlchan.net>
2020-09-29 11:11:26 +0100

    Use the new location of the VPC terraform state

--- api/terraform/shared/locals.tf
+++ api/terraform/shared/locals.tf
@@ -2,8 +2,8 @@ locals {
   namespace        = "catalogue_api"
   namespace_hyphen = replace(local.namespace, "_", "-")
 
-  vpc_id          = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_id
-  private_subnets = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_private_subnets
+  vpc_id          = local.catalogue_vpcs["catalogue_vpc_id"]
+  private_subnets = local.catalogue_vpcs["catalogue_vpc_private_subnets"]
 
   prod_domain_name    = "catalogue.api.wellcomecollection.org"
   staging_domain_name = "catalogue.api-stage.wellcomecollection.org"
--- mets_adapter/terraform/locals.tf
+++ mets_adapter/terraform/locals.tf
@@ -20,6 +20,6 @@ locals {
 
   infra_bucket    = data.terraform_remote_state.shared_infra.outputs.infra_bucket
   dlq_alarm_arn   = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
-  vpc_id          = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_id
-  private_subnets = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_delta_private_subnets
+  vpc_id          = local.catalogue_vpcs["catalogue_vpc_delta_id"]
+  private_subnets = local.catalogue_vpcs["catalogue_vpc_delta_private_subnets"]
 }
