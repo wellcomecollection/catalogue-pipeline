1ef359b536a046c26636b3c786b14628a35e0144
Alex Chan <alex@alexwlchan.net>
2020-09-29 10:52:27 +0100

    Tidy up some Terraform interpolation syntax

--- api/terraform/shared/locals.tf
+++ api/terraform/shared/locals.tf
@@ -1,6 +1,6 @@
 locals {
   namespace        = "catalogue_api"
-  namespace_hyphen = "${replace(local.namespace, "_", "-")}"
+  namespace_hyphen = replace(local.namespace, "_", "-")
 
   vpc_id          = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_id
   private_subnets = data.terraform_remote_state.shared_infra.outputs.catalogue_vpc_private_subnets
