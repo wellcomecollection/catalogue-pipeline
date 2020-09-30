727d85c557af30491443e81a0ba0fdfe7d3b6f21
Alex Chan <alex@alexwlchan.net>
2020-09-30 14:54:52 +0100

    Declare the booleans explicitly on WorksIncludes
    This makes it impossible for us to forget to add a field in includeAll() -
    the compiler will insist on every argument being defined.

--- common/display/src/main/scala/uk/ac/wellcome/display/models/WorksIncludes.scala
+++ common/display/src/main/scala/uk/ac/wellcome/display/models/WorksIncludes.scala
@@ -17,20 +17,71 @@ object WorkInclude {
   case object SucceededBy extends WorkInclude
 }
 
+case class BetterWorksIncludes(
+  identifiers: Boolean,
+  items: Boolean,
+  subjects: Boolean,
+  genres: Boolean,
+  contributors: Boolean,
+  production: Boolean,
+  notes: Boolean,
+  images: Boolean,
+  parts: Boolean,
+  partOf: Boolean,
+  precededBy: Boolean,
+  succeededBy: Boolean
+) {
+  def anyRelation: Boolean = parts || partOf || precededBy || succeededBy
+}
+
+case object BetterWorksIncludes {
+  def apply(includes: List[WorkInclude]): BetterWorksIncludes =
+    BetterWorksIncludes(
+      identifiers = includes.contains(WorkInclude.Identifiers),
+      items = includes.contains(WorkInclude.Items),
+      subjects = includes.contains(WorkInclude.Subjects),
+      genres = includes.contains(WorkInclude.Genres),
+      contributors = includes.contains(WorkInclude.Contributors),
+      production = includes.contains(WorkInclude.Production),
+      notes = includes.contains(WorkInclude.Notes),
+      images = includes.contains(WorkInclude.Images),
+      parts = includes.contains(WorkInclude.Parts),
+      partOf = includes.contains(WorkInclude.PartOf),
+      precededBy = includes.contains(WorkInclude.PrecededBy),
+      succeededBy = includes.contains(WorkInclude.SucceededBy),
+  )
+
+  def includeAll(): BetterWorksIncludes =
+    BetterWorksIncludes(
+      identifiers = true,
+      items = true,
+      subjects = true,
+      genres = true,
+      contributors = true,
+      production = true,
+      notes = true,
+      images = true,
+      parts = true,
+      partOf = true,
+      precededBy = true,
+      succeededBy = true
+    )
+}
+
 case class WorksIncludes(includes: List[WorkInclude]) {
-  def identifiers = includes.contains(WorkInclude.Identifiers)
-  def items = includes.contains(WorkInclude.Items)
-  def subjects = includes.contains(WorkInclude.Subjects)
-  def genres = includes.contains(WorkInclude.Genres)
-  def contributors = includes.contains(WorkInclude.Contributors)
-  def production = includes.contains(WorkInclude.Production)
-  def notes = includes.contains(WorkInclude.Notes)
-  def images = includes.contains(WorkInclude.Images)
-  def parts = includes.contains(WorkInclude.Parts)
-  def partOf = includes.contains(WorkInclude.PartOf)
-  def precededBy = includes.contains(WorkInclude.PrecededBy)
-  def succeededBy = includes.contains(WorkInclude.SucceededBy)
-  def anyRelation = parts || partOf || precededBy || succeededBy
+  def identifiers: Boolean = includes.contains(WorkInclude.Identifiers)
+  def items: Boolean = includes.contains(WorkInclude.Items)
+  def subjects: Boolean = includes.contains(WorkInclude.Subjects)
+  def genres: Boolean = includes.contains(WorkInclude.Genres)
+  def contributors: Boolean = includes.contains(WorkInclude.Contributors)
+  def production: Boolean = includes.contains(WorkInclude.Production)
+  def notes: Boolean = includes.contains(WorkInclude.Notes)
+  def images: Boolean = includes.contains(WorkInclude.Images)
+  def parts: Boolean = includes.contains(WorkInclude.Parts)
+  def partOf: Boolean = includes.contains(WorkInclude.PartOf)
+  def precededBy: Boolean = includes.contains(WorkInclude.PrecededBy)
+  def succeededBy: Boolean = includes.contains(WorkInclude.SucceededBy)
+  def anyRelation: Boolean = parts || partOf || precededBy || succeededBy
 }
 
 object WorksIncludes {
