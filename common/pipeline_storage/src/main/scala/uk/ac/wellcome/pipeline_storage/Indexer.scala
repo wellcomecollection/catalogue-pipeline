e9018c9a42360cb8c5a7d97a501698e837dcec42
jamie <j.parkinson@wellcome.ac.uk>
2020-10-02 16:33:02 +0100

    Update general comment for our document versioning vs work versioning rationale

--- common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/Indexer.scala
+++ common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/Indexer.scala
@@ -24,6 +24,40 @@ trait Indexable[T] {
   def version(document: T): Int
 }
 
+/**
+  * Versioning:
+  *
+  * When the merger makes the decision to merge some works, it modifies
+  * the content of the affected works. Despite the content of these works
+  * being modified, their version remains the same. However, the merger
+  * attaches the number of merged sources (`nMergedSources`) to the target
+  * work and may choose to redirect some of the source works.
+  *
+  * When we ingest those works, we need to make sure that the merger
+  * modified works are never overridden by unmerged works for the same
+  * version still running through the pipeline.
+  *
+  * We also need to make sure that, if a work is modified by a source in
+  * such a way that it shouldn't be merged (or redirected) anymore, the
+  * new unmerged version is ingested and never replaced by the previous
+  * merged version still running through the pipeline.
+  *
+  * We can do that by ingesting works into Elasticsearch with a document
+  * version derived from a combination of work version, number of merged
+  * sources and work type.
+  *
+  * More specifically, by multiplying the work version by 10, we make sure
+  * that a new version of a work always wins over previous versions
+  * (merged or unmerged, provided that there are fewer than 10 merged
+  * sources, which we think is a reasonable assumption).
+  *
+  * We make sure that a merger modified work always wins over other works
+  * with the same version, by adding the number of merged sources to
+  * work.version * 10.
+  *
+  * The exact same logic is applied to image versions, using both the
+  * merge state and the version numbers of the image's SourceWorks.
+  */
 object Indexable {
 
   implicit val imageIndexable: Indexable[AugmentedImage] =
@@ -45,29 +79,6 @@ object Indexable {
       def id(work: Work[Identified]) =
         work.state.canonicalId
 
-      /**
-        * When the merger makes the decision to merge some works, it modifies
-        * the content of the affected works. Despite the content of these works
-        * being modified, their version remains the same. Instead, the merger
-        * sets the merged flag to true for the target work and changes the type
-        * of the other works to redirected.
-        *
-        * When we ingest those works, we need to make sure that the merger
-        * modified works are never overridden by unmerged works for the same
-        * version still running through the pipeline.
-        * We also need to make sure that, if a work is modified by a source in
-        * such a way that it shouldn't be merged (or redirected) anymore, the
-        * new unmerged version is ingested and never replaced by the previous
-        * merged version still running through the pipeline.
-        *
-        * We can do that by ingesting works into Elasticsearch with a version
-        * derived by a combination of work version, merged flag and work type.
-        * More specifically, by multiplying the work version by 10, we make sure
-        * that a new version of a work always wins over previous versions
-        * (merged or unmerged). We make sure that a merger modified work always
-        * wins over other works with the same version, by adding one to
-        * work.version * 10.
-        */
       def version(work: Work[Identified]) =
         work match {
           case Work.Visible(version, _, state) =>
