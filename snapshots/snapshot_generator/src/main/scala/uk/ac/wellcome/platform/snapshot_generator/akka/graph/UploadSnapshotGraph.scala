194c0ea567ab89c19e2dd48a3f67bc7821c0ea5f
Robert Kenny <kenoir@gmail.com>
2020-10-05 14:19:03 +0100

    comment and break up graph for clarity

--- snapshots/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/akka/graph/UploadSnapshotGraph.scala
+++ snapshots/snapshot_generator/src/main/scala/uk/ac/wellcome/platform/snapshot_generator/akka/graph/UploadSnapshotGraph.scala
@@ -25,18 +25,48 @@ object UploadSnapshotGraph {
     index: Index,
     s3Settings: S3Settings,
     s3ObjectLocation: S3ObjectLocation)(implicit actorSystem: ActorSystem)
-    : RunnableGraph[(Future[Int], Future[MultipartUploadResult])] =
+    : RunnableGraph[(Future[Int], Future[MultipartUploadResult])] = {
+
+    // We start with a "source" of display works
+    val displayWorkSource = DisplayWorkSource(elasticClient, index)
+
+    // We want to route to both a counter, and to S3
+    val countingSink = CountingSink()
+    val s3Sink = S3Sink(s3Settings)(s3ObjectLocation)
+
     RunnableGraph.fromGraph(
-      GraphDSL.create(CountingSink(), S3Sink(s3Settings)(s3ObjectLocation))(
+      GraphDSL.create(countingSink, s3Sink)(
         (_, _)) { implicit builder => (countingSinkShape, s3SinkShape) =>
         import GraphDSL.Implicits._
 
         val broadcast = builder.add(Broadcast[DisplayWork](outputPorts = 2))
 
-        DisplayWorkSource(elasticClient, index) ~> broadcast.in
+
+        // The Graph DSL begins here.
+        // See: https://doc.akka.io/docs/akka/current/stream/stream-graphs.html
+
+        // "Broadcast" our display works to two "ports"
+        // See: https://doc.akka.io/docs/akka/current/stream/operators/Broadcast.html
+        displayWorkSource ~> broadcast.in
+
+        // First port sends messages to the countingSink to count the number of works
         broadcast.out(0) ~> countingSinkShape
-        broadcast
-          .out(1) ~> DisplayWorkToJsonStringFlow() ~> StringToGzipFlow() ~> s3SinkShape
+
+        // Second port
+        broadcast.out(1) ~>(
+            // convert display work to string
+            DisplayWorkToJsonStringFlow()
+          ) ~>(
+            // consume strings and gzip bytes
+            StringToGzipFlow()
+          ) ~>(
+            // send those bytes to S3
+            s3SinkShape
+          )
+
+        // "ClosedShape" indicates that this is the end of the graph and there is a single output.
+        // The results of the broadcast via both ports is returned as a tuple when this graph is run.
         ClosedShape
       })
+  }
 }
