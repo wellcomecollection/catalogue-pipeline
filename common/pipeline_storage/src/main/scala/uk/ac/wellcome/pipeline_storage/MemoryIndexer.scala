8a15f82ad27aa9bc9e3d310fe9dfb1f4fcd87234
Nick Ward <ward.nickjames@gmail.com>
2020-10-06 15:56:31 +0100

    MemoryIndexer

---
+++ common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/MemoryIndexer.scala
@@ -0,0 +1,13 @@
+package uk.ac.wellcome.pipeline_storage
+
+import scala.concurrent.Future
+import scala.collection.mutable.Map
+
+class MemoryIndexer[T: Indexable](val index: Map[String, T] = Map.empty)
+    extends Indexer[T] {
+
+  def index(documents: Seq[T]): Future[Either[Seq[T], Seq[T]]] = {
+    documents.map(doc => index.put(indexable.id(doc), doc))
+    Future.successful(Right(documents))
+  }
+}
