e634ff62f4621791c42a1f6737aad39586b70448
Alex Chan <alex@alexwlchan.net>
2020-10-07 07:44:00 +0100

    Remove an unused terraform remote_state block
    [skip ci]

--- api/terraform/data_api/terraform.tf
+++ api/terraform/data_api/terraform.tf
@@ -9,18 +9,6 @@ terraform {
   }
 }
 
-data "terraform_remote_state" "catalogue_api_shared" {
-  backend = "s3"
-
-  config = {
-    role_arn = "arn:aws:iam::756629837203:role/catalogue-read_only"
-
-    bucket = "wellcomecollection-catalogue-infra-delta"
-    key    = "terraform/catalogue/api/shared.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 data "terraform_remote_state" "accounts_catalogue" {
   backend = "s3"
 
