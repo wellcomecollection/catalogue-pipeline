7c7f2e3899b4ef8a8da1637a99beaee1ed248921
jamie <j.parkinson@wellcome.ac.uk>
2020-10-15 16:25:45 +0100

    [wip] bump last updated time on works to latest from all matcher sets

--- pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/models/MergerOutcome.scala
+++ pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/models/MergerOutcome.scala
@@ -1,5 +1,7 @@
 package uk.ac.wellcome.platform.merger.models
 
+import java.time.Instant
+
 import uk.ac.wellcome.models.work.internal._
 import WorkState.{Merged, Source}
 import WorkFsm._
@@ -9,7 +11,18 @@ import WorkFsm._
  * all merged, redirected, and remaining works, as well as all merged images
  */
 case class MergerOutcome(works: Seq[Work[Merged]],
-                         images: Seq[MergedImage[DataState.Unidentified]])
+                         images: Seq[MergedImage[DataState.Unidentified]]) {
+  def lastUpdated(lastUpdated: Instant): MergerOutcome = MergerOutcome(
+    works = works.map(touchWorkLastUpdated(lastUpdated)),
+    images = images.map(touchImageLastUpdated(lastUpdated)),
+  )
+
+  private def touchImageLastUpdated(lastUpdated: Instant)(
+    image: MergedImage[DataState.Unidentified])
+    : MergedImage[DataState.Unidentified] = ???
+  private def touchWorkLastUpdated(lastUpdated: Instant)(
+    work: Work[WorkState.Merged]): Work[WorkState.Merged] = ???
+}
 
 object MergerOutcome {
 
--- pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerService.scala
+++ pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/MergerWorkerService.scala
@@ -1,16 +1,19 @@
 package uk.ac.wellcome.platform.merger.services
 
+import java.time.Instant
+
 import akka.Done
 import io.circe.Encoder
 
 import scala.concurrent.{ExecutionContext, Future}
-import uk.ac.wellcome.models.matcher.{MatchedIdentifiers, MatcherResult}
+import uk.ac.wellcome.models.matcher.MatcherResult
 import uk.ac.wellcome.typesafe.Runnable
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.messaging.MessageSender
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
+import uk.ac.wellcome.models.work.internal.{Work, WorkState}
 
 class MergerWorkerService[WorkDestination, ImageDestination](
   sqsStream: SQSStream[NotificationMessage],
@@ -27,21 +30,29 @@ class MergerWorkerService[WorkDestination, ImageDestination](
   private def processMessage(message: NotificationMessage): Future[Unit] =
     for {
       matcherResult <- Future.fromTry(fromJson[MatcherResult](message.body))
-      _ <- Future.sequence(matcherResult.works.map { applyMerge })
+      workSets <- Future.sequence {
+        matcherResult.works.map { matchedIdentifiers =>
+          playbackService.fetchAllWorks(matchedIdentifiers.identifiers.toList)
+        }
+      }
+      lastUpdated = workSets.flatMap(_.flatten.map(_.state.modifiedTime)).max
+      _ <- Future.sequence {
+        workSets.map(applyMerge(_, lastUpdated))
+      }
     } yield ()
 
-  private def applyMerge(matchedIdentifiers: MatchedIdentifiers): Future[Unit] =
+  private def applyMerge(maybeWorks: Seq[Option[Work[WorkState.Source]]],
+                         lastUpdated: Instant): Future[Unit] = {
+    val merged = mergerManager
+      .applyMerge(maybeWorks = maybeWorks)
+      .lastUpdated(lastUpdated)
+    val worksFuture = sendMessages(workSender, merged.works)
+    val imagesFuture = sendMessages(imageSender, merged.images)
     for {
-      maybeWorks <- playbackService.fetchAllWorks(
-        matchedIdentifiers.identifiers.toList)
-      merged = mergerManager.applyMerge(maybeWorks = maybeWorks)
-      (worksFuture, imagesFuture) = (
-        sendMessages(workSender, merged.works),
-        sendMessages(imageSender, merged.images)
-      )
       _ <- worksFuture
       _ <- imagesFuture
     } yield ()
+  }
 
   private def sendMessages[Destination, T](
     sender: MessageSender[Destination],
