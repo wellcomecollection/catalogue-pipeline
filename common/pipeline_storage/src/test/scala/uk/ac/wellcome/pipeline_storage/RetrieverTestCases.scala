1c42e4f31aaab0db9cd5c64b02ebbcf657147b95
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-16 12:52:02 +0100

    Write some generic RetrieverTestCases

---
+++ common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/RetrieverTestCases.scala
@@ -0,0 +1,32 @@
+package uk.ac.wellcome.pipeline_storage
+
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.elasticsearch.model.CanonicalId
+import uk.ac.wellcome.fixtures.TestWith
+
+trait RetrieverTestCases[Context, T]
+  extends AnyFunSpec
+    with Matchers
+    with ScalaFutures {
+  def withContext[R](documents: Seq[T])(testWith: TestWith[Context, R]): R
+
+  def withRetriever[R](testWith: TestWith[Retriever[T], R])(implicit context: Context): R
+
+  def createT: T
+
+  implicit val id: CanonicalId[T]
+
+  it("retrieves a document") {
+    val t = createT
+
+    withContext(documents = Seq(t)) { implicit context =>
+      val future = withRetriever { _.apply(id.canonicalId(t)) }
+
+      whenReady(future) {
+        _ shouldBe t
+      }
+    }
+  }
+}
---
+++ common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/elastic/ElasticRetrieverTest.scala
@@ -0,0 +1,48 @@
+package uk.ac.wellcome.pipeline_storage.elastic
+
+import com.sksamuel.elastic4s.Index
+import uk.ac.wellcome.elasticsearch.NoStrictMapping
+import uk.ac.wellcome.elasticsearch.model.CanonicalId
+import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.models.work.generators.IdentifiersGenerators
+import uk.ac.wellcome.pipeline_storage.{ElasticRetriever, Retriever, RetrieverTestCases}
+import uk.ac.wellcome.pipeline_storage.fixtures.{ElasticIndexerFixtures, SampleDocument}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class ElasticRetrieverTest
+  extends RetrieverTestCases[Index, SampleDocument]
+    with ElasticsearchFixtures
+    with ElasticIndexerFixtures
+    with IdentifiersGenerators {
+
+  import SampleDocument._
+
+  override def withContext[R](documents: Seq[SampleDocument])(testWith: TestWith[Index, R]): R =
+    withLocalElasticsearchIndex(config = NoStrictMapping) { index =>
+      withElasticIndexer[SampleDocument, R](index) { indexer =>
+        whenReady(indexer.index(documents)) { _ =>
+          assertElasticsearchEventuallyHas(index, documents: _*)
+
+          testWith(index)
+        }
+      }
+    }
+
+  override def withRetriever[R](
+    testWith: TestWith[Retriever[SampleDocument], R])(implicit index: Index): R =
+    testWith(
+      new ElasticRetriever(elasticClient, index = index)
+    )
+
+  override def createT: SampleDocument =
+    SampleDocument(
+      version = 1,
+      canonicalId = createCanonicalId,
+      title = randomAlphanumeric()
+    )
+
+  override implicit val id: CanonicalId[SampleDocument] =
+    (doc: SampleDocument) => doc.canonicalId
+}
