d61f65ab78ceef48ef7b6d3c5d3637ae8168e076
alicefuzier <alicefuzier@gmail.com>
2020-10-16 15:23:45 +0100

    Get rid of concurrent stuff

--- scripts/add_created_date.py
+++ scripts/add_created_date.py
@@ -1,5 +1,3 @@
-import concurrent.futures
-import itertools
 import json
 import math
 
@@ -30,63 +28,38 @@ class StorageManifestScanner:
     storage_table_name = "vhs-storage-manifests-2020-07-24"
     adapter_table_name = "mets-adapter-store"
 
-    def __init__(self, total_segments, max_scans_in_parallel):
+    def __init__(self):
         self.s3 = aws_resource("s3", role_arn=self.storage_role)
         dynamodb_storage_role = aws_resource("dynamodb", self.storage_role)
-        dynamodb_adapter_role = aws_resource("dynamodb", self.adapter_role)
-        self.total_segments = total_segments
-        self.max_scans_in_parallel = max_scans_in_parallel
-        self.adapter_table = dynamodb_adapter_role.Table(self.adapter_table_name)
+        self.dynamodb_adapter_role = aws_resource("dynamodb", self.adapter_role).meta.client
         self.storage_table = dynamodb_storage_role.Table(self.storage_table_name)
 
+
+    @property
+    def paginator(self):
+        return self.dynamodb_adapter_role.get_paginator("scan")
+
+
     def scan(self):
-        tasks_to_do = [
-            {"Segment": segment, "TotalSegments": self.total_segments}
-            for segment in range(self.total_segments)
-        ]
-
-        scans_to_run = iter(tasks_to_do)
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = {
-                executor.submit(self.adapter_table.scan, **scan_params): scan_params
-                for scan_params in itertools.islice(scans_to_run, self.max_scans_in_parallel)
-            }
-            while futures:
-                # Wait for the first future to complete.
-                done, _ = concurrent.futures.wait(
-                    futures, return_when=concurrent.futures.FIRST_COMPLETED
-                )
-
-                for fut in done:
-                    for item in fut.result()["Items"]:
-                        id = item["id"]
-                        if 'createdDate' not in item['payload'].keys():
-                            stored = self.storage_table.query(
-                                                     KeyConditionExpression=Key('id').eq(f"digitised/{id}"),
-                                                     Limit = 1,
-                                                     ScanIndexForward = False)["Items"][0]
-                            try:
-                                bucket = stored["payload"]["bucket"]
-                            except KeyError as e:
-                                bucket = stored["payload"]["namespace"]
-                            try:
-                                key = stored["payload"]["key"]
-                            except KeyError:
-                                key = stored["payload"]["path"]
-
-                            bag_manifest = json.loads(self.s3.Object(bucket, key).get()["Body"].read().decode("utf-8"))
-                            yield id, bag_manifest["createdDate"]
-
-                    scan_params = futures.pop(fut)
-
+        for page in self.paginator.paginate(TableName=self.adapter_table_name):
+            for item in page["Items"]:
+                id = item["id"]
+                if 'createdDate' not in item['payload'].keys():
+                    stored = self.storage_table.query(
+                                             KeyConditionExpression=Key('id').eq(f"digitised/{id}"),
+                                             Limit = 1,
+                                             ScanIndexForward = False)["Items"][0]
                     try:
-                        scan_params["ExclusiveStartKey"] = fut.result()["LastEvaluatedKey"]
+                        bucket = stored["payload"]["bucket"]
                     except KeyError:
-                        break
-                    tasks_to_do.append(scan_params)
+                        bucket = stored["payload"]["namespace"]
+                    try:
+                        key = stored["payload"]["key"]
+                    except KeyError:
+                        key = stored["payload"]["path"]
 
-                for scan_params in itertools.islice(scans_to_run, len(done)):
-                    futures[executor.submit(self.adapter_table.scan, **scan_params)] = scan_params
+                    bag_manifest = json.loads(self.s3.Object(bucket, key).get()["Body"].read().decode("utf-8"))
+                    yield id, bag_manifest["createdDate"]
 
 
 def update(table, batch_writer, id, created_date):
@@ -99,7 +72,7 @@ def update(table, batch_writer, id, created_date):
 
 
 def main():
-    scanner = StorageManifestScanner(total_segments=400, max_scans_in_parallel=50)
+    scanner = StorageManifestScanner()
     dynamodb = aws_resource("dynamodb", platform_dev_role)
     table = dynamodb.Table(adapter_table_name)
     with table.batch_writer() as batch_writer:
