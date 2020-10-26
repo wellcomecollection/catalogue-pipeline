d8cfa210af2f11e2f988bf7fa6b4af455d5ae09b
Alex Chan <alex@alexwlchan.net>
2020-10-26 14:43:15 +0000

    Test on SQS DLQ redrives rather than queue attributes

--- pipeline/ingestor/ingestor_common/src/test/scala/uk/ac/wellcome/platform/ingestor/common/services/IngestorWorkerServiceTest.scala
+++ pipeline/ingestor/ingestor_common/src/test/scala/uk/ac/wellcome/platform/ingestor/common/services/IngestorWorkerServiceTest.scala
@@ -5,7 +5,6 @@ import com.sksamuel.elastic4s.http.JavaClient
 import org.apache.http.HttpHost
 import org.elasticsearch.client.RestClient
 import org.scalatest.funspec.AnyFunSpec
-import software.amazon.awssdk.services.sqs.model.QueueAttributeName
 import uk.ac.wellcome.elasticsearch.ElasticCredentials
 import uk.ac.wellcome.fixtures.RandomGenerators
 import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
@@ -71,28 +70,17 @@ class IngestorWorkerServiceTest
     }
   }
 
-  it("does not delete a message from the queue if it fails processing") {
-    withLocalSqsQueue() { queue =>
-      val index = createIndex
+  it("leaves a message on the queue if it fails processing") {
+    val index = createIndex
+
+    withLocalSqsQueuePair(visibilityTimeout = 1) { case QueuePair(queue, dlq) =>
       withElasticIndexer[SampleDocument, Any](index) { indexer =>
         withWorkerService[SampleDocument, Any](queue, index, indexer) { _ =>
-          sendNotificationToSQS(
-            queue = queue,
-            body = "not a json string -- this will fail parsing"
-          )
-
-          // After a message is read, it stays invisible for 1 second and then it gets sent again.		               assertQueueHasSize(queue, size = 1)
-          // So we wait for longer than the visibility timeout and then we assert that it has become
-          // invisible again, which means that the ingestor picked it up again,
-          // and so it wasn't deleted as part of the first run.
-          // TODO Write this test using dead letter queues once https://github.com/adamw/elasticmq/issues/69 is closed
-          Thread.sleep(2000)
+          sendInvalidJSONto(queue)
 
           eventually {
-            getQueueAttribute(
-              queue,
-              attributeName =
-                QueueAttributeName.APPROXIMATE_NUMBER_OF_MESSAGES_NOT_VISIBLE) shouldBe "1"
+            assertQueueEmpty(queue)
+            assertQueueHasSize(dlq, size = 1)
           }
         }
       }
