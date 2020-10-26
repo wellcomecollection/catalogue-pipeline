5db69e06b0d8896fb382c35fb8eea28827e53ffb
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2020-10-26 14:49:15 +0000

    Apply auto-formatting rules

--- pipeline/terraform/stack/service_work_relation_embedder.tf
+++ pipeline/terraform/stack/service_work_relation_embedder.tf
@@ -1,10 +1,10 @@
 module "relation_embedder_queue" {
-  source = "git::github.com/wellcomecollection/terraform-aws-sqs//queue?ref=v1.1.2"
+  source                     = "git::github.com/wellcomecollection/terraform-aws-sqs//queue?ref=v1.1.2"
   queue_name                 = "${local.namespace_hyphen}_relation_embedder"
   topic_arns                 = [module.merger_works_topic.arn]
   visibility_timeout_seconds = 1200
-  aws_region      = var.aws_region
-  alarm_topic_arn = var.dlq_alarm_arn
+  aws_region                 = var.aws_region
+  alarm_topic_arn            = var.dlq_alarm_arn
 }
 
 module "relation_embedder" {
