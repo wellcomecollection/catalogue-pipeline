008efafdd50bc9cc7fc34bc92de9de84dd48cd7b
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-30 07:43:45 +0000

    Sierra transformer: use functions to break up SierraIdentifiers

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraIdentifiers.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraIdentifiers.scala
@@ -9,56 +9,55 @@ import uk.ac.wellcome.sierra_adapter.model.SierraBibNumber
 
 // Populate wwork:identifiers.
 //
-//    We populate with three identifier schemes:
+//    We populate with the following identifiers:
 //
-//    -   "sierra-system-number" for the full record type (incl. prefix and
-//        check digit)
 //    -   "sierra-identifier" for the 7-digit internal ID
+//
 //    -   "isbn" from MARC tag 020 subfield $a.  This is repeatable.
 //        https://www.loc.gov/marc/bibliographic/bd020.html
 //
 //    -   "issn" from MARC tag 022 ǂa.  This is repeatable.
 //        https://www.loc.gov/marc/bibliographic/bd022.html
 //
-//    Adding other identifiers is out-of-scope for now.
-//
 object SierraIdentifiers extends SierraDataTransformer with SierraQueryOps {
 
   type Output = List[SourceIdentifier]
 
-  def apply(bibId: SierraBibNumber, bibData: SierraBibData) = {
+  def apply(bibId: SierraBibNumber, bibData: SierraBibData): List[SourceIdentifier] = {
     val sierraIdentifier = SourceIdentifier(
       identifierType = IdentifierType("sierra-identifier"),
       ontologyType = "Work",
       value = bibId.withoutCheckDigit
     )
 
-    val isbnIdentifiers: List[SourceIdentifier] =
-      bibData
-        .subfieldsWithTag("020" -> "a")
-        .contents
-        .distinct
-        .map { value =>
-          SourceIdentifier(
-            identifierType = IdentifierType("isbn"),
-            ontologyType = "Work",
-            value = value
-          )
-        }
-
-    val issnIdentifiers: List[SourceIdentifier] =
-      bibData
-        .subfieldsWithTag("022" -> "a")
-        .contents
-        .distinct
-        .map { value =>
-          SourceIdentifier(
-            identifierType = IdentifierType("issn"),
-            ontologyType = "Work",
-            value = value
-          )
-        }
-
-    List(sierraIdentifier) ++ isbnIdentifiers ++ issnIdentifiers
+    List(sierraIdentifier) ++ getIsbnIdentifiers(bibData) ++ getIssnIdentifiers(bibData)
   }
+
+  // Find ISBN (International Serial Book Number) identifiers from MARC 020 ǂa.
+  private def getIsbnIdentifiers(bibData: SierraBibData): List[SourceIdentifier] =
+    bibData
+      .subfieldsWithTag("020" -> "a")
+      .contents
+      .distinct
+      .map { value =>
+        SourceIdentifier(
+          identifierType = IdentifierType("isbn"),
+          ontologyType = "Work",
+          value = value
+        )
+      }
+
+  // Find ISSN (International Standard Serial Number) identifiers from MARC 022 ǂa.
+  private def getIssnIdentifiers(bibData: SierraBibData): List[SourceIdentifier] =
+    bibData
+      .subfieldsWithTag("022" -> "a")
+      .contents
+      .distinct
+      .map { value =>
+        SourceIdentifier(
+          identifierType = IdentifierType("issn"),
+          ontologyType = "Work",
+          value = value
+        )
+      }
 }
