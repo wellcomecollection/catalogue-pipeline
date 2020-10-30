4f0a56e866fe0e8a9d9d2bc6154a91d65c3872ec
Alex Chan <a.chan@wellcome.ac.uk>
2020-10-30 13:04:30 +0000

    Request works from Elasticsearch 25 at a time

--- pipeline/relation_embedder/src/main/scala/uk/ac/wellcome/relation_embedder/RelationEmbedderWorkerService.scala
+++ pipeline/relation_embedder/src/main/scala/uk/ac/wellcome/relation_embedder/RelationEmbedderWorkerService.scala
@@ -3,10 +3,9 @@ package uk.ac.wellcome.relation_embedder
 import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 import scala.util.{Failure, Success}
-import akka.Done
+import akka.{Done, NotUsed}
 import akka.stream.scaladsl._
 import akka.stream.Materializer
-
 import uk.ac.wellcome.typesafe.Runnable
 import uk.ac.wellcome.messaging.MessageSender
 import uk.ac.wellcome.messaging.sns.NotificationMessage
@@ -33,29 +32,42 @@ class RelationEmbedderWorkerService[MsgDestination](
       sqsStream.foreach(this.getClass.getSimpleName, processMessage)
     }
 
-  def processMessage(message: NotificationMessage): Future[Unit] =
-    Source
-      .future(workRetriever.lookupSingleId(message.body))
-      .mapAsync(1) { work =>
-        relationsService
-          .getOtherAffectedWorks(work)
-          .map(work.sourceIdentifier :: _)
-      }
-      .mapConcat(identity)
-      .mapAsync(2) { sourceIdentifier =>
-        workRetriever.lookupSingleId(sourceIdentifier.toString)
-      }
-      .mapAsync(2) { work =>
-        relationsService.getRelations(work).map(work.transition[Denormalised])
-      }
+  def processMessage(message: NotificationMessage): Future[Unit] = {
+    val affectedWorkIds: Source[SourceIdentifier, NotUsed] =
+      Source
+        .future(workRetriever.lookupSingleId(message.body))
+        .mapAsync(1) { work =>
+          relationsService
+            .getOtherAffectedWorks(work)
+            .map(work.sourceIdentifier :: _)
+        }
+        .mapConcat(identity)
+
+    val affectedWorks =
+      affectedWorkIds
+        .grouped(25)
+        .mapAsync(1) { identifiers =>
+          workRetriever
+            .lookupMultipleIds(identifiers.map { _.toString })
+            .map { _.values.toList }
+        }
+        .mapConcat(identity)
+
+    val denormalisedWorks =
+      affectedWorks
+        .mapAsync(2) { work =>
+          relationsService.getRelations(work).map(work.transition[Denormalised])
+        }
+
+    denormalisedWorks
       .groupedWithin(batchSize, flushInterval)
-      .mapAsync(2) { work =>
-        workIndexer.index(work).flatMap {
+      .mapAsync(2) { works =>
+        workIndexer.index(works).flatMap {
           case Left(failedWorks) =>
             Future.failed(
               new Exception(s"Failed indexing works: $failedWorks")
             )
-          case Right(works) => Future.successful(works.toList)
+          case Right(_) => Future.successful(works.toList)
         }
       }
       .mapConcat(identity)
@@ -67,4 +79,5 @@ class RelationEmbedderWorkerService[MsgDestination](
       }
       .runWith(Sink.ignore)
       .map(_ => ())
+  }
 }
