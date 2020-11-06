b042516e330640a9743fecbaa4a63ccb598ef67a
alicefuzier <alicefuzier@gmail.com>
2020-11-06 10:20:55 +0000

    Remove multiGetfrom ElsaticRetriever

--- common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverException.scala
+++ common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverException.scala
@@ -2,14 +2,5 @@ package uk.ac.wellcome.pipeline_storage
 
 class RetrieverException(message: String) extends RuntimeException(message)
 
-class RetrieverNotFoundException(message: String)
-    extends RetrieverException(message)
-
-object RetrieverNotFoundException {
-  def id(id: String): RetrieverNotFoundException =
-    new RetrieverNotFoundException(s"Nothing found with ID $id!")
-
-  def ids(ids: Seq[String]): RetrieverNotFoundException =
-    new RetrieverNotFoundException(
-      s"Nothing found with ID(s) ${ids.mkString(", ")}!")
-}
+class RetrieverNotFoundException(id: String)
+  extends RetrieverException(s"Nothing found with ID $id!")
