f680d1c5bd4bf8004f5edb3d35d7fc66cd63b643
Alex Chan <a.chan@wellcome.org>
2020-11-09 12:39:11 +0000

    Use a Map to store the Notes data, not a List
    This has a couple of effects:
    
    *   Creating Notes should now be constant time, rather than O(N) in the
        number of different NotesField mappings we define.  We can add new
        notes with impunity!
    
        This is because the Map lets us loop through all the VarFields once,
        rather than looping through for every Notes field.
    
    *   The notes will now be presented in the same order as the original
        MARC, rather than an implementation-specific order.

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraNotes.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraNotes.scala
@@ -3,7 +3,8 @@ package uk.ac.wellcome.platform.transformer.sierra.transformers
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.platform.transformer.sierra.source.{
   SierraBibData,
-  SierraQueryOps
+  SierraQueryOps,
+  VarField
 }
 
 case class NotesField(createNote: String => Note,
@@ -13,46 +14,50 @@ object SierraNotes extends SierraDataTransformer with SierraQueryOps {
 
   type Output = List[Note]
 
-  val notesFields =
-    List(
-      "500" -> NotesField(GeneralNote),
-      "501" -> NotesField(GeneralNote),
-      "502" -> NotesField(DissertationNote),
-      "504" -> NotesField(BibliographicalInformation),
-      "505" -> NotesField(ContentsNote),
-      "508" -> NotesField(CreditsNote),
-      "510" -> NotesField(PublicationsNote),
-      "511" -> NotesField(CreditsNote),
-      "518" -> NotesField(TimeAndPlaceNote),
-      "524" -> NotesField(CiteAsNote),
-      "533" -> NotesField(ReproductionNote),
-      "534" -> NotesField(ReproductionNote),
-      "535" -> NotesField(LocationOfOriginalNote),
-      "536" -> NotesField(FundingInformation),
-      "540" -> NotesField(TermsOfUse),
-      "542" -> NotesField(CopyrightNote),
-      "545" -> NotesField(BiographicalNote),
-      "547" -> NotesField(GeneralNote),
-      "562" -> NotesField(GeneralNote),
-      "563" -> NotesField(BindingInformation, suppressedSubfields = Set("5")),
-      "581" -> NotesField(PublicationsNote),
-      "585" -> NotesField(ExhibitionsNote),
-      "586" -> NotesField(AwardsNote),
-      "593" -> NotesField(CopyrightNote),
-    )
+  val notesFields = Map(
+    "500" -> NotesField(GeneralNote),
+    "501" -> NotesField(GeneralNote),
+    "502" -> NotesField(DissertationNote),
+    "504" -> NotesField(BibliographicalInformation),
+    "505" -> NotesField(ContentsNote),
+    "508" -> NotesField(CreditsNote),
+    "510" -> NotesField(PublicationsNote),
+    "511" -> NotesField(CreditsNote),
+    "518" -> NotesField(TimeAndPlaceNote),
+    "524" -> NotesField(CiteAsNote),
+    "533" -> NotesField(ReproductionNote),
+    "534" -> NotesField(ReproductionNote),
+    "535" -> NotesField(LocationOfOriginalNote),
+    "536" -> NotesField(FundingInformation),
+    "540" -> NotesField(TermsOfUse),
+    "542" -> NotesField(CopyrightNote),
+    "545" -> NotesField(BiographicalNote),
+    "547" -> NotesField(GeneralNote),
+    "562" -> NotesField(GeneralNote),
+    "563" -> NotesField(BindingInformation, suppressedSubfields = Set("5")),
+    "581" -> NotesField(PublicationsNote),
+    "585" -> NotesField(ExhibitionsNote),
+    "586" -> NotesField(AwardsNote),
+    "593" -> NotesField(CopyrightNote),
+  )
 
   def apply(bibData: SierraBibData) =
-    notesFields
-      .flatMap {
-        case (tag, NotesField(createNote, suppressedSubfields)) =>
-          bibData
-            .varfieldsWithTags(tag)
-            .map { varfield =>
-              varfield
-                .subfieldsWithoutTags(suppressedSubfields.toSeq: _*)
-                .contents
-                .mkString(" ")
-            }
-            .map(createNote)
+    bibData.varFields
+      .map {
+        case vf @ VarField(_, Some(marcTag), _, _, _, _) =>
+            Some((vf, notesFields.get(marcTag)))
+        case _ => None
+      }
+      .collect {
+        case Some((vf, Some(notesField))) => (vf, notesField)
+      }
+      .map { case (vf, NotesField(createNote, suppressedSubfields)) =>
+        val contents =
+          vf
+            .subfieldsWithoutTags(suppressedSubfields.toSeq: _*)
+            .contents
+            .mkString(" ")
+
+        createNote(contents)
       }
 }
