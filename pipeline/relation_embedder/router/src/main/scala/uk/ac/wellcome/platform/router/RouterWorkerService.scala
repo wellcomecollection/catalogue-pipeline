463cbeafc7935eaebecde0abb854f740887e330f
alicefuzier <alicefuzier@gmail.com>
2020-11-11 14:29:24 +0000

    getting first test to pass

--- pipeline/relation_embedder/router/src/main/scala/uk/ac/wellcome/platform/router/RouterWorkerService.scala
+++ pipeline/relation_embedder/router/src/main/scala/uk/ac/wellcome/platform/router/RouterWorkerService.scala
@@ -10,19 +10,23 @@ import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.pipeline_storage.Retriever
 import uk.ac.wellcome.typesafe.Runnable
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class RouterWorkerService[MsgDestination](
   sqsStream: SQSStream[NotificationMessage],
   msgSender: MessageSender[MsgDestination],
   workRetriever: Retriever[Work[Merged]]
-)
+)(implicit ec: ExecutionContext)
     extends Runnable {
 
   def run(): Future[Done] =
     sqsStream.foreach(this.getClass.getSimpleName, processMessage)
 
   private def processMessage(message: NotificationMessage): Future[Unit] = {
-    ???
+    workRetriever.apply(message.body).map{ work =>
+      work.data.collectionPath.get
+    }.flatMap{ path =>
+      Future.fromTry(msgSender.sendT(path))
+    }
   }
 }
