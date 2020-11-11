40d8ccf2abc2926806e674a84af786113d28e076
Nick Ward <ward.nickjames@gmail.com>
2020-11-11 10:25:56 +0000

    Get pipeline ready for deploy

--- pipeline/terraform/stack/service_work_id_minter.tf
+++ pipeline/terraform/stack/service_work_id_minter.tf
@@ -1,7 +1,7 @@
 module "work_id_minter_queue" {
   source                     = "git::github.com/wellcomecollection/terraform-aws-sqs//queue?ref=v1.1.2"
   queue_name                 = "${local.namespace_hyphen}_work_id_minter"
-  topic_arns                 = [module.relation_embedder_output_topic.arn]
+  topic_arns                 = [module.merger_works_topic.arn]
   aws_region                 = var.aws_region
   alarm_topic_arn            = var.dlq_alarm_arn
   visibility_timeout_seconds = 120
