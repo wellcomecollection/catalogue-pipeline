232ab9672c2d208a56e2903929dee8989c5894ef
Alex Chan <a.chan@wellcome.org>
2020-11-12 10:54:21 +0000

    Consolidate the definition of Works in WorksFilteredAggregationsTest

--- api/api/src/test/scala/uk/ac/wellcome/platform/api/works/WorksFilteredAggregationsTest.scala
+++ api/api/src/test/scala/uk/ac/wellcome/platform/api/works/WorksFilteredAggregationsTest.scala
@@ -1,33 +1,38 @@
 package uk.ac.wellcome.platform.api.works
 
-import uk.ac.wellcome.models.work.internal.Language
+import uk.ac.wellcome.models.work.internal.{Language, Work, WorkState}
 import uk.ac.wellcome.models.work.internal.Format._
 import uk.ac.wellcome.models.Implicits._
 
 class WorksFilteredAggregationsTest extends ApiWorksTestBase {
 
+  val bark = Language("Bark", Some("dogs"))
+  val meow = Language("Meow", Some("cats"))
+  val quack = Language("Quack", Some("ducks"))
+  val croak = Language("Croak", Some("frogs"))
+
+  val works: List[Work.Visible[WorkState.Identified]] = List(
+    (Books, bark),
+    (Journals, meow),
+    (Pictures, quack),
+    (Audio, bark),
+    (Books, bark),
+    (Books, bark),
+    (Journals, quack),
+    (Books, meow),
+    (Journals, quack),
+    (Audio, croak)
+  ).map {
+    case (format, language) =>
+      identifiedWork()
+        .format(format)
+        .language(language)
+  }
+
   it(
     "filters an aggregation with a filter that is not paired to the aggregation") {
     withWorksApi {
       case (worksIndex, routes) =>
-        val works = List(
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Meow", Some("cats"))),
-          (Pictures, Language("Quack", Some("ducks"))),
-          (Audio, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Books, Language("Meow", Some("cats"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Audio, Language("Croak", Some("frogs")))
-        ).map {
-          case (format, language) =>
-            identifiedWork()
-              .format(format)
-              .language(language)
-        }
-
         insertIntoElasticsearch(worksIndex, works: _*)
         assertJsonResponse(
           routes,
@@ -79,24 +84,6 @@ class WorksFilteredAggregationsTest extends ApiWorksTestBase {
     "filters an aggregation with a filter that is paired to another aggregation") {
     withWorksApi {
       case (worksIndex, routes) =>
-        val works = List(
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Meow", Some("cats"))),
-          (Pictures, Language("Quack", Some("ducks"))),
-          (Audio, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Books, Language("Meow", Some("cats"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Audio, Language("Croak", Some("frogs")))
-        ).map {
-          case (format, language) =>
-            identifiedWork()
-              .format(format)
-              .language(language)
-        }
-
         insertIntoElasticsearch(worksIndex, works: _*)
         assertJsonResponse(
           routes,
@@ -188,24 +175,6 @@ class WorksFilteredAggregationsTest extends ApiWorksTestBase {
   it("filters results but not aggregations paired with an applied filter") {
     withWorksApi {
       case (worksIndex, routes) =>
-        val works = List(
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Meow", Some("cats"))),
-          (Pictures, Language("Quack", Some("ducks"))),
-          (Audio, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Books, Language("Bark", Some("dogs"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Books, Language("Meow", Some("cats"))),
-          (Journals, Language("Quack", Some("ducks"))),
-          (Audio, Language("Croak", Some("frogs")))
-        ).map {
-          case (format, language) =>
-            identifiedWork()
-              .format(format)
-              .language(language)
-        }
-
         insertIntoElasticsearch(worksIndex, works: _*)
         assertJsonResponse(
           routes,
