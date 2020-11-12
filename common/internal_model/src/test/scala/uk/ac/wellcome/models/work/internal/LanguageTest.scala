1884838cde582775ca91ccc247bdb49015e16ba0
Alex Chan <a.chan@wellcome.org>
2020-11-12 11:40:11 +0000

    Use table-driven test cases in LanguageTest

--- common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/LanguageTest.scala
+++ common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/LanguageTest.scala
@@ -1,27 +1,30 @@
 package uk.ac.wellcome.models.work.internal
 
+import org.scalatest.EitherValues
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 
-class LanguageTest extends AnyFunSpec with Matchers {
-  it("gets labels for known code or errors") {
-    val withValidCode = Language.fromCode("yo")
-    val withInvalidCode = Language.fromCode("no such code")
-
-    withValidCode should be(Right(Language(label = "Yoruba", id = "yo")))
-    withInvalidCode shouldBe a[Left[_, _]]
+class LanguageTest extends AnyFunSpec with Matchers with EitherValues with TableDrivenPropertyChecks {
+  it("gets the language from a known code") {
+    Language.fromCode("yo").right.value shouldBe Language(label = "Yoruba", id = "yo")
   }
 
-  it(
-    "gets Language with code from known labels and omits code for unknown labels") {
-    val withValidLabel = Language.fromLabel("Yoruba")
-    val withMultiLabel1 = Language.fromLabel("Haitian")
-    val withMultiLabel2 = Language.fromLabel("Haitian Creole")
-    val withInvalidLabel = Language.fromLabel("no such label")
+  it("errors if asked to retrieve an unknown code") {
+    Language.fromCode("no such code").left.value shouldBe a[Exception]
+  }
 
-    withValidLabel shouldBe Right(Language(label = "Yoruba", id = "yo"))
-    withMultiLabel1 shouldBe Right(Language(label = "Haitian", id = "ht"))
-    withMultiLabel2 shouldBe Right(Language(label = "Haitian Creole", id = "ht"))
-    withInvalidLabel shouldBe Right(Language(label = "no such label", id = None))
+  val labelTestCases = Table(
+    ("label", "expectedLanguage"),
+    ("Yoruba", Language(label = "Yoruba", id = "yo")),
+    ("Haitian", Language(label = "Haitian", id = "ht")),
+    ("Haitian Creole", Language(label = "Haitian Creole", id = "ht")),
+    ("no such label", Language(label = "no such label", id = None)),
+  )
+
+  it("gets the language from a label") {
+    forAll(labelTestCases) { case (label, expectedLanguage) =>
+      Language.fromLabel(label).right.value shouldBe expectedLanguage
+    }
   }
 }
