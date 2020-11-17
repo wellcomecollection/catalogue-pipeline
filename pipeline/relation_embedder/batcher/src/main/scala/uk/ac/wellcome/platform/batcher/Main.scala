0fcdd46d79cbea31daa9ecca1d6bab3b3ea6d412
Nick Ward <ward.nickjames@gmail.com>
2020-11-17 14:13:49 +0000

    Batcher config

--- pipeline/relation_embedder/batcher/src/main/scala/uk/ac/wellcome/platform/batcher/Main.scala
+++ pipeline/relation_embedder/batcher/src/main/scala/uk/ac/wellcome/platform/batcher/Main.scala
@@ -1,38 +1,32 @@
-package uk.ac.wellcome.platform.router
+package uk.ac.wellcome.platform.batcher
 
-// import akka.actor.ActorSystem
+import scala.concurrent.ExecutionContext
+import scala.concurrent.duration._
+import akka.actor.ActorSystem
+import akka.stream.Materializer
 import com.typesafe.config.Config
 
-// import uk.ac.wellcome.messaging.sns.NotificationMessage
-// import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
+import uk.ac.wellcome.messaging.sns.NotificationMessage
+import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
-// import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
-// import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
-
-// import scala.concurrent.ExecutionContext
+import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 
 object Main extends WellcomeTypesafeApp {
   runWithConfig { config: Config =>
-    // implicit val actorSystem: ActorSystem =
-    //   AkkaBuilder.buildActorSystem()
-    // implicit val executionContext: ExecutionContext =
-    //   AkkaBuilder.buildExecutionContext()
+    implicit val executionContext: ExecutionContext =
+      AkkaBuilder.buildExecutionContext()
+    implicit val actorSystem: ActorSystem =
+      AkkaBuilder.buildActorSystem()
+    implicit val materializer: Materializer =
+      AkkaBuilder.buildMaterializer()
 
-    ???
-
-    // new RouterWorkerService(
-    //   sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
-    //   worksMsgSender = SNSBuilder
-    //     .buildSNSMessageSender(
-    //       config,
-    //       namespace = "work-sender",
-    //       subject = "Sent from the router"),
-    //   pathsMsgSender = SNSBuilder
-    //     .buildSNSMessageSender(
-    //       config,
-    //       namespace = "path-sender",
-    //       subject = "Sent from the router"),
-    //   workRetriever = new ElasticRetriever(esClient, mergedIndex)
-    // )
+    new BatcherWorkerService(
+      msgStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
+      msgSender = SNSBuilder.buildSNSMessageSender(config, subject = "Sent from batcher"),
+      flushInterval = config.requireInt("batcher.flush_interval_minutes").minutes,
+      maxProcessedPaths = config.requireInt("batcher.max_processed_paths"),
+      maxBatchSize = config.requireInt("batcher.max_batch_size")
+    )
   }
 }
