38a1f50834ba8ddd87e5eef7fdae4889d50835c5
Nick Ward <ward.nickjames@gmail.com>
2020-11-20 15:03:15 +0000

    Give router access to es indices

--- pipeline/terraform/stack/service_work_router.tf
+++ pipeline/terraform/stack/service_work_router.tf
@@ -26,6 +26,9 @@ module "router" {
 
     paths_topic_arn = module.router_path_output_topic.arn
     works_topic_arn = module.router_work_output_topic.arn
+
+    es_merged_index       = local.es_works_merged_index
+    es_denormalised_index = local.es_works_denormalised_index
   }
 
   secret_env_vars = {
