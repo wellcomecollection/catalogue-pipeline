4e7e45f69c62869ff31ac815f456ae1afa4d9394
jamie <j.parkinson@wellcome.ac.uk>
2020-11-23 10:38:47 +0000

    Add extra fields to API image source

--- common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayImageSource.scala
+++ common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayImageSource.scala
@@ -12,6 +12,15 @@ case class DisplayImageSource(
   @Schema(
     description = "Identifer of the image source"
   ) id: String,
+  @Schema(
+    description = "The title of the image source"
+  ) title: Option[String],
+  @Schema(
+    description = "The primary contributor associated with the image source"
+  ) contributor: Option[DisplayContributor],
+  @Schema(
+    description = "The languages of the image source"
+  ) languages: Option[List[DisplayLanguage]],
   @JsonKey("type") @Schema(
     name = "type",
     description = "What kind of source this is"
@@ -22,10 +31,18 @@ object DisplayImageSource {
 
   def apply(
     imageSource: ImageSource[DataState.Identified]): DisplayImageSource =
+    imageSource match {
+      case works: SourceWorks[DataState.Identified] => DisplayImageSource(works)
+    }
+
+  def apply(source: SourceWorks[DataState.Identified]): DisplayImageSource =
     new DisplayImageSource(
-      id = imageSource.id.canonicalId,
-      ontologyType = imageSource match {
-        case SourceWorks(_, _) => "Work"
-      }
+      id = source.id.canonicalId,
+      title = source.canonicalWork.data.title,
+      contributor = source.canonicalWork.data.contributors.headOption
+        .map(DisplayContributor(_, includesIdentifiers = false)),
+      languages =
+        Some(source.canonicalWork.data.languages.map(DisplayLanguage(_))),
+      ontologyType = "works"
     )
 }
