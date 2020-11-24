4b95946d3296d42f5e53bf46bce2329e44a48727
Alex Chan <a.chan@wellcome.org>
2020-11-24 18:53:58 +0000

    Add tests for more failure cases in TransformerWorker

--- pipeline/transformer/transformer_common/src/test/scala/uk/ac/wellcome/transformer/common/worker/TransformerWorkerTest.scala
+++ pipeline/transformer/transformer_common/src/test/scala/uk/ac/wellcome/transformer/common/worker/TransformerWorkerTest.scala
@@ -1,6 +1,6 @@
 package uk.ac.wellcome.transformer.common.worker
 
-import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.concurrent.{Eventually, IntegrationPatience, ScalaFutures}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import uk.ac.wellcome.akka.fixtures.Akka
@@ -17,6 +17,9 @@ import uk.ac.wellcome.storage.Version
 import uk.ac.wellcome.storage.store.VersionedStore
 import uk.ac.wellcome.storage.store.memory.MemoryVersionedStore
 import WorkState.Source
+import io.circe.Encoder
+
+import scala.util.{Failure, Try}
 
 trait TestData
 case object ValidTestData extends TestData
@@ -43,6 +46,8 @@ class TransformerWorkerTest
     extends AnyFunSpec
     with ScalaFutures
     with Matchers
+    with Eventually
+    with IntegrationPatience
     with Akka
     with SQS
     with SNS {
@@ -61,51 +66,92 @@ class TransformerWorkerTest
           sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
           sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
 
-          Thread.sleep(500)
-
-          assertQueueEmpty(dlq)
-          assertQueueEmpty(queue)
+          eventually {
+            assertQueueEmpty(dlq)
+            assertQueueEmpty(queue)
+          }
         }
     }
   }
 
-  it("sends failed messages to the DLQ if it can't read from store") {
-    withLocalSqsQueuePair() {
-      case QueuePair(queue, dlq) =>
-        withWorker(queue, records = Map.empty) { _ =>
-          sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
+  describe("sends failures to the DLQ") {
+    it("if it can't parse the JSON on the queue") {
+      withLocalSqsQueuePair() {
+        case QueuePair(queue, dlq) =>
+          withWorker(queue) { _ =>
+            sendInvalidJSONto(queue)
 
-          Thread.sleep(2000)
+            eventually {
+              assertQueueHasSize(dlq, size = 1)
+              assertQueueEmpty(queue)
+            }
+          }
+      }
+    }
 
-          assertQueueHasSize(dlq, size = 1)
-          assertQueueEmpty(queue)
-        }
+    it("if it can't find the source record in the store") {
+      withLocalSqsQueuePair() {
+        case QueuePair(queue, dlq) =>
+          withWorker(queue) { _ =>
+            sendNotificationToSQS(queue, Version("A", 1))
+
+            eventually {
+              assertQueueHasSize(dlq, size = 1)
+              assertQueueEmpty(queue)
+            }
+          }
+      }
+    }
+
+    it("if the work can't be transformed") {
+      val records = Map(
+        Version("A", 1) -> ValidTestData,
+        Version("B", 2) -> ValidTestData,
+        Version("C", 3) -> InvalidTestData
+      )
+
+      withLocalSqsQueuePair() {
+        case QueuePair(queue, dlq) =>
+          withWorker(queue, records = records) { _ =>
+            sendNotificationToSQS(queue, Version("A", 1))
+            sendNotificationToSQS(queue, Version("B", 2))
+            sendNotificationToSQS(queue, Version("C", 3))
+
+            eventually {
+              assertQueueHasSize(dlq, size = 1)
+              assertQueueEmpty(queue)
+            }
+          }
+      }
+    }
+
+    it("if it can't send a message") {
+      val brokenSender = new MemoryMessageSender() {
+        override def sendT[T](t: T)(implicit encoder: Encoder[T]): Try[Unit] =
+          Failure(new Throwable("BOOM!"))
+      }
+
+      val records = Map(Version("A", 1) -> ValidTestData)
+
+      withLocalSqsQueuePair() {
+        case QueuePair(queue, dlq) =>
+          withWorker(queue, records = records, sender = brokenSender) { _ =>
+            sendNotificationToSQS(queue, Version("A", 1))
+
+            eventually {
+              assertQueueHasSize(dlq, size = 1)
+              assertQueueEmpty(queue)
+            }
+          }
+      }
     }
   }
 
-  it("sends failed messages to the DLQ if the transformer errors") {
-    val records = Map(
-      Version("A", 1) -> ValidTestData,
-      Version("B", 2) -> ValidTestData,
-      Version("C", 3) -> InvalidTestData
-    )
-
-    withLocalSqsQueuePair() {
-      case QueuePair(queue, dlq) =>
-        withWorker(queue, records = records) { _ =>
-          sendNotificationToSQS[Version[String, Int]](queue, Version("A", 1))
-          sendNotificationToSQS[Version[String, Int]](queue, Version("B", 2))
-          sendNotificationToSQS[Version[String, Int]](queue, Version("C", 3))
-
-          Thread.sleep(2000)
-
-          assertQueueHasSize(dlq, size = 1)
-          assertQueueEmpty(queue)
-        }
-    }
-  }
-
-  def withWorker[R](queue: Queue, records: Map[Version[String, Int], TestData])(
+  def withWorker[R](
+    queue: Queue,
+    records: Map[Version[String, Int], TestData] = Map.empty,
+    sender: MemoryMessageSender = new MemoryMessageSender()
+  )(
     testWith: TestWith[Unit, R]
   ): R =
     withActorSystem { implicit actorSystem =>
@@ -114,7 +160,7 @@ class TransformerWorkerTest
 
         val worker = new TestTransformerWorker(
           stream = stream,
-          sender = new MemoryMessageSender(),
+          sender = sender,
           store = store
         )
 
