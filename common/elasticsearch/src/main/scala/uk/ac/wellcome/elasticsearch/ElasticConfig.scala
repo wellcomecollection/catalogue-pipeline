2815eaf52248713bbfdbae3b9dca6abab29e4686
Nick Ward <ward.nickjames@gmail.com>
2020-11-26 08:53:02 +0000

    Deploy 25-11-2020 index

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
@@ -10,7 +10,7 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  val indexDate = "2020-11-23"
+  val indexDate = "2020-11-25"
 
   def apply(): ElasticConfig =
     ElasticConfig(
