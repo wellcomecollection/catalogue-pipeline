d5082b5598c1c2844bfc932e8010d3078b89cdf0
Alex Chan <a.chan@wellcome.org>
2020-11-29 19:45:16 +0000

    Use the new MiroVHSReader in the Miro transformer

--- pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
+++ pipeline/transformer/transformer_miro/src/main/scala/uk/ac/wellcome/platform/transformer/miro/Main.scala
@@ -4,17 +4,15 @@ import akka.actor.ActorSystem
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.s3.AmazonS3
 import com.typesafe.config.Config
-import org.scanamo.auto._
 import uk.ac.wellcome.bigmessaging.typesafe.BigMessagingBuilder
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.typesafe.SQSBuilder
 import uk.ac.wellcome.platform.transformer.miro.Implicits._
-import uk.ac.wellcome.platform.transformer.miro.models.MiroVHSRecord
-import uk.ac.wellcome.platform.transformer.miro.services.MiroTransformerWorkerService
+import uk.ac.wellcome.platform.transformer.miro.services.{
+  MiroTransformerWorkerService,
+  MiroVHSReader
+}
 import uk.ac.wellcome.platform.transformer.miro.source.MiroRecord
-import uk.ac.wellcome.storage.Identified
-import uk.ac.wellcome.storage.store.Readable
-import uk.ac.wellcome.storage.store.dynamo.DynamoSingleVersionStore
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.streaming.Codec._
 import uk.ac.wellcome.storage.typesafe.{DynamoBuilder, S3Builder}
@@ -36,16 +34,9 @@ object Main extends WellcomeTypesafeApp {
     implicit val dynamoClient: AmazonDynamoDB =
       DynamoBuilder.buildDynamoClient(config)
 
-    val dynamoStore = new DynamoSingleVersionStore[String, MiroVHSRecord](
+    val miroVhsReader = new MiroVHSReader(
       config = DynamoBuilder.buildDynamoConfig(config)
     )
-
-    val miroVhsReader = new Readable[String, MiroVHSRecord] {
-      override def get(id: String): ReadEither =
-        dynamoStore
-          .getLatest(id)
-          .map { case Identified(_, record) => Identified(id, record) }
-    }
 
     new MiroTransformerWorkerService(
       stream = SQSBuilder.buildSQSStream[NotificationMessage](config),
