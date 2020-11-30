a22d973b1d1019e5f4dfcbc23f3d60ee7ff94ef1
Alex Chan <a.chan@wellcome.org>
2020-11-30 20:31:53 +0000

    Use the new fixture in the PipelineStorageStream tests

--- common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/PipelineStorageStreamTest.scala
+++ common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/PipelineStorageStreamTest.scala
@@ -5,23 +5,19 @@ import com.sksamuel.elastic4s.requests.indexes.admin.IndexExistsResponse
 import com.sksamuel.elastic4s.{ElasticClient, Index, Response}
 import org.scalatest.concurrent.PatienceConfiguration.Timeout
 import org.scalatest.funspec.AnyFunSpec
-import uk.ac.wellcome.akka.fixtures.Akka
 import uk.ac.wellcome.elasticsearch.{ElasticClientBuilder, NoStrictMapping}
 import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
-import uk.ac.wellcome.fixtures.{RandomGenerators, TestWith}
 import uk.ac.wellcome.json.JsonUtil
 import uk.ac.wellcome.json.JsonUtil._
-import uk.ac.wellcome.messaging.fixtures.SQS
 import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
 import uk.ac.wellcome.messaging.memory.MemoryMessageSender
-import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.models.work.generators.IdentifiersGenerators
 import uk.ac.wellcome.pipeline_storage.fixtures.{
   ElasticIndexerFixtures,
+  PipelineStorageStreamFixtures,
   SampleDocument
 }
 
-import scala.collection.mutable
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -31,45 +27,11 @@ class PipelineStorageStreamTest
     extends AnyFunSpec
     with ElasticIndexerFixtures
     with IdentifiersGenerators
-    with RandomGenerators
     with ElasticsearchFixtures
-    with SQS
-    with Akka {
-
-  val pipelineStorageConfig = PipelineStorageConfig(
-    batchSize = 1,
-    flushInterval = 1 seconds,
-    parallelism = 10)
+    with PipelineStorageStreamFixtures {
 
   def indexer(index: Index, elasticClient: ElasticClient = elasticClient) =
     new ElasticIndexer[SampleDocument](elasticClient, index, NoStrictMapping)
-
-  def withPipelineStream[R](
-    indexer: Indexer[SampleDocument] = new MemoryIndexer[SampleDocument](
-      index = mutable.Map[String, SampleDocument]()
-    ),
-    sender: MemoryMessageSender = new MemoryMessageSender(),
-    visibilityTimeout: Int = 1,
-    pipelineStorageConfig: PipelineStorageConfig = pipelineStorageConfig)(
-    testWith: TestWith[
-      (PipelineStorageStream[NotificationMessage, SampleDocument, String],
-       QueuePair,
-       MemoryMessageSender),
-      R]): R =
-    withActorSystem { implicit ac =>
-      withLocalSqsQueuePair(visibilityTimeout) {
-        case q @ QueuePair(queue, _) =>
-          withSQSStream[NotificationMessage, R](queue) { stream =>
-            testWith(
-              (
-                new PipelineStorageStream(stream, indexer, sender)(
-                  pipelineStorageConfig),
-                q,
-                sender))
-          }
-
-      }
-    }
 
   it("creates the index at startup if it doesn't already exist") {
     val index = createIndex
@@ -78,72 +40,90 @@ class PipelineStorageStreamTest
         .execute(indexExists(index.name))
         .await
     response.result.isExists shouldBe false
-    withPipelineStream(indexer(index)) {
-      case (pipelineStream, _, _) =>
-        pipelineStream.foreach("test_stream", _ => Future.successful(None))
-        eventuallyIndexExists(index)
+    withLocalSqsQueue() { queue =>
+      withPipelineStream(queue = queue, indexer = indexer(index)) {
+        pipelineStream =>
+          pipelineStream.foreach("test_stream", _ => Future.successful(None))
+          eventuallyIndexExists(index)
+      }
     }
   }
 
   it("ingests a single document and sends it on") {
     val index = createIndex
     val document = SampleDocument(1, createCanonicalId, randomAlphanumeric())
-    withPipelineStream(indexer(index), visibilityTimeout = 10) {
-      case (pipelineStream, QueuePair(queue, dlq), messageSender) =>
-        sendNotificationToSQS(
-          queue = queue,
-          message = document
-        )
-        pipelineStream.foreach(
-          "test stream",
-          message =>
-            Future
-              .fromTry(JsonUtil.fromJson[SampleDocument](message.body))
-              .map(Option(_)))
-        assertElasticsearchEventuallyHas(index = index, document)
-        eventually {
-          messageSender.messages.map(_.body) should contain(
-            document.canonicalId)
-          assertQueueEmpty(queue)
-          assertQueueEmpty(dlq)
-        }
-    }
 
+    val sender = new MemoryMessageSender
+
+    withLocalSqsQueuePair(visibilityTimeout = 10) { case QueuePair(queue, dlq) =>
+      withPipelineStream(queue = queue, indexer = indexer(index), sender = sender) {
+        pipelineStream =>
+          sendNotificationToSQS(
+            queue = queue,
+            message = document
+          )
+          pipelineStream.foreach(
+            "test stream",
+            message =>
+              Future
+                .fromTry(JsonUtil.fromJson[SampleDocument](message.body))
+                .map(Option(_)))
+          assertElasticsearchEventuallyHas(index = index, document)
+          eventually {
+            sender.messages.map(_.body) should contain(
+              document.canonicalId)
+            assertQueueEmpty(queue)
+            assertQueueEmpty(dlq)
+          }
+      }
+    }
   }
 
   it("processes a message and does not ingest if result of process is a None") {
     val index = createIndex
     val document = SampleDocument(1, createCanonicalId, randomAlphanumeric())
-    withPipelineStream(indexer(index), visibilityTimeout = 10) {
-      case (pipelineStream, QueuePair(queue, dlq), messageSender) =>
-        sendNotificationToSQS(
-          queue = queue,
-          message = document
-        )
-        pipelineStream.foreach("test stream", _ => Future.successful(None))
 
-        eventually {
-          assertElasticsearchEmpty(index = index)
-          messageSender.messages.map(_.body) shouldBe empty
-          assertQueueEmpty(queue)
-          assertQueueEmpty(dlq)
-        }
+    val sender = new MemoryMessageSender
+
+    withLocalSqsQueuePair(visibilityTimeout = 10) { case QueuePair(queue, dlq) =>
+      withPipelineStream(queue = queue, indexer = indexer(index), sender = sender) {
+        pipelineStream =>
+          sendNotificationToSQS(
+            queue = queue,
+            message = document
+          )
+          pipelineStream.foreach("test stream", _ => Future.successful(None))
+
+          eventually {
+            assertElasticsearchEmpty(index = index)
+            sender.messages.map(_.body) shouldBe empty
+            assertQueueEmpty(queue)
+            assertQueueEmpty(dlq)
+          }
+      }
     }
-
   }
 
   it("ingests lots of documents") {
     val index = createIndex
     val documents = (1 to 250).map(_ =>
       SampleDocument(1, createCanonicalId, randomAlphanumeric()))
-    withPipelineStream(
-      indexer(index),
-      visibilityTimeout = 30,
-      pipelineStorageConfig = PipelineStorageConfig(
-        batchSize = 150,
-        flushInterval = 10 seconds,
-        parallelism = 10)) {
-      case (pipelineStream, QueuePair(queue, dlq), messageSender) =>
+
+    val pipelineStorageConfig = PipelineStorageConfig(
+      batchSize = 150,
+      flushInterval = 10 seconds,
+      parallelism = 10
+    )
+
+    val sender = new MemoryMessageSender
+
+    withLocalSqsQueuePair(visibilityTimeout = 30) { case QueuePair(queue, dlq) =>
+      withPipelineStream(
+        queue = queue,
+        indexer = indexer(index),
+        sender = sender,
+        pipelineStorageConfig = pipelineStorageConfig
+      ) { pipelineStream =>
         documents.foreach(
           doc =>
             sendNotificationToSQS(
@@ -158,18 +138,21 @@ class PipelineStorageStreamTest
               .map(Option(_)))
         assertElasticsearchEventuallyHas(index = index, documents: _*)
         eventually(Timeout(scaled(60 seconds))) {
-          messageSender.messages.map(_.body) should contain theSameElementsAs (documents
-            .map(_.canonicalId))
+          sender.messages.map(_.body) should contain theSameElementsAs documents
+            .map(_.canonicalId)
           assertQueueEmpty(queue)
           assertQueueEmpty(dlq)
         }
+      }
     }
   }
 
   it("leaves a message on the queue if it fails processing") {
     val index = createIndex
-    withPipelineStream(indexer(index)) {
-      case (pipelineStream, QueuePair(queue, dlq), messageSender) =>
+    val sender = new MemoryMessageSender
+
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withPipelineStream(queue = queue, indexer = indexer(index)) { pipelineStream =>
         sendInvalidJSONto(queue)
         pipelineStream.foreach(
           "test stream",
@@ -180,8 +163,9 @@ class PipelineStorageStreamTest
         eventually {
           assertQueueEmpty(queue)
           assertQueueHasSize(dlq, size = 1)
-          messageSender.messages shouldBe empty
+          sender.messages shouldBe empty
         }
+      }
     }
   }
 
@@ -194,16 +178,18 @@ class PipelineStorageStreamTest
       username = "elastic",
       password = "dontletmein"
     )
-    withPipelineStream(indexer(index, brokenClient)) {
-      case (pipelineStream, _, _) =>
-        whenReady(
-          pipelineStream
-            .foreach("test stream", _ => Future.successful(None))
-            .failed) { exception =>
-          exception shouldBe a[RuntimeException]
-        }
-    }
 
+    withLocalSqsQueue() { queue =>
+      withPipelineStream(queue = queue, indexer = indexer(index, brokenClient)) {
+        pipelineStream =>
+          whenReady(
+            pipelineStream
+              .foreach("test stream", _ => Future.successful(None))
+              .failed) { exception =>
+            exception shouldBe a[RuntimeException]
+          }
+      }
+    }
   }
 
   it("does not delete from the queue documents that failed indexing") {
@@ -231,26 +217,31 @@ class PipelineStorageStreamTest
           failingDocuments.keySet.contains(d.canonicalId))))
       }
     }
-    withPipelineStream(indexer) {
-      case (pipelineStream, QueuePair(queue, dlq), messageSender) =>
-        documents.foreach(
-          doc =>
-            sendNotificationToSQS(
-              queue = queue,
-              message = doc
-          ))
-        pipelineStream.foreach(
-          "test stream",
-          message =>
-            Future
-              .fromTry(JsonUtil.fromJson[SampleDocument](message.body))
-              .map(Option(_)))
-        eventually {
-          messageSender.messages.map(_.body) should contain theSameElementsAs (successfulDocuments
-            .map(_.canonicalId))
-          assertQueueEmpty(queue)
-          assertQueueHasSize(dlq, size = 5)
-        }
+
+    val sender = new MemoryMessageSender
+
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withPipelineStream(queue = queue, indexer = indexer, sender = sender) {
+        pipelineStream =>
+          documents.foreach(
+            doc =>
+              sendNotificationToSQS(
+                queue = queue,
+                message = doc
+              ))
+          pipelineStream.foreach(
+            "test stream",
+            message =>
+              Future
+                .fromTry(JsonUtil.fromJson[SampleDocument](message.body))
+                .map(Option(_)))
+          eventually {
+            sender.messages.map(_.body) should contain theSameElementsAs successfulDocuments
+              .map(_.canonicalId)
+            assertQueueEmpty(queue)
+            assertQueueHasSize(dlq, size = 5)
+          }
+      }
     }
   }
 
@@ -266,19 +257,21 @@ class PipelineStorageStreamTest
         Failure(new Throwable("BOOM!"))
     }
 
-    withPipelineStream(sender = brokenSender) {
-      case (pipelineStream, QueuePair(queue, dlq), _) =>
-        sendNotificationToSQS(queue, document)
+    withLocalSqsQueuePair() { case QueuePair(queue, dlq) =>
+      withPipelineStream(queue = queue, indexer = indexer(createIndex), sender = brokenSender) {
+        pipelineStream =>
+          sendNotificationToSQS(queue, document)
 
-        pipelineStream.foreach(
-          "test stream",
-          _ => Future.successful(Some(document))
-        )
+          pipelineStream.foreach(
+            "test stream",
+            _ => Future.successful(Some(document))
+          )
 
-        eventually {
-          assertQueueEmpty(queue)
-          assertQueueHasSize(dlq, size = 1)
-        }
+          eventually {
+            assertQueueEmpty(queue)
+            assertQueueHasSize(dlq, size = 1)
+          }
+      }
     }
   }
 }
