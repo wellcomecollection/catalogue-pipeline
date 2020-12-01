b395b7eefc7ab950b138fd48d71d1bf338b542be
Alex Chan <a.chan@wellcome.org>
2020-12-01 07:42:06 +0000

    Tidy up the whitespace around PipelineStorageStreamBuilder

--- common/pipeline_storage_typesafe/src/main/scala/uk/ac/wellcome/pipeline_storage/typesafe/PipelineStorageStreamBuilder.scala
+++ common/pipeline_storage_typesafe/src/main/scala/uk/ac/wellcome/pipeline_storage/typesafe/PipelineStorageStreamBuilder.scala
@@ -12,25 +12,23 @@ import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration.DurationInt
-object PipelineStorageStreamBuilder {
 
-  def buildPipelineStorageConfig(config: Config): PipelineStorageConfig = {
+object PipelineStorageStreamBuilder {
+  def buildPipelineStorageConfig(config: Config): PipelineStorageConfig =
     PipelineStorageConfig(
       config.requireInt("pipeline_storage.batch_size"),
       config.requireInt("pipeline_storage.flush_interval_seconds").seconds,
       config.getIntOption("pipeline_storage.parallelism").getOrElse(10)
     )
-  }
 
   def buildPipelineStorageStream[In, Out, MsgDestination](
     sqsStream: SQSStream[In],
     indexer: Indexer[Out],
     messageSender: MessageSender[MsgDestination])(config: Config)(
     implicit ec: ExecutionContext)
-    : PipelineStorageStream[In, Out, MsgDestination] = {
+    : PipelineStorageStream[In, Out, MsgDestination] =
     new PipelineStorageStream[In, Out, MsgDestination](
       sqsStream,
       indexer,
       messageSender)(buildPipelineStorageConfig(config))
-  }
 }
