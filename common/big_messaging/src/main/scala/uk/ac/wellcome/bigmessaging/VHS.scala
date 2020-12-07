4b500b074980df068446dd5ce8262dff07977571
Alex Chan <a.chan@wellcome.org>
2020-12-07 11:02:45 +0000

    Clarify that VHS is also a DynamoHybridStore

--- common/big_messaging/src/main/scala/uk/ac/wellcome/bigmessaging/VHS.scala
+++ common/big_messaging/src/main/scala/uk/ac/wellcome/bigmessaging/VHS.scala
@@ -1,15 +1,12 @@
 package uk.ac.wellcome.bigmessaging
 
-import java.util.UUID
-
 import uk.ac.wellcome.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
-import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
+import uk.ac.wellcome.storage.store.dynamo.{DynamoHashStore, DynamoHybridStore}
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.store.{
   HybridStoreWithMaxima,
   VersionedHybridStore
 }
-import uk.ac.wellcome.storage.Version
 
 class VHS[T](val hybridStore: VHSInternalStore[T])
     extends VersionedHybridStore[
@@ -22,11 +19,7 @@ class VHS[T](val hybridStore: VHSInternalStore[T])
 class VHSInternalStore[T](
   prefix: S3ObjectLocationPrefix)(
   implicit
-  val indexedStore: DynamoHashStore[String, Int, S3ObjectLocation],
-  val typedStore: S3TypedStore[T]
-) extends HybridStoreWithMaxima[String, Int, S3ObjectLocation, T] {
-
-  override protected def createTypeStoreId(
-    id: Version[String, Int]): S3ObjectLocation =
-    prefix.asLocation(id.id, id.version.toString, UUID.randomUUID().toString)
-}
+  indexedStore: DynamoHashStore[String, Int, S3ObjectLocation],
+  typedStore: S3TypedStore[T]
+) extends DynamoHybridStore[T](prefix)(indexedStore, typedStore)
+  with HybridStoreWithMaxima[String, Int, S3ObjectLocation, T]
