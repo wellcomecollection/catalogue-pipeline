5bc1231a3cd5b3ffbf2ec6e6329ef13027bf3761
Alex Chan <a.chan@wellcome.org>
2020-12-07 10:57:45 +0000

    Remove an unused type

--- common/big_messaging_typesafe/src/main/scala/uk/ac/wellcome/bigmessaging/typesafe/VHSBuilder.scala
+++ common/big_messaging_typesafe/src/main/scala/uk/ac/wellcome/bigmessaging/typesafe/VHSBuilder.scala
@@ -7,18 +7,12 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
-import uk.ac.wellcome.storage.store.Store
-import uk.ac.wellcome.storage.maxima.Maxima
-import uk.ac.wellcome.storage.Version
 import uk.ac.wellcome.storage.typesafe.{DynamoBuilder, S3Builder}
 import uk.ac.wellcome.storage.streaming.Codec
 import uk.ac.wellcome.bigmessaging.{VHS, VHSInternalStore}
 import uk.ac.wellcome.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
 
 object VHSBuilder {
-
-  type IndexStore =
-    Store[Version[String, Int], S3ObjectLocation] with Maxima[String, Int]
 
   def build[T](config: Config, namespace: String = "vhs")(
     implicit codec: Codec[T]): VHS[T] = {
