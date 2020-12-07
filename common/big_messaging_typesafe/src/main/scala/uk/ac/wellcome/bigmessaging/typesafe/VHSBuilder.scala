6aa208e809105f4e1c43c880f27b7b7bfefe8aa5
Alex Chan <a.chan@wellcome.org>
2020-12-07 10:30:03 +0000

    Inline a couple of methods on VHSBuilder

--- common/big_messaging_typesafe/src/main/scala/uk/ac/wellcome/bigmessaging/typesafe/VHSBuilder.scala
+++ common/big_messaging_typesafe/src/main/scala/uk/ac/wellcome/bigmessaging/typesafe/VHSBuilder.scala
@@ -6,7 +6,6 @@ import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
-import uk.ac.wellcome.storage.dynamo.DynamoConfig
 import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.store.Store
 import uk.ac.wellcome.storage.maxima.Maxima
@@ -22,24 +21,20 @@ object VHSBuilder {
     Store[Version[String, Int], S3ObjectLocation] with Maxima[String, Int]
 
   def build[T](config: Config, namespace: String = "vhs")(
-    implicit codec: Codec[T]): VHS[T] =
-    VHSBuilder.build(
-      buildObjectLocationPrefix(config, namespace = namespace),
-      DynamoBuilder.buildDynamoConfig(config, namespace = namespace),
-      DynamoBuilder.buildDynamoClient(config),
-      S3Builder.buildS3Client(config)
-    )
+    implicit codec: Codec[T]): VHS[T] = {
+    implicit val s3Client: AmazonS3 = S3Builder.buildS3Client(config)
+    implicit val dynamoClient: AmazonDynamoDB =
+      DynamoBuilder.buildDynamoClient(config)
 
-  def build[T](objectLocationPrefix: S3ObjectLocationPrefix,
-               dynamoConfig: DynamoConfig,
-               dynamoClient: AmazonDynamoDB,
-               s3Client: AmazonS3)(implicit codec: Codec[T]): VHS[T] = {
-    implicit val s3 = s3Client;
+    val dynamoConfig =
+      DynamoBuilder.buildDynamoConfig(config, namespace = namespace)
+
     new VHS(
       new VHSInternalStore(
-        objectLocationPrefix,
-        createIndexStore(dynamoClient, dynamoConfig),
-        S3TypedStore[T])
+        prefix = buildObjectLocationPrefix(config, namespace = namespace),
+        indexStore =
+          new DynamoHashStore[String, Int, S3ObjectLocation](dynamoConfig),
+        dataStore = S3TypedStore[T])
     )
   }
 
@@ -51,10 +46,4 @@ object VHSBuilder {
         .getStringOption(s"aws.$namespace.s3.globalPrefix")
         .getOrElse("")
     )
-
-  private def createIndexStore(dynamoClient: AmazonDynamoDB,
-                               dynamoConfig: DynamoConfig): IndexStore = {
-    implicit val dynamo = dynamoClient;
-    new DynamoHashStore[String, Int, S3ObjectLocation](dynamoConfig)
-  }
 }
