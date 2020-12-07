0ed29a8278afccc7f1dc0c366468b8aa835271fa
Alex Chan <a.chan@wellcome.org>
2020-12-07 10:55:12 +0000

    Insist on another, specific type

--- common/big_messaging/src/main/scala/uk/ac/wellcome/bigmessaging/VHS.scala
+++ common/big_messaging/src/main/scala/uk/ac/wellcome/bigmessaging/VHS.scala
@@ -4,9 +4,9 @@ import java.util.UUID
 
 import uk.ac.wellcome.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
 import uk.ac.wellcome.storage.store.dynamo.DynamoHashStore
+import uk.ac.wellcome.storage.store.s3.S3TypedStore
 import uk.ac.wellcome.storage.store.{
   HybridStoreWithMaxima,
-  TypedStore,
   VersionedHybridStore
 }
 import uk.ac.wellcome.storage.Version
@@ -22,7 +22,7 @@ class VHS[T](val hybridStore: VHSInternalStore[T])
 class VHSInternalStore[T](
   prefix: S3ObjectLocationPrefix,
   val indexedStore: DynamoHashStore[String, Int, S3ObjectLocation],
-  val typedStore: TypedStore[S3ObjectLocation, T]
+  val typedStore: S3TypedStore[T]
 ) extends HybridStoreWithMaxima[String, Int, S3ObjectLocation, T] {
 
   override protected def createTypeStoreId(
