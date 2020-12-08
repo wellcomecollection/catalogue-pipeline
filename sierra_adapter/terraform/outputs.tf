4dff5607bdd93a5f2480c5a8e06ac744f371fea9
Alex Chan <alex@alexwlchan.net>
2020-12-08 21:30:27 +0000

    Remove a couple of unused SNS topic outputs
    The other parts of the catalogue that rely on the Sierra adapter use the
    topic ARNS, not the topic names.

--- sierra_adapter/terraform/outputs.tf
+++ sierra_adapter/terraform/outputs.tf
@@ -1,13 +1,5 @@
-output "merged_items_topic_name" {
-  value = module.sierra-adapter-20200604.merged_items_topic_name
-}
-
 output "merged_items_topic_arn" {
   value = module.sierra-adapter-20200604.merged_items_topic_arn
-}
-
-output "merged_bibs_topic_name" {
-  value = module.sierra-adapter-20200604.merged_bibs_topic_name
 }
 
 output "merged_bibs_topic_arn" {
