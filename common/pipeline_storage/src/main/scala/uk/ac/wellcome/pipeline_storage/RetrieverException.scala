f0e3b7e4a7f79410e6ac9367f7506931d1fcc7f8
Nick Ward <ward.nickjames@gmail.com>
2020-12-09 15:23:22 +0000

    Don't insert blank space when no responseContext in RetrieverNotFoundException

--- common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverException.scala
+++ common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverException.scala
@@ -3,4 +3,4 @@ package uk.ac.wellcome.pipeline_storage
 class RetrieverException(message: String) extends RuntimeException(message)
 
 class RetrieverNotFoundException(id: String, responseContext: Option[String] = None)
-    extends RetrieverException(s"Nothing found with ID $id! ${responseContext.getOrElse("")}")
+    extends RetrieverException(s"Nothing found with ID $id!${responseContext.map(List(" ", _).mkString("")).getOrElse("")}")
