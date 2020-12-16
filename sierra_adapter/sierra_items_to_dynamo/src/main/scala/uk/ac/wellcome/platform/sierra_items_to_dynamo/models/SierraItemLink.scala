43f1f623669910ff25913ba644cd0d1138afc0c7
Alex Chan <alex@alexwlchan.net>
2020-12-16 12:14:00 +0000

    We don't need to store the ID twice

--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/models/SierraItemLink.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/uk/ac/wellcome/platform/sierra_items_to_dynamo/models/SierraItemLink.scala
@@ -2,14 +2,12 @@ package uk.ac.wellcome.platform.sierra_items_to_dynamo.models
 
 import uk.ac.wellcome.sierra_adapter.model.{
   SierraBibNumber,
-  SierraItemNumber,
   SierraItemRecord
 }
 
 import java.time.Instant
 
 case class SierraItemLink(
-  id: SierraItemNumber,
   bibIds: List[SierraBibNumber],
   unlinkedBibIds: List[SierraBibNumber],
   modifiedDate: Instant
@@ -18,7 +16,6 @@ case class SierraItemLink(
 case object SierraItemLink {
   def apply(record: SierraItemRecord): SierraItemLink =
     SierraItemLink(
-      id = record.id,
       bibIds = record.bibIds,
       unlinkedBibIds = record.unlinkedBibIds,
       modifiedDate = record.modifiedDate
