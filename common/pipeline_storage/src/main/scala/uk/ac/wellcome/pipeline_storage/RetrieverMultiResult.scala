4f1b8baa675f44e22088bea73e8720ff527a60e4
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2020-12-18 08:12:13 +0000

    Apply auto-formatting rules

--- common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverMultiResult.scala
+++ common/pipeline_storage/src/main/scala/uk/ac/wellcome/pipeline_storage/RetrieverMultiResult.scala
@@ -6,7 +6,8 @@ case class RetrieverMultiResult[T](
 ) {
   require(
     found.keySet.intersect(notFound.keySet).isEmpty,
-    s"The Retriever both found and did not find the same key: ${found.keySet.intersect(notFound.keySet)}. " +
+    s"The Retriever both found and did not find the same key: ${found.keySet
+      .intersect(notFound.keySet)}. " +
       "This probably indicates a programming error."
   )
 }
