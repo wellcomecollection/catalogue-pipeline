9975a168b79c171449e05d6893b844fc96681c5c
jamie <j.parkinson@wellcome.ac.uk>
2021-01-04 15:13:40 +0000

    [wip] convert colours to HSV before clustering

--- pipeline/inferrer/palette_inferrer/app/palette_encoder.py
+++ pipeline/inferrer/palette_inferrer/app/palette_encoder.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.cluster import KMeans
 from joblib import Parallel, delayed
 
+from .color import rgb_to_hsv, max_hue, max_sat, max_val
+
 
 class PaletteEncoder:
     def __init__(self, palette_weights, bin_sizes):
@@ -17,32 +19,53 @@ class PaletteEncoder:
         self.delayed_process_image = delayed(self.process_image)
 
     @staticmethod
+    def hsv_ints_to_cartesian(hsv):
+        hsv_scaled = hsv / [max_hue, max_sat, max_val]
+        h_x = hsv_scaled[..., 1] * np.cos(hsv_scaled[..., 0])
+        s_y = hsv_scaled[..., 1] * np.sin(hsv_scaled[..., 0])
+        v_z = hsv_scaled[..., 2]
+        return np.stack([h_x, s_y, v_z], axis=-1)
+
+    @staticmethod
+    def cartesian_to_hsv(cartesian):
+        h = np.arctan2(cartesian[..., 1], cartesian[..., 0])
+        s = np.linalg.norm(cartesian[..., 0:2], axis=-1)
+        v = cartesian[..., 2]
+        return np.stack(
+            [h, s, v],
+            axis=-1
+        ).astype("uint32")
+
+    @staticmethod
     def get_significant_colors(image, n):
         """
         extract n significant colours from the image pixels by taking the
         centres of n kmeans clusters of the image's pixels arranged in colour
         space. Returns colours in descending order of cluster cardinality.
         """
-        pixels = np.array(image).reshape(-1, 3)
+        rgb_image = image.convert("RGB")  # Should be RGB already but make sure
+        rgb_pixels = np.array(rgb_image).reshape(-1, 3)
 
         # Only cluster distinct colours but weight them by their frequency
         # As per https://arxiv.org/pdf/1101.0395.pdf
-        distinct_colors, distinct_color_freqs = np.unique(
-            pixels, axis=0, return_counts=True
+        distinct_rgb_colors, distinct_color_freqs = np.unique(
+            rgb_pixels, axis=0, return_counts=True
         )
 
         # Fewer distinct colours than clusters
-        if len(distinct_colors) <= n:
+        if len(distinct_rgb_colors) <= n:
             sort_indices = distinct_color_freqs.argsort()[::-1]
-            sorted_by_freq = distinct_colors[sort_indices]
-            size_diff = n - len(distinct_colors)
+            sorted_by_freq = distinct_rgb_colors[sort_indices]
+            size_diff = n - len(distinct_rgb_colors)
             # pad with the most frequently occurring distinct colour
             return np.pad(
                 sorted_by_freq, pad_width=((size_diff, 0), (0, 0)), mode="edge"
             )
 
+        distinct_hsv_colors = rgb_to_hsv(distinct_rgb_colors)
+        distinct_cartesian_colors = PaletteEncoder.hsv_ints_to_cartesian(distinct_hsv_colors)
         clusters = KMeans(n_clusters=n).fit(
-            distinct_colors, sample_weight=distinct_color_freqs
+            distinct_cartesian_colors, sample_weight=distinct_color_freqs
         )
 
         # Sort clusters by the sum of the weights they contain
@@ -59,7 +82,9 @@ class PaletteEncoder:
             np.argsort(centroid_labels)
         ]
 
-        return consistently_indexed_centroids[labels_by_weight]
+        return PaletteEncoder.cartesian_to_hsv(
+            consistently_indexed_centroids[labels_by_weight]
+        )
 
     @staticmethod
     def get_bin_index(color, d):
