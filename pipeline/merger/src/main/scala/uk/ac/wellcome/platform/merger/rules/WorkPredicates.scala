66f562f35d14ef1bd99627144782d9f43db800d5
Alex Chan <alex@alexwlchan.net>
2021-01-05 13:53:35 +0000

    Add a new predicate for audiovisual works

--- pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/rules/WorkPredicates.scala
+++ pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/rules/WorkPredicates.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.platform.merger.rules
 
 import uk.ac.wellcome.models.work.internal._
 import WorkState.Identified
+import uk.ac.wellcome.models.work.internal.Format.Audiovisual
 
 object WorkPredicates {
   type WorkPredicate = Work[Identified] => Boolean
@@ -112,6 +113,12 @@ object WorkPredicates {
 
   private def format(format: Format)(work: Work[Identified]): Boolean =
     work.data.format.contains(format)
+
+  def isAudiovisual(work: Work[Identified]): Boolean =
+    work.data.format match {
+      case Some(f) if f.isInstanceOf[Audiovisual] => true
+      case _ => false
+    }
 
   private def satisfiesAll(predicates: (Work[Identified] => Boolean)*)(
     work: Work[Identified]): Boolean = predicates.forall(_(work))
