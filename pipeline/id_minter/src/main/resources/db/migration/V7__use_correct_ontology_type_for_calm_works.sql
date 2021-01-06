0dea217074cb0df3cf7a9d323904346ae3c75ea8
Nick Ward <ward.nickjames@gmail.com>
2021-01-06 17:34:49 +0000

    Delete incorrectly generated calm record ids

--- pipeline/id_minter/src/main/resources/db/migration/V7__use_correct_ontology_type_for_calm_works.sql
+++ pipeline/id_minter/src/main/resources/db/migration/V7__use_correct_ontology_type_for_calm_works.sql
@@ -1,3 +1,4 @@
 USE ${database};
 
+DELETE FROM ${tableName} WHERE OntologyType="Work" AND SourceSystem="calm-record-id";
 UPDATE ${tableName} SET OntologyType="Work" WHERE SourceSystem="calm-record-id";
