4e79405de3533e26632f406c21b0e0d7fa81d299
Nick Ward <ward.nickjames@gmail.com>
2021-01-07 16:14:09 +0000

    Fix test for Mets deleted data

--- pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/transformer/MetsDataTest.scala
+++ pipeline/transformer/transformer_mets/src/test/scala/uk/ac/wellcome/platform/transformer/mets/transformer/MetsDataTest.scala
@@ -70,6 +70,7 @@ class MetsDataTest
     metsData.toWork(version, createdDate).right.get shouldBe Work
       .Deleted[Source](
         version = version,
+        data = WorkData[DataState.Unidentified](),
         state = Source(expectedSourceIdentifier, createdDate),
         deletedReason = Some(DeletedFromSource("Mets"))
       )
