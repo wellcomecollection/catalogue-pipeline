f1d072ee8de9e11d23fbff8cf3a7ce7c11f02c9f
Alex Chan <alex@alexwlchan.net>
2021-01-07 08:26:48 +0000

    Remove the version constraints that are deprecated in terraform 0.14

--- pipeline/terraform/terraform.tf
+++ pipeline/terraform/terraform.tf
@@ -1,15 +1,12 @@
 provider "aws" {
-  region  = "eu-west-1"
-  version = "~> 2.0"
+  region = "eu-west-1"
 
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-admin"
   }
 }
 
-provider "template" {
-  version = "~> 2.1"
-}
+provider "template" {}
 
 terraform {
   backend "s3" {
