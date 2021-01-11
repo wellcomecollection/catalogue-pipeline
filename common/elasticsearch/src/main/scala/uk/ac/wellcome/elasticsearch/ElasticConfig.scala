6871ff516429d85964a9b04a580b84419702cba7
Alex Chan <alex@alexwlchan.net>
2021-01-11 23:21:03 +0000

    Bump the name in the ElasticConfig

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
@@ -10,7 +10,7 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  val indexDate = "2021-01-07"
+  val indexDate = "2021-01-08"
 
   def apply(): ElasticConfig =
     ElasticConfig(
