adc7029ced0fc93a17c417d9b48c2eeddc77d79f
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-01-12 17:09:36 +0000

    Apply auto-formatting rules

--- pipeline/terraform/stack/service_transformer_sierra.tf
+++ pipeline/terraform/stack/service_transformer_sierra.tf
@@ -19,8 +19,8 @@ module "sierra_transformer" {
   cluster_arn  = aws_ecs_cluster.cluster.arn
 
   env_vars = {
-    transformer_queue_id   = module.sierra_transformer_queue.url
-    metrics_namespace      = "${local.namespace_hyphen}_sierra_transformer"
+    transformer_queue_id = module.sierra_transformer_queue.url
+    metrics_namespace    = "${local.namespace_hyphen}_sierra_transformer"
 
     sns_topic_arn = module.sierra_transformer_output_topic.arn
 
