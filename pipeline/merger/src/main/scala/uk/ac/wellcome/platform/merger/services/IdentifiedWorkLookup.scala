171ff1a150a6825fe3ba2096270e226fac7cb4d3
Alex Chan <alex@alexwlchan.net>
2021-01-13 09:11:18 +0000

    IdentifiedWorkLookup should handle the case where there are no versions

--- pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/IdentifiedWorkLookup.scala
+++ pipeline/merger/src/main/scala/uk/ac/wellcome/platform/merger/services/IdentifiedWorkLookup.scala
@@ -21,44 +21,33 @@ class IdentifiedWorkLookup(retriever: Retriever[Work[Identified]])(
         case WorkIdentifier(identifier, Some(_)) => identifier
       }
 
-    // A WorkIdentifier can have a version "None" if a Work in the pipeline points to
-    // it, but we haven't seen a Work with this identifier yet.
-    //
-    // e.g. Suppose a Work with identifier A says "I am matched to identifier B", but
-    // the pipeline hasn't seen a Work with identifier B yet.  Then the merger would
-    // receive something like:
-    //
-    //      WorkIdentifier("A", version = Some(1)), WorkIdentifier("B", version = None)
-    //
-    // Every message from the matcher should be triggered by a Work in the pipeline,
-    // so every set of identifiers it sends should include at least one WorkIdentifier
-    // with a non-empty version.
-    //
-    // If it doesn't, something has gone wrong and we should give up immediately.
-    assert(
-      workIds.nonEmpty,
-      s"At least one of the WorkIdentifiers should have a version ($workIdentifiers)"
-    )
-
-    retriever(workIds)
-      .map {
-        case RetrieverMultiResult(works, notFound) if notFound.isEmpty =>
-          workIdentifiers
-            .map {
-              case WorkIdentifier(_, None) => None
-              case WorkIdentifier(id, Some(version)) =>
-                val work = works(id)
-                // We only want to get the exact versions of the works specified
-                // by the matcher.
-                //
-                // e.g. if the matcher said "combine Av1 and Bv2", and we look
-                // in the retriever and find {Av2, Bv3}, we shouldn't merge
-                // these -- we should wait for the matcher to confirm we should
-                // still be merging these two works.
-                if (work.version == version) Some(work) else None
-            }
-        case RetrieverMultiResult(_, notFound) =>
-          throw new RuntimeException(s"Works not found: $notFound")
-      }
+    // If none of the work identifiers had a version, we'll discard anything
+    // we get from the retriever, so skip going out to it.
+    if (workIds.isEmpty) {
+      Future.successful(
+        workIdentifiers.map { _ => None }
+      )
+    } else {
+      retriever(workIds)
+        .map {
+          case RetrieverMultiResult(works, notFound) if notFound.isEmpty =>
+            workIdentifiers
+              .map {
+                case WorkIdentifier(_, None) => None
+                case WorkIdentifier(id, Some(version)) =>
+                  val work = works(id)
+                  // We only want to get the exact versions of the works specified
+                  // by the matcher.
+                  //
+                  // e.g. if the matcher said "combine Av1 and Bv2", and we look
+                  // in the retriever and find {Av2, Bv3}, we shouldn't merge
+                  // these -- we should wait for the matcher to confirm we should
+                  // still be merging these two works.
+                  if (work.version == version) Some(work) else None
+              }
+          case RetrieverMultiResult(_, notFound) =>
+            throw new RuntimeException(s"Works not found: $notFound")
+        }
+    }
   }
 }
