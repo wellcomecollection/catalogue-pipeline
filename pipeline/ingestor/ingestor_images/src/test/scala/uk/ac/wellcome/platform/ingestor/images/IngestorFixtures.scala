8ddd587353dd0cdb22a3cc689e3103c77d9d23ad
Nick Ward <ward.nickjames@gmail.com>
2021-01-13 10:38:39 +0000

    Image ingestor test compiling

--- pipeline/ingestor/ingestor_images/src/test/scala/uk/ac/wellcome/platform/ingestor/images/IngestorFixtures.scala
+++ pipeline/ingestor/ingestor_images/src/test/scala/uk/ac/wellcome/platform/ingestor/images/IngestorFixtures.scala
@@ -1,51 +1,46 @@
 package uk.ac.wellcome.platform.ingestor.images
 
-import io.circe.Decoder
-import org.scalatest.Suite
-import uk.ac.wellcome.akka.fixtures.Akka
-import uk.ac.wellcome.bigmessaging.fixtures.BigMessagingFixture
-import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.messaging.fixtures.SQS.Queue
-import uk.ac.wellcome.pipeline_storage.Indexer
-import uk.ac.wellcome.platform.ingestor.common.models.IngestorConfig
-import uk.ac.wellcome.pipeline_storage.fixtures.ElasticIndexerFixtures
-
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
+import org.scalatest.Suite
+
+import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.messaging.fixtures.SQS.Queue
+import uk.ac.wellcome.models.work.internal._
+import uk.ac.wellcome.pipeline_storage.fixtures.{
+  ElasticIndexerFixtures,
+  PipelineStorageStreamFixtures
+}
+import uk.ac.wellcome.pipeline_storage.{
+  Indexer,
+  PipelineStorageConfig,
+  Retriever
+}
+import ImageState.{Augmented, Indexed}
 
 trait IngestorFixtures
     extends ElasticIndexerFixtures
-    with BigMessagingFixture
-    with Akka {
+    with PipelineStorageStreamFixtures {
   this: Suite =>
 
-  def withWorkerService[T: Decoder, R](queue: Queue, indexer: Indexer[T])(
-    testWith: TestWith[ImageIngestorWorkerService[T, T], R]): R =
-    withWorkerService(queue, indexer, identity[T])(testWith)
+  def withWorkerService[R](queue: Queue,
+                           retriever: Retriever[Image[Augmented]],
+                           indexer: Indexer[Image[Indexed]])(
+    testWith: TestWith[ImageIngestorWorkerService[String], R]): R = {
+    val config = PipelineStorageConfig(
+      batchSize = 100,
+      flushInterval = 1 second,
+      parallelism = 10)
+    withPipelineStream(queue, indexer, pipelineStorageConfig = config) {
+      msgStream =>
+        val workerService = new ImageIngestorWorkerService(
+          pipelineStream = msgStream,
+          imageRetriever = retriever
+        )
 
-  def withWorkerService[In: Decoder, Out, R](queue: Queue,
-                                             indexer: Indexer[Out],
-                                             transform: In => Out)(
-    testWith: TestWith[ImageIngestorWorkerService[In, Out], R]): R =
-    withActorSystem { implicit actorSystem =>
-      {
-        withBigMessageStream[In, R](queue) { messageStream =>
-          val ingestorConfig = IngestorConfig(
-            batchSize = 100,
-            flushInterval = 1 seconds
-          )
+        workerService.run()
 
-          val workerService = new ImageIngestorWorkerService(
-            documentIndexer = indexer,
-            ingestorConfig = ingestorConfig,
-            messageStream = messageStream,
-            transformBeforeIndex = transform
-          )
-
-          workerService.run()
-
-          testWith(workerService)
-        }
-      }
+        testWith(workerService)
     }
+  }
 }
