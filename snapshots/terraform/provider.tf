9967f5186d6f81f29e0416f01ee5b32e43019320
Alex Chan <a.chan@wellcome.org>
2021-01-13 12:09:15 +0000

    Remove a provider that we're no longer using

--- snapshots/terraform/provider.tf
+++ snapshots/terraform/provider.tf
@@ -5,14 +5,3 @@ provider "aws" {
     role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
   }
 }
-
-provider "aws" {
-  alias = "platform"
-
-  region = "eu-west-1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
-  }
-}
-
