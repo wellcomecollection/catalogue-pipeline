48733cfb6ec3800942c601cc9bf0f250b9e4ef16
Alex Chan <a.chan@wellcome.org>
2021-01-14 14:45:45 +0000

    Create a model for the new MetsSourceData

--- common/source_model/src/main/scala/weco/catalogue/source_model/mets/MetsSourceData.scala
+++ common/source_model/src/main/scala/weco/catalogue/source_model/mets/MetsSourceData.scala
@@ -1,8 +1,33 @@
 package weco.catalogue.source_model.mets
 
-import uk.ac.wellcome.storage.s3.S3ObjectLocation
+import uk.ac.wellcome.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
 
 import java.time.Instant
+
+sealed trait NewMetsSourceData {
+  val createdDate: Instant
+  val version: Int
+}
+
+case class MetsFileWithImages(
+  root: S3ObjectLocationPrefix,
+  filename: String,
+  manifestations: List[String],
+  createdDate: Instant,
+  version: Int
+) extends NewMetsSourceData {
+
+  def xmlLocation: S3ObjectLocation =
+    root.asLocation(filename)
+
+  def manifestationLocations: List[S3ObjectLocation] =
+    manifestations.map { root.asLocation(_) }
+}
+
+case class DeletedMetsFile(
+  createdDate: Instant,
+  version: Int
+) extends NewMetsSourceData
 
 /** METS location data to send onwards to the transformer.
   */
