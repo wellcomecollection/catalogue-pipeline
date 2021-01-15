f6e5749980453506c1f05abddf3b8a75bdfda2bd
Alex Chan <a.chan@wellcome.org>
2021-01-15 17:02:54 +0000

    Changes to the Terraform lock file shouldn't trigger a full rebuild

--- .buildkite/scripts/run_job.py
+++ .buildkite/scripts/run_job.py
@@ -40,6 +40,9 @@ def should_run_sbt_project(repo, project_name, changed_paths):
 
         if path.startswith("api/diff_tool"):
             continue
+           
+        if os.path.basename(path) == ".terraform.lock.hcl":
+            continue
 
         if path.endswith("Makefile"):
             if os.path.dirname(project.folder) == os.path.dirname(path):
