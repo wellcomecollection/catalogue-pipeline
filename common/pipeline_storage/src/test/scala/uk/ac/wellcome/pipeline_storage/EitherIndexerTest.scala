b97e19163be5f9acb01e44e2af2eda782b8226af
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-01-18 11:12:16 +0000

    Apply auto-formatting rules

--- common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/EitherIndexerTest.scala
+++ common/pipeline_storage/src/test/scala/uk/ac/wellcome/pipeline_storage/EitherIndexerTest.scala
@@ -4,19 +4,29 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import scala.concurrent.ExecutionContext.Implicits.global
 
-import uk.ac.wellcome.models.work.generators.{WorkGenerators, ImageGenerators}
+import uk.ac.wellcome.models.work.generators.{ImageGenerators, WorkGenerators}
 import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
-import uk.ac.wellcome.elasticsearch.{MergedWorkIndexConfig, InitialImageIndexConfig}
+import uk.ac.wellcome.elasticsearch.{
+  InitialImageIndexConfig,
+  MergedWorkIndexConfig
+}
 import uk.ac.wellcome.models.work.internal._
 import uk.ac.wellcome.models.Implicits._
 import uk.ac.wellcome.elasticsearch.model.CanonicalId
 import WorkState.Merged
-import ImageState.Initial 
+import ImageState.Initial
 
-class EitherIndexerTest extends AnyFunSpec with Matchers with ElasticsearchFixtures with WorkGenerators with ImageGenerators {
+class EitherIndexerTest
+    extends AnyFunSpec
+    with Matchers
+    with ElasticsearchFixtures
+    with WorkGenerators
+    with ImageGenerators {
 
-  implicit val workId: CanonicalId[Work.Visible[Merged]] = (work: Work[Merged]) => work.id
-  implicit val imageId: CanonicalId[Image[Initial]] = (image: Image[Initial]) => image.id
+  implicit val workId: CanonicalId[Work.Visible[Merged]] =
+    (work: Work[Merged]) => work.id
+  implicit val imageId: CanonicalId[Image[Initial]] = (image: Image[Initial]) =>
+    image.id
 
   val works = (1 to 3).map(_ => mergedWork()).toList
   val images = (1 to 3).map(_ => createImageData.toInitialImage).toList
@@ -24,7 +34,7 @@ class EitherIndexerTest extends AnyFunSpec with Matchers with ElasticsearchFixtu
   val worksAndImages = works.map(Left(_)) ++ images.map(Right(_))
 
   it("indexes a list of either works or images") {
-    
+
     withLocalInitialImagesIndex { imageIndex =>
       withLocalMergedWorksIndex { workIndex =>
         val indexer = new EitherIndexer(
@@ -43,8 +53,8 @@ class EitherIndexerTest extends AnyFunSpec with Matchers with ElasticsearchFixtu
         whenReady(indexer(worksAndImages)) { result =>
           result shouldBe a[Right[_, _]]
 
-          assertElasticsearchEventuallyHas(workIndex, works:_*)
-          assertElasticsearchEventuallyHas(imageIndex, images:_*)
+          assertElasticsearchEventuallyHas(workIndex, works: _*)
+          assertElasticsearchEventuallyHas(imageIndex, images: _*)
         }
       }
     }
@@ -68,7 +78,7 @@ class EitherIndexerTest extends AnyFunSpec with Matchers with ElasticsearchFixtu
 
         whenReady(indexer(works.map(Left(_)))) { result =>
           result shouldBe a[Right[_, _]]
-          assertElasticsearchEventuallyHas(workIndex, works:_*)
+          assertElasticsearchEventuallyHas(workIndex, works: _*)
         }
       }
     }
