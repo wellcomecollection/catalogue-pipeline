351ccc7f2b3b7673cdbe6a5d3163e296eac080a9
Alex Chan <a.chan@wellcome.org>
2021-01-21 06:17:05 +0000

    Automatically store the pipeline_storage hosts in Secrets Manager

--- infrastructure/critical/elastic.tf
+++ infrastructure/critical/elastic.tf
@@ -27,6 +27,22 @@ resource "ec_deployment" "pipeline_storage" {
   }
 }
 
+locals {
+  pipeline_storage_elastic_id     = ec_deployment.pipeline_storage.elasticsearch[0].resource_id
+  pipeline_storage_elastic_region = ec_deployment.pipeline_storage.elasticsearch[0].region
+}
+
+module "pipeline_storage_secrets" {
+  source = "./modules/secrets"
+
+  key_value_map = {
+    "elasticsearch/pipeline_storage_delta/public_host" = "${local.pipeline_storage_elastic_id}.${local.pipeline_storage_elastic_region}.aws.found.io"
+
+    # See https://www.elastic.co/guide/en/cloud/current/ec-traffic-filtering-vpc.html
+    "elasticsearch/pipeline_storage_delta/private_host" = "${local.pipeline_storage_elastic_id}.vpce.${local.pipeline_storage_elastic_region}.aws.elastic-cloud.com"
+  }
+}
+
 resource "aws_security_group" "allow_catalogue_pipeline_elastic_cloud_vpce" {
   provider = aws
 
