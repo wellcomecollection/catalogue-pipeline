bff6f2aadb41007bfad9cc0e41a93bcf9c076b75
Alex Chan <a.chan@wellcome.org>
2021-01-21 14:14:15 +0000

    Bump the index for the API and snapshot generator

--- common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
+++ common/elasticsearch/src/main/scala/uk/ac/wellcome/elasticsearch/ElasticConfig.scala
@@ -10,7 +10,7 @@ case class ElasticConfig(
 object ElasticConfig {
   // We use this to share config across API applications
   // i.e. The API and the snapshot generator.
-  val indexDate = "2021-01-12"
+  val indexDate = "2021-01-21"
 
   def apply(): ElasticConfig =
     ElasticConfig(
