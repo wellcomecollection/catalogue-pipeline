4e9ee55c8bec19037a267910a4745bb97f1dbcda
Alex Chan <a.chan@wellcome.org>
2021-01-27 18:09:12 +0000

    Add a test for filtered aggregations with images

---
+++ api/api/src/test/scala/uk/ac/wellcome/platform/api/images/ImagesFilteredAggregationsTest.scala
@@ -0,0 +1,66 @@
+package uk.ac.wellcome.platform.api.images
+
+import uk.ac.wellcome.models.work.internal._
+import uk.ac.wellcome.models.Implicits._
+
+class ImagesFilteredAggregationsTest extends ApiImagesTestBase {
+  it("filters and aggregates by license") {
+    val ccByImages = (1 to 5).map { _ =>
+      createLicensedImage(License.CCBY)
+    }
+
+    val pdmImages = (1 to 2).map { _ =>
+      createLicensedImage(License.PDM)
+    }
+
+    val oglImages = (1 to 3).map { _ =>
+      createLicensedImage(License.OGL)
+    }
+
+    val images = ccByImages ++ pdmImages ++ oglImages
+
+    withImagesApi {
+      case (imagesIndex, routes) =>
+        insertImagesIntoElasticsearch(imagesIndex, images: _*)
+        val expectedImages = ccByImages ++ pdmImages
+
+        assertJsonResponse(routes, s"/$apiPrefix/images?aggregations=locations.license&locations.license=cc-by,pdm") {
+          Status.OK -> s"""
+            {
+              ${resultList(apiPrefix, totalResults = expectedImages.size)},
+              "aggregations": {
+                "type" : "Aggregations",
+                "license": {
+                  "type" : "Aggregation",
+                  "buckets": [
+                    {
+                      "data" : ${license(License.CCBY)},
+                      "count" : ${ccByImages.size},
+                      "type" : "AggregationBucket"
+                    },
+                    {
+                      "data" : ${license(License.PDM)},
+                      "count" : ${pdmImages.size},
+                      "type" : "AggregationBucket"
+                    }
+                    ,
+                    {
+                      "data" : ${license(License.OGL)},
+                      "count" : 0,
+                      "type" : "AggregationBucket"
+                    }
+                  ]
+                }
+              },
+              "results": [
+                ${expectedImages
+            .sortBy { _.state.canonicalId }
+            .map(imageResponse)
+            .mkString(",")}
+              ]
+            }
+          """
+        }
+    }
+  }
+}
