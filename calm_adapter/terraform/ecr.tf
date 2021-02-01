baa89e959cf797d05015e9d45b43df0a8b767c71
jamie <j.parkinson@wellcome.ac.uk>
2021-02-01 10:23:10 +0000

    Add ECR repo for deletion checker

--- calm_adapter/terraform/ecr.tf
+++ calm_adapter/terraform/ecr.tf
@@ -1,3 +1,7 @@
 resource "aws_ecr_repository" "ecr_repository_calm_adapter" {
   name = "uk.ac.wellcome/calm_adapter"
 }
+
+resource "aws_ecr_repository" "ecr_repository_calm_deletion_checker" {
+  name = "uk.ac.wellcome/calm_deletion_checker"
+}
