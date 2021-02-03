2f8ff5d6a2eee75ca44d35d7b89f455cfa430192
Alex Chan <a.chan@wellcome.org>
2021-02-03 09:36:11 +0000

    Make it clearer the path contains a b number

--- sierra_adapter/get_sierra_transformable_record.py
+++ sierra_adapter/get_sierra_transformable_record.py
@@ -94,7 +94,7 @@ def main(bnumber, skip_decoding):
             for item_record in transformable["itemRecords"].values():
                 item_record["data"] = json.loads(item_record["data"])
 
-        _, tmp_path = tempfile.mkstemp(suffix=".json", prefix=bnumber)
+        _, tmp_path = tempfile.mkstemp(suffix=".json", prefix=f"b{bnumber}_")
         with open(tmp_path, "w") as outfile:
             outfile.write(json.dumps(transformable, indent=2))
 
