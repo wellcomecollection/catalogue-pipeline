7defee931228744dbdccb179f34645de42f0ed71
jamie <j.parkinson@wellcome.ac.uk>
2021-02-03 11:04:58 +0000

    Add deletion flag to CalmSourcePayload

--- reindexer/reindex_worker/src/main/scala/uk/ac/wellcome/platform/reindex/reindex_worker/models/source/CalmSourceRecord.scala
+++
@@ -1,9 +0,0 @@
-package uk.ac.wellcome.platform.reindex.reindex_worker.models.source
-
-import uk.ac.wellcome.storage.s3.S3ObjectLocation
-
-case class CalmSourceRecord(
-  id: String,
-  payload: S3ObjectLocation,
-  version: Int
-)
