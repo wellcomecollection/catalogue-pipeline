66fdf19516eab48028a162cbfb404c735c444e3f
Alex Chan <a.chan@wellcome.org>
2021-02-04 08:48:48 +0000

    Group the availableOnline tests into their own describe() block

--- common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/DerivedDataTest.scala
+++ common/internal_model/src/test/scala/uk/ac/wellcome/models/work/internal/DerivedDataTest.scala
@@ -11,28 +11,28 @@ class DerivedDataTest
     with ImageGenerators {
 
   describe("DerivedWorkData") {
+    describe("availableOnline") {
+      it("is true if there's a digital location on an item") {
+        val work = denormalisedWork().items(
+          List(createDigitalItem, createIdentifiedPhysicalItem))
+        val derivedWorkData = DerivedWorkData(work.data)
 
-    it("sets availableOnline = true if there is a digital location on an item") {
-      val work = denormalisedWork().items(
-        List(createDigitalItem, createIdentifiedPhysicalItem))
-      val derivedWorkData = DerivedWorkData(work.data)
+        derivedWorkData.availableOnline shouldBe true
+      }
 
-      derivedWorkData.availableOnline shouldBe true
-    }
+      it("is false if there isn't a digital location on any items") {
+        val work = denormalisedWork().items(List(createIdentifiedPhysicalItem))
+        val derivedWorkData = DerivedWorkData(work.data)
 
-    it(
-      "sets availableOnline = false if there is no digital location on any items") {
-      val work = denormalisedWork().items(List(createIdentifiedPhysicalItem))
-      val derivedWorkData = DerivedWorkData(work.data)
+        derivedWorkData.availableOnline shouldBe false
+      }
 
-      derivedWorkData.availableOnline shouldBe false
-    }
+      it("is false if there aren't any items") {
+        val work = denormalisedWork().items(Nil)
+        val derivedWorkData = DerivedWorkData(work.data)
 
-    it("handles empty items list") {
-      val work = denormalisedWork().items(Nil)
-      val derivedWorkData = DerivedWorkData(work.data)
-
-      derivedWorkData.availableOnline shouldBe false
+        derivedWorkData.availableOnline shouldBe false
+      }
     }
 
     it("derives contributorAgents from a heterogenous list of contributors") {
