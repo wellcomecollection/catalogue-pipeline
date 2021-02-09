7a67f68b53ae9db5138eb36a173814d79fc91a43
Alex Chan <a.chan@wellcome.org>
2021-02-09 11:38:53 +0000

    Add display helpers for the new LocationType model

--- common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayLocationType.scala
+++ common/display/src/main/scala/uk/ac/wellcome/display/models/DisplayLocationType.scala
@@ -2,7 +2,7 @@ package uk.ac.wellcome.display.models
 
 import io.circe.generic.extras.JsonKey
 import io.swagger.v3.oas.annotations.media.Schema
-import uk.ac.wellcome.models.work.internal.LocationType
+import uk.ac.wellcome.models.work.internal.{LocationType, NewLocationType}
 
 @Schema(
   name = "LocationType"
@@ -19,4 +19,25 @@ object DisplayLocationType {
       id = locationType.id,
       label = locationType.label
     )
+
+  def apply(locationType: NewLocationType): DisplayLocationType =
+    locationType match {
+      case NewLocationType.ClosedStores =>
+        DisplayLocationType("closed-stores", "Closed Stores")
+
+      case NewLocationType.OpenShelves =>
+        DisplayLocationType("open-shelves", "Open Shelves")
+
+      case NewLocationType.OnExhibition =>
+        DisplayLocationType("on-exhibition", "On Exhibition")
+
+      case NewLocationType.IIIFImageAPI =>
+        DisplayLocationType("iiif-image", "IIIF Image API")
+
+      case NewLocationType.IIIFPresentationAPI =>
+        DisplayLocationType("iiif-presentation", "IIIF Presentation API")
+
+      case NewLocationType.OnlineResource =>
+        DisplayLocationType("online-resource", "Online Resource")
+    }
 }
