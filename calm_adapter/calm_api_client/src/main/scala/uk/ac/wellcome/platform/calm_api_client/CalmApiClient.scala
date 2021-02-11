8b77c3ff1d320c4aebfecb52258ec2c82937e687
jamie <j.parkinson@wellcome.ac.uk>
2021-02-11 17:54:19 +0000

    Remove confusing CalmHttpClient and keep all requesting logic in CalmApiClients

--- calm_adapter/calm_api_client/src/main/scala/uk/ac/wellcome/platform/calm_api_client/CalmApiClient.scala
+++ calm_adapter/calm_api_client/src/main/scala/uk/ac/wellcome/platform/calm_api_client/CalmApiClient.scala
@@ -1,28 +1,26 @@
 package uk.ac.wellcome.platform.calm_api_client
 
-import akka.http.scaladsl.model.{
-  ContentTypes,
-  HttpMethods,
-  HttpRequest,
-  StatusCodes
-}
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.{
   BasicHttpCredentials,
   Cookie,
   RawHeader
 }
-import akka.stream.Materializer
+import akka.stream.{Materializer, RestartSettings}
 
+import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
-class CalmApiClient(
-  url: String,
-  username: String,
-  password: String,
-)(implicit ec: ExecutionContext,
-  mat: Materializer,
-  httpClient: CalmHttpClient) {
-  import CalmHttpResponseParser._
+// CalmApiClients provide methods to access the Calm API
+// Implementors must provide a `request` method to take a
+// CalmXmlRequest and return its Response.
+trait CalmApiClient {
+  protected def request[Request <: CalmXmlRequest: CalmHttpResponseParser](
+    request: Request,
+    cookie: Option[Cookie]
+  ): Future[Request#Response]
 
   def search(query: CalmQuery,
              cookie: Option[Cookie] = None): Future[CalmSession] =
@@ -33,19 +31,59 @@ class CalmApiClient(
     implicit p: CalmHttpResponseParser[CalmSummaryRequest])
     : Future[CalmRecord] =
     request(CalmSummaryRequest(pos), cookie)
+}
 
-  private def request[Request <: CalmXmlRequest](request: Request,
-                                                 cookie: Option[Cookie])(
+// HttpClients turn HttpRequests into HttpResponses via their
+// singleRequest method
+trait HttpClient {
+  def singleRequest(request: HttpRequest)(
+    implicit actorSystem: ActorSystem): Future[HttpResponse]
+}
+
+trait AkkaHttpClient extends HttpClient {
+  def singleRequest(request: HttpRequest)(
+    implicit actorSystem: ActorSystem): Future[HttpResponse] =
+    Http().singleRequest(request)
+}
+
+class HttpCalmApiClient(
+  url: String,
+  username: String,
+  password: String,
+  minBackoff: FiniteDuration = 100 milliseconds,
+  maxBackoff: FiniteDuration = 30 seconds,
+  randomFactor: Double = 0.2,
+  maxRestarts: Int = 10
+)(implicit materializer: Materializer)
+    extends CalmApiClient { this: HttpClient =>
+
+  private implicit val actorSystem: ActorSystem = materializer.system
+  private implicit val ec: ExecutionContext = materializer.executionContext
+
+  private implicit val restartSettings: RestartSettings = RestartSettings(
+    minBackoff = minBackoff,
+    maxBackoff = maxBackoff,
+    randomFactor = randomFactor,
+  ).withMaxRestarts(maxRestarts, minBackoff)
+
+  def request[Request <: CalmXmlRequest](request: Request,
+                                         cookie: Option[Cookie])(
     implicit parse: CalmHttpResponseParser[Request]): Future[Request#Response] =
-    httpClient(createHttpRequest(request, cookie))
-      .flatMap { resp =>
-        resp.status match {
-          case StatusCodes.OK => parse(resp)
-          case status =>
-            Future.failed(
-              new Exception(s"Unexpected status from CALM API: $status"))
+    RetryFuture {
+      singleRequest(createHttpRequest(request, cookie))
+        .map { resp =>
+          resp.status match {
+            case StatusCodes.OK => resp
+            case status =>
+              throw new Exception(s"Unexpected status: $status")
+          }
         }
+    }.recover {
+        case lastException =>
+          throw new RuntimeException(
+            s"Max retries attempted when calling Calm API. Last failure was: ${lastException.getMessage}")
       }
+      .flatMap(parse.apply)
 
   private def createHttpRequest(xmlRequest: CalmXmlRequest,
                                 cookie: Option[Cookie]): HttpRequest = {
@@ -63,5 +101,23 @@ class CalmApiClient(
       case None         => request
     }
   }
-
 }
+
+class AkkaHttpCalmApiClient(
+  url: String,
+  username: String,
+  password: String,
+  minBackoff: FiniteDuration = 100 milliseconds,
+  maxBackoff: FiniteDuration = 30 seconds,
+  randomFactor: Double = 0.2,
+  maxRestarts: Int = 10
+)(implicit mat: Materializer)
+    extends HttpCalmApiClient(
+      url,
+      username,
+      password,
+      minBackoff,
+      maxBackoff,
+      randomFactor,
+      maxRestarts)
+    with AkkaHttpClient
