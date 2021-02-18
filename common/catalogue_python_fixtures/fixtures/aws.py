19a32c6a0cf99d17c5c1dca3729789a5344ca7b6
jamie <j.parkinson@wellcome.ac.uk>
2021-02-18 14:28:42 +0000

    Add common package for pytest plugin containing fixtures

---
+++ common/catalogue_python_fixtures/fixtures/aws.py
@@ -0,0 +1,65 @@
+import boto3
+import json
+from moto import mock_sns, mock_sqs
+import pytest
+from random import randint
+
+
+@pytest.fixture(scope="function")
+def mock_sns_client():
+    with mock_sns():
+        yield boto3.client("sns", region_name="eu-west-1")
+
+
+@pytest.fixture(scope="function")
+def mock_sqs_client():
+    with mock_sqs():
+        yield boto3.client("sqs", region_name="eu-west-1")
+
+
+@pytest.fixture(scope="function")
+def test_topic_arn(mock_sns_client):
+    test_topic = mock_sns_client.create_topic(
+        Name=f"test-topic-{randint(0, 999):03d}"
+    )
+    yield test_topic["TopicArn"]
+
+
+@pytest.fixture(scope="function")
+def get_test_topic_messages(test_topic_arn, mock_sqs_client, mock_sns_client):
+    test_queue = mock_sqs_client.create_queue(
+        QueueName=f"test-queue-{randint(0, 999):03d}"
+    )
+    test_queue_url = test_queue["QueueUrl"]
+    test_queue_attributes = mock_sqs_client.get_queue_attributes(
+        QueueUrl=test_queue_url,
+        AttributeNames=["QueueArn"]
+    )
+    test_queue_arn = test_queue_attributes["Attributes"]["QueueArn"]
+
+    mock_sns_client.subscribe(
+        TopicArn=test_topic_arn,
+        Protocol="sqs",
+        Endpoint=test_queue_arn
+    )
+
+    def get_messages():
+        yield from (
+            json.loads(json.loads(json.loads(m["Body"])["Message"])["default"])
+            for m in _get_queue_messages(mock_sqs_client, test_queue_url)
+        )
+
+    return get_messages
+
+
+def _get_queue_messages(sqs_client, url):
+    while True:
+        resp = sqs_client.receive_message(
+            QueueUrl=url,
+            MaxNumberOfMessages=10
+        )
+
+        try:
+            yield from resp["Messages"]
+        except KeyError:
+            return
--- snapshots/snapshot_scheduler/src/test_snapshot_scheduler.py
+++ snapshots/snapshot_scheduler/src/test_snapshot_scheduler.py
@@ -1,57 +1,26 @@
 # -*- encoding: utf-8 -*-
 
-import boto3
 import datetime
-import json
-from moto import mock_sns, mock_sqs
 import os
 from unittest.mock import patch
 
 import snapshot_scheduler
 
 
-@mock_sns
-@mock_sqs
-def test_writes_message_to_sqs():
-    sns_client = boto3.client("sns", region_name="eu-west-1")
-    sqs_client = boto3.client("sqs", region_name="eu-west-1")
-
-    test_topic = sns_client.create_topic(Name="test-topic")
-    topic_arn = test_topic["TopicArn"]
-
-    test_queue = sqs_client.create_queue(QueueName="test-queue")
-    test_queue_url = test_queue["QueueUrl"]
-    test_queue_attributes = sqs_client.get_queue_attributes(
-        QueueUrl=test_queue_url,
-        AttributeNames=["QueueArn"]
-    )
-    test_queue_arn = test_queue_attributes["Attributes"]["QueueArn"]
-    sns_client.subscribe(
-        TopicArn=topic_arn,
-        Protocol="sqs",
-        Endpoint=test_queue_arn
-    )
-
+def test_writes_message_to_sqs(test_topic_arn, mock_sns_client, get_test_topic_messages):
     public_bucket_name = "public-bukkit"
     public_object_key_v2 = "v2/works.json.gz"
 
     patched_os_environ = {
-        "TOPIC_ARN": topic_arn,
+        "TOPIC_ARN": test_topic_arn,
         "PUBLIC_BUCKET_NAME": public_bucket_name,
         "PUBLIC_OBJECT_KEY_V2": public_object_key_v2,
     }
 
     with patch.dict(os.environ, patched_os_environ, clear=True):
-        snapshot_scheduler.main(sns_client=sns_client)
+        snapshot_scheduler.main(sns_client=mock_sns_client)
 
-    test_queue_messages = sqs_client.receive_message(
-        QueueUrl=test_queue_url,
-        MaxNumberOfMessages=10
-    )
-    messages = [
-        json.loads(json.loads(json.loads(m["Body"])["Message"])["default"])
-        for m in test_queue_messages["Messages"]
-    ]
+    messages = list(get_test_topic_messages())
     assert len(messages) == 1
 
     snapshot_job = messages[0]
