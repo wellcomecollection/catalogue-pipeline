6f6c932f2946df26efd4001fa5011111cdea2ae5
jamie <j.parkinson@wellcome.ac.uk>
2021-02-18 10:03:46 +0000

    Test snapshot scheduler with tox

--- snapshots/snapshot_scheduler/src/test_snapshot_scheduler.py
+++ snapshots/snapshot_scheduler/src/test_snapshot_scheduler.py
@@ -1,14 +1,37 @@
 # -*- encoding: utf-8 -*-
 
+import boto3
 import datetime
 import json
+from moto import mock_sns, mock_sqs
 import os
-
 from unittest.mock import patch
+
 import snapshot_scheduler
 
 
-def test_writes_message_to_sqs(sns_client, topic_arn):
+@mock_sns
+@mock_sqs
+def test_writes_message_to_sqs():
+    sns_client = boto3.client("sns", region_name="eu-west-1")
+    sqs_client = boto3.client("sqs", region_name="eu-west-1")
+
+    test_topic = sns_client.create_topic(Name="test-topic")
+    topic_arn = test_topic["TopicArn"]
+
+    test_queue = sqs_client.create_queue(QueueName="test-queue")
+    test_queue_url = test_queue["QueueUrl"]
+    test_queue_attributes = sqs_client.get_queue_attributes(
+        QueueUrl=test_queue_url,
+        AttributeNames=["QueueArn"]
+    )
+    test_queue_arn = test_queue_attributes["Attributes"]["QueueArn"]
+    sns_client.subscribe(
+        TopicArn=topic_arn,
+        Protocol="sqs",
+        Endpoint=test_queue_arn
+    )
+
     public_bucket_name = "public-bukkit"
     public_object_key_v2 = "v2/works.json.gz"
 
@@ -21,10 +44,17 @@ def test_writes_message_to_sqs(sns_client, topic_arn):
     with patch.dict(os.environ, patched_os_environ, clear=True):
         snapshot_scheduler.main(sns_client=sns_client)
 
-    messages = sns_client.list_messages()
+    test_queue_messages = sqs_client.receive_message(
+        QueueUrl=test_queue_url,
+        MaxNumberOfMessages=10
+    )
+    messages = [
+        json.loads(json.loads(json.loads(m["Body"])["Message"])["default"])
+        for m in test_queue_messages["Messages"]
+    ]
     assert len(messages) == 1
 
-    snapshot_job = json.loads(messages[0][":message"])
+    snapshot_job = messages[0]
     assert snapshot_job["s3Location"] == {
         "bucket": public_bucket_name,
         "key": public_object_key_v2,
