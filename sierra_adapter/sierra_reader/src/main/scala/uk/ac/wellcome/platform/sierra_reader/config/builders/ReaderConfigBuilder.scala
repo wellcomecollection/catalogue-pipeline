eed08026d6936bf4f9b869b94a3014fd0689c1c5
Alex Chan <a.chan@wellcome.org>
2021-02-22 14:05:11 +0000

    Add holdings to the Sierra reader

--- sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/config/builders/ReaderConfigBuilder.scala
+++ sierra_adapter/sierra_reader/src/main/scala/uk/ac/wellcome/platform/sierra_reader/config/builders/ReaderConfigBuilder.scala
@@ -2,10 +2,7 @@ package uk.ac.wellcome.platform.sierra_reader.config.builders
 
 import com.typesafe.config.Config
 import uk.ac.wellcome.platform.sierra_reader.config.models.ReaderConfig
-import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes.{
-  bibs,
-  items
-}
+import uk.ac.wellcome.platform.sierra_reader.models.SierraResourceTypes._
 import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 
 object ReaderConfigBuilder {
@@ -13,6 +10,7 @@ object ReaderConfigBuilder {
     val resourceType = config.requireString("reader.resourceType") match {
       case s: String if s == bibs.toString  => bibs
       case s: String if s == items.toString => items
+      case s: String if s == holdings.toString => holdings
       case s: String =>
         throw new IllegalArgumentException(
           s"$s is not a valid Sierra resource type")
