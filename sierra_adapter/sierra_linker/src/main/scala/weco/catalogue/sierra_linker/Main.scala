f0031a9a9a6deae9b225bd4d8a25b062bee2d799
Alex Chan <a.chan@wellcome.org>
2021-02-22 14:02:58 +0000

    We can pick the resource type now!

--- sierra_adapter/sierra_linker/src/main/scala/weco/catalogue/sierra_linker/Main.scala
+++ sierra_adapter/sierra_linker/src/main/scala/weco/catalogue/sierra_linker/Main.scala
@@ -2,16 +2,19 @@ package weco.catalogue.sierra_linker
 
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
+import org.scanamo.DynamoFormat
 import org.scanamo.generic.auto._
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
-import uk.ac.wellcome.sierra_adapter.model.SierraItemNumber
+import uk.ac.wellcome.sierra_adapter.model.{SierraHoldingsNumber, SierraItemNumber, SierraRecordTypes}
 import uk.ac.wellcome.storage.store.dynamo.DynamoSingleVersionStore
 import uk.ac.wellcome.storage.typesafe.DynamoBuilder
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
+import weco.catalogue.sierra_linker.holdings.HoldingsLinkingRecordStore
 import weco.catalogue.sierra_linker.items.ItemLinkingRecordStore
 
 import scala.concurrent.ExecutionContext
@@ -23,19 +26,32 @@ object Main extends WellcomeTypesafeApp {
     implicit val executionContext: ExecutionContext =
       AkkaBuilder.buildExecutionContext()
 
+    val sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
+    val messageSender = SNSBuilder.buildSNSMessageSender(config, subject = "Sierra linker")
+
+    config.requireString("reader.resourceType") match {
+      case s: String if s == SierraRecordTypes.items.toString =>
+        val versionedStore = createVersionedStore[SierraItemNumber](config)
+        val linkStore = new ItemLinkingRecordStore(versionedStore)
+        new SierraLinkerWorkerService(sqsStream, linkStore, messageSender)
+
+      case s: String if s == SierraRecordTypes.holdings.toString =>
+        val versionedStore = createVersionedStore[SierraHoldingsNumber](config)
+        val linkStore = new HoldingsLinkingRecordStore(versionedStore)
+        new SierraLinkerWorkerService(sqsStream, linkStore, messageSender)
+
+      case s: String =>
+        throw new IllegalArgumentException(
+          s"$s is not a Sierra resource type that you can link")
+    }
+  }
+
+  def createVersionedStore[Id](config: Config)(implicit format: DynamoFormat[Id]): DynamoSingleVersionStore[Id, LinkingRecord] = {
     implicit val dynamoClient: DynamoDbClient =
       DynamoBuilder.buildDynamoClient(config)
 
-    val versionedStore =
-      new DynamoSingleVersionStore[SierraItemNumber, LinkingRecord](
-        config = DynamoBuilder.buildDynamoConfig(config)
-      )
-
-    new SierraLinkerWorkerService(
-      sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
-      linkStore = new ItemLinkingRecordStore(versionedStore),
-      messageSender = SNSBuilder
-        .buildSNSMessageSender(config, subject = "Sierra linker")
+    new DynamoSingleVersionStore[Id, LinkingRecord](
+      config = DynamoBuilder.buildDynamoConfig(config)
     )
   }
 }
