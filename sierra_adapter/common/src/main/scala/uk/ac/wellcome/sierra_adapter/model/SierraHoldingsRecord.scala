06ec1524939b2e53f4c3743e89739d37502cf1d0
Alex Chan <a.chan@wellcome.org>
2021-02-22 13:37:07 +0000

    Create a type for Sierra holdings number and records

---
+++ sierra_adapter/common/src/main/scala/uk/ac/wellcome/sierra_adapter/model/SierraHoldingsRecord.scala
@@ -0,0 +1,41 @@
+package uk.ac.wellcome.sierra_adapter.model
+
+import java.time.Instant
+
+import uk.ac.wellcome.json.JsonUtil._
+
+import scala.util.{Failure, Success}
+
+case class SierraHoldingsRecord(
+  id: SierraHoldingsNumber,
+  data: String,
+  modifiedDate: Instant,
+  bibIds: List[SierraBibNumber],
+  unlinkedBibIds: List[SierraBibNumber] = List()
+) extends AbstractSierraRecord
+
+case object SierraHoldingsRecord {
+
+  private case class SierraAPIData(bibIds: List[String])
+
+  /** This apply method is for parsing JSON bodies that come from the
+   * Sierra API.
+   */
+  def apply(id: String,
+            data: String,
+            modifiedDate: Instant): SierraHoldingsRecord = {
+    val bibIds = fromJson[SierraAPIData](data) match {
+      case Success(apiData) => apiData.bibIds
+      case Failure(e) =>
+        throw new IllegalArgumentException(
+          s"Error parsing bibIds from JSON <<$data>> ($e)")
+    }
+
+    SierraHoldingsRecord(
+      id = SierraHoldingsNumber(id),
+      data = data,
+      modifiedDate = modifiedDate,
+      bibIds = bibIds.map { SierraBibNumber }
+    )
+  }
+}
