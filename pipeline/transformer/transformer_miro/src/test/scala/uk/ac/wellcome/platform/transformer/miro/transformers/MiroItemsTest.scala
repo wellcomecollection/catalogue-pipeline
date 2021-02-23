85e122b76c835a6b1a90f11ccd122365a8e2cd1b
Robert Kenny <kenoir@users.noreply.github.com>
2021-02-23 16:08:38 +0000

    Remove .jpg from iiif digital locations in miro transformer (#1403)
    * Remove .jpg from iiif digital locations in miro transformer
    
    * update miro transformer tests
    
    * Apply auto-formatting rules
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- pipeline/transformer/transformer_miro/src/test/scala/uk/ac/wellcome/platform/transformer/miro/transformers/MiroItemsTest.scala
+++ pipeline/transformer/transformer_miro/src/test/scala/uk/ac/wellcome/platform/transformer/miro/transformers/MiroItemsTest.scala
@@ -24,8 +24,7 @@ class MiroItemsTest
         Item(
           id = IdState.Unidentifiable,
           locations = List(DigitalLocation(
-            url =
-              "https://iiif.wellcomecollection.org/image/B0011308.jpg/info.json",
+            url = "https://iiif.wellcomecollection.org/image/B0011308/info.json",
             locationType = LocationType.IIIFImageAPI,
             license = Some(License.CC0),
             credit = Some("Ezra Feilden")
