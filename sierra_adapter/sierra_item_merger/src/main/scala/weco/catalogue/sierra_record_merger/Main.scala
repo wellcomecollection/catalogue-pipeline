eb7f83af06794d48ce76fe9804a7245859828c9f
Alex Chan <a.chan@wellcome.org>
2021-02-24 11:31:28 +0000

    Finish making everything inside the merger generic

--- sierra_adapter/sierra_item_merger/src/main/scala/weco/catalogue/sierra_record_merger/Main.scala
+++ sierra_adapter/sierra_item_merger/src/main/scala/weco/catalogue/sierra_record_merger/Main.scala
@@ -1,11 +1,11 @@
-package uk.ac.wellcome.platform.sierra_item_merger
+package weco.catalogue.sierra_record_merger
 
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
-import uk.ac.wellcome.sierra_adapter.model.Implicits._
 import uk.ac.wellcome.sierra_adapter.model.SierraTransformable
+import uk.ac.wellcome.sierra_adapter.model.Implicits._
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
 import weco.catalogue.sierra_record_merger.services.{Updater, Worker}
@@ -19,13 +19,13 @@ object Main extends WellcomeTypesafeApp {
     implicit val executionContext: ExecutionContext =
       AkkaBuilder.buildExecutionContext()
 
-    val updaterService = new Updater(
+    val updater = new Updater(
       sourceVHS = SourceVHSBuilder.build[SierraTransformable](config)
     )
 
     new Worker(
       sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
-      updater = updaterService,
+      updater = updater,
       messageSender =
         SNSBuilder.buildSNSMessageSender(config, subject = "Sierra item merger")
     )
