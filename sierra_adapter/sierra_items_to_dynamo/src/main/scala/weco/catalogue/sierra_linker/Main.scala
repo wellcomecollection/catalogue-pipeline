ce651acaefa9c331b3c4c8d90a3ba558e34e7136
Alex Chan <a.chan@wellcome.org>
2021-02-24 07:33:03 +0000

    Make the items/holdings linker a single config switch

--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/weco/catalogue/sierra_linker/Main.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/weco/catalogue/sierra_linker/Main.scala
@@ -2,16 +2,27 @@ package weco.catalogue.sierra_linker
 
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
+import org.scanamo.DynamoFormat
 import org.scanamo.generic.auto._
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.typesafe.{SNSBuilder, SQSBuilder}
-import uk.ac.wellcome.sierra_adapter.model.SierraItemNumber
+import uk.ac.wellcome.sierra_adapter.model.{
+  AbstractSierraRecord,
+  SierraHoldingsNumber,
+  SierraHoldingsRecord,
+  SierraItemNumber,
+  SierraItemRecord,
+  SierraRecordTypes,
+  TypedSierraRecordNumber
+}
+import uk.ac.wellcome.sierra_adapter.model.SierraRecordTypes._
 import uk.ac.wellcome.storage.store.dynamo.DynamoSingleVersionStore
 import uk.ac.wellcome.storage.typesafe.DynamoBuilder
 import uk.ac.wellcome.typesafe.WellcomeTypesafeApp
 import uk.ac.wellcome.typesafe.config.builders.AkkaBuilder
+import uk.ac.wellcome.typesafe.config.builders.EnrichConfig._
 import weco.catalogue.sierra_linker.dynamo.Implicits._
 import weco.catalogue.sierra_linker.models.{Link, LinkOps}
 import weco.catalogue.sierra_linker.services.{LinkStore, SierraLinkerWorker}
@@ -25,21 +36,58 @@ object Main extends WellcomeTypesafeApp {
     implicit val executionContext: ExecutionContext =
       AkkaBuilder.buildExecutionContext()
 
+    val sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config)
+
+    val messageSender =
+      SNSBuilder.buildSNSMessageSender(config, subject = "Sierra linker")
+
+    val resourceType = config.requireString("linker.resourceType") match {
+      case s: String if s == bibs.toString     => bibs
+      case s: String if s == items.toString    => items
+      case s: String if s == holdings.toString => holdings
+      case s: String =>
+        throw new IllegalArgumentException(
+          s"$s is not a valid Sierra resource type")
+    }
+
+    import LinkOps._
+
+    resourceType match {
+      case SierraRecordTypes.items =>
+        new SierraLinkerWorker(
+          sqsStream = sqsStream,
+          linkStore = createLinkStore[SierraItemNumber, SierraItemRecord](config),
+          messageSender = messageSender
+        )
+
+      case SierraRecordTypes.holdings =>
+        new SierraLinkerWorker(
+          sqsStream = sqsStream,
+          linkStore = createLinkStore[SierraHoldingsNumber, SierraHoldingsRecord](config),
+          messageSender = messageSender
+        )
+
+      case other =>
+        throw new IllegalArgumentException(
+          s"$other is not a resource that can be linked"
+        )
+    }
+  }
+
+  def createLinkStore[Id <: TypedSierraRecordNumber,
+                      Record <: AbstractSierraRecord[Id]](config: Config)(
+    implicit
+    linkOps: LinkOps[Record],
+    format: DynamoFormat[Id]
+  ): LinkStore[Id, Record] = {
     implicit val dynamoClient: DynamoDbClient =
       DynamoBuilder.buildDynamoClient(config)
 
     val versionedStore =
-      new DynamoSingleVersionStore[SierraItemNumber, Link](
+      new DynamoSingleVersionStore[Id, Link](
         config = DynamoBuilder.buildDynamoConfig(config)
       )
 
-    import LinkOps._
-
-    new SierraLinkerWorker(
-      sqsStream = SQSBuilder.buildSQSStream[NotificationMessage](config),
-      linkStore = new LinkStore(versionedStore),
-      messageSender = SNSBuilder
-        .buildSNSMessageSender(config, subject = "Sierra Items to Dynamo")
-    )
+    new LinkStore[Id, Record](versionedStore)
   }
 }
--- sierra_adapter/sierra_items_to_dynamo/src/main/scala/weco/catalogue/sierra_linker/dynamo/Implicits.scala
+++ sierra_adapter/sierra_items_to_dynamo/src/main/scala/weco/catalogue/sierra_linker/dynamo/Implicits.scala
@@ -1,7 +1,7 @@
 package weco.catalogue.sierra_linker.dynamo
 
 import org.scanamo.DynamoFormat
-import uk.ac.wellcome.sierra_adapter.model.{SierraBibNumber, SierraItemNumber}
+import uk.ac.wellcome.sierra_adapter.model.{SierraBibNumber, SierraHoldingsNumber, SierraItemNumber}
 
 object Implicits {
   implicit val formatBibNumber: DynamoFormat[SierraBibNumber] =
@@ -15,6 +15,13 @@ object Implicits {
     DynamoFormat
       .coercedXmap[SierraItemNumber, String, IllegalArgumentException](
         SierraItemNumber,
+        _.withoutCheckDigit
+      )
+
+  implicit val formatHoldingsNumber: DynamoFormat[SierraHoldingsNumber] =
+    DynamoFormat
+      .coercedXmap[SierraHoldingsNumber, String, IllegalArgumentException](
+        SierraHoldingsNumber,
         _.withoutCheckDigit
       )
 }
