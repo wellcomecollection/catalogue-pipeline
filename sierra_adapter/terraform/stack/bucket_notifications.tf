d03e042da014183373c54ba7d11e39c19b7ffd49
Alex Chan <a.chan@wellcome.org>
2021-02-26 15:01:59 +0000

    Remember to notify the S3 demultiplexer when the holdings reader writes records

--- sierra_adapter/terraform/stack/bucket_notifications.tf
+++ sierra_adapter/terraform/stack/bucket_notifications.tf
@@ -14,4 +14,11 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     filter_prefix       = "records_items/"
     filter_suffix       = ".json"
   }
+
+  lambda_function {
+    lambda_function_arn = module.holdings_reader.demultiplexer_arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "records_holdings/"
+    filter_suffix       = ".json"
+  }
 }
