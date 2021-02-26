56b8087e340cb4db53c594ef4d94de6b5c29d76f
Alex Chan <a.chan@wellcome.org>
2021-02-26 13:33:55 +0000

    Add an implementation of the "Splitter"

---
+++ sierra_adapter/sierra_indexer/src/main/scala/weco/catalogue/sierra_indexer/services/Splitter.scala
@@ -0,0 +1,124 @@
+package weco.catalogue.sierra_indexer.services
+
+import com.sksamuel.elastic4s.ElasticApi.{must, rangeQuery, termQuery, termsQuery}
+import com.sksamuel.elastic4s.{Index, Indexes}
+import com.sksamuel.elastic4s.requests.delete.DeleteByQueryRequest
+import com.sksamuel.elastic4s.requests.indexes.IndexRequest
+import io.circe.parser._
+import io.circe.{Json, ParsingFailure}
+import uk.ac.wellcome.json.JsonUtil._
+import weco.catalogue.sierra_adapter.models.{SierraRecordTypes, SierraTransformable}
+import weco.catalogue.sierra_indexer.models.Parent
+
+// This object splits a SierraTransformable into indexable pieces
+// that can be sent to Elasticsearch.
+class Splitter(indexPrefix: String) {
+  import JsonOps._
+
+  private val varFieldIndex = Index(s"${indexPrefix}_varFields")
+  private val fixedFieldsIndex = Index(s"${indexPrefix}_fixedFields")
+
+  def split(t: SierraTransformable): Either[Seq[(Parent, ParsingFailure)], (Seq[IndexRequest], Seq[DeleteByQueryRequest])] = {
+    for {
+      apiData <- getSierraApiData(t)
+
+      mainRecords = List(
+        apiData.map { case (parent, json) =>
+          IndexRequest(
+            index = Index(s"${indexPrefix}_${parent.recordType}"),
+            id = Some(parent.id.withoutCheckDigit),
+            source = Some(json.remainder.noSpaces)
+          )
+        }
+      ).flatten
+
+      varFields = apiData.flatMap { case (parent, json) =>
+        json.varFields.zipWithIndex
+          .map {
+            case (varField, position) =>
+              IndexRequest(
+                index = varFieldIndex,
+                id = Some(s"${parent.id}-$position"),
+                source = Some(
+                  toJson(
+                    Map("parent" -> parent, "position" -> position, "varField" -> varField)
+                  ).get
+                )
+              )
+          }
+      }
+
+      varFieldDeletions = apiData.map { case (parent, json) =>
+        DeleteByQueryRequest(
+          indexes = Indexes(varFieldIndex.name),
+          query =
+            must(
+              termQuery("parent.id", parent.id),
+              rangeQuery("position").gte(json.varFields.length)
+            )
+        )
+      }
+
+      fixedFields = apiData.flatMap { case (parent, json) =>
+        json.fixedFields
+          .map {
+            case (code, fixedField) =>
+              IndexRequest(
+                index = fixedFieldsIndex,
+                id = Some(s"${parent.id}-$code"),
+                source = Some(
+                  toJson(
+                    Map("parent" -> parent, "code" -> code, "fixedField" -> fixedField)
+                  ).get
+                )
+              )
+          }
+      }
+
+      fixedFieldDeletions = apiData.map { case (parent, json) =>
+        DeleteByQueryRequest(
+          indexes = Indexes(varFieldIndex.name),
+          query =
+            must(termQuery("parent.id", parent.id))
+              .not(
+                termsQuery("code", json.fixedFields.keys)
+              )
+        )
+      }
+    } yield (
+      mainRecords ++ varFields ++ fixedFields,
+      varFieldDeletions ++ fixedFieldDeletions
+    )
+  }
+
+  private def getSierraApiData(t: SierraTransformable): Either[Seq[(Parent, ParsingFailure)], Seq[(Parent, Json)]] = {
+    val bibData = t.maybeBibRecord match {
+      case Some(bibRecord) =>
+        Seq(
+          Parent(SierraRecordTypes.bibs, bibRecord.id) -> parse(bibRecord.data)
+        )
+      case None => Seq()
+    }
+
+    val itemData: Seq[(Parent, Either[ParsingFailure, Json])] = t.itemRecords
+      .values
+      .map { itemRecord =>
+        Parent(SierraRecordTypes.items, itemRecord.id) -> parse(itemRecord.data)
+      }
+      .toSeq
+
+    val holdingsData: Seq[(Parent, Either[ParsingFailure, Json])] = t.holdingsRecords
+      .values
+      .map { holdingsRecord =>
+        Parent(SierraRecordTypes.holdings, holdingsRecord.id) -> parse(holdingsRecord.data)
+      }
+      .toSeq
+
+    val data = bibData ++ itemData ++ holdingsData
+
+    val successes = data.collect { case (parent, Right(json)) => (parent, json) }
+    val failures = data.collect { case (parent, Left(err)) => (parent, err) }
+
+    Either.cond(failures.isEmpty, successes, failures)
+  }
+}
