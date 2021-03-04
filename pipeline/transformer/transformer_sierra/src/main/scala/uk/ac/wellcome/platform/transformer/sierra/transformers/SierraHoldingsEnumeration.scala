ad1eecb1c66a68269cf375e80c28b4c952b6c48f
Alex Chan <a.chan@wellcome.org>
2021-03-04 13:58:43 +0000

    Implement the first example of a holdings enumeration

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsEnumeration.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsEnumeration.scala
@@ -1,7 +1,10 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
-import uk.ac.wellcome.platform.transformer.sierra.source.VarField
+import grizzled.slf4j.Logging
+import uk.ac.wellcome.platform.transformer.sierra.source.{MarcSubfield, SierraQueryOps, VarField}
 import weco.catalogue.sierra_adapter.models.TypedSierraRecordNumber
+
+import scala.util.{Failure, Success, Try}
 
 // The 85X/86X pairs are used to store structured captions -- the 85X contains
 // the labels, the 856X contains the values.
@@ -15,10 +18,98 @@ import weco.catalogue.sierra_adapter.models.TypedSierraRecordNumber
 //
 // The behaviour of this class is partly based on the published descriptions,
 // partly on the observed behaviour on the old wellcomelibrary.org website.
-object SierraHoldingsEnumeration {
+object SierraHoldingsEnumeration extends SierraQueryOps with Logging {
   val labelTag = "853"
   val valueTag = "863"
 
-  def apply(id: TypedSierraRecordNumber, varFields: List[VarField]): List[String] =
-    List()
+  def apply(id: TypedSierraRecordNumber, varFields: List[VarField]): List[String] = {
+
+    // The 85X and 86X pairs are associated based on the contents of subfield 8.
+    //
+    // The first number of subfield 8 is the link, which is a positive integer.
+    // The second number is the sequence, also an integer, preceded by a decimal point.
+    // e.g. "1.6" has link "1" and sequence "6".
+    //
+    // The 85X fields have a link value only; the 86X fields have a link and a sequence
+    // value.  Go through and extract these values.
+    val labels =
+      varFields
+        .filter { _.marcTag.contains(labelTag) }
+        .flatMap { createLabel(id, _) }
+
+    val values =
+      varFields
+        .filter { _.marcTag.contains(valueTag) }
+        .flatMap { createValue(id, _) }
+
+    // Now we turn this into a Map
+    val labelsLookup = labels
+      .map { case label @ Label(link, _) => link -> label }
+      .toMap
+    assert(labelsLookup.size == labels.size)
+
+    values
+      .flatMap { value =>
+        labelsLookup.get(value.link) match {
+          case Some(label) => Some((label, value))
+          case None =>
+            warn(s"${id.withoutCheckDigit}: an instance of $valueTag refers to a missing sequence number in $labelTag: ${value.varField}")
+            None
+        }
+      }
+      .map { case (label, value) => createString(label, value) }
+  }
+
+  private def createString(label: Label, value: Value): String =
+    value
+      .varField.subfields
+      .filterNot { _.tag == "8" }
+      .flatMap { sf =>
+        label.varField.subfieldsWithTag(sf.tag).headOption match {
+          // This isn't explicitly stated anywhere, but it seems like the presence
+          // of parens around the label means you skip the label text, and wrap
+          // the value in parens.
+          case Some(MarcSubfield(_, subfieldLabel)) if subfieldLabel.startsWith("(") =>
+            Some(s"(${sf.content})")
+
+          case Some(MarcSubfield(_, subfieldLabel)) =>
+            Some(s"$subfieldLabel${sf.content}")
+
+          case None => None
+        }
+      }
+      .mkString(" ")
+
+  private def createLabel(id: TypedSierraRecordNumber, vf: VarField): Option[Label] =
+    vf.subfieldsWithTag("8").headOption match {
+      case Some(MarcSubfield(_, content)) =>
+        Try { content.toInt } match {
+          case Success(link) => Some(Label(link, vf))
+          case Failure(_) =>
+            warn(s"${id.withoutCheckDigit}: an instance of $labelTag subfield ǂ8 has a non-numeric value: $content")
+            None
+        }
+
+      case None =>
+        warn(s"${id.withoutCheckDigit}: an instance of $labelTag is missing subfield ǂ8")
+        None
+    }
+
+  private def createValue(id: TypedSierraRecordNumber, vf: VarField): Option[Value] =
+    vf.subfieldsWithTag("8").headOption match {
+      case Some(MarcSubfield(_, content)) =>
+        Try { content.split('.').map(_.toInt).toSeq } match {
+          case Success(Seq(link, sequence)) => Some(Value(link, sequence, vf))
+          case _ =>
+            warn(s"${id.withoutCheckDigit}: an instance of $labelTag subfield ǂ8 could not be parsed as a link/sequence: $content")
+            None
+        }
+
+      case None =>
+        warn(s"${id.withoutCheckDigit}: an instance of $labelTag is missing subfield ǂ8")
+        None
+    }
+
+  private case class Label(link: Int, varField: VarField)
+  private case class Value(link: Int, sequence: Int, varField: VarField)
 }
--- pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsEnumerationTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsEnumerationTest.scala
@@ -3,7 +3,7 @@ package uk.ac.wellcome.platform.transformer.sierra.transformers
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import uk.ac.wellcome.platform.transformer.sierra.generators.MarcGenerators
-import uk.ac.wellcome.platform.transformer.sierra.source.VarField
+import uk.ac.wellcome.platform.transformer.sierra.source.{MarcSubfield, VarField}
 import weco.catalogue.sierra_adapter.generators.SierraGenerators
 
 class SierraHoldingsEnumerationTest extends AnyFunSpec with Matchers with MarcGenerators with SierraGenerators {
@@ -14,6 +14,35 @@ class SierraHoldingsEnumerationTest extends AnyFunSpec with Matchers with MarcGe
     )
 
     getEnumerations(varFields) shouldBe List()
+  }
+
+  describe("handles the examples from the Sierra documentation") {
+    // As part of adding holdings records to the Catalogue, I got a document titled
+    // "Storing holdings data in 85X/86X pairs".  It featured a couple of examples
+    // for holdings displays, which I reproduce here.
+
+    it("handles a single 853/863 pair") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "853",
+          subfields = List(
+            MarcSubfield(tag = "8", content = "10"),
+            MarcSubfield(tag = "a", content = "vol."),
+            MarcSubfield(tag = "i", content = "(year)")
+          )
+        ),
+        createVarFieldWith(
+          marcTag = "863",
+          subfields = List(
+            MarcSubfield(tag = "8", content = "10.1"),
+            MarcSubfield(tag = "a", content = "1"),
+            MarcSubfield(tag = "i", content = "1995")
+          )
+        )
+      )
+
+      getEnumerations(varFields) shouldBe List("vol.1 (1995)")
+    }
   }
 
   def getEnumerations(varFields: List[VarField]): List[String] =
