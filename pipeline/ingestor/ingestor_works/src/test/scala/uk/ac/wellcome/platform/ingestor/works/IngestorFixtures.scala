b87a5b102b210cb1a312efe0f1e29f4559569fbc
alicefuzier <alicefuzier@gmail.com>
2021-03-05 18:01:54 +0000

    Make works ingestor compile

--- pipeline/ingestor/ingestor_works/src/test/scala/uk/ac/wellcome/platform/ingestor/works/IngestorFixtures.scala
+++ pipeline/ingestor/ingestor_works/src/test/scala/uk/ac/wellcome/platform/ingestor/works/IngestorFixtures.scala
@@ -2,19 +2,16 @@ package uk.ac.wellcome.platform.ingestor.works
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import org.scalatest.Suite
-
 import uk.ac.wellcome.fixtures.TestWith
 import uk.ac.wellcome.messaging.fixtures.SQS.Queue
+import uk.ac.wellcome.models.index.IndexFixtures
 import uk.ac.wellcome.models.work.internal.WorkState.{Denormalised, Indexed}
 import uk.ac.wellcome.models.work.internal._
-import uk.ac.wellcome.pipeline_storage.fixtures.{
-  ElasticIndexerFixtures,
-  PipelineStorageStreamFixtures
-}
+import uk.ac.wellcome.pipeline_storage.fixtures.PipelineStorageStreamFixtures
 import uk.ac.wellcome.pipeline_storage.{Indexer, Retriever}
 
 trait IngestorFixtures
-    extends ElasticIndexerFixtures
+    extends IndexFixtures
     with PipelineStorageStreamFixtures {
   this: Suite =>
 
