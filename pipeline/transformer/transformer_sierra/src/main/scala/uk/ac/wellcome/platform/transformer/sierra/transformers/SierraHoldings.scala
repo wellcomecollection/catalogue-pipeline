60f0c9e616ff55210185addc42f436a4b008e528
Alex Chan <a.chan@wellcome.org>
2021-03-09 08:28:10 +0000

    Create some digital items based on electronic holdings

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldings.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldings.scala
@@ -1,12 +1,45 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
 import uk.ac.wellcome.models.work.internal.{Holdings, IdState, Item}
-import uk.ac.wellcome.platform.transformer.sierra.source.SierraHoldingsData
-import weco.catalogue.sierra_adapter.models.{SierraBibNumber, SierraHoldingsNumber}
+import uk.ac.wellcome.platform.transformer.sierra.source.{
+  FixedField,
+  SierraHoldingsData
+}
+import weco.catalogue.sierra_adapter.models.{
+  SierraBibNumber,
+  SierraHoldingsNumber
+}
 
 object SierraHoldings {
   type Output = (List[Item[IdState.Unminted]], List[Holdings])
 
-  def apply(id: SierraBibNumber, holdingsDataMap: Map[SierraHoldingsNumber, SierraHoldingsData]): Output =
-    (List(), List())
+  def apply(id: SierraBibNumber, holdingsDataMap: Map[SierraHoldingsNumber, SierraHoldingsData]): Output = {
+
+    // We start by looking at fixed field 40, which contains a Sierra location code.
+    // The value 'elro' tells us this is an online resource; if so, we create a series
+    // of digital items.  Otherwise, we create a Holdings object.
+    //
+    // Note: the value in this field is padded to 5 spaces, so the magic string is "elro ",
+    // not "elro".
+    val (electronicHoldings, _) =
+      holdingsDataMap
+        .filterNot { case (_, data) => data.deleted || data.suppressed }
+        .partition {
+          case (_, holdingsData) =>
+            holdingsData.fixedFields.get("40") match {
+              case Some(FixedField(_, value)) if value.trim == "elro" => true
+              case _ => false
+            }
+        }
+
+    val digitalItems: List[Item[IdState.Unminted]] =
+      electronicHoldings
+        .toList
+        .sortBy { case (id, _) => id.withCheckDigit }
+        .flatMap { case (id, data) =>
+          SierraElectronicResources(id, data.varFields)
+        }
+
+    (digitalItems, List())
+  }
 }
--- pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraHoldingsTest.scala
@@ -2,15 +2,274 @@ package uk.ac.wellcome.platform.transformer.sierra.transformers
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
-import uk.ac.wellcome.models.work.internal.{Holdings, IdState, Item}
-import uk.ac.wellcome.platform.transformer.sierra.source.SierraHoldingsData
+import uk.ac.wellcome.models.work.internal.IdState.Unidentifiable
+import uk.ac.wellcome.models.work.internal.LocationType.OnlineResource
+import uk.ac.wellcome.models.work.internal.{
+  AccessCondition,
+  AccessStatus,
+  DigitalLocation,
+  Holdings,
+  IdState,
+  Item
+}
+import uk.ac.wellcome.platform.transformer.sierra.generators.MarcGenerators
+import uk.ac.wellcome.platform.transformer.sierra.source.{
+  FixedField,
+  MarcSubfield,
+  SierraHoldingsData,
+  VarField
+}
 import weco.catalogue.sierra_adapter.generators.SierraGenerators
 import weco.catalogue.sierra_adapter.models.SierraHoldingsNumber
 
-class SierraHoldingsTest extends AnyFunSpec with Matchers with SierraGenerators {
+class SierraHoldingsTest extends AnyFunSpec with Matchers with MarcGenerators with SierraGenerators {
   it("an empty map becomes an empty list of items and holdings") {
     getItems(holdingsDataMap = Map.empty) shouldBe empty
     getHoldings(holdingsDataMap = Map.empty) shouldBe empty
+  }
+
+  describe("creates digital items if fixed field 40 = 'elro'") {
+    it("does not create items if there are no instances of field 856") {
+      // This example is based on b1017055 / h1068096
+      val varFields = List(
+        VarField(
+          content = Some("00000ny   22000003n 4500"),
+          fieldTag = Some("_")
+        ),
+        VarField(
+          content = Some("HighWire - Free Full Text"),
+          fieldTag = Some("p")
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe empty
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("creates a single digital item based on field 856") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://resolver.example.com/journal"),
+            MarcSubfield(tag = "z", content = "Connect to Example Journals")
+          )
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe List(
+        Item(
+          id = Unidentifiable,
+          locations = List(
+            DigitalLocation(
+              url = "https://resolver.example.com/journal",
+              locationType = OnlineResource,
+              linkText = Some("Connect to Example Journals"),
+              accessConditions = List(
+                AccessCondition(
+                  status = Some(AccessStatus.LicensedResources)
+                )
+              )
+            )
+          )
+        )
+      )
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("creates multiple items based on multiple instance of 856 on the same holdings") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://resolver.example.com/journal"),
+            MarcSubfield(tag = "z", content = "Connect to Example Journals")
+          )
+        ),
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://example.org/subscriptions")
+          )
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe List(
+        Item(
+          id = Unidentifiable,
+          locations = List(
+            DigitalLocation(
+              url = "https://resolver.example.com/journal",
+              locationType = OnlineResource,
+              linkText = Some("Connect to Example Journals"),
+              accessConditions = List(
+                AccessCondition(
+                  status = Some(AccessStatus.LicensedResources)
+                )
+              )
+            )
+          )
+        ),
+        Item(
+          id = Unidentifiable,
+          locations = List(
+            DigitalLocation(
+              url = "https://example.org/subscriptions",
+              locationType = OnlineResource,
+              accessConditions = List(
+                AccessCondition(
+                  status = Some(AccessStatus.LicensedResources)
+                )
+              )
+            )
+          )
+        )
+      )
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("creates multiple items based on multiple holdings records") {
+      val varFields1 = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://resolver.example.com/journal"),
+            MarcSubfield(tag = "z", content = "Connect to Example Journals")
+          )
+        )
+      )
+
+      val varFields2 = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://example.org/subscriptions")
+          )
+        )
+      )
+
+      val dataMap = Map("1000001" -> varFields1, "2000002" -> varFields2)
+        .map { case (id, varFields) =>
+          SierraHoldingsNumber(id) -> SierraHoldingsData(
+            fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+            varFields = varFields
+          )
+        }
+
+      getItems(dataMap) shouldBe List(
+        Item(
+          id = Unidentifiable,
+          locations = List(
+            DigitalLocation(
+              url = "https://resolver.example.com/journal",
+              locationType = OnlineResource,
+              linkText = Some("Connect to Example Journals"),
+              accessConditions = List(
+                AccessCondition(
+                  status = Some(AccessStatus.LicensedResources)
+                )
+              )
+            )
+          )
+        ),
+        Item(
+          id = Unidentifiable,
+          locations = List(
+            DigitalLocation(
+              url = "https://example.org/subscriptions",
+              locationType = OnlineResource,
+              accessConditions = List(
+                AccessCondition(
+                  status = Some(AccessStatus.LicensedResources)
+                )
+              )
+            )
+          )
+        )
+      )
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("skips field 856 if fixed field 40 is not 'elro'") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://resolver.example.com/journal"),
+            MarcSubfield(tag = "z", content = "Connect to Example Journals")
+          )
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "stax ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe empty
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("ignores electronic holdings that are deleted") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://deleted.example.org/journal")
+          )
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        deleted = true,
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe empty
+      getHoldings(dataMap) shouldBe empty
+    }
+
+    it("ignores electronic holdings that are suppressed") {
+      val varFields = List(
+        createVarFieldWith(
+          marcTag = "856",
+          subfields = List(
+            MarcSubfield(tag = "u", content = "https://suppressed.example.org/journal")
+          )
+        )
+      )
+
+      val holdingsData = SierraHoldingsData(
+        suppressed = true,
+        fixedFields = Map("40" -> FixedField(label = "LOCATION", value = "elro ")),
+        varFields = varFields
+      )
+      val dataMap = Map(createSierraHoldingsNumber -> holdingsData)
+
+      getItems(dataMap) shouldBe empty
+      getHoldings(dataMap) shouldBe empty
+    }
   }
 
   private def getItems(holdingsDataMap: Map[SierraHoldingsNumber, SierraHoldingsData]): List[Item[IdState.Unminted]] = {
