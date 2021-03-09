9cb355a2bfb663b32f2b5aba9d5e361a5175173a
alicefuzier <alicefuzier@gmail.com>
2021-03-09 11:35:53 +0000

    Fix snapshot generator

--- snapshots/snapshot_generator/src/test/scala/uk/ac/wellcome/platform/snapshot_generator/fixtures/SnapshotServiceFixture.scala
+++ snapshots/snapshot_generator/src/test/scala/uk/ac/wellcome/platform/snapshot_generator/fixtures/SnapshotServiceFixture.scala
@@ -7,14 +7,14 @@ import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import com.sksamuel.elastic4s.Index
 import com.sksamuel.elastic4s.ElasticClient
 import org.scalatest.Suite
-import uk.ac.wellcome.elasticsearch.test.fixtures.ElasticsearchFixtures
 import uk.ac.wellcome.platform.snapshot_generator.services.SnapshotService
 import uk.ac.wellcome.fixtures.TestWith
+import uk.ac.wellcome.models.index.IndexFixtures
 import uk.ac.wellcome.platform.snapshot_generator.models.SnapshotGeneratorConfig
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
-trait SnapshotServiceFixture extends ElasticsearchFixtures { this: Suite =>
+trait SnapshotServiceFixture extends IndexFixtures { this: Suite =>
 
   val mapper = new ObjectMapper with ScalaObjectMapper
 
