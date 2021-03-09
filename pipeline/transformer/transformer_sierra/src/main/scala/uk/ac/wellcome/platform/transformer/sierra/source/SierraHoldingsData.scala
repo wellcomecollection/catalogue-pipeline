87352c07d7b0d337d87e1f35265c839e2b31c6d8
Alex Chan <a.chan@wellcome.org>
2021-03-09 08:07:11 +0000

    We need to pay attention to the suppressed flag on holdings

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/source/SierraHoldingsData.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/source/SierraHoldingsData.scala
@@ -2,6 +2,7 @@ package uk.ac.wellcome.platform.transformer.sierra.source
 
 case class SierraHoldingsData(
   deleted: Boolean = false,
+  suppressed: Boolean = false,
   fixedFields: Map[String, FixedField] = Map(),
   varFields: List[VarField] = List()
 )
