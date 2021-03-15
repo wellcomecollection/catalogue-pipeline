7384cce130d0b81c619f7317c5e0453cc7388a1a
alicefuzier <alicefuzier@gmail.com>
2021-03-15 17:38:42 +0000

    Don't use dates in index names

--- pipeline/terraform/stack/locals.tf
+++ pipeline/terraform/stack/locals.tf
@@ -6,11 +6,11 @@ locals {
   es_works_merged_index       = "works-merged-${var.pipeline_date}"
   es_works_identified_index   = "works-identified-${var.pipeline_date}"
   es_works_denormalised_index = "works-denormalised-${var.pipeline_date}"
-  es_works_index              = "works-${var.pipeline_date}"
+  es_works_index              = "works"
 
   es_images_initial_index   = "images-initial-${var.pipeline_date}"
   es_images_augmented_index = "images-augmented-${var.pipeline_date}"
-  es_images_index           = "images-${var.pipeline_date}"
+  es_images_index           = "images"
 
   # The max number of connections allowed by the instance.
   # specified at /infrastructure/critical/rds_id_minter.tf
