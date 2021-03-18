19b6a327503a56f7edfd4477b28df631e43bf6fd
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-03-18 15:13:41 +0000

    Apply auto-formatting rules

--- common/internal_model/src/test/scala/weco/catalogue/internal_model/image/ParentWorkTest.scala
+++ common/internal_model/src/test/scala/weco/catalogue/internal_model/image/ParentWorkTest.scala
@@ -11,7 +11,9 @@ class ParentWorkTest extends AnyFunSpec with Matchers with ImageGenerators {
     it("removes the imageData from a merged work") {
       val w =
         mergedWork()
-          .imageData((1 to 3).map { _ => createImageData.toIdentified }.toList)
+          .imageData((1 to 3).map { _ =>
+            createImageData.toIdentified
+          }.toList)
 
       w.toParentWork.data.imageData shouldBe empty
     }
@@ -19,7 +21,9 @@ class ParentWorkTest extends AnyFunSpec with Matchers with ImageGenerators {
     it("removes the imageData from an identified work") {
       val w =
         identifiedWork()
-          .imageData((1 to 3).map { _ => createImageData.toIdentified }.toList)
+          .imageData((1 to 3).map { _ =>
+            createImageData.toIdentified
+          }.toList)
 
       w.toParentWork.data.imageData shouldBe empty
     }
