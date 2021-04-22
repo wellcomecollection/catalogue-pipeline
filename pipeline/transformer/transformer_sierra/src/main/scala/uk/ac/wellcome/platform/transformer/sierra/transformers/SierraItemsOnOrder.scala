071306171bff31002a3632200c57668dea44d3f0
Alex Chan <a.chan@wellcome.org>
2021-04-22 13:50:05 +0100

    Get the degenerate test case passing (nothing in => nothing out)

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraItemsOnOrder.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraItemsOnOrder.scala
@@ -1,16 +1,43 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
+import uk.ac.wellcome.platform.transformer.sierra.source.{
+  SierraItemData,
+  SierraOrderData
+}
 import weco.catalogue.internal_model.identifiers.DataState.Unidentified
 import weco.catalogue.internal_model.work.Item
-import weco.catalogue.source_model.sierra.SierraTransformable
+import weco.catalogue.source_model.sierra.{
+  SierraItemNumber,
+  SierraOrderNumber,
+  TypedSierraRecordNumber
+}
 
 /** This transformer creates catalogue items that correspond to items that
   * are "on order" or "awaiting cataloguing" -- which don't have their own
   * item record yet.
   *
+  *
+  * To understand how this works, it's useful to understand the ordering
+  * process:
+  *
+  *   1)  A staff member orders a book.  They create a skeleton bib record
+  *       in Sierra, which is linked to an order record with status 'o' ("on order").
+  *       At this point, there are no item records.
+  *
+  *   2)  When the book arrives, it's "received".  The RDATE on the order
+  *       record gets populated, the status is updated to 'a' ("fully paid"),
+  *       and invoice information gets attached to the order record.
+  *
+  *   3)  At some point after that, an item record is created.  This supercedes
+  *       the order record.
+  *
   */
 object SierraItemsOnOrder {
-  def apply(transformable: SierraTransformable): List[Item[Unidentified]] = {
+  def apply(
+    id: TypedSierraRecordNumber,
+    itemDataMap: Map[SierraItemNumber, SierraItemData],
+    orderDataMap: Map[SierraOrderNumber, SierraOrderData]
+  ): List[Item[Unidentified]] = {
     List()
   }
 }
--- pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraItemsOnOrderTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraItemsOnOrderTest.scala
@@ -2,14 +2,25 @@ package uk.ac.wellcome.platform.transformer.sierra.transformers
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import uk.ac.wellcome.platform.transformer.sierra.generators.SierraDataGenerators
 
-class SierraItemsOnOrderTest extends AnyFunSpec with Matchers {
+class SierraItemsOnOrderTest extends AnyFunSpec with Matchers with SierraDataGenerators {
   it("returns nothing if there are no orders or items") {
-    true shouldBe false
+    val id = createSierraBibNumber
+
+    SierraItemsOnOrder(id, itemDataMap = Map(), orderDataMap = Map()) shouldBe empty
   }
 
   describe("returns 'on order' items") {
     it("if there are orders with status 'o' and no RDATE") {
+      true shouldBe false
+    }
+
+    it("unless the order is suppressed") {
+      true shouldBe false
+    }
+
+    it("unless the order is deleted") {
       true shouldBe false
     }
 
@@ -20,6 +31,14 @@ class SierraItemsOnOrderTest extends AnyFunSpec with Matchers {
 
   describe("returns 'awaiting cataloguing' items") {
     it("if there are orders with status 'a' and an RDATE") {
+      true shouldBe false
+    }
+
+    it("unless the order is suppressed") {
+      true shouldBe false
+    }
+
+    it("unless the order is deleted") {
       true shouldBe false
     }
 
