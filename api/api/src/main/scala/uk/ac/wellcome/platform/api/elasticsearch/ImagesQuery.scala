87a7eaf5fe8b0be577deccdb96638979e76610dd
Harrison Pim <h.pim@wellcome.org>
2021-04-23 11:46:16 +0100

    Images query (#1573)
    * update the images query with features from the latest catalogue query
    
    * add a test for the images query json
    
    * Apply auto-formatting rules
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- api/api/src/main/scala/uk/ac/wellcome/platform/api/elasticsearch/ImagesQuery.scala
+++ api/api/src/main/scala/uk/ac/wellcome/platform/api/elasticsearch/ImagesQuery.scala
@@ -13,82 +13,8 @@ import com.sksamuel.elastic4s.requests.searches.queries.matches.{
   FieldWithOptionalBoost,
   MultiMatchQuery
 }
-import uk.ac.wellcome.models.index.WorksAnalysis.whitespaceAnalyzer
 
-case object ImagesMultiMatcher {
-  def apply(q: String): BoolQuery = {
-    val fields = Map(
-      Some(1000) -> Seq(
-        "data.contributors.agent.label"
-      ),
-      Some(100) -> Seq(
-        "data.title",
-        "data.title.english",
-        "data.title.shingles",
-        "data.alternativeTitles",
-      ),
-      Some(10) -> Seq(
-        "data.subjects.concepts.label",
-        "data.genres.concepts.label",
-        "data.production.*.label",
-      ),
-      None -> Seq(
-        "data.description",
-        "data.physicalDescription",
-        "data.language.label",
-        "data.edition",
-        "data.notes.content",
-        "data.lettering",
-        "data.collectionPath.path",
-        "data.collectionPath.label"
-      )
-    ) flatMap {
-      case (boost, fieldNames) =>
-        fieldNames.map { _ -> boost }
-    } flatMap {
-      case (fieldName, boost) =>
-        toWorkField(fieldName).map { workField =>
-          (workField, boost)
-        }
-    } map {
-      case (field, boost) =>
-        FieldWithOptionalBoost(field, boost.map(_.toDouble))
-    } toSeq
-
-    val sourceWorkIdFields = Seq(
-      "id.canonicalId",
-      "id.sourceIdentifier.value",
-      "data.otherIdentifiers.value"
-    )
-
-    val idFields = (Seq(
-      "state.canonicalId",
-      "state.sourceIdentifier.value",
-    ) ++ sourceWorkIdFields
-      .flatMap(toWorkField))
-      .map(fi => FieldWithOptionalBoost(fi, None))
-
-    should(
-      MultiMatchQuery(
-        text = q,
-        fields = fields,
-        `type` = Some(CROSS_FIELDS),
-        operator = Some(AND),
-      ),
-      prefixQuery("data.title.keyword", q).boost(1000),
-      MultiMatchQuery(
-        fields = idFields,
-        text = q,
-        `type` = Some(BEST_FIELDS),
-        operator = Some(OR),
-        analyzer = Some(whitespaceAnalyzer.name),
-      ).boost(1000)
-    ).minimumShouldMatch(1)
-  }
-
-  def toWorkField(field: String): Seq[String] =
-    Seq(s"source.canonicalWork.$field", s"source.redirectedWork.$field")
-}
+import uk.ac.wellcome.models.index.WorksAnalysis.{languages, whitespaceAnalyzer}
 
 case object ImageSimilarity {
   def blended: (String, Index) => Query =
@@ -111,5 +37,141 @@ case object ImageSimilarity {
         maxQueryTerms = Some(1000),
         minShouldMatch = Some("1")
       )
+  }
+}
+
+case object ImagesMultiMatcher {
+
+  def toWorkField(
+    field: String
+  ): Seq[String] =
+    Seq(s"source.canonicalWork.$field", s"source.redirectedWork.$field")
+
+  def boostedWorkFields(
+    boost: Option[Double] = None,
+    fields: Seq[String]
+  ): Seq[FieldWithOptionalBoost] =
+    fields.flatMap(toWorkField).map(FieldWithOptionalBoost(_, boost))
+
+  val titleFields = Seq(
+    "data.alternativeTitles",
+    "data.title.english",
+    "data.title.shingles",
+    "data.title"
+  )
+
+  val idFields: Seq[FieldWithOptionalBoost] = boostedWorkFields(
+    fields = Seq(
+      "id.canonicalId",
+      "id.sourceIdentifier.value",
+      "data.otherIdentifiers.value"
+    )
+  ) ++ Seq(
+    "state.canonicalId",
+    "state.sourceIdentifier.value"
+  ).map(FieldWithOptionalBoost(_, None))
+
+  val dataFields: Seq[FieldWithOptionalBoost] = boostedWorkFields(
+    boost = Some(1000.0),
+    fields = Seq("data.contributors.agent.label")
+  ) ++
+    boostedWorkFields(
+      boost = Some(10.0),
+      fields = Seq(
+        "data.subjects.concepts.label",
+        "data.genres.concepts.label",
+        "data.production.*.label"
+      )
+    ) ++
+    boostedWorkFields(
+      fields = Seq(
+        "data.description",
+        "data.physicalDescription",
+        "data.language.label",
+        "data.edition",
+        "data.collectionPath.path",
+        "data.collectionPath.label"
+      )
+    )
+
+  def apply(q: String): BoolQuery = {
+    boolQuery()
+      .should(
+        MultiMatchQuery(
+          fields = idFields,
+          queryName = Some("identifiers"),
+          text = q,
+          `type` = Some(BEST_FIELDS),
+          operator = Some(OR),
+          analyzer = Some(whitespaceAnalyzer.name)
+        ).boost(1000),
+        MultiMatchQuery(
+          q,
+          queryName = Some("data"),
+          `type` = Some(CROSS_FIELDS),
+          operator = Some(AND),
+          fields = dataFields
+        ),
+        dismax(
+          queries = Seq(
+            BoolQuery(
+              queryName = Some("redirected title prefix"),
+              boost = Some(1000.0),
+              must = List(
+                prefixQuery("source.redirectedWork.data.title.keyword", q),
+                matchPhraseQuery("source.redirectedWork.data.title", q)
+              )
+            ),
+            BoolQuery(
+              queryName = Some("canonical title prefix"),
+              boost = Some(1000.0),
+              must = List(
+                prefixQuery("source.canonicalWork.data.title.keyword", q),
+                matchPhraseQuery("source.canonicalWork.data.title", q)
+              )
+            ),
+            MultiMatchQuery(
+              q,
+              queryName = Some("title exact spellings"),
+              fields = boostedWorkFields(
+                boost = Some(100.0),
+                fields = titleFields
+              ),
+              `type` = Some(BEST_FIELDS),
+              operator = Some(AND)
+            ),
+            MultiMatchQuery(
+              q,
+              queryName = Some("title alternative spellings"),
+              fields = boostedWorkFields(
+                boost = Some(80.0),
+                fields = titleFields
+              ),
+              `type` = Some(BEST_FIELDS),
+              operator = Some(AND),
+              fuzziness = Some("AUTO")
+            ),
+            MultiMatchQuery(
+              q,
+              queryName = Some("non-english text"),
+              `type` = Some(BEST_FIELDS),
+              operator = Some(AND),
+              fields = boostedWorkFields(
+                boost = Some(100.0),
+                fields = languages
+                  .flatMap(
+                    language =>
+                      Seq(
+                        s"data.title.${language}",
+                        s"data.notes.${language}",
+                        s"data.lettering.${language}"
+                    )
+                  )
+              )
+            )
+          )
+        )
+      )
+      .minimumShouldMatch(1)
   }
 }
