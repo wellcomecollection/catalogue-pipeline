7434be111ca37ddfe3bbb430a415e8a87bb60e81
Alex Chan <a.chan@wellcome.org>
2021-05-07 10:13:37 +0100

    Suppress shelfmarks for archives and manuscripts

--- pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraShelfmark.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/uk/ac/wellcome/platform/transformer/sierra/transformers/SierraShelfmark.scala
@@ -1,23 +1,38 @@
 package uk.ac.wellcome.platform.transformer.sierra.transformers
 
 import uk.ac.wellcome.platform.transformer.sierra.source.{
+  SierraBibData,
   SierraItemData,
+  SierraMaterialType,
   SierraQueryOps
 }
+import weco.catalogue.internal_model.work.Format.ArchivesAndManuscripts
 
 object SierraShelfmark extends SierraQueryOps {
-  def apply(itemData: SierraItemData): Option[String] =
-    // We use the contents of field 949 subfield ǂa for now.  We expect we'll
-    // gradually be pickier about whether we use this value, or whether we
-    // suppress it.
-    //
-    // We used to use the callNumber field on Sierra item records, but this
-    // draws from some MARC fields that we don't want (including 999).
-    itemData.varFields
-      .filter { vf =>
-        vf.marcTag.contains("949")
-      }
-      .subfieldsWithTags("a")
-      .headOption
-      .map { _.content.trim }
+  def apply(bibData: SierraBibData, itemData: SierraItemData): Option[String] =
+    bibData.materialType match {
+      // The shelfmarks for Archives & Manuscripts are a duplicate of the
+      // reference number and the box number, the latter of which we don't
+      // want to expose publicly.
+      //
+      // e.g. PP/ABC/D.2/3:Box 5
+      //
+      case Some(SierraMaterialType(ArchivesAndManuscripts.id)) =>
+        None
+
+      case _ =>
+        // We use the contents of field 949 subfield ǂa for now.  We expect we'll
+        // gradually be pickier about whether we use this value, or whether we
+        // suppress it.
+        //
+        // We used to use the callNumber field on Sierra item records, but this
+        // draws from some MARC fields that we don't want (including 999).
+        itemData.varFields
+          .filter { vf =>
+            vf.marcTag.contains("949")
+          }
+          .subfieldsWithTags("a")
+          .headOption
+          .map { _.content.trim }
+    }
 }
