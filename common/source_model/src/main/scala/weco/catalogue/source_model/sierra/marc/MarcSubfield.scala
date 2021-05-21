63cc83b87ce8d053677ef95c66834933a74b3da6
Alex Chan <a.chan@wellcome.org>
2021-05-21 16:06:09 +0100

    Move the varfield JSON example to the right class

--- common/source_model/src/main/scala/weco/catalogue/source_model/sierra/marc/MarcSubfield.scala
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/marc/MarcSubfield.scala
@@ -1,29 +1,5 @@
 package weco.catalogue.source_model.sierra.marc
 
-// Examples of varFields from the Sierra JSON:
-//
-//    {
-//      "fieldTag": "b",
-//      "content": "X111658"
-//    }
-//
-//    {
-//      "fieldTag": "a",
-//      "marcTag": "949",
-//      "ind1": "0",
-//      "ind2": "0",
-//      "subfields": [
-//        {
-//          "tag": "1",
-//          "content": "STAX"
-//        },
-//        {
-//          "tag": "2",
-//          "content": "sepam"
-//        }
-//      ]
-//    }
-//
 case class MarcSubfield(
   tag: String,
   content: String
--- common/source_model/src/main/scala/weco/catalogue/source_model/sierra/marc/VarField.scala
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/marc/VarField.scala
@@ -2,6 +2,30 @@ package weco.catalogue.source_model.sierra.marc
 
 import io.circe.generic.extras.JsonKey
 
+// Examples of varFields from the Sierra JSON:
+//
+//    {
+//      "fieldTag": "b",
+//      "content": "X111658"
+//    }
+//
+//    {
+//      "fieldTag": "a",
+//      "marcTag": "949",
+//      "ind1": "0",
+//      "ind2": "0",
+//      "subfields": [
+//        {
+//          "tag": "1",
+//          "content": "STAX"
+//        },
+//        {
+//          "tag": "2",
+//          "content": "sepam"
+//        }
+//      ]
+//    }
+//
 case class VarField(
   content: Option[String] = None,
   marcTag: Option[String] = None,
