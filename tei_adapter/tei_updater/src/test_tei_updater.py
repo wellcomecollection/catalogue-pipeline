6a7eb3d73b2ead3a509631b542b461b2d669b1a5
Alice Fuzier Cayla <alicefuzier@gmail.com>
2021-05-24 15:59:20 +0100

    Update tei_adapter/tei_updater/src/test_tei_updater.py
    Co-authored-by: Robert Kenny <kenoir@users.noreply.github.com>

--- tei_adapter/tei_updater/src/test_tei_updater.py
+++ tei_adapter/tei_updater/src/test_tei_updater.py
@@ -37,6 +37,7 @@ def test_tree_does_not_exist(
     key = "tree.json"
     mock_s3_client = boto3.client("s3", region_name="us-east-1")
     mock_s3_client.create_bucket(Bucket=bucket)
+    
     with mock.patch.dict(
         os.environ,
         {
