4c6ee35168f25cdfd5de8ac142d3090516245c61
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-05-24 11:03:52 +0000

    Apply auto-formatting rules

--- tei_adapter/tei_updater/src/tei_updater.py
+++ tei_adapter/tei_updater/src/tei_updater.py
@@ -10,38 +10,39 @@ from deepdiff import DeepDiff
 from wellcome_aws_utils import sns_utils
 from wellcome_aws_utils.lambda_utils import log_on_error
 
+
 def get_stored_tree(s3, bucket, key):
     try:
-        content_object = s3.get_object(Bucket=bucket,Key=key)
+        content_object = s3.get_object(Bucket=bucket, Key=key)
         body = content_object["Body"].read()
         old_tree = json.loads(body)
         return old_tree
     except ClientError as ex:
-        if ex.response['Error']['Code'] == 'NoSuchKey':
+        if ex.response["Error"]["Code"] == "NoSuchKey":
             return None
         else:
             raise ex
 
-def get_new_tree(url,session=None):
+
+def get_new_tree(url, session=None):
     session = session or requests.Session()
-    response =session.get(url)
+    response = session.get(url)
     response.raise_for_status()
     new_tree = {}
-    response_tree= response.json()
-    assert response_tree['truncated'] is False
-    for entry in response_tree['tree']:
-        if entry['type'] == 'blob':
-            new_tree[entry['path']] = {
-                'sha': entry['sha'],
-                'url': entry['url']
-            }
+    response_tree = response.json()
+    assert response_tree["truncated"] is False
+    for entry in response_tree["tree"]:
+        if entry["type"] == "blob":
+            new_tree[entry["path"]] = {"sha": entry["sha"], "url": entry["url"]}
     return new_tree
 
+
 def get_path_from_diff(deep_diff_path):
-    return deep_diff_path.replace("root['","").replace("']","")
+    return deep_diff_path.replace("root['", "").replace("']", "")
+
 
 def diff_trees(old_tree, new_tree):
-    diff = DeepDiff(old_tree, new_tree, view='tree')
+    diff = DeepDiff(old_tree, new_tree, view="tree")
     values_changed = diff.pop("values_changed", [])
     items_added = diff.pop("dictionary_item_added", [])
     items_removed = diff.pop("dictionary_item_removed", [])
@@ -49,18 +50,32 @@ def diff_trees(old_tree, new_tree):
     messages = []
     # assert that the diff only contains the three keys above
     if values_changed:
-        paths_changed = {get_path_from_diff(changed.up.path()) for changed in values_changed}
-        messages += [{'path':path, 'url': new_tree[path]['url'] } for path in paths_changed]
+        paths_changed = {
+            get_path_from_diff(changed.up.path()) for changed in values_changed
+        }
+        messages += [
+            {"path": path, "url": new_tree[path]["url"]} for path in paths_changed
+        ]
     if items_added:
-        messages += [{'path':get_path_from_diff(added.path()), 'url': new_tree[get_path_from_diff(added.path())]['url'] } for added in items_added]
+        messages += [
+            {
+                "path": get_path_from_diff(added.path()),
+                "url": new_tree[get_path_from_diff(added.path())]["url"],
+            }
+            for added in items_added
+        ]
     if items_removed:
-        messages += [{'path':get_path_from_diff(removed.path()), 'deleted': True } for removed in items_removed]
+        messages += [
+            {"path": get_path_from_diff(removed.path()), "deleted": True}
+            for removed in items_removed
+        ]
     return messages
+
 
 @log_on_error
 def main(event, _ctxt=None, s3_client=None, sns_client=None, session=None):
     topic_arn = os.environ["TOPIC_ARN"]
-    bucket_name= os.environ["BUCKET_NAME"]
+    bucket_name = os.environ["BUCKET_NAME"]
     key = os.environ["TREE_FILE_KEY"]
     github_api_url = os.environ["GITHUB_API_URL"]
 
@@ -73,7 +88,9 @@ def main(event, _ctxt=None, s3_client=None, sns_client=None, session=None):
     if old_tree:
         messages = diff_trees(old_tree, new_tree)
     else:
-        messages = [{'path': path, 'url': entry['url']} for path,entry in new_tree.items()]
+        messages = [
+            {"path": path, "url": entry["url"]} for path, entry in new_tree.items()
+        ]
 
     for message in messages:
         sns_utils.publish_sns_message(
@@ -82,5 +99,5 @@ def main(event, _ctxt=None, s3_client=None, sns_client=None, session=None):
             message=message,
             subject="source: tei_tree_updater.main",
         )
-    new_tree_json = json.dumps(new_tree).encode('UTF-8')
+    new_tree_json = json.dumps(new_tree).encode("UTF-8")
     s3_client.put_object(Body=(bytes(new_tree_json)), Bucket=bucket_name, Key=key)
--- tei_adapter/tei_updater/src/test_tei_updater.py
+++ tei_adapter/tei_updater/src/test_tei_updater.py
@@ -20,6 +20,7 @@ pytest_plugins = "catalogue_aws_fixtures"
 with Betamax.configure() as config:
     config.cassette_library_dir = "."
 
+
 @pytest.fixture
 def session():
     session = requests.Session()
@@ -27,92 +28,121 @@ def session():
         vcr.use_cassette("test_tei_updater")
         yield session
 
+
 @mock_s3
-def test_tree_does_not_exist(mock_sns_client,test_topic_arn, get_test_topic_messages, session):
+def test_tree_does_not_exist(
+    mock_sns_client, test_topic_arn, get_test_topic_messages, session
+):
     bucket = "bukkit"
     key = "tree.json"
     mock_s3_client = boto3.client("s3", region_name="us-east-1")
     mock_s3_client.create_bucket(Bucket=bucket)
-    with mock.patch.dict(os.environ, {"TOPIC_ARN": test_topic_arn,
-                                      "BUCKET_NAME": bucket,
-                                      "TREE_FILE_KEY": key,
-                                      "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true"}):
+    with mock.patch.dict(
+        os.environ,
+        {
+            "TOPIC_ARN": test_topic_arn,
+            "BUCKET_NAME": bucket,
+            "TREE_FILE_KEY": key,
+            "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true",
+        },
+    ):
         main({}, s3_client=mock_s3_client, sns_client=mock_sns_client, session=session)
     messages = get_test_topic_messages()
     assert len(list(messages)) == 653
-    content_object = mock_s3_client.get_object(Bucket=bucket,Key=key)
+    content_object = mock_s3_client.get_object(Bucket=bucket, Key=key)
     body = content_object["Body"].read()
     saved_tree = json.loads(body)
     assert len(saved_tree.keys()) == 653
 
+
 @mock_s3
-def test_changes_to_old_tree_sent(mock_sns_client,test_topic_arn, get_test_topic_messages, session):
+def test_changes_to_old_tree_sent(
+    mock_sns_client, test_topic_arn, get_test_topic_messages, session
+):
     bucket = "bukkit"
     key = "tree.json"
     mock_s3_client = boto3.client("s3", region_name="us-east-1")
     mock_s3_client.create_bucket(Bucket=bucket)
 
-    with open('src/tei_tree.json','rb') as f:
+    with open("src/tei_tree.json", "rb") as f:
         mock_s3_client.put_object(Bucket=bucket, Key=key, Body=f)
-    with mock.patch.dict(os.environ, {"TOPIC_ARN": test_topic_arn,
-                                      "BUCKET_NAME": bucket,
-                                      "TREE_FILE_KEY": key,
-                                      "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true"}):
+    with mock.patch.dict(
+        os.environ,
+        {
+            "TOPIC_ARN": test_topic_arn,
+            "BUCKET_NAME": bucket,
+            "TREE_FILE_KEY": key,
+            "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true",
+        },
+    ):
         main({}, s3_client=mock_s3_client, sns_client=mock_sns_client, session=session)
     messages = get_test_topic_messages()
     assert len(list(messages)) == 2
-    content_object = mock_s3_client.get_object(Bucket=bucket,Key=key)
+    content_object = mock_s3_client.get_object(Bucket=bucket, Key=key)
     body = content_object["Body"].read()
     saved_tree = json.loads(body)
     assert len(saved_tree.keys()) == 653
 
 
 @mock.patch("tei_updater.requests.get")
-def test_truncated_tree_results_in_error(mock_get, mock_sns_client,test_topic_arn, get_test_topic_messages):
+def test_truncated_tree_results_in_error(
+    mock_get, mock_sns_client, test_topic_arn, get_test_topic_messages
+):
     mock_get.return_value.ok = True
-    mock_get.return_value.json.return_value = {"tree":[],"truncated": True}
+    mock_get.return_value.json.return_value = {"tree": [], "truncated": True}
     bucket = "bukkit"
     key = "tree.json"
     mock_s3_client = boto3.client("s3", region_name="us-east-1")
-    with mock.patch.dict(os.environ, {"TOPIC_ARN": test_topic_arn,
-                                      "BUCKET_NAME": bucket,
-                                      "TREE_FILE_KEY": key,
-                                      "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true"}):
+    with mock.patch.dict(
+        os.environ,
+        {
+            "TOPIC_ARN": test_topic_arn,
+            "BUCKET_NAME": bucket,
+            "TREE_FILE_KEY": key,
+            "GITHUB_API_URL": "https://api.github.com/repos/wellcomecollection/wellcome-collection-tei/git/trees/master?recursive=true",
+        },
+    ):
 
         with pytest.raises(Exception):
             main({}, s3_client=mock_s3_client, sns_client=mock_sns_client)
     messages = get_test_topic_messages()
     assert len(list(messages)) == 0
     with pytest.raises(ClientError) as e:
-        mock_s3_client.get_object(Bucket=bucket,Key=key)
-        assert e.response['Error']['Code'] == 'NoSuchKey'
+        mock_s3_client.get_object(Bucket=bucket, Key=key)
+        assert e.response["Error"]["Code"] == "NoSuchKey"
 
 
 def test_elements_added_changed_deleted_are_returned():
-    old_tree = {"filea": {'sha': 'ababababa', 'url': 'http://filea'},
-                "fileb": {'sha': 'bfvnwhgvdf', 'url': 'http://fileb'} ,
-                "filec":{'sha': 'bgfbhsg', 'url': 'http://filec'}}
-    new_tree = {"fileb": {'sha': 'dgfhkjgew', 'url': 'http://filebb'} ,
-                "filec":{'sha': 'bgfbhsg', 'url': 'http://filec'},
-                "filed":{'sha': 'dkgef', 'url': 'http://filed'},
-                }
+    old_tree = {
+        "filea": {"sha": "ababababa", "url": "http://filea"},
+        "fileb": {"sha": "bfvnwhgvdf", "url": "http://fileb"},
+        "filec": {"sha": "bgfbhsg", "url": "http://filec"},
+    }
+    new_tree = {
+        "fileb": {"sha": "dgfhkjgew", "url": "http://filebb"},
+        "filec": {"sha": "bgfbhsg", "url": "http://filec"},
+        "filed": {"sha": "dkgef", "url": "http://filed"},
+    }
     diffs = diff_trees(old_tree, new_tree)
     expected_diffs = [
-        {'path': "fileb", 'url':'http://filebb'},
-        {'path': "filed", 'url':'http://filed'},
-        {'path': "filea", 'deleted': True}
+        {"path": "fileb", "url": "http://filebb"},
+        {"path": "filed", "url": "http://filed"},
+        {"path": "filea", "deleted": True},
     ]
     assert diffs == expected_diffs
 
 
 def test_more_types_of_diff_is_error():
-    old_tree = {"filea": {'sha': 'ababababa', 'url': 'http://filea'},
-                "fileb": {'sha': 'bfvnwhgvdf', 'url': 'http://fileb'} ,
-                "filec":{'sha': 'bgfbhsg', 'url': 'http://filec'}}
+    old_tree = {
+        "filea": {"sha": "ababababa", "url": "http://filea"},
+        "fileb": {"sha": "bfvnwhgvdf", "url": "http://fileb"},
+        "filec": {"sha": "bgfbhsg", "url": "http://filec"},
+    }
     # filea.url has changes type so it will be returned under type_changed which we don't expect
-    new_tree =  {"filea": {'sha': 'ababababa', 'url': 2},
-                 "fileb": {'sha': 'bfvnwhgvdf', 'url': 'http://fileb'} ,
-                 "filec":{'sha': 'bgfbhsg', 'url': 'http://filec'}}
+    new_tree = {
+        "filea": {"sha": "ababababa", "url": 2},
+        "fileb": {"sha": "bfvnwhgvdf", "url": "http://fileb"},
+        "filec": {"sha": "bgfbhsg", "url": "http://filec"},
+    }
     with pytest.raises(AssertionError):
         diff_trees(old_tree, new_tree)
-
