e41d4e7c18f5da1b1853ddfb75c2b5a1a90cd736
jamie <j.parkinson@wellcome.ac.uk>
2021-05-24 12:50:38 +0100

    Allow cluster traffic via the identity VPC privatelink

--- infrastructure/critical/elastic_catalogue.tf
+++ infrastructure/critical/elastic_catalogue.tf
@@ -9,6 +9,7 @@ resource "ec_deployment" "catalogue" {
     data.terraform_remote_state.infra_critical.outputs["ec_public_internet_traffic_filter_id"],
     data.terraform_remote_state.infra_critical.outputs["ec_platform_privatelink_traffic_filter_id"],
     data.terraform_remote_state.infra_critical.outputs["ec_catalogue_privatelink_traffic_filter_id"],
+    data.terraform_remote_state.infra_critical.outputs["ec_identity_privatelink_traffic_filter_id"]
   ]
 
   elasticsearch {
