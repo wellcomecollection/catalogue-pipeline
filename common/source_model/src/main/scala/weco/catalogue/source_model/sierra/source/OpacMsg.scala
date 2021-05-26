72dd292a9f15fbcf96d59442ff9c1f74af99130f
Alex Chan <a.chan@wellcome.org>
2021-05-26 11:28:24 +0100

    Add a list of Status and OpacMsg codes
    Making these named variables should make the access condition code easier
    to follow.

---
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/source/OpacMsg.scala
@@ -0,0 +1,13 @@
+package weco.catalogue.source_model.sierra.source
+
+object OpacMsg {
+  val OnlineRequest = "f"
+  val ManualRequest = "n"
+  val OpenShelves = "o"
+  val ByAppointment = "a"
+  val AtDigitisation = "b"
+  val DonorPermission = "q"
+  val Unavailable = "u"
+  val StaffUseOnly = "s"
+  val AskAtDesk = "i"
+}
---
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/source/Status.scala
@@ -0,0 +1,11 @@
+package weco.catalogue.source_model.sierra.source
+
+object Status {
+  val Available = "-"
+  val PermissionRequired = "y"
+  val Missing = "m"
+  val Unavailable = "r"
+  val Closed = "h"
+  val Restricted = "6"
+  val OnHoldshelf = "!"
+}
