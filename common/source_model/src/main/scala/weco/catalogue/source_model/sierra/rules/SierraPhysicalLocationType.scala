f7c9b431b3ac021c321baff8a41770e768dae394
Alex Chan <a.chan@wellcome.org>
2021-05-28 10:32:32 +0100

    Don't log a "we can't map this location" for bound/contained in above

--- common/source_model/src/main/scala/weco/catalogue/source_model/sierra/rules/SierraPhysicalLocationType.scala
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/rules/SierraPhysicalLocationType.scala
@@ -53,11 +53,22 @@ object SierraPhysicalLocationType extends Logging {
         None
 
       case _ =>
-        warn(
-          s"${id.withCheckDigit}: Unable to map Sierra location name to LocationType: $name"
-        )
+        if (!knownUnknownLocations.contains(name)) {
+          warn(
+            s"${id.withCheckDigit}: Unable to map Sierra location name to LocationType: $name"
+          )
+        }
         None
     }
+
+  // This is a list of location names that we know can't be mapped to a location type,
+  // for which we don't log a warning to avoid cluttering up logs.
+  //
+  // https://en.wikipedia.org/wiki/There_are_known_knowns
+  private val knownUnknownLocations = Set(
+    "bound in above",
+    "Contained in above"
+  )
 
   implicit class StringOps(s: String) {
     def hasSubstring(substrings: String*): Boolean =
