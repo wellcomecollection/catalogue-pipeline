742c8a05bf1b9e760111501e91a338ffed0feb81
Alex Chan <a.chan@wellcome.org>
2021-06-08 09:24:56 +0100

    Publish the source_model library to S3

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,5 +1,7 @@
+import com.typesafe.sbt.GitPlugin.autoImport.git
 import sbt.Keys._
 import sbt._
+import sbtbuildinfo.BuildInfoPlugin.autoImport.{BuildInfoKey, buildInfoKeys}
 
 object Publish {
   val settings: Seq[Def.Setting[_]] = Seq(
@@ -9,5 +11,11 @@ object Publish {
     ),
     publishArtifact in Test := true,
     publishArtifact in (Compile, packageDoc) := false
+  )
+
+  val sharedLibrarySettings: Seq[Def.Setting[_]] = Seq(
+    git.baseVersion:= sys.env.getOrElse("BUILDKITE_BUILD_NUMBER","0"),
+    git.formattedShaVersion := git.gitHeadCommit.value map { sha => s"${git.baseVersion.value}.$sha" },
+    buildInfoKeys := Seq[BuildInfoKey](name, version)
   )
 }
