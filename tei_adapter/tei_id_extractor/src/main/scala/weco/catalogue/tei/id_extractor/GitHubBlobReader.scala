ae107b5216c3fef6d6c197a77cd066f6bda2eb3f
alicefuzier <alicefuzier@gmail.com>
2021-06-16 07:16:20 +0100

    Send more headers in the request

--- tei_adapter/tei_id_extractor/src/main/scala/weco/catalogue/tei/id_extractor/GitHubBlobReader.scala
+++ tei_adapter/tei_id_extractor/src/main/scala/weco/catalogue/tei/id_extractor/GitHubBlobReader.scala
@@ -3,6 +3,7 @@ package weco.catalogue.tei.id_extractor
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers._
 import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport._
 import uk.ac.wellcome.json.JsonUtil._
@@ -14,10 +15,12 @@ import scala.util.{Failure, Success, Try}
 class GitHubBlobReader(implicit ac: ActorSystem) {
   implicit val ec = ac.dispatcher
   def getBlob(uri: URI): Future[String] = {
-    val request = HttpRequest(uri=Uri(uri.toString))
+    val request = HttpRequest(uri = Uri(uri.toString), headers = List(
+      Accept(MediaType.applicationWithFixedCharset("vnd.github.v3+json", HttpCharsets.`UTF-8`)),
+      `Cache-Control`(CacheDirectives.`no-cache`), Connection("keep-alive")))
     for {
       response <- Http().singleRequest(request)
-      blob <- unmarshalAs[Blob](response, uri)
+      blob <- unmarshalAs[Blob](response, request)
       decoded <- Future.fromTry(decodeBase64(blob.content))
     } yield decoded
 
@@ -29,14 +32,19 @@ class GitHubBlobReader(implicit ac: ActorSystem) {
   new String(bytes, java.nio.charset.StandardCharsets.UTF_8)
 }
 
-  private def unmarshalAs[T](response: HttpResponse, uri: URI)(implicit um: Unmarshaller[ResponseEntity, T]): Future[T] = {
+  private def unmarshalAs[T](response: HttpResponse, request: HttpRequest)(implicit um: Unmarshaller[ResponseEntity, T]): Future[T] = {
     response match {
       case HttpResponse(StatusCodes.OK, _, entity, _) =>
-        Unmarshal(entity).to[T]
+        Unmarshal(entity).to[T].recoverWith{
+          case t: Throwable =>
+
+            Future.failed(new RuntimeException(s"Failed to marshal Github api response for url ${request.uri.toString} with headers ${response.headers.toString()}: ${entity.toString}", t)
+              )
+        }
       case HttpResponse(code, _, entity, _) =>
         Unmarshal(entity).to[String].transform{
-          case Success(responseEntity) => Failure(new RuntimeException(s"The GitHub api returned an error: ${code.value} for url ${uri.toString}: $responseEntity"))
-          case _ => Failure(new RuntimeException(s"The GitHub api returned an error: ${code.value} for url ${uri.toString}"))
+          case Success(responseEntity) => Failure(new RuntimeException(s"The GitHub api returned an error: ${code.value} for url ${request.uri.toString} with headers ${response.headers.toString()}: $responseEntity"))
+          case _ => Failure(new RuntimeException(s"The GitHub api returned an error: ${code.value} for url ${request.uri.toString} with headers ${response.headers.toString()}: ${entity.toString}"))
         }
     }
   }
