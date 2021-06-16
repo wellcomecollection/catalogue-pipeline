bd82a0488b362582bae7e58bba238f6107499744
alicefuzier <alicefuzier@gmail.com>
2021-06-16 14:26:02 +0100

    terraform

---
+++ tei_adapter/terraform/rds_id_extractor.tf
@@ -0,0 +1,81 @@
+data "aws_ssm_parameter" "rds_username" {
+  name = "/aws/reference/secretsmanager/catalogue/tei_id_extractor/rds_user"
+}
+
+data "aws_ssm_parameter" "rds_password" {
+  name = "/aws/reference/secretsmanager/catalogue/tei_id_extractor/rds_password"
+}
+
+locals {
+  rds_username = data.aws_ssm_parameter.rds_username.value
+  rds_password = data.aws_ssm_parameter.rds_password.value
+}
+
+resource "aws_db_subnet_group" "default" {
+  subnet_ids = local.private_subnets
+}
+
+resource "aws_security_group" "database_sg" {
+  description = "Allows connection to RDS instance via TCP and egress to the world"
+  vpc_id      = local.vpc_id
+  name        = "tei_id_extractor_database_sg"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 3306
+    to_port   = 3306
+
+    cidr_blocks = [
+      local.admin_cidr_ingress,
+    ]
+  }
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = [aws_security_group.rds_ingress_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "tei_id_extractor_rds_cluster" {
+  source             = "../../infrastructure/critical/modules/rds"
+  cluster_identifier = "tei-adapter-cluster-delta"
+  database_name      = "pathid"
+  username           = local.rds_username
+  password           = local.rds_password
+
+  # This instance class provides us with 45 connections per instance.
+  instance_count = 1
+  instance_class = "db.t3.medium"
+
+  db_security_group_id = aws_security_group.database_sg.id
+
+  aws_db_subnet_group_name = aws_db_subnet_group.default.name
+}
+
+resource "aws_security_group" "rds_ingress_security_group" {
+  name        = "tei_adapter_rds_ingress_security_group"
+  description = "Allow traffic to rds database"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  tags = {
+    Name = "tei-adapter-rds-access"
+  }
+}
+
--- tei_adapter/terraform/tei_id_extractor.tf
+++ tei_adapter/terraform/tei_id_extractor.tf
@@ -1,10 +1,10 @@
 module "tei_id_extractor_queue" {
   source                     = "git::github.com/wellcomecollection/terraform-aws-sqs//queue?ref=v1.1.2"
-  queue_name                 = "tei-windows"
+  queue_name                 = "tei-id-extractor"
   topic_arns                 = [module.tei_updater_lambda.topic_arn]
   aws_region                 = local.aws_region
   alarm_topic_arn            = local.dlq_alarm_arn
-  visibility_timeout_seconds = 10800
+  visibility_timeout_seconds = 60
 }
 
 module "tei_id_extractor" {
@@ -15,17 +15,29 @@ module "tei_id_extractor" {
   image = local.tei_id_extractor_image
 
   env_vars = {
-    sqs_url   = module.tei_id_extractor_queue.url
-    sns_topic = module.tei_id_extractor_topic.arn
-  }
-  secret_env_vars = {
+    metrics_namespace         = "${local.namespace}_tei_id_extractor"
+    queue_url   = module.tei_id_extractor_queue.url
+    topic_arn = module.tei_id_extractor_topic.arn
+    bucket = aws_s3_bucket.tei_adapter.id
+
+    max_connections = 10
   }
 
-  min_capacity = 0
+  secret_env_vars = {
+    db_host          = "rds/tei-adapter-cluster-delta/endpoint"
+    db_port              = "rds/tei-adapter-cluster-delta/port"
+    db_username          = "catalogue/tei_id_extractor/rds_user"
+    db_password          = "catalogue/tei_id_extractor/rds_password"
+    github_token = "catalogue/tei_id_extractor/github_token"
+  }
+
+  // The total number of connections to RDS across all tasks from all ID minter
+  // services must not exceed the maximum supported by the RDS instance.
+  min_capacity = 1
   max_capacity = 2
 
-  cpu    = 512
-  memory = 1024
+  cpu    = 1024
+  memory = 2048
 
   cluster_name             = aws_ecs_cluster.cluster.name
   cluster_arn              = aws_ecs_cluster.cluster.arn
@@ -35,6 +47,7 @@ module "tei_id_extractor" {
 
   security_group_ids = [
     aws_security_group.egress.id,
+    aws_security_group.rds_ingress_security_group.id
   ]
 
   deployment_service_env  = local.release_label
@@ -65,7 +78,17 @@ data "aws_iam_policy_document" "allow_cloudwatch_push_metrics" {
   }
 }
 
-module "adapter_scaling_alarm" {
+resource "aws_iam_role_policy" "tei_id_extractor_publish_policy" {
+  role   = module.tei_id_extractor.task_role_name
+  policy = data.aws_iam_policy_document.publish_to_tei_id_extractor_topic.json
+}
+
+resource "aws_iam_role_policy" "tei_id_extractor_put_policy" {
+  role   = module.tei_id_extractor.task_role_name
+  policy = data.aws_iam_policy_document.allow_s3_read_write.json
+}
+
+module "tei_id_extractor_scaling_alarm" {
   source     = "git::github.com/wellcomecollection/terraform-aws-sqs//autoscaling?ref=v1.1.2"
   queue_name = module.tei_id_extractor_queue.name
 
