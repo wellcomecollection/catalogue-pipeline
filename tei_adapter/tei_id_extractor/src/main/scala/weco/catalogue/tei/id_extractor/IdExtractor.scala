16a51e561df650fbee7f933c99e664bb18b7456f
alicefuzier <alicefuzier@gmail.com>
2021-06-17 14:09:57 +0100

    Comment and tidy up

--- tei_adapter/tei_id_extractor/src/main/scala/weco/catalogue/tei/id_extractor/IdExtractor.scala
+++ tei_adapter/tei_id_extractor/src/main/scala/weco/catalogue/tei/id_extractor/IdExtractor.scala
@@ -5,16 +5,17 @@ import scala.util.Try
 import scala.xml.XML
 
 object IdExtractor {
-  def extractId(blobContent: String, uri: URI): Try[String] = {
-    Try {
+  // Extracts an id from the root node of a TEI file.
+  // The root node looks like <TEI xmlns="http://www.tei-c.org/ns/1.0" xml:id="manuscript_15651">
+  // and we want to extract the value of the xml:id attribute, "manuscript_15651" in the example
+  def extractId(blobContent: String, uri: URI): Try[String] = Try {
       val xml = XML.loadString(blobContent)
       xml.attributes
         .collectFirst {
           case metadata if metadata.key == "id" => metadata.value.text
         }
         .getOrElse(throw new RuntimeException(
-          s"Could not find an id in XML at ${uri.toString}"))
+          s"Could not find an id in XML at $uri"))
     }
-  }
 
 }
