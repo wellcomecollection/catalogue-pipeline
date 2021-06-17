53ba83aa1aab7d90f8cef886a7a18b9e0474efa0
alicefuzier <alicefuzier@gmail.com>
2021-06-17 17:22:05 +0100

    test that we can read the message as a Version

--- tei_adapter/tei_adapter/src/test/scala/weco/tei/adapter/TeiAdapterWorkerServiceTest.scala
+++ tei_adapter/tei_adapter/src/test/scala/weco/tei/adapter/TeiAdapterWorkerServiceTest.scala
@@ -3,16 +3,18 @@ package weco.tei.adapter;
 import org.scalatest.concurrent.{Eventually, IntegrationPatience}
 import org.scalatest.funspec.AnyFunSpec
 import uk.ac.wellcome.akka.fixtures.Akka
+import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.fixtures.SQS
 import uk.ac.wellcome.messaging.fixtures.SQS.QueuePair
-import uk.ac.wellcome.storage.s3.S3ObjectLocation
-import weco.catalogue.tei.models.{TeiIdChangeMessage, TeiMetadata, TeiStoreRecord}
-
-import java.time.ZonedDateTime
-import uk.ac.wellcome.json.JsonUtil._
 import uk.ac.wellcome.messaging.memory.MemoryMessageSender
 import uk.ac.wellcome.messaging.sns.NotificationMessage
 import uk.ac.wellcome.messaging.sqs.SQSStream
+import uk.ac.wellcome.storage.Version
+import uk.ac.wellcome.storage.s3.S3ObjectLocation
+import weco.catalogue.source_model.TeiSourcePayload
+import weco.catalogue.source_model.tei.{TeiIdChangeMessage, TeiMetadata}
+
+import java.time.ZonedDateTime
 
 class TeiAdapterWorkerServiceTest extends AnyFunSpec with SQS with Eventually with Akka with IntegrationPatience{
     it("processes a message from the tei id extractor"){
@@ -30,7 +32,8 @@ class TeiAdapterWorkerServiceTest extends AnyFunSpec with SQS with Eventually wi
             service.run()
 
             eventually {
-              messageSender.getMessages[TeiStoreRecord] should contain only(TeiStoreRecord(message.id, TeiMetadata(false, message.s3Location, message.timeModified), 1))
+              messageSender.getMessages[Version[String, Int]]() should contain only (Version(message.id, 1))
+              messageSender.getMessages[TeiSourcePayload] should contain only (TeiSourcePayload(message.id,TeiMetadata(false, message.s3Location, message.timeModified), 1))
               assertQueueEmpty(queue)
               assertQueueEmpty(dlq)
             }
