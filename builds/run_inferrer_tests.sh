7061de14c17c3eff984d95f83e682fb91e3f68e5
Alex Chan <a.chan@wellcome.org>
2021-07-04 13:14:46 +0100

    We don't need a project directory for inferrer apps

--- builds/run_inferrer_tests.sh
+++ builds/run_inferrer_tests.sh
@@ -6,9 +6,8 @@ set -o verbose
 
 PROJECT="$1"
 
-PROJECT_DIRECTORY=$(jq -r .folder ".sbt_metadata/$PROJECT.json")
-
 ROOT=$(git rev-parse --show-toplevel)
 BUILDS_DIR="$ROOT/builds"
 
-$BUILDS_DIR/run_python_tests.sh "$PROJECT_DIRECTORY"
+$BUILDS_DIR/run_python_tests.sh pipeline/inferrer/$service_name
+$BUILDS_DIR/run_inference_manager_integration_tests.sh
