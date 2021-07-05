3ad227dd76bcf05aaed617320741a43fc2e56984
Alex Chan <a.chan@wellcome.org>
2021-07-05 17:40:56 +0100

    install boto3 before publishing Lambdas

--- .buildkite/scripts/run_lambda_job.py
+++ .buildkite/scripts/run_lambda_job.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from commands import run_build_script
+from commands import run_build_script, _subprocess_run
 from git_utils import (
     local_current_head,
     get_sha1_for_tag,
@@ -55,6 +55,7 @@ if __name__ == "__main__":
         run_build_script("run_python_tests.sh", path)
 
         if is_default_branch():
+            _subprocess_run("pip3", "install", "--user", "boto3")
             run_build_script(
                 "publish_lambda_zip.py",
                 path,
