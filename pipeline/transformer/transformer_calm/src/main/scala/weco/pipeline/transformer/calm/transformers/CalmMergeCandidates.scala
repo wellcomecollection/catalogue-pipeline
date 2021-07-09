46f509a555ea6db702c52569a33b3d80513f4d96
jamie <j.parkinson@wellcome.ac.uk>
2021-07-09 14:11:53 +0100

    Use validation in CalmMergeCandidates

--- pipeline/transformer/transformer_calm/src/main/scala/weco/pipeline/transformer/calm/transformers/CalmMergeCandidates.scala
+++ pipeline/transformer/transformer_calm/src/main/scala/weco/pipeline/transformer/calm/transformers/CalmMergeCandidates.scala
@@ -8,6 +8,7 @@ import weco.catalogue.internal_model.identifiers.{
 import weco.catalogue.internal_model.work.MergeCandidate
 import weco.catalogue.source_model.calm.CalmRecord
 import weco.pipeline.transformer.calm.models.CalmRecordOps
+import weco.pipeline.transformer.identifiers.SourceIdentifierValidation._
 
 object CalmMergeCandidates extends CalmRecordOps {
   def apply(record: CalmRecord): List[MergeCandidate[IdState.Identifiable]] =
@@ -15,30 +16,36 @@ object CalmMergeCandidates extends CalmRecordOps {
 
   private def miroMergeCandidates(record: CalmRecord) =
     // The internal field "Wheels" is mapped to "MiroID"
-    record.getList("Wheels").map { miroIdentifier =>
-      MergeCandidate(
-        id = IdState.Identifiable(
-          SourceIdentifier(
-            identifierType = IdentifierType.MiroImageNumber,
-            ontologyType = "Work",
-            value = miroIdentifier
-          )
-        ),
-        reason = Some("CALM/Miro work")
-      )
-    }
+    record
+      .getList("Wheels")
+      .flatMap { miroId =>
+        SourceIdentifier(
+          identifierType = IdentifierType.MiroImageNumber,
+          ontologyType = "Work",
+          value = miroId
+        ).validatedWithWarning
+      }
+      .map { sourceIdentifier =>
+        MergeCandidate(
+          id = IdState.Identifiable(sourceIdentifier),
+          reason = Some("CALM/Miro work")
+        )
+      }
 
   private def sierraMergeCandidate(record: CalmRecord) =
-    record.get("BNumber").map { bNumber =>
-      MergeCandidate(
-        id = IdState.Identifiable(
-          SourceIdentifier(
-            identifierType = IdentifierType.SierraSystemNumber,
-            ontologyType = "Work",
-            value = bNumber
-          )
-        ),
-        reason = Some("CALM/Sierra harvest work")
-      )
-    }
+    record
+      .get("BNumber")
+      .flatMap { id =>
+        SourceIdentifier(
+          identifierType = IdentifierType.SierraSystemNumber,
+          ontologyType = "Work",
+          value = id
+        ).validatedWithWarning
+      }
+      .map { sourceIdentifier =>
+        MergeCandidate(
+          id = IdState.Identifiable(sourceIdentifier),
+          reason = Some("CALM/Sierra harvest work")
+        )
+      }
 }
