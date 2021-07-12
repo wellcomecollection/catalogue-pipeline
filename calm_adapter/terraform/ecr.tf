90919a84503a8284110d6eb05bb52fd373d1c684
Alex Chan <a.chan@wellcome.org>
2021-07-12 12:39:25 +0100

    Add a repository for the Calm indexer

--- calm_adapter/terraform/ecr.tf
+++ calm_adapter/terraform/ecr.tf
@@ -6,7 +6,12 @@ resource "aws_ecr_repository" "ecr_repository_calm_deletion_checker" {
   name = "uk.ac.wellcome/calm_deletion_checker"
 }
 
+resource "aws_ecr_repository" "calm_indexer" {
+  name = "uk.ac.wellcome/calm_indexer"
+}
+
 locals {
   calm_adapter_image          = "${aws_ecr_repository.ecr_repository_calm_adapter.repository_url}:env.${local.release_label}"
   calm_deletion_checker_image = "${aws_ecr_repository.ecr_repository_calm_deletion_checker.repository_url}:env.${local.release_label}"
+  calm_indexer_image          = "${aws_ecr_repository.calm_indexer.repository_url}:env.${local.release_label}"
 }
