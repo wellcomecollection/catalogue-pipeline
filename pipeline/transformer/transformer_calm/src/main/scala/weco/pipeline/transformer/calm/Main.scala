b820b5509d769dfb0aefd254ef199e2b637eec9b
Alex Chan <a.chan@wellcome.org>
2021-07-14 19:16:43 +0100

    Fix the Calm transformer

--- pipeline/transformer/transformer_calm/src/main/scala/weco/pipeline/transformer/calm/Main.scala
+++ pipeline/transformer/transformer_calm/src/main/scala/weco/pipeline/transformer/calm/Main.scala
@@ -4,28 +4,23 @@ import akka.actor.ActorSystem
 import com.amazonaws.services.s3.AmazonS3
 import com.typesafe.config.Config
 import weco.catalogue.internal_model.index.WorksIndexConfig
+import weco.catalogue.internal_model.work.Work
+import weco.catalogue.internal_model.work.WorkState.Source
+import weco.catalogue.source_model.calm.CalmRecord
+import weco.elasticsearch.typesafe.ElasticBuilder
 import weco.json.JsonUtil._
 import weco.messaging.sns.NotificationMessage
 import weco.messaging.typesafe.{SNSBuilder, SQSBuilder}
-import weco.catalogue.internal_model.work.WorkState.Source
-import weco.elasticsearch.typesafe.ElasticBuilder
-import weco.pipeline_storage.typesafe.ElasticSourceRetrieverBuilder
+import weco.pipeline.transformer.calm.services.CalmTransformerWorker
+import weco.pipeline_storage.typesafe.{ElasticIndexerBuilder, ElasticSourceRetrieverBuilder, PipelineStorageStreamBuilder}
 import weco.storage.store.s3.S3TypedStore
 import weco.storage.typesafe.S3Builder
 import weco.typesafe.WellcomeTypesafeApp
-import weco.typesafe.config.builders.{AWSClientConfigBuilder, AkkaBuilder}
-import weco.catalogue.internal_model.work.Work
-import weco.catalogue.source_model.calm.CalmRecord
-import weco.pipeline.transformer.calm.services.CalmTransformerWorker
-import weco.pipeline_storage.typesafe.{
-  ElasticIndexerBuilder,
-  ElasticSourceRetrieverBuilder,
-  PipelineStorageStreamBuilder
-}
+import weco.typesafe.config.builders.AkkaBuilder
 
 import scala.concurrent.ExecutionContext
 
-object Main extends WellcomeTypesafeApp with AWSClientConfigBuilder {
+object Main extends WellcomeTypesafeApp {
   runWithConfig { config: Config =>
     implicit val actorSystem: ActorSystem =
       AkkaBuilder.buildActorSystem()
