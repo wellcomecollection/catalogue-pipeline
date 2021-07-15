7bf7c8df3fb680f0add099636dd8496a57bef66d
Alex Chan <a.chan@wellcome.org>
2021-07-15 10:44:30 +0100

    Use v1.2.1 of the SQS queue module in the Calm adapter

--- calm_adapter/terraform/locals.tf
+++ calm_adapter/terraform/locals.tf
@@ -4,7 +4,6 @@ locals {
 
   infra_bucket           = data.terraform_remote_state.shared_infra.outputs.infra_bucket
   account_id             = data.aws_caller_identity.current.account_id
-  aws_region             = "eu-west-1"
   dlq_alarm_arn          = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
   vpc_id                 = local.catalogue_vpcs["catalogue_vpc_delta_id"]
   private_subnets        = local.catalogue_vpcs["catalogue_vpc_delta_private_subnets"]
