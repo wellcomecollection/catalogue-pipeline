214324e955a81418e8ed87d487fa5e64b22ba03f
Alex Chan <a.chan@wellcome.org>
2021-07-19 11:12:32 +0100

    Add a warning for language codes that aren't three digits

--- common/internal_model/src/main/scala/weco/catalogue/internal_model/languages/Language.scala
+++ common/internal_model/src/main/scala/weco/catalogue/internal_model/languages/Language.scala
@@ -1,3 +1,15 @@
 package weco.catalogue.internal_model.languages
 
-case class Language(id: String, label: String)
+import grizzled.slf4j.Logging
+
+case class Language(id: String, label: String) extends Logging {
+
+  // We use the three-digit language codes in several of the transformers
+  // (Sierra, Calm) and consistency allows us to aggregate/filter across sources.
+  //
+  // This isn't a hard requirement (yet), but a warning for us if we're putting
+  // something obviously different in this field.
+  if (id.length != 3) {
+    warn(s"Expected a three-digit MARC language code as the ID, got $id. Is that correct?")
+  }
+}
