37fa056e9328d66c8b313b71e609b52fdea25f73
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-07-26 14:33:00 +0000

    Apply auto-formatting rules

--- pipeline/merger/src/main/scala/weco/pipeline/merger/Main.scala
+++ pipeline/merger/src/main/scala/weco/pipeline/merger/Main.scala
@@ -49,7 +49,7 @@ object Main extends WellcomeTypesafeApp {
       config.getStringOption("merger.rules.mode").getOrElse("default")
     val mergerRules = mergerMode match {
       case "tei" => TeiPlatformMerger
-      case _ => DefaultPlatformMerger
+      case _     => DefaultPlatformMerger
     }
     val mergerManager = new MergerManager(
       mergerRules = mergerRules
