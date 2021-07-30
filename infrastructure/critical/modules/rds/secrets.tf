57cacbf5171169efd66dd2d4a8fdf954b6b7931f
Alex Chan <a.chan@wellcome.org>
2021-07-30 12:56:52 +0100

    Use the shared secrets module for RDS secrets

--- infrastructure/critical/modules/rds/secrets.tf
+++ infrastructure/critical/modules/rds/secrets.tf
@@ -1,26 +1,9 @@
-resource "aws_secretsmanager_secret" "endpoint" {
-  name = "rds/${var.cluster_identifier}/endpoint"
-}
+module "secrets" {
+  source = "github.com/wellcomecollection/terraform-aws-secrets.git?ref=v1.2.0"
 
-resource "aws_secretsmanager_secret_version" "endpoint" {
-  secret_id     = aws_secretsmanager_secret.endpoint.id
-  secret_string = aws_rds_cluster.default.endpoint
-}
-
-resource "aws_secretsmanager_secret" "reader_endpoint" {
-  name = "rds/${var.cluster_identifier}/reader_endpoint"
-}
-
-resource "aws_secretsmanager_secret_version" "reader_endpoint" {
-  secret_id     = aws_secretsmanager_secret.reader_endpoint.id
-  secret_string = aws_rds_cluster.default.reader_endpoint
-}
-
-resource "aws_secretsmanager_secret" "port" {
-  name = "rds/${var.cluster_identifier}/port"
-}
-
-resource "aws_secretsmanager_secret_version" "port" {
-  secret_id     = aws_secretsmanager_secret.port.id
-  secret_string = aws_rds_cluster.default.port
+  key_value_map = {
+    "rds/${var.cluster_identifier}/endpoint"        = aws_rds_cluster.default.endpoint
+    "rds/${var.cluster_identifier}/reader_endpoint" = aws_rds_cluster.default.reader_endpoint
+    "rds/${var.cluster_identifier}/port"            = aws_rds_cluster.default.port
+  }
 }
