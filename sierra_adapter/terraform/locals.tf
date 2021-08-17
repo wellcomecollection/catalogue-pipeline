f385032d27610d2a280da3417b0e61918014a97a
Alex Chan <a.chan@wellcome.org>
2021-08-17 08:42:40 +0100

    use the new SQS DLQ alarm ARNs everywhere

--- sierra_adapter/terraform/locals.tf
+++ sierra_adapter/terraform/locals.tf
@@ -1,7 +1,7 @@
 locals {
   lambda_error_alarm_arn = data.terraform_remote_state.shared_infra.outputs.lambda_error_alarm_arn
 
-  dlq_alarm_arn = data.terraform_remote_state.shared_infra.outputs.dlq_alarm_arn
+  dlq_alarm_arn = data.terraform_remote_state.monitoring.outputs.platform_dlq_alarm_topic_arn
 
   vpc_id          = local.catalogue_vpcs["catalogue_vpc_delta_id"]
   private_subnets = local.catalogue_vpcs["catalogue_vpc_delta_private_subnets"]
