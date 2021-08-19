baca723c04e1231344cd22414930534ff2257b38
Alex Chan <a.chan@wellcome.org>
2021-08-19 12:48:33 +0100

    Can we retrieve secrets here?

--- .buildkite/scripts/deploy_latest_pipeline.py
+++ .buildkite/scripts/deploy_latest_pipeline.py
@@ -4,6 +4,8 @@ import json
 import re
 import urllib.request
 
+import boto3
+
 
 def get_internal_model_version():
     with open("project/Dependencies.scala") as infile:
@@ -14,18 +16,42 @@ def get_internal_model_version():
         return internal_model_line.split()[-1].strip('"')
 
 
-if __name__ == "__main__":
+def get_current_pipeline_date():
+    """
+    Uses the /_elasticConfig endpoint on the prod API to find out what
+    pipeline is currently feeding the API.
+
+    Returns the pipeline date, e.g. 2021-08-19
+
+    """
     resp = json.load(
         urllib.request.urlopen(
             "https://api.wellcomecollection.org/catalogue/v2/_elasticConfig"
         )
     )
 
-    works_index = resp["worksIndex"]
-    pipeline_date = re.match(
-        r"^works-indexed-(?P<date>\d{4}-\d{2}-\d{2})$", works_index
-    ).group("date")
+    # The works index name is a string that looks something like
+    #
+    #     works-indexed-2021-08-19
+    #
+    index_regex = re.compile(r"^works-indexed-(?P<date>\d{4}-\d{2}-\d{2})$")
+    return index_regex.match(resp["worksIndex"]).group("date")
+
+
+def get_secret_value(sess, *, secret_id):
+    """
+    Retrieve a secret from Secrets Manager.
+    """
+    client = sess.client("secretsmanager")
+    return client.get_secret_value(SecretId=secret_id)["SecretValue"]
+
+
+if __name__ == "__main__":
+    pipeline_date = get_current_pipeline_date()
     print(f"The current prod pipeline is {pipeline_date}")
 
     internal_model_version = get_internal_model_version()
     print(f"The current version of internal model is {internal_model_version}")
+
+    sess = boto3.Session()
+    print(get_secret_value(sess, secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/read_only/es_username"))
