0a8c705a633b4ba78eb86e27ee00a30d68ebab91
Alex Chan <a.chan@wellcome.org>
2021-08-31 09:26:14 +0100

    log a bit more info

--- scripts/get_invisibility_reason_report.py
+++ scripts/get_invisibility_reason_report.py
@@ -21,30 +21,30 @@ if __name__ == "__main__":
 
     pipeline_date = index_config["worksIndex"].replace("works-indexed-", "")
 
-    # username = get_secret_string(
-    #     sess,
-    #     secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/read_only/es_username",
-    # )
-    # password = get_secret_string(
-    #     sess,
-    #     secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/read_only/es_password",
-    # )
-    # host = get_secret_string(
-    #     sess, secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/public_host"
-    # )
-    #
-    # es = Elasticsearch(f"https://{host}:9243", http_auth=(username, password))
-    #
-    # scanner = scan(
-    #     es,
-    #     index=index_config["worksIndex"],
-    #     query={"query": {"bool": {"filter": [{"term": {"type": "Invisible"}}]}}, "_source": ["state", "invisibilityReasons"]},
-    #     scroll="1m"
-    # )
-    #
-    # with open(f'invisible_works_{pipeline_date}.json', 'w') as outfile:
-    #     for item in tqdm.tqdm(scanner):
-    #         outfile.write(json.dumps(item) + "\n")
+    username = get_secret_string(
+        sess,
+        secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/read_only/es_username",
+    )
+    password = get_secret_string(
+        sess,
+        secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/read_only/es_password",
+    )
+    host = get_secret_string(
+        sess, secret_id=f"elasticsearch/pipeline_storage_{pipeline_date}/public_host"
+    )
+
+    es = Elasticsearch(f"https://{host}:9243", http_auth=(username, password))
+
+    scanner = scan(
+        es,
+        index=index_config["worksIndex"],
+        query={"query": {"bool": {"filter": [{"term": {"type": "Invisible"}}]}}, "_source": ["state", "invisibilityReasons"]},
+        scroll="1m"
+    )
+
+    with open(f'invisible_works_{pipeline_date}.json', 'w') as outfile:
+        for item in tqdm.tqdm(scanner):
+            outfile.write(json.dumps(item) + "\n")
 
     invisibility_reasons = collections.defaultdict(list)
 
@@ -52,7 +52,7 @@ if __name__ == "__main__":
         work = json.loads(line)
 
         for reason in work["_source"]["invisibilityReasons"]:
-            invisibility_reasons[(reason["type"], reason.get("message"))].append(
+            invisibility_reasons[(reason["type"], reason.get("message", reason.get("info")))].append(
                 work["_source"]
             )
 
@@ -70,9 +70,8 @@ if __name__ == "__main__":
             outfile.write("<ul>\n")
             for w in works:
                 outfile.write(
-                    f"  <li>{w['state']['sourceIdentifier']['identifierType']}/{w['state']['sourceIdentifier']['value']}"
+                    f"  <li>{w['state']['sourceIdentifier']['identifierType']['id']}/{w['state']['sourceIdentifier']['value']}"
                 )
             outfile.write("</ul>\n\n")
 
-    # with open("invisibility_reasons.json", "w") as of:
-    #     of.write(json.dumps(list(invisibility_reasons.items()), indent=2, sort_keys=True))
+    print(f"invisibility_reasons_{pipeline_date}.html")
