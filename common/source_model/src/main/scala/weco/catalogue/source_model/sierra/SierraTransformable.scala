db9c935f8aa2a8728f6d6fd5d887a3ac693b11eb
Alex Chan <a.chan@wellcome.org>
2021-09-06 15:24:04 +0100

    Remove some unnecessary helper methods

--- common/source_model/src/main/scala/weco/catalogue/source_model/sierra/SierraTransformable.scala
+++ common/source_model/src/main/scala/weco/catalogue/source_model/sierra/SierraTransformable.scala
@@ -40,32 +40,4 @@ object SierraTransformable {
     SierraTransformable(
       sierraId = bibRecord.id,
       maybeBibRecord = Some(bibRecord))
-
-  def createNew(
-    sierraId: SierraBibNumber,
-    itemRecords: Seq[SierraItemRecord] = List(),
-    holdingsRecords: Seq[SierraHoldingsRecord] = List(),
-    orderRecords: Seq[SierraOrderRecord] = List()
-  ): SierraTransformable =
-    SierraTransformable(
-      sierraId = sierraId,
-      maybeBibRecord = None,
-      itemRecords = itemRecords.map { record => record.id -> record }.toMap,
-      holdingsRecords = holdingsRecords.map { record => record.id -> record }.toMap,
-      orderRecords = orderRecords.map { record => record.id -> record }.toMap,
-    )
-
-  def createNew2(
-    bibRecord: SierraBibRecord,
-    itemRecords: Seq[SierraItemRecord] = List(),
-    holdingsRecords: Seq[SierraHoldingsRecord] = List(),
-    orderRecords: Seq[SierraOrderRecord] = List()
-  ): SierraTransformable =
-    SierraTransformable(
-      sierraId = bibRecord.id,
-      maybeBibRecord = Some(bibRecord),
-      itemRecords = itemRecords.map { record => record.id -> record }.toMap,
-      holdingsRecords = holdingsRecords.map { record => record.id -> record }.toMap,
-      orderRecords = orderRecords.map { record => record.id -> record }.toMap,
-    )
 }
