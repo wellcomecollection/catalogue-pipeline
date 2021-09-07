0f500a76339ad7c6eea909843731ae0a3fa4d537
Alex Chan <a.chan@wellcome.org>
2021-09-07 05:57:41 +0100

    Remove some unused locals and provider

--- infrastructure/critical/terraform.tf
+++ infrastructure/critical/terraform.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = ">= 0.12.29"
 
-  required_providers {
-    ec = {
-      source  = "elastic/ec"
-      version = "0.2.1"
-    }
-  }
-
   backend "s3" {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
 
@@ -30,23 +23,9 @@ data "terraform_remote_state" "accounts_catalogue" {
   }
 }
 
-data "terraform_remote_state" "infra_critical" {
-  backend = "s3"
-
-  config = {
-    role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
-
-    bucket = "wellcomecollection-platform-infra"
-    key    = "terraform/platform-infrastructure/shared.tfstate"
-    region = "eu-west-1"
-  }
-}
-
 locals {
   catalogue_vpcs = data.terraform_remote_state.accounts_catalogue.outputs
 }
-
-provider "ec" {}
 
 provider "aws" {
   region = "eu-west-1"
