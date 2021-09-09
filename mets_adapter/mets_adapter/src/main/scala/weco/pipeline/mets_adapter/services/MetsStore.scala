9a38323c8b405908a306721f1e5be63a4b8be880
Alex Chan <a.chan@wellcome.org>
2021-09-09 21:58:12 +0100

    Don't catch the VersionAlreadyExistsError in MetsStore
    Notice that all the tests still pass!  That's because we changed how the
    VersionedStore behaves -- now the `put()` is idempotent, which is exactly
    what we were trying to achieve by catching VersionAlreadyExistsError.

--- mets_adapter/mets_adapter/src/main/scala/weco/pipeline/mets_adapter/services/MetsStore.scala
+++ mets_adapter/mets_adapter/src/main/scala/weco/pipeline/mets_adapter/services/MetsStore.scala
@@ -1,22 +1,13 @@
 package weco.pipeline.mets_adapter.services
 
-import grizzled.slf4j.Logging
 import weco.catalogue.source_model.mets.MetsSourceData
 import weco.storage.store.VersionedStore
-import weco.storage.{Identified, Version, VersionAlreadyExistsError}
+import weco.storage.{Identified, Version}
 
-class MetsStore(val store: VersionedStore[String, Int, MetsSourceData])
-    extends Logging {
-
+class MetsStore(val store: VersionedStore[String, Int, MetsSourceData]) {
   def storeData(key: Version[String, Int], data: MetsSourceData)
     : Either[Throwable, Identified[Version[String, Int], MetsSourceData]] =
     store
       .put(key)(data)
-      .left
-      .flatMap {
-        case VersionAlreadyExistsError(_) =>
-          warn(s"$key already exists in store so re-publishing")
-          Right(Identified(key, data))
-        case err => Left(err.e)
-      }
+      .left.map(_.e)
 }
