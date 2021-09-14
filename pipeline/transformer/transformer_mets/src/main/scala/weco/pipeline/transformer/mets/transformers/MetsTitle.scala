6f7443308cedbc514908a1d6cd6628bdf5d08f67
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2021-09-14 10:04:41 +0000

    Apply auto-formatting rules

--- pipeline/transformer/transformer_mets/src/main/scala/weco/pipeline/transformer/mets/transformers/MetsTitle.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/weco/pipeline/transformer/mets/transformers/MetsTitle.scala
@@ -7,21 +7,21 @@ import scala.xml.Elem
 object MetsTitle {
 
   /** The title is encoded in the METS.  For example:
-   *
-   * <mets:dmdSec ID="DMDLOG_0000">
-   *   <mets:mdWrap MDTYPE="MODS">
-   *     <mets:xmlData>
-   *       <mods:mods>
-   *         <mods:titleInfo>
-   *           <mods:title>Reduction and treatment of a fracture of the calcaneus</mods:title>
-   *         </mods:titleInfo>
-   *       </mods:mods>
-   *     </mets:xmlData>
-   *   </mets:mdWrap>
-   * </mets:dmdSec>
-   *
-   * The title is "Reduction and treatment of a fracture of the calcaneus"
-   */
+    *
+    * <mets:dmdSec ID="DMDLOG_0000">
+    *   <mets:mdWrap MDTYPE="MODS">
+    *     <mets:xmlData>
+    *       <mods:mods>
+    *         <mods:titleInfo>
+    *           <mods:title>Reduction and treatment of a fracture of the calcaneus</mods:title>
+    *         </mods:titleInfo>
+    *       </mods:mods>
+    *     </mets:xmlData>
+    *   </mets:mdWrap>
+    * </mets:dmdSec>
+    *
+    * The title is "Reduction and treatment of a fracture of the calcaneus"
+    */
   def apply(root: Elem): Result[String] = {
     val titleNodes =
       (root \\ "dmdSec" \ "mdWrap" \\ "titleInfo" \ "title").toList.distinct
