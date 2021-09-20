4d37a89d3b12232a2bba6e5ca5c89683b64f9bc0
Alex Chan <a.chan@wellcome.org>
2021-09-20 07:34:36 +0100

    Remove the DataState comment, which is outdated and unhelpful

--- common/internal_model/src/main/scala/weco/catalogue/internal_model/identifiers/DataState.scala
+++ common/internal_model/src/main/scala/weco/catalogue/internal_model/identifiers/DataState.scala
@@ -1,12 +1,5 @@
 package weco.catalogue.internal_model.identifiers
 
-/** Container type for IdState types, that is used by WorkData / ImageData
-  * with three associated types:
-  *
-  * - Id (references an ID type, always with a source identifier)
-  * - MaybeId (references an ID type, potentially with a source identifier)
-  * - WorkImage (references the type of the image inside the work data)
-  */
 sealed trait DataState {
   type Id
   type MaybeId
