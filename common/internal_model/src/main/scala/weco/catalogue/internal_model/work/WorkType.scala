6403fc1ef26c9e142c79558183d8ecabff6aeb21
Alex Chan <a.chan@wellcome.org>
2021-09-20 11:49:09 +0100

    Add a comment about a seemingly-unused method

--- common/internal_model/src/main/scala/weco/catalogue/internal_model/work/WorkType.scala
+++ common/internal_model/src/main/scala/weco/catalogue/internal_model/work/WorkType.scala
@@ -20,5 +20,6 @@ object WorkType {
   implicit val workTypeDecoder: Decoder[WorkType] =
     deriveEnumerationDecoder[WorkType]
 
+  // This is used in the API for the WorkType filter.
   def getName(workType: WorkType): String = workType.asJson.asString.get
 }
