a4d9f6128f324d7e9a85289322fe5306bc9d76b0
alicefuzier <alicefuzier@gmail.com>
2021-09-21 17:07:26 +0100

    Extra tests for CollectionPathRule

--- pipeline/merger/src/test/scala/weco/pipeline/merger/rules/CollectionPathRuleTest.scala
+++ pipeline/merger/src/test/scala/weco/pipeline/merger/rules/CollectionPathRuleTest.scala
@@ -10,11 +10,9 @@ import weco.pipeline.merger.models.FieldMergeResult
 class CollectionPathRuleTest extends AnyFunSpec
   with SourceWorkGenerators with Inside with Matchers {
 
-
-  private val expectedCollectionPath = CollectionPath("A/B/C/1")
+  val expectedCollectionPath = CollectionPath("A/B/C/1")
   val calmWork = calmIdentifiedWork().collectionPath(expectedCollectionPath)
   val teiWork = teiIdentifiedWork()
-
 
   it(
     "puts the calm collectionPath on the tei work") {
@@ -22,6 +20,20 @@ class CollectionPathRuleTest extends AnyFunSpec
       case FieldMergeResult(collectionPath, _) =>
         collectionPath shouldBe defined
         collectionPath shouldBe Some(expectedCollectionPath)
+    }
+  }
+
+  it("doesn't copy the collectionPath from a non calm work"){
+    inside(CollectionPathRule.merge(teiWork, List(sierraIdentifiedWork().collectionPath(expectedCollectionPath)))) {
+      case FieldMergeResult(collectionPath, _) =>
+        collectionPath shouldNot be(defined)
+    }
+  }
+
+  it("doesn't copy the collectionPath to a non tei work"){
+    inside(CollectionPathRule.merge(sierraIdentifiedWork(), List(calmWork))) {
+      case FieldMergeResult(collectionPath, _) =>
+        collectionPath shouldNot be(defined)
     }
   }
 
