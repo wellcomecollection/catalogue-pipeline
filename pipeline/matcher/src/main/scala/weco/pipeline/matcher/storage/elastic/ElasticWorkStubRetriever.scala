43e7d551e6ad43c6e0931324b379ca084a571eac
Alex Chan <a.chan@wellcome.org>
2021-09-27 09:40:32 +0100

    further reduce complexity in the WorkStubRetriever

--- pipeline/matcher/src/main/scala/weco/pipeline/matcher/storage/elastic/ElasticWorkStubRetriever.scala
+++ pipeline/matcher/src/main/scala/weco/pipeline/matcher/storage/elastic/ElasticWorkStubRetriever.scala
@@ -1,15 +1,13 @@
 package weco.pipeline.matcher.storage.elastic
 
 import com.sksamuel.elastic4s.ElasticDsl._
-import com.sksamuel.elastic4s.circe._
-import com.sksamuel.elastic4s.requests.get.{GetRequest, GetResponse}
+import com.sksamuel.elastic4s.requests.get.GetRequest
 import com.sksamuel.elastic4s.{ElasticClient, Index}
 import weco.json.JsonUtil._
 import weco.pipeline.matcher.models.WorkStub
-import weco.pipeline_storage.elastic.ElasticRetriever
+import weco.pipeline_storage.elastic.ElasticSourceRetriever
 
 import scala.concurrent.ExecutionContext
-import scala.util.Try
 
 /** The matcher only needs to know about linking data on works; most of the fields
   * it can completely ignore.
@@ -18,13 +16,10 @@ import scala.util.Try
   * reduce the amount of data we have to get out of Elasticsearch (and pay to send through
   * the VPC endpoint).
   */
-class ElasticWorkStubRetriever(val client: ElasticClient, val index: Index)(
-  implicit val ec: ExecutionContext
-) extends ElasticRetriever[WorkStub] {
+class ElasticWorkStubRetriever(client: ElasticClient, index: Index)(
+  implicit ec: ExecutionContext
+) extends ElasticSourceRetriever[WorkStub](client, index) {
 
   override def createGetRequest(id: String): GetRequest =
     get(index, id).fetchSourceInclude("state", "version")
-
-  override def parseGetResponse(response: GetResponse): Try[WorkStub] =
-    response.safeTo[WorkStub]
 }
