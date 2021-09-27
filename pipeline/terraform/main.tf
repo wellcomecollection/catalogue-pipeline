05fec0940bd86c2c5f01017470b497a99ce9abac
Alex Chan <a.chan@wellcome.org>
2021-09-27 18:48:48 +0100

    turn off reindexing

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -99,7 +99,7 @@ module "catalogue_pipeline_2021-09-27" {
   pipeline_date = "2021-09-27"
   release_label = "2021-09-27"
 
-  is_reindexing = true
+  is_reindexing = false
 
   # Boilerplate that shouldn't change between pipelines.
 
