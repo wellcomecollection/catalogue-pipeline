cdf6091b15a6044bb63dea8090e28045738f2cc6
Alex Chan <a.chan@wellcome.org>
2021-11-05 08:07:06 +0000

    Add the ESTC identifier type to internal model

--- common/internal_model/src/main/scala/weco/catalogue/internal_model/identifiers/IdentifierType.scala
+++ common/internal_model/src/main/scala/weco/catalogue/internal_model/identifiers/IdentifierType.scala
@@ -125,4 +125,10 @@ object IdentifierType extends Enum[IdentifierType] {
     val id = "fihrist"
     val label = "Fihrist Authority"
   }
+
+  // http://estc.bl.uk/
+  case object ESTC extends IdentifierType {
+    val id = "bl-estc-citation-no"
+    val label = "British Library English Short Title Catalogue"
+  }
 }
