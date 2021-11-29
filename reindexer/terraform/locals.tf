768e0c5f2505860809dc0c22be11ac37ecdf772d
Alex Chan <a.chan@wellcome.org>
2021-11-29 15:12:02 +0000

    Remove the now-defunct Miro inventory table from the reindexer TF

--- reindexer/terraform/locals.tf
+++ reindexer/terraform/locals.tf
@@ -1,26 +1,24 @@
 locals {
   environment = "prod"
 
-  vhs_sierra_table_name         = data.terraform_remote_state.sierra_adapter.outputs.vhs_table_name
-  vhs_miro_table_name           = data.terraform_remote_state.catalogue_infra_critical.outputs.vhs_miro_table_name
-  vhs_miro_inventory_table_name = data.terraform_remote_state.catalogue_infra_critical.outputs.vhs_miro_inventory_table_name
-  mets_dynamo_table_name        = data.terraform_remote_state.mets_adapter.outputs.mets_dynamo_table_name
-  tei_dynamo_table_name         = data.terraform_remote_state.tei_adapter.outputs.tei_adapter_dynamo_table_name
-  vhs_calm_table_name           = data.terraform_remote_state.calm_adapter.outputs.vhs_table_name
+  vhs_sierra_table_name  = data.terraform_remote_state.sierra_adapter.outputs.vhs_table_name
+  vhs_miro_table_name    = data.terraform_remote_state.catalogue_infra_critical.outputs.vhs_miro_table_name
+  mets_dynamo_table_name = data.terraform_remote_state.mets_adapter.outputs.mets_dynamo_table_name
+  tei_dynamo_table_name  = data.terraform_remote_state.tei_adapter.outputs.tei_adapter_dynamo_table_name
+  vhs_calm_table_name    = data.terraform_remote_state.calm_adapter.outputs.vhs_table_name
 
-  reporting_miro_reindex_topic_arn           = data.terraform_remote_state.shared_infra.outputs.reporting_miro_reindex_topic_arn
-  reporting_miro_inventory_reindex_topic_arn = data.terraform_remote_state.shared_infra.outputs.reporting_miro_inventory_reindex_topic_arn
-  reporting_sierra_reindex_topic_arn         = data.terraform_remote_state.shared_infra.outputs.reporting_sierra_reindex_topic_arn
-  catalogue_miro_reindex_topic_arn           = data.terraform_remote_state.shared_infra.outputs.catalogue_miro_reindex_topic_arn
-  catalogue_sierra_reindex_topic_arn         = data.terraform_remote_state.shared_infra.outputs.catalogue_sierra_reindex_topic_arn
-  mets_reindexer_topic_name                  = module.mets_reindexer_topic.name
-  mets_reindexer_topic_arn                   = module.mets_reindexer_topic.arn
-  tei_reindexer_topic_arn                    = module.tei_reindexer_topic.arn
-  calm_reindexer_topic_name                  = module.calm_reindexer_topic.name
-  calm_reindexer_topic_arn                   = module.calm_reindexer_topic.arn
-  calm_deletion_checker_topic_name           = module.calm_deletion_checker_topic.name
-  calm_deletion_checker_topic_arn            = module.calm_deletion_checker_topic.arn
-  miro_updates_topic_arn                     = data.terraform_remote_state.shared_infra.outputs.miro_updates_topic_arn
+  reporting_miro_reindex_topic_arn   = data.terraform_remote_state.shared_infra.outputs.reporting_miro_reindex_topic_arn
+  reporting_sierra_reindex_topic_arn = data.terraform_remote_state.shared_infra.outputs.reporting_sierra_reindex_topic_arn
+  catalogue_miro_reindex_topic_arn   = data.terraform_remote_state.shared_infra.outputs.catalogue_miro_reindex_topic_arn
+  catalogue_sierra_reindex_topic_arn = data.terraform_remote_state.shared_infra.outputs.catalogue_sierra_reindex_topic_arn
+  mets_reindexer_topic_name          = module.mets_reindexer_topic.name
+  mets_reindexer_topic_arn           = module.mets_reindexer_topic.arn
+  tei_reindexer_topic_arn            = module.tei_reindexer_topic.arn
+  calm_reindexer_topic_name          = module.calm_reindexer_topic.name
+  calm_reindexer_topic_arn           = module.calm_reindexer_topic.arn
+  calm_deletion_checker_topic_name   = module.calm_deletion_checker_topic.name
+  calm_deletion_checker_topic_arn    = module.calm_deletion_checker_topic.arn
+  miro_updates_topic_arn             = data.terraform_remote_state.shared_infra.outputs.miro_updates_topic_arn
 
   vpc_id          = local.catalogue_vpcs["catalogue_vpc_delta_id"]
   private_subnets = local.catalogue_vpcs["catalogue_vpc_delta_private_subnets"]
@@ -64,12 +62,6 @@ locals {
       destination = "catalogue_miro_updates"
       table       = local.vhs_miro_table_name
       topic       = local.miro_updates_topic_arn
-    },
-    {
-      source      = "miro_inventory"
-      destination = "reporting"
-      table       = local.vhs_miro_inventory_table_name
-      topic       = local.reporting_miro_inventory_reindex_topic_arn
     },
     {
       source      = "mets"
