7c5367e9aebc808803f98d6b10e0da2b7b7891b7
Alex Chan <a.chan@wellcome.org>
2021-11-29 13:43:52 +0000

    Flip the merger over to the new module

--- pipeline/terraform/stack/service_merger.tf
+++ pipeline/terraform/stack/service_merger.tf
@@ -1,33 +1,33 @@
-module "merger_input_queue" {
-  source          = "git::github.com/wellcomecollection/terraform-aws-sqs//queue?ref=v1.2.1"
-  queue_name      = "${local.namespace}_merger_input"
-  topic_arns      = [module.matcher_output_topic.arn]
-  alarm_topic_arn = var.dlq_alarm_arn
+module "merger_works_output_topic" {
+  source = "../modules/topic"
+
+  name       = "${local.namespace}_merger_works_output"
+  role_names = [module.merger.task_role_name]
+}
+
+module "merger_images_output_topic" {
+  source = "../modules/topic"
+
+  name       = "${local.namespace}_merger_images_output"
+  role_names = [module.merger.task_role_name]
+}
+
+module "merger" {
+  source = "../modules/fargate_service"
+
+  name            = "merger"
+  container_image = local.merger_image
+
+  topic_arns = [
+    module.matcher_output_topic.arn,
+  ]
 
   # This has to be longer than the `flush_interval_seconds` in the merger.
   # It also has to be long enough for the Work to actually get processed,
   # and some of them are quite big.
-  visibility_timeout_seconds = 20 * 60
-}
-module "merger" {
-  source = "../modules/service"
-
-  namespace = local.namespace
-  name      = "merger"
-
-  container_image = local.merger_image
-  security_group_ids = [
-    aws_security_group.egress.id,
-  ]
-
-  elastic_cloud_vpce_sg_id = var.ec_privatelink_security_group_id
-
-  cluster_name = aws_ecs_cluster.cluster.name
-  cluster_arn  = data.aws_ecs_cluster.cluster.arn
+  queue_visibility_timeout_seconds = 20 * 60
 
   env_vars = {
-    metrics_namespace       = "merger"
-    merger_queue_id         = module.merger_input_queue.url
     merger_works_topic_arn  = module.merger_works_output_topic.arn
     merger_images_topic_arn = module.merger_images_output_topic.arn
 
@@ -44,41 +44,27 @@ module "merger" {
   cpu    = 2048
   memory = 4096
 
-  use_fargate_spot = true
-
-  subnets = var.subnets
-
   min_capacity = var.min_capacity
   max_capacity = local.max_capacity
+
+  # Below this line is boilerplate that should be the same across
+  # all Fargate services.
+  egress_security_group_id             = aws_security_group.egress.id
+  elastic_cloud_vpce_security_group_id = var.ec_privatelink_security_group_id
+
+  cluster_name = aws_ecs_cluster.cluster.name
+  cluster_arn  = aws_ecs_cluster.cluster.id
 
   scale_down_adjustment = local.scale_down_adjustment
   scale_up_adjustment   = local.scale_up_adjustment
 
-  queue_read_policy = module.merger_input_queue.read_policy
+  dlq_alarm_topic_arn = var.dlq_alarm_arn
 
-  deployment_service_env  = var.release_label
-  deployment_service_name = "merger"
-  shared_logging_secrets  = var.shared_logging_secrets
-}
+  subnets = var.subnets
 
-module "merger_works_output_topic" {
-  source = "../modules/topic"
+  namespace = local.namespace
 
-  name       = "${local.namespace}_merger_works_output"
-  role_names = [module.merger.task_role_name]
-}
+  deployment_service_env = var.release_label
 
-module "merger_images_output_topic" {
-  source = "../modules/topic"
-
-  name       = "${local.namespace}_merger_images_output"
-  role_names = [module.merger.task_role_name]
-}
-
-module "merger_scaling_alarm" {
-  source     = "git::github.com/wellcomecollection/terraform-aws-sqs//autoscaling?ref=v1.2.1"
-  queue_name = module.merger_input_queue.name
-
-  queue_high_actions = [module.merger.scale_up_arn]
-  queue_low_actions  = [module.merger.scale_down_arn]
+  shared_logging_secrets = var.shared_logging_secrets
 }
