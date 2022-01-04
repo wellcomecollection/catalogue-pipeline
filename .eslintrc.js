7d789ed93b7308c4fe1555210da92bf7cee068b1
Alex Chan <a.chan@wellcome.org>
2022-01-04 13:46:49 +0000

    Remove some unnecessary eslint config for this repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,9 @@
-const sharedPlugins = [
-  'import',
-  'react',
-  'react-hooks',
-  'jest',
-  'prettier',
-  'standard',
-];
+const sharedPlugins = ['import', 'prettier', 'standard'];
 
 const sharedExtends = [
   'standard',
-  'plugin:react/recommended',
   'plugin:prettier/recommended',
-  'plugin:jest-playwright/recommended',
   'prettier',
-  'prettier/react',
   'prettier/standard',
 ];
 
@@ -22,21 +12,13 @@ const sharedRules = {
   'no-multi-str': 'off',
   'no-mixed-operators': 'off',
   'no-return-assign': 'off',
-  'react/react-in-jsx-scope': 'off',
-  'react-hooks/rules-of-hooks': 'error',
   'prettier/prettier': 'error',
-  'no-restricted-syntax': [
-    'error',
-    "JSXElement.children > [expression.callee.property.name='stringify']",
-  ],
+  'no-restricted-syntax': ['error'],
 };
 
 module.exports = {
   parser: 'babel-eslint',
   plugins: [...sharedPlugins, 'flowtype'],
-  env: {
-    'jest/globals': true,
-  },
   extends: [
     ...sharedExtends,
     'prettier/flowtype',
@@ -47,26 +29,12 @@ module.exports = {
     'flowtype/no-types-missing-file-annotation': 'off',
     'flowtype/space-after-type-colon': [2, 'always', { allowLineBreak: true }],
   },
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
   overrides: [
     {
       files: ['*.ts', '*.tsx'],
       parser: '@typescript-eslint/parser',
       plugins: [...sharedPlugins, '@typescript-eslint'],
-      extends: [
-        ...sharedExtends,
-        'plugin:@typescript-eslint/recommended',
-        'plugin:jest-playwright/recommended',
-      ],
+      extends: [...sharedExtends, 'plugin:@typescript-eslint/recommended'],
       rules: {
         ...sharedRules,
         'no-use-before-define': 'off',
@@ -79,9 +47,6 @@ module.exports = {
           'error',
           { ignoreRestSiblings: true },
         ],
-        // This rule does not support FunctionComponent<Props> and so
-        // makes using (eg) children props more of a pain than it should be
-        'react/prop-types': 'off',
       },
     },
   ],
