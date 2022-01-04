9e005babb9a373c08eedf9ad7163be10c6a039fc
Alex Chan <a.chan@wellcome.org>
2022-01-04 12:18:33 +0000

    Use the canned modules for the SNS Publish policies

--- calm_adapter/terraform/topics.tf
+++ calm_adapter/terraform/topics.tf
@@ -8,38 +8,14 @@ module "calm_deletions_topic" {
   name   = "calm-deletions"
 }
 
-data "aws_iam_policy_document" "publish_to_adapter_topic" {
-  statement {
-    actions = [
-      "sns:Publish",
-    ]
-
-    resources = [
-      module.calm_adapter_topic.arn
-    ]
-  }
-}
-
-data "aws_iam_policy_document" "publish_to_deletions_topic" {
-  statement {
-    actions = [
-      "sns:Publish",
-    ]
-
-    resources = [
-      module.calm_deletions_topic.arn
-    ]
-  }
-}
-
 resource "aws_iam_role_policy" "adapter_policy" {
   role   = module.adapter_worker.task_role_name
-  policy = data.aws_iam_policy_document.publish_to_adapter_topic.json
+  policy = module.calm_adapter_topic.publish_policy
 }
 
 resource "aws_iam_role_policy" "deletion_checker_policy" {
   role   = module.deletion_checker_worker.task_role_name
-  policy = data.aws_iam_policy_document.publish_to_deletions_topic.json
+  policy = module.calm_deletions_topic.publish_policy
 }
 
 resource "aws_sns_topic" "calm_windows_topic" {
