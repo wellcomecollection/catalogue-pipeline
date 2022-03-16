45f961eb33203dd465dbf7593acd2b9dfed2c3e0
Paul Butcher <paul.butcher@gmail.com>
2022-03-16 15:26:48 +0000

    generate collection paths for 773/774 relationships

--- pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/transformers/SierraCollectionPathTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/transformers/SierraCollectionPathTest.scala
@@ -23,7 +23,7 @@ class SierraCollectionPathTest
     getCollectionPath(varFields) shouldBe None
   }
 
-  it("returns None, if a relevant field does not have the $w subfield") {
+  it("returns None, if no 773/774 fields have the $w subfield") {
     forAll(
       Table(
         "marcTag",
@@ -33,7 +33,27 @@ class SierraCollectionPathTest
       val varFields = List(
         VarField(
           marcTag = Some(marcTag),
-          content = Some("banana")
+          content = Some("title-only reference to the other document")
+        )
+      )
+      getCollectionPath(varFields) shouldBe None
+    }
+  }
+
+  it("returns None, if an otherwise matching document has no 001 field") {
+    forAll(
+      Table(
+        "marcTag",
+        "773",
+        "774"
+      )) { (marcTag) =>
+      val varFields = List(
+        VarField(
+          marcTag = Some(marcTag),
+          subfields = List(
+            Subfield(tag = "t", content = "A Constituent"),
+            Subfield(tag = "w", content = "This value does not matter, it just matters that it exists")
+          )
         )
       )
       getCollectionPath(varFields) shouldBe None
@@ -50,15 +70,31 @@ class SierraCollectionPathTest
         marcTag = Some("774"),
         subfields = List(
           Subfield(tag = "t", content = "A Constituent"),
-          Subfield(tag = "w", content = "(Wcat)28931i")
+          Subfield(tag = "w", content = "This value does not matter, it just matters that it exists")
         )
       )
     )
     getCollectionPath(varFields).get shouldBe CollectionPath(path = "12345i")
   }
 
-  it(
-    "constructs a value from a 773 field, consisting of $w, $g and the document's own id") {
+  it("constructs a value from a 773 field, consisting of $w and the document's own id") {
+    val varFields = List(
+      VarField(
+        marcTag = Some("001"),
+        content = Some("56789i")
+      ),
+      VarField(
+        marcTag = Some("773"),
+        subfields = List(
+          Subfield(tag = "t", content = "A Host"),
+          Subfield(tag = "w", content = "(Wcat)12345i")
+        )
+      )
+    )
+    getCollectionPath(varFields).get shouldBe CollectionPath(path = "12345i/56789i")
+  }
+
+  it("constructs a value from a 773 field, including $g if present") {
     val varFields = List(
       VarField(
         marcTag = Some("001"),
@@ -73,20 +109,12 @@ class SierraCollectionPathTest
         )
       )
     )
-    getCollectionPath(varFields) shouldBe CollectionPath(
-      path = "12345i/page4_56789i")
-    // CollectionPath is path (ids) and label (concat the $g and title?),
-    // In CALM records, they match,
-    // In TEI records, the path object does not necessarily have a label
-    //
-    // 001   28914i
-    // 245 00 Charing Cross Hospital: a portrait of house surgeons. Photograph, 1906.
-    // 773   Basil Hood. Photograph album,|gpage 5.|w(Wcat)9175i
-    // path: 9175i/page_5_28914i
-    // label: Basil Hood. Photograph album/page 5.
+    getCollectionPath(varFields).get shouldBe CollectionPath(
+      path = "12345i/page_4_56789i")
   }
 
-  it("constructs a value from a 773 field, without a $g") {
+
+  it("constructs a value from the correct 773 field, even when there are multiple") {
     val varFields = List(
       VarField(
         marcTag = Some("001"),
@@ -95,16 +123,27 @@ class SierraCollectionPathTest
       VarField(
         marcTag = Some("773"),
         subfields = List(
-          Subfield(tag = "t", content = "A Host"),
-          Subfield(tag = "w", content = "(Wcat)12345i")
+          Subfield(tag = "t", content = "Not this one"),
+        )
+      ),
+      VarField(
+        marcTag = Some("773"),
+        subfields = List(
+          Subfield(tag = "t", content = "Or this one"),
+          Subfield(tag = "g", content = "Not this page either")
+        )
+      ),
+      VarField(
+        marcTag = Some("773"),
+        subfields = List(
+          Subfield(tag = "t", content = "This one"),
+          Subfield(tag = "w", content = "(Wcat)12345i"),
+          Subfield(tag = "g", content = "vol. 4")
         )
       )
-    )
-    getCollectionPath(varFields) shouldBe CollectionPath(path = "12345i/56789i")
-  }
 
-  it("removes field-final punctuation from the $t subfield") {
-    fail()
+    )
+    getCollectionPath(varFields).get shouldBe CollectionPath(path = "12345i/vol_4_56789i")
   }
 
   private def getCollectionPath(
