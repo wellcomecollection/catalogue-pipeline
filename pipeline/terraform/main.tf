a5ed107a2b32c15be1af1420531b75b16bde2c06
Alex Chan <alex@alexwlchan.net>
2022-04-06 16:21:48 +0100

    Turn off reindexing; this pipeline is done

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -5,7 +5,7 @@ module "catalogue_pipeline_" {
   release_label = "2022-04-04"
 
   reindexing_state = {
-    connect_reindex_topics   = true
+    connect_reindex_topics   = false
     scale_up_tasks           = false
     scale_up_elastic_cluster = false
     scale_up_id_minter_db    = false
