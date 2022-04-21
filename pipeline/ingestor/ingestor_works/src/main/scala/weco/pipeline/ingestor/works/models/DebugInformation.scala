a25dbab17cb3269651f683ff0558ee7306201a53
Alex Chan <alex@alexwlchan.net>
2022-04-21 10:59:24 +0100

    Put the identifier in here too

--- pipeline/ingestor/ingestor_works/src/main/scala/weco/pipeline/ingestor/works/models/DebugInformation.scala
+++ pipeline/ingestor/ingestor_works/src/main/scala/weco/pipeline/ingestor/works/models/DebugInformation.scala
@@ -1,6 +1,6 @@
 package weco.pipeline.ingestor.works.models
 
-import weco.catalogue.internal_model.identifiers.IdState
+import weco.catalogue.internal_model.identifiers.{IdState, SourceIdentifier}
 import weco.catalogue.internal_model.work.{DeletedReason, InvisibilityReason}
 
 /** This is information we put in the Elasticsearch index because it's
@@ -9,29 +9,30 @@ import weco.catalogue.internal_model.work.{DeletedReason, InvisibilityReason}
  *
  */
 sealed trait DebugInformation {
-
-  // Note: this is the version of the source record in the adapter; we
-  // include it for tracing a Work back to the source, but because of
-  // merging in the pipeline we can't rely on it for ordering.
+  val sourceIdentifier: SourceIdentifier
   val sourceVersion: Int
 }
 
 object DebugInformation {
   case class Visible(
+    sourceIdentifier: SourceIdentifier,
     sourceVersion: Int,
     redirectSources: Seq[IdState.Identified]
   ) extends DebugInformation
 
   case class Invisible(
+    sourceIdentifier: SourceIdentifier,
     sourceVersion: Int,
     invisibilityReasons: List[InvisibilityReason]
   ) extends DebugInformation
 
   case class Redirected(
+    sourceIdentifier: SourceIdentifier,
     sourceVersion: Int
   ) extends DebugInformation
 
   case class Deleted(
+    sourceIdentifier: SourceIdentifier,
     sourceVersion: Int,
     deletedReason: DeletedReason
   ) extends DebugInformation
