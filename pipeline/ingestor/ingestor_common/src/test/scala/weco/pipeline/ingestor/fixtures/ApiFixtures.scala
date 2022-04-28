d0f8bc52e2161a76101b3a1e47feed699cf5a2c7
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-04-28 13:32:24 +0000

    Apply auto-formatting rules

--- pipeline/ingestor/ingestor_common/src/test/scala/weco/pipeline/ingestor/fixtures/ApiFixtures.scala
+++ pipeline/ingestor/ingestor_common/src/test/scala/weco/pipeline/ingestor/fixtures/ApiFixtures.scala
@@ -10,16 +10,20 @@ import java.time.Instant
 import scala.util.Random
 
 trait ApiFixtures extends RandomGenerators {
-  case class Fixture(description: String, createdAt: Instant = Instant.now(), id: String, document: Json)
+  case class Fixture(description: String,
+                     createdAt: Instant = Instant.now(),
+                     id: String,
+                     document: Json)
 
   override protected lazy val random: Random =
     new Random(0)
 
   def saveFixtures(fixtures: List[(String, Fixture)]): Unit =
-    fixtures.foreach { case (id, fixture) =>
-      val file = new File(s"fixtures/$id.json")
-      val pw = new PrintWriter(file)
-      pw.write(fixture.asJson.spaces2)
-      pw.close()
+    fixtures.foreach {
+      case (id, fixture) =>
+        val file = new File(s"fixtures/$id.json")
+        val pw = new PrintWriter(file)
+        pw.write(fixture.asJson.spaces2)
+        pw.close()
     }
 }
--- pipeline/ingestor/ingestor_works/src/test/scala/weco/pipeline/ingestor/works/CreateApiFixturesTest.scala
+++ pipeline/ingestor/ingestor_works/src/test/scala/weco/pipeline/ingestor/works/CreateApiFixturesTest.scala
@@ -7,17 +7,22 @@ import weco.catalogue.internal_model.work.generators.WorkGenerators
 import weco.json.JsonUtil._
 import weco.pipeline.ingestor.fixtures.ApiFixtures
 
-class CreateApiFixturesTest extends AnyFunSpec with Matchers with WorkGenerators with ApiFixtures {
+class CreateApiFixturesTest
+    extends AnyFunSpec
+    with Matchers
+    with WorkGenerators
+    with ApiFixtures {
   it("creates the fixtures") {
     val fixtures = denormalisedWorks(count = 5)
       .sortBy { _.state.canonicalId }
       .zipWithIndex
-      .map { case (work, index) =>
-        s"list-of-works.$index" -> Fixture(
-          description = "one of a list of works",
-          id = work.id,
-          document = WorkTransformer.deriveData(work).asJson
-        )
+      .map {
+        case (work, index) =>
+          s"list-of-works.$index" -> Fixture(
+            description = "one of a list of works",
+            id = work.id,
+            document = WorkTransformer.deriveData(work).asJson
+          )
       }
 
     saveFixtures(fixtures)
