dc68399d0a8f595023359a85264c8a7613abe1cd
Alex Chan <alex@alexwlchan.net>
2022-04-28 16:42:20 +0100

    Don't write an example document if it's already up-to-date

--- pipeline/ingestor/ingestor_common/src/test/scala/weco/pipeline/ingestor/fixtures/ExampleDocumentUtils.scala
+++ pipeline/ingestor/ingestor_common/src/test/scala/weco/pipeline/ingestor/fixtures/ExampleDocumentUtils.scala
@@ -2,12 +2,13 @@ package weco.pipeline.ingestor.fixtures
 
 import io.circe.Json
 import io.circe.syntax._
+import org.apache.commons.io.FileUtils
 import weco.fixtures.RandomGenerators
 import weco.json.JsonUtil._
 
 import java.io.{File, PrintWriter}
 import java.time.Instant
-import scala.util.Random
+import scala.util.{Random, Success}
 
 trait ExampleDocumentUtils extends RandomGenerators {
   case class ExampleDocument(description: String, createdAt: Instant = Instant.now(), id: String, document: Json)
@@ -34,11 +35,28 @@ trait ExampleDocumentUtils extends RandomGenerators {
 
   writeReadme()
 
-  def saveDocuments(fixtures: Seq[(String, ExampleDocument)]): Unit =
-    fixtures.foreach { case (id, fixture) =>
+  def saveDocuments(documents: Seq[(String, ExampleDocument)]): Unit =
+    documents.foreach { case (id, doc) =>
       val file = new File(s"pipeline/ingestor/example_documents/$id.json")
-      val pw = new PrintWriter(file)
-      pw.write(fixture.asJson.spaces2)
-      pw.close()
+
+      val isAlreadyUpToDate =
+        if (file.exists()) {
+          val existingContents = FileUtils.readFileToString(file, "UTF-8")
+
+          fromJson[ExampleDocument](existingContents) match {
+            case Success(ExampleDocument(existingDescription, _, existingId, existingDocument)) =>
+              existingDescription == doc.description && existingId == doc.id && existingDocument == doc.document
+
+            case _ => false
+          }
+        } else {
+          false
+        }
+
+      if (!isAlreadyUpToDate) {
+        val pw = new PrintWriter(file)
+        pw.write(doc.asJson.spaces2)
+        pw.close()
+      }
     }
 }
