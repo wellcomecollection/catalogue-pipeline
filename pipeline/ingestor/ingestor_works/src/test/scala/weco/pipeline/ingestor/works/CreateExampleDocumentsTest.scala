ae62228f01d27db98e0b326c2f1e6e38def86692
Alex Chan <alex@alexwlchan.net>
2022-04-28 17:26:10 +0100

    Continue sorting out examples

--- pipeline/ingestor/ingestor_works/src/test/scala/weco/pipeline/ingestor/works/CreateExampleDocumentsTest.scala
+++ pipeline/ingestor/ingestor_works/src/test/scala/weco/pipeline/ingestor/works/CreateExampleDocumentsTest.scala
@@ -4,19 +4,61 @@ import io.circe.Json
 import io.circe.syntax._
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
-import weco.catalogue.internal_model.work.{Work, WorkState}
-import weco.catalogue.internal_model.work.generators.WorkGenerators
+import weco.catalogue.internal_model.identifiers.IdState
+import weco.catalogue.internal_model.work.{InstantRange, Period, ProductionEvent, Work, WorkState}
+import weco.catalogue.internal_model.work.generators.{ItemsGenerators, WorkGenerators}
 import weco.json.JsonUtil._
 import weco.pipeline.ingestor.fixtures.ExampleDocumentUtils
 
-import java.time.Instant
+import java.time.{Instant, LocalDate, Month}
 
-class CreateExampleDocumentsTest extends AnyFunSpec with Matchers with WorkGenerators with ExampleDocumentUtils {
+trait PeriodGenerators {
+  def createPeriodForYear(year: String): Period[IdState.Minted] =
+    Period(
+      id = IdState.Unidentifiable,
+      label = year,
+      range = Some(
+        InstantRange(
+          from = LocalDate.of(year.toInt, Month.JANUARY, 1),
+          to = LocalDate.of(year.toInt, Month.DECEMBER, 31),
+          label = year
+        )
+      )
+    )
+}
+
+/** Creates the example documents we use in teh API tests.
+  *
+  * These tests use a seeded RNG to ensure deterministic results; to prevent
+  * regenerating existing examples and causing unnecessary churn in the API tests
+  * when values change, I suggest adding new examples at the bottom of this file.
+  */
+class CreateExampleDocumentsTest extends AnyFunSpec with Matchers with WorkGenerators with ItemsGenerators with PeriodGenerators with ExampleDocumentUtils {
   it("creates the example documents") {
     saveWorks(
-      works = denormalisedWorks(count = 5),
-      description = "an arbitrary list of works",
-      id = "list-of-works"
+      works = (1 to 5).map(_ => denormalisedWork()),
+      description = "an arbitrary list of visible works",
+      id = "works.visible"
+    )
+    saveWorks(
+      works = (1 to 3).map(_ => denormalisedWork().invisible()),
+      description = "an arbitrary list of invisible works",
+      id = "works.invisible"
+    )
+    saveWorks(
+      works = (1 to 2).map(_ => denormalisedWork().redirected(
+        redirectTarget = IdState.Identified(
+          canonicalId = createCanonicalId,
+          sourceIdentifier = createSourceIdentifier
+        )
+      )),
+      description = "an arbitrary list of redirected works",
+      id = "works.redirected"
+    )
+    saveWorks(
+      works = (1 to 4).map(_ => denormalisedWork().deleted()),
+      description = "an arbitrary list of deleted works",
+      id = "works.deleted"
     )
 
     saveWork(
@@ -26,6 +68,42 @@ class CreateExampleDocumentsTest extends AnyFunSpec with Matchers with WorkGener
       description = "a work with optional top-level fields",
       id = "work-with-edition-and-duration"
     )
+
+    saveWork(
+      work = denormalisedWork().title("A drawing of a dodo"),
+      description = "a work with 'dodo' in the title",
+      id = "work-title-dodo"
+    )
+    saveWork(
+      work = denormalisedWork().title("A mezzotint of a mouse"),
+      description = "a work with 'mouse' in the title",
+      id = "work-title-mouse"
+    )
+
+    saveWork(
+      work = denormalisedWork().thumbnail(createDigitalLocation),
+      description = "a work with a thumbnail",
+      id = "work-thumbnail"
+    )
+
+    Seq("1900", "1976", "1904", "2020", "1098").map { year =>
+      saveWork(
+        work = denormalisedWork()
+          .production(
+            List(
+              ProductionEvent(
+                label = randomAlphanumeric(25),
+                places = List(),
+                agents = List(),
+                dates = List(createPeriodForYear(year))
+              )
+            )
+          )
+          .title(s"Production event in $year"),
+        description = s"a work with a production event in $year",
+        id = s"work-production.$year"
+      )
+    }
   }
 
   private def saveWork(
@@ -36,7 +114,7 @@ class CreateExampleDocumentsTest extends AnyFunSpec with Matchers with WorkGener
     saveWorks(works = List(work), description, id)
 
   private def saveWorks(
-    works: List[Work[WorkState.Denormalised]],
+    works: Seq[Work[WorkState.Denormalised]],
     description: String,
     id: String
   ): Unit = {
@@ -70,10 +148,8 @@ class CreateExampleDocumentsTest extends AnyFunSpec with Matchers with WorkGener
       // This is a fixed date so we get consistent values in the indexedTime
       // field in the generated documents.
       val transformer = new WorkTransformer {
-        override protected def getIndexedTime: Instant = {
-          println(s"@@AWLC calling my getIndexedTime")
+        override protected def getIndexedTime: Instant =
           Instant.parse("2020-10-15T15:51:00.00Z")
-        }
       }
 
       transformer.deriveData(work).asJson
