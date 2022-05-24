a0ae5ab4eeb58d486c4bbdf90edeeccb3cc5ea62
Alex Chan <alex@alexwlchan.net>
2022-05-24 18:23:18 +0100

    Remove a now-unused method from WorkGenerators

--- common/internal_model/src/test/scala/weco/catalogue/internal_model/work/generators/WorkGenerators.scala
+++ common/internal_model/src/test/scala/weco/catalogue/internal_model/work/generators/WorkGenerators.scala
@@ -285,38 +285,6 @@ trait WorkGenerators
       })
   }
 
-  implicit class IndexedWorkOps(work: Work.Visible[Indexed])(
-    implicit updateState: UpdateState[Indexed]
-  ) {
-
-    // Note: this method is unused in the pipeline repo, but it's used to
-    // test the relation filters in the catalogue-api repo.
-    //
-    // See https://github.com/wellcomecollection/catalogue-api/blob/be1d4c3f4fca07c0f0ebcc45e600d27192a723ed/search/src/test/scala/weco/api/search/works/WorksFiltersTest.scala#L939-L980
-    //
-    def ancestors(works: Work.Visible[Indexed]*): Work.Visible[Indexed] =
-      Work.Visible[Indexed](
-        work.version,
-        work.data,
-        updateState(
-          work.state.copy(
-            relations = work.state.relations.copy(
-              ancestors = works.toList.zipWithIndex.map {
-                case (work, idx) =>
-                  Relation(
-                    work = work,
-                    depth = idx + 1,
-                    numChildren = 1,
-                    numDescendents = works.length - idx
-                  )
-              }
-            )
-          ),
-          work.data
-        )
-      )
-  }
-
   trait UpdateState[State <: WorkState] {
     def apply(state: State, data: WorkData[State#WorkDataState]): State
   }
