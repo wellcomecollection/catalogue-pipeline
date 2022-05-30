f06ea345e8a3e662752db0d049df69598ed9245f
Alex Chan <alex@alexwlchan.net>
2022-05-30 14:00:00 +0100

    Check you've run weco-deploy before creating services
    This will fail at the plan stage if you haven't.

---
+++ pipeline/terraform/stack/check_weco_deploy.tf
@@ -0,0 +1,10 @@
+# This is a crude check that you've remembered to run weco-deploy
+# before running a new pipeline.  It looks for the tag created by
+# weco-deploy in one of the ECR repositories.
+#
+# If you get an error telling you this tag can't be found, you probably
+# forgot to do something in weco-deploy.
+data "aws_ecr_image" "check_weco_deploy_tags_exist" {
+  repository_name = "uk.ac.wellcome/transformer_tei"
+  image_tag       = "env.${var.pipeline_date}"
+}
