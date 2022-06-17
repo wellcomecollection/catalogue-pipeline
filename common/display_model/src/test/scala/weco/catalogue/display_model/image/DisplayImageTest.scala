0d91843a7b9d2bb996387ed030721a5212184857
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-06-17 11:25:13 +0000

    Apply auto-formatting rules

--- common/display_model/src/test/scala/weco/catalogue/display_model/image/DisplayImageTest.scala
+++ common/display_model/src/test/scala/weco/catalogue/display_model/image/DisplayImageTest.scala
@@ -6,10 +6,7 @@ import weco.catalogue.display_model.locations.DisplayDigitalLocation
 import weco.catalogue.internal_model.generators.ImageGenerators
 import weco.catalogue.internal_model.locations.LocationType
 
-class DisplayImageTest
-    extends AnyFunSpec
-    with Matchers
-    with ImageGenerators {
+class DisplayImageTest extends AnyFunSpec with Matchers with ImageGenerators {
   it(
     "sets the thumbnail to the first iiif-image location it finds in locations") {
     val imageLocation = createDigitalLocationWith(
