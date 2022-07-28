32df058b6ab043d9c20b58e6e94761ebd3529739
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-07-28 14:49:04 +0000

    Apply auto-formatting rules

--- pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/transformers/SierraAbstractConcepts.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/transformers/SierraAbstractConcepts.scala
@@ -23,8 +23,7 @@ trait SierraAbstractConcepts extends Logging {
     *   - Multiple identifier fields: unidentifiable, we don't know what to use
     *
     */
-  def getIdState(ontologyType: String,
-                           varField: VarField): IdState.Unminted = {
+  def getIdState(ontologyType: String, varField: VarField): IdState.Unminted = {
     getIdentifierSubfieldContents(varField) match {
       case Seq(subfieldContent) =>
         maybeAddIdentifier(
