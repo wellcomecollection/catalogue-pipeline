adf96e9fe66bd7dcd0989c40feb323e233e2b633
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-08-04 09:03:22 +0000

    Apply auto-formatting rules

--- pipeline/transformer/transformer_common/src/main/scala/weco/pipeline/transformer/transformers/ConceptsTransformer.scala
+++ pipeline/transformer/transformer_common/src/main/scala/weco/pipeline/transformer/transformers/ConceptsTransformer.scala
@@ -96,10 +96,10 @@ trait ConceptsTransformer {
   implicit class PeriodOps[State](p: Period[State]) {
     def identifiable(idState: Option[IdState.Identifiable] = None)
       : Period[IdState.Identifiable] =
-    // The label of a period may contain superfluous content that doesn't change the referent of the period
-    // e.g. years duplicated in Roman, punctuation, diverse renditions of the term "floruit".
-    // The id is therefore based on that "preprocessed" label, rather than simply passing through the
-    // label as written.
+      // The label of a period may contain superfluous content that doesn't change the referent of the period
+      // e.g. years duplicated in Roman, punctuation, diverse renditions of the term "floruit".
+      // The id is therefore based on that "preprocessed" label, rather than simply passing through the
+      // label as written.
       p.copy(
         id = newIdIfNeeded(
           p.id,
