2eb7839a3c4dd32727266234d0dd0ba140ab6926
jamie <j.parkinson@wellcome.ac.uk>
2022-08-24 14:46:43 +0100

    New pipeline 08-24, remove old pipelines

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,65 +1,3 @@
-module "catalogue_pipeline_2022-07-26" {
-  source = "./stack"
-
-  pipeline_date = "2022-07-26"
-  release_label = "2022-07-26"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # This pipeline is disabled to avoid polluting the ID minter database
-  # with label-derived IDs that aren't normalised.
-  max_capacity = 0
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
-module "catalogue_pipeline_2022-08-04" {
-  source = "./stack"
-
-  pipeline_date = "2022-08-04"
-  release_label = "2022-08-04"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # This pipeline is disabled to avoid polluting the ID minter database
-  # with label-derived IDs that aren't normalised.
-  max_capacity = 0
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
 module "catalogue_pipeline_2022-08-08" {
   source = "./stack"
 
@@ -86,3 +24,31 @@ module "catalogue_pipeline_2022-08-08" {
     aws.catalogue = aws.catalogue
   }
 }
+
+module "catalogue_pipeline_2022-08-24" {
+  source = "./stack"
+
+  pipeline_date = "2022-08-24"
+  release_label = "2022-08-24"
+
+  reindexing_state = {
+    listen_to_reindexer      = false
+    scale_up_tasks           = false
+    scale_up_elastic_cluster = false
+    scale_up_id_minter_db    = false
+    scale_up_matcher_db      = false
+  }
+
+  # Boilerplate that shouldn't change between pipelines.
+
+  adapter_config    = local.adapter_config
+  inferrer_config   = local.inferrer_config
+  monitoring_config = local.monitoring_config
+  network_config    = local.network_config
+  rds_config        = local.rds_config
+
+  providers = {
+    aws.catalogue = aws.catalogue
+  }
+}
+
