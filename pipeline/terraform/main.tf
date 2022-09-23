923325826ef3a3b7e425714d8578c97a94458796
Alex Chan <alex@alexwlchan.net>
2022-09-23 07:14:29 +0100

    use the release label, not the pipeline date

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -63,7 +63,7 @@ module "catalogue_pipeline_2022-09-23" {
   source = "./stack"
 
   pipeline_date = "2022-09-23"
-  release_label = "2022-09-22"
+  release_label = "2022-09-23"
 
   reindexing_state = {
     listen_to_reindexer      = false
