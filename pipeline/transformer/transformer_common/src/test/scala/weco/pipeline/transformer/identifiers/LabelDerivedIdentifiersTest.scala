ce1146aaa1558a3ddd0d8df0e89c06d687f5e082
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-09-23 07:24:24 +0000

    Apply auto-formatting rules

--- pipeline/transformer/transformer_common/src/test/scala/weco/pipeline/transformer/identifiers/LabelDerivedIdentifiersTest.scala
+++ pipeline/transformer/transformer_common/src/test/scala/weco/pipeline/transformer/identifiers/LabelDerivedIdentifiersTest.scala
@@ -9,7 +9,8 @@ class LabelDerivedIdentifiersTest extends AnyFunSpec with Matchers {
   it("removes whitespace that's added after we filter out non-ASCII characters") {
     // This comes from b16631614
     val label = "Miki\u0107, \u017delimir \u0110."
-    val identifier = identifiers.identifierFromText(label = label, ontologyType = "Person")
+    val identifier =
+      identifiers.identifierFromText(label = label, ontologyType = "Person")
     identifier.sourceIdentifier.value shouldBe "mikic, zelimir"
   }
 }
