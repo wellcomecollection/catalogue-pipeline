3644e4148d9e4832b7f414da199374a17072ccc4
Alex Chan <alex@alexwlchan.net>
2022-09-26 09:51:55 +0100

    Truncate label-derived IDs to 256 characters

--- pipeline/transformer/transformer_common/src/main/scala/weco/pipeline/transformer/identifiers/LabelDerivedIdentifiers.scala
+++ pipeline/transformer/transformer_common/src/main/scala/weco/pipeline/transformer/identifiers/LabelDerivedIdentifiers.scala
@@ -32,10 +32,22 @@ trait LabelDerivedIdentifiers {
       .replaceAll("[^\\p{ASCII}]", "")
       .trim
 
+    // We have a handful of works that need label-derived identifiers which are
+    // longer than the 'SourceId' column in the ID minter database.
+    //
+    // (Here "a handful" is approximately two dozen as of September 2022, and within
+    // that set it looks like there are some duplicate labels.)
+    //
+    // These labels aren't meant to be human-readable, and truncating is easier than
+    // making the ID minter handle extreme edge cases.
+    val truncatedLabel = normalizedLabel
+      .slice(0, 256)
+      .trim
+
     IdState.Identifiable(
       sourceIdentifier = SourceIdentifier(
         identifierType = IdentifierType.LabelDerived,
-        value = normalizedLabel,
+        value = truncatedLabel,
         ontologyType = ontologyType
       )
     )
