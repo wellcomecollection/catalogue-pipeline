dda5c395da398cca90228c58606bb9c7a3e8c318
Alex Chan <alex@alexwlchan.net>
2022-10-03 17:20:50 +0100

    add the 2022-10-03 pipeline

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,37 +1,3 @@
-module "catalogue_pipeline_2022-08-24" {
-  source = "./stack"
-
-  pipeline_date = "2022-08-24"
-  release_label = "2022-08-24"
-
-  # This pipeline is running Elasticsearch 8.4.2, which seems to be
-  # broken with the queries we use.  I don't want to delete it without
-  # double checking (in case it's useful for further investigation),
-  # but we don't need to be running anything through it.
-  max_capacity    = 0
-  es_cluster_size = "3x2g"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
 module "catalogue_pipeline_2022-09-23" {
   source = "./stack"
 
@@ -58,4 +24,32 @@ module "catalogue_pipeline_2022-09-23" {
     aws.catalogue = aws.catalogue
   }
 }
+
+module "catalogue_pipeline_2022-10-03" {
+  source = "./stack"
+
+  pipeline_date = "2022-10-03"
+  release_label = "2022-10-03"
+
+  reindexing_state = {
+    listen_to_reindexer      = false
+    scale_up_tasks           = false
+    scale_up_elastic_cluster = false
+    scale_up_id_minter_db    = false
+    scale_up_matcher_db      = false
+  }
+
+  # Boilerplate that shouldn't change between pipelines.
+
+  adapter_config    = local.adapter_config
+  inferrer_config   = local.inferrer_config
+  monitoring_config = local.monitoring_config
+  network_config    = local.network_config
+  rds_config        = local.rds_config
+
+  providers = {
+    aws.catalogue = aws.catalogue
+  }
+}
+
 
