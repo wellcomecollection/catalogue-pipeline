7dc76104160b4bc07b4387b39dcf37c9ce1b8285
Alex Chan <alex@alexwlchan.net>
2022-10-03 07:44:19 +0100

    Store all the parent work fields on ImageQueryableValues

--- pipeline/ingestor/ingestor_images/src/main/scala/weco/pipeline/ingestor/images/models/ImageQueryableValues.scala
+++ pipeline/ingestor/ingestor_images/src/main/scala/weco/pipeline/ingestor/images/models/ImageQueryableValues.scala
@@ -1,26 +1,26 @@
 package weco.pipeline.ingestor.images.models
 
 import io.circe.generic.extras.JsonKey
-import weco.catalogue.internal_model.identifiers.IdState
 import weco.catalogue.internal_model.image.{ImageSource, ParentWork}
-import weco.catalogue.internal_model.work.{Genre, Subject}
+import weco.catalogue.internal_model.work.Relations
+import weco.pipeline.ingestor.common.models.WorkQueryableValues
 
 case class ImageQueryableValues(
-  @JsonKey("source.subjects.label") sourceSubjectLabels: Seq[String],
-  @JsonKey("source.genres.label") sourceGenreLabels: Seq[String],
+  @JsonKey("source") source: WorkQueryableValues,
 )
 
 case object ImageQueryableValues {
   def apply(source: ImageSource): ImageQueryableValues =
     source match {
-      case ParentWork(_, workData, _) =>
-        create(workData.subjects, workData.genres)
+      case ParentWork(id, workData, _) =>
+        ImageQueryableValues(
+          source = WorkQueryableValues(
+            id = id.canonicalId,
+            sourceIdentifier = id.sourceIdentifier,
+            workData = workData,
+            relations = Relations.none,
+            availabilities = Set()
+          )
+        )
     }
-
-  private def create(subjects: Seq[Subject[IdState.Minted]],
-                     genres: Seq[Genre[IdState.Minted]]): ImageQueryableValues =
-    ImageQueryableValues(
-      sourceSubjectLabels = subjects.map(_.label),
-      sourceGenreLabels = genres.map(_.label)
-    )
 }
