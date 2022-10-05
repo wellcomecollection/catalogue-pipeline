c5df26eab95c224e5681ae0d7dbc67b4b8633e2c
Alex Chan <alex@alexwlchan.net>
2022-10-05 13:50:10 +0100

    Delete the 09-23 pipeline
    We weren't using it and the ingestor is heavily backed up; we couldn't
    roll back to it if we wanted to.

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,30 +1,3 @@
-module "catalogue_pipeline_2022-09-23" {
-  source = "./stack"
-
-  pipeline_date = "2022-09-23"
-  release_label = "2022-09-23"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
 module "catalogue_pipeline_2022-10-03" {
   source = "./stack"
 
