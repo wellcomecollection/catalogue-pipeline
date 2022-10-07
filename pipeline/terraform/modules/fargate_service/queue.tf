22a567ab2e23b05bf849c1dd29a110c1e7d3fad6
Paul Butcher <paul.butcher@gmail.com>
2022-10-07 16:40:30 +0100

    reduce retention period and retries on ingestor_works

--- pipeline/terraform/modules/fargate_service/queue.tf
+++ pipeline/terraform/modules/fargate_service/queue.tf
@@ -6,7 +6,7 @@ module "input_queue" {
   topic_arns                 = var.topic_arns
   visibility_timeout_seconds = var.queue_visibility_timeout_seconds
   max_receive_count          = var.max_receive_count
-
+  message_retention_seconds  = var.message_retention_seconds
   alarm_topic_arn = var.fargate_service_boilerplate.dlq_alarm_topic_arn
 }
 
