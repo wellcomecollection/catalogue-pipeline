6eab788accd0a49ec7860f52b33ed0d775252445
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-11-15 14:01:47 +0000

    Apply auto-formatting rules

--- common/internal_model/src/main/scala/weco/catalogue/internal_model/index/IndexMapping.scala
+++ common/internal_model/src/main/scala/weco/catalogue/internal_model/index/IndexMapping.scala
@@ -2,23 +2,24 @@ package weco.catalogue.internal_model.index
 
 import weco.json.JsonUtil.toJson
 
-
 object IndexMapping {
+
   /**
-   * Return a JSON string suitable for posting to Elasticsearch as a Mapping object.
-   *
-   * Given a JSON string representing the properties for an index mapping,
-   * populate a "mappings" object accordingly.
-   *
-   * This essentially replaces the practice of calling methods on an elastic4s MappingDefinition
-   * to update a mapping with values for dynamic and _meta etc.
-   * (which do not work with a definition initialised with rawSource),
-   * allowing us to configure the properties in a mapping with plain JSON.
-   *
-   */
+    * Return a JSON string suitable for posting to Elasticsearch as a Mapping object.
+    *
+    * Given a JSON string representing the properties for an index mapping,
+    * populate a "mappings" object accordingly.
+    *
+    * This essentially replaces the practice of calling methods on an elastic4s MappingDefinition
+    * to update a mapping with values for dynamic and _meta etc.
+    * (which do not work with a definition initialised with rawSource),
+    * allowing us to configure the properties in a mapping with plain JSON.
+    *
+    */
   def apply(propertiesJson: String, buildVersion: String): String = {
     val version = buildVersion.split("\\.").toList
-    val meta = toJson(Map(s"model.versions.${version.head}" -> version.tail.head)).get
+    val meta = toJson(
+      Map(s"model.versions.${version.head}" -> version.tail.head)).get
     s"""{
        |"properties": $propertiesJson,
        |"_meta": $meta,
--- common/internal_model/src/test/scala/weco/catalogue/internal_model/index/IndexMappingTest.scala
+++ common/internal_model/src/test/scala/weco/catalogue/internal_model/index/IndexMappingTest.scala
@@ -4,10 +4,7 @@ import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import weco.json.utils.JsonAssertions
 
-class IndexMappingTest
-  extends AnyFunSpec
-  with Matchers
-  with JsonAssertions {
+class IndexMappingTest extends AnyFunSpec with Matchers with JsonAssertions {
 
   it("formats a strict mapping document containing the given properties") {
     val properties = """{"modifiedTime": {"type": "date"}}"""
