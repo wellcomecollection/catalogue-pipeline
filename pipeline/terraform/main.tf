85bd86879af76613924f7a63297f901ee8b50de3
Paul Butcher <paul.butcher@gmail.com>
2022-11-23 16:18:07 +0000

    Add new pipeline

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -52,3 +52,30 @@ module "catalogue_pipeline_2022-11-19" {
     aws.catalogue = aws.catalogue
   }
 }
+
+module "catalogue_pipeline_2022-11-19" {
+  source = "./stack"
+
+  pipeline_date = "2022-11-28"
+  release_label = "2022-11-28"
+
+  reindexing_state = {
+    listen_to_reindexer      = true
+    scale_up_tasks           = true
+    scale_up_elastic_cluster = true
+    scale_up_id_minter_db    = true
+    scale_up_matcher_db      = true
+  }
+
+  # Boilerplate that shouldn't change between pipelines.
+
+  adapter_config    = local.adapter_config
+  inferrer_config   = local.inferrer_config
+  monitoring_config = local.monitoring_config
+  network_config    = local.network_config
+  rds_config        = local.rds_config
+
+  providers = {
+    aws.catalogue = aws.catalogue
+  }
+}
