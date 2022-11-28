bf0197310ee921dc7ef4778765cae335ea1ef7cf
Paul Butcher <paul.butcher@gmail.com>
2022-11-28 11:24:52 +0000

    remove 11-03

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,58 +1,3 @@
-module "catalogue_pipeline_2022-11-03" {
-  source = "./stack"
-
-  pipeline_date = "2022-11-03"
-  release_label = "2022-11-03"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
-
-module "catalogue_pipeline_2022-11-19" {
-  source = "./stack"
-
-  pipeline_date = "2022-11-19"
-  release_label = "2022-11-19"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
-
 module "catalogue_pipeline_2022-11-28" {
   source = "./stack"
 
@@ -60,11 +5,11 @@ module "catalogue_pipeline_2022-11-28" {
   release_label = "2022-11-28"
 
   reindexing_state = {
-    listen_to_reindexer      = true
-    scale_up_tasks           = true
-    scale_up_elastic_cluster = true
-    scale_up_id_minter_db    = true
-    scale_up_matcher_db      = true
+    listen_to_reindexer      = false
+    scale_up_tasks           = false
+    scale_up_elastic_cluster = false
+    scale_up_id_minter_db    = false
+    scale_up_matcher_db      = false
   }
 
   # Boilerplate that shouldn't change between pipelines.
