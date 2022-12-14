4f24973075e97d0cae8d37fd17927fbd580bdfb3
Alex Chan <a.chan@wellcome.org>
2022-12-14 09:34:42 +0000

    Update pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/transformers/SierraCurrentFrequencyTest.scala
    Co-authored-by: Paul Butcher <paul.butcher@gmail.com>

--- pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/transformers/SierraCurrentFrequencyTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/transformers/SierraCurrentFrequencyTest.scala
@@ -13,6 +13,30 @@ class SierraCurrentFrequencyTest extends AnyFunSpec with Matchers with SierraDat
     SierraCurrentFrequency(bibData) shouldBe None
   }
 
+it("combines multiple 310 fields into a single string") {
+     // This is based on b32506697
+     val bibData = createSierraBibDataWith(
+       varFields = List(
+         VarField(
+           marcTag = "310",
+           subfields = List(
+             Subfield(tag = "a", content = "Annual,"),
+             Subfield(tag = "b", content = "2007-2012"),
+           )
+         )
+         VarField(
+           marcTag = "310",
+           subfields = List(
+             Subfield(tag = "a", content = "Solsticial,"),
+             Subfield(tag = "b", content = "2004-2006"),
+           )
+         )
+       )
+     )
+
+     SierraCurrentFrequency(bibData) shouldBe Some("Annual, 2007-2012 Solsticial, 2004-2006")
+   }
+
   it("combines 310 ǂa and ǂb") {
     // This is based on b32506697
     val bibData = createSierraBibDataWith(
