24a581914a8198b689962ff751867f53df685548
Paul Butcher <paul.butcher@gmail.com>
2022-12-15 13:36:21 +0000

    remove 11-19 pipeline

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -1,29 +1,3 @@
-module "catalogue_pipeline_2022-11-19" {
-  source = "./stack"
-
-  pipeline_date = "2022-11-28"
-  release_label = "2022-11-28"
-
-  reindexing_state = {
-    listen_to_reindexer      = false
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
-  }
-
-  # Boilerplate that shouldn't change between pipelines.
-
-  adapter_config    = local.adapter_config
-  inferrer_config   = local.inferrer_config
-  monitoring_config = local.monitoring_config
-  network_config    = local.network_config
-  rds_config        = local.rds_config
-
-  providers = {
-    aws.catalogue = aws.catalogue
-  }
-}
 
 module "catalogue_pipeline_2022-11-28" {
   source = "./stack"
