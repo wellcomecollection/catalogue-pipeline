05fe929e6574830c50d59deca837ada791f94258
Paul Butcher <paul.butcher@gmail.com>
2023-01-09 09:21:43 +0000

    handle compound subjects

--- pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/SubjectsAndContributors.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/SubjectsAndContributors.scala
@@ -6,9 +6,26 @@ import weco.catalogue.internal_model.identifiers.{
   IdentifierType,
   SourceIdentifier
 }
-import weco.catalogue.internal_model.work.{Contributor, Subject}
+import weco.catalogue.internal_model.work.{
+  AbstractRootConcept,
+  Agent,
+  Concept,
+  Contributor,
+  Meeting,
+  Organisation,
+  Period,
+  Person,
+  Place,
+  Subject
+}
+import weco.pipeline.transformer.sierra.transformers.{
+  SierraContributors,
+  SierraSubjects
+}
+import weco.sierra.models.data.SierraBibData
+import weco.sierra.models.identifiers.SierraBibNumber
 
-object ConceptTypeHarmoniser extends Logging {
+object SubjectsAndContributors extends Logging {
 
   /**
     * Given lists of subjects and contributors, ensure that any top-level Concepts
@@ -38,10 +55,38 @@ object ConceptTypeHarmoniser extends Logging {
     *     - extracting type from subject is more reliable, trust that definition more than this one.
     */
   def apply(
+    bibId: SierraBibNumber,
+    bibData: SierraBibData
+  ): (List[Subject[IdState.Unminted]], List[Contributor[IdState.Unminted]]) =
+    SubjectsAndContributors(
+      subjects = SierraSubjects(bibId, bibData),
+      contributors = SierraContributors(bibData)
+    )
+
+  def apply(
     subjects: List[Subject[IdState.Unminted]],
     contributors: List[Contributor[IdState.Unminted]]
   ): (List[Subject[IdState.Unminted]], List[Contributor[IdState.Unminted]]) = {
-    val harmonisedSubjects = subjects
+    val fixedSubjects = harmonisedSubjects(subjects)
+    (fixedSubjects, contributors)
+  }
+
+  /**
+    * Given a list of subjects, ensure that any of them that have the same sourceIdentifier type and value
+    * also have the same ontologytype in the sourceidentifier.
+    *
+    * In a Subject, the Subject itself represents a Concept, and it also contains a list of Concepts.
+    *
+    * When the subject is a compound consisting of more than one constituent concept, those constituents
+    * are only derived from the label, so there is nothing to do to them here.
+    *
+    * A simple subject, consisting of only one constituent concept will also need to have that concept
+    * harmonised.
+    */
+  private def harmonisedSubjects(
+    subjects: List[Subject[IdState.Unminted]]
+  ): List[Subject[IdState.Unminted]] = {
+    subjects
       .groupBy(
         // Only Items (which are not Concepts) have ids with more than one sourceidentifier
         subject =>
@@ -59,9 +104,15 @@ object ConceptTypeHarmoniser extends Logging {
       }
       .toList
       .distinct
-    (harmonisedSubjects, contributors)
   }
 
+  /**
+    * Return the sourceIdentifier information, ignoring its ontologytype.
+    *
+    * As this is only used to perform a type-insensitive match, a tuple of the values will do.
+    *
+    * //TODO: should this be a property of SourceIdentifier?
+    */
   private def typeFreeSourceIdentifer(
     sourceIdentifierOption: Option[SourceIdentifier]
   ): Option[(IdentifierType, String)] =
@@ -80,26 +131,56 @@ object ConceptTypeHarmoniser extends Logging {
       subjects.map(_.id.sourceIdentifier.ontologyType)
     )
     subjects map { subject =>
+      val newSourceIdentifier =
+        subject.id.sourceIdentifier.copy(ontologyType = bestType)
       subject.copy(
         id = subject.id
           .copy(
-            sourceIdentifier =
-              subject.id.sourceIdentifier.copy(ontologyType = bestType)
+            sourceIdentifier = newSourceIdentifier
+          ),
+        concepts = if (subject.concepts.length == 1) {
+          val oldConcept = subject.concepts.head
+          List(
+            conceptTypeMap(bestType)(
+              oldConcept.id.copy(sourceIdentifier = newSourceIdentifier),
+              oldConcept.label
+            )
           )
+        } else subject.concepts
       )
     }
   }
 
+  private val conceptTypeMap
+    : Map[String, (IdState.Identifiable, String) => AbstractRootConcept[
+      IdState.Identifiable
+    ]] = Map(
+    "Concept" -> (new Concept[IdState.Identifiable](_, _)),
+    "Agent" -> (new Agent[IdState.Identifiable](_, _)),
+    "Place" -> (new Place[IdState.Identifiable](_, _)),
+    "Period" -> (new Period[IdState.Identifiable](_, _)),
+    "Meeting" -> (new Meeting[IdState.Identifiable](_, _)),
+    "Organisation" -> (new Organisation[IdState.Identifiable](_, _)),
+    "Person" -> (new Person[IdState.Identifiable](_, _))
+  )
+
+  /**
+    * Given a list of Abstract Concept Ontology Types, return the most specific one from the list
+    *
+    * Given that there isn't a linear hierarchy of all types, the actual algorithm for this is fairly crude.
+    * There are two "base" or "vague" types, and all the others are equally as "specific".
+    * In real data, this function will not be called for many documents, and when it is, it
+    * will almost always be to resolve disharmony between one of the vague types and one more specific one.
+    */
   private def mostSpecificType(types: List[String]): String = {
-    //
     // Filter out the vague types.
     val specificTypes = types.filterNot(Seq("Agent", "Concept").contains(_))
     specificTypes match {
       // if there are no more specific types, Agent is more specific than Concept.
       case Nil => if (types.contains("Agent")) "Agent" else "Concept"
-      // Ideally, the specific list will have only one entry.
+      // Ideally, and in most cases, the specific list will have only one entry.
       case List(bestType) => bestType
-      // If not, log if there are more than one, and return the head.
+      // If not, log that there are more than one, and return the head.
       case listOfTypes =>
         info(
           s"Multiple specific types encountered for the same id: $specificTypes, choosing $listOfTypes.head"
--- pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/SubjectsAndContributorsTest.scala
+++ pipeline/transformer/transformer_sierra/src/test/scala/weco/pipeline/transformer/sierra/SubjectsAndContributorsTest.scala
@@ -10,7 +10,7 @@ import weco.catalogue.internal_model.identifiers.{
   IdentifierType,
   SourceIdentifier
 }
-import weco.catalogue.internal_model.work.Subject
+import weco.catalogue.internal_model.work.{Concept, Person, Place, Subject}
 
 class SubjectsAndContributorsTest
     extends AnyFunSpec
@@ -18,6 +18,8 @@ class SubjectsAndContributorsTest
     with TableDrivenPropertyChecks
     with Inspectors {
   describe("Harmonising subjects") {
+    //TODO: Make sure it also fixes the single constituent concept
+    // when a subject is a simple one.
     it("replaces vague concepts with more specific ones") {
       forAll(
         Table(
@@ -73,101 +75,218 @@ class SubjectsAndContributorsTest
         }
       }
     }
-  }
 
-  it("collapses subjects that are no longer unique") {
-    val vagueSubject = new Subject(
-      label = "Maimonides",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Concept",
-          "sh00000000"
+    it("collapses subjects that are no longer unique") {
+      val vagueSubject = new Subject(
+        label = "Maimonides",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Concept",
+            "sh00000000"
+          )
         )
       )
-    )
-    val specificSubject1 = new Subject(
-      label = "Maimonides",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Person",
-          "sh00000000"
+      val specificSubject1 = new Subject(
+        label = "Maimonides",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Person",
+            "sh00000000"
+          )
         )
       )
-    )
-    val specificSubject2 = new Subject(
-      label = "Maimonides",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Organisation",
-          "sh00000000"
+      val specificSubject2 = new Subject(
+        label = "Maimonides",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Organisation",
+            "sh00000000"
+          )
         )
       )
-    )
-    val (subjects, _) =
-      SubjectsAndContributors(
-        subjects = List(specificSubject1, specificSubject2, vagueSubject),
-        contributors = Nil
-      )
-    // They all had teh same label before, so they are no longer unique having harmonised their types
-    subjects.length shouldBe 1
-
-    subjects.head
-      .asInstanceOf[Subject[IdState.Identifiable]]
-      .id
-      .sourceIdentifier
-      .ontologyType shouldBe "Person"
-
-  }
-
-  it("chooses the first specific concept if there are more than one") {
-    val vagueSubject = new Subject(
-      label = "Maimonides, in his work on Logic",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Concept",
-          "sh00000000"
+      val (subjects, _) =
+        SubjectsAndContributors(
+          subjects = List(specificSubject1, specificSubject2, vagueSubject),
+          contributors = Nil
         )
-      )
-    )
-    val specificSubject1 = new Subject(
-      label = "Maimonides",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Person",
-          "sh00000000"
-        )
-      )
-    )
-    val specificSubject2 = new Subject(
-      label = "Maimonides and his chums",
-      id = Identifiable(
-        SourceIdentifier(
-          IdentifierType.LCSubjects,
-          "Organisation",
-          "sh00000000"
-        )
-      )
-    )
-    val (subjects, _) =
-      SubjectsAndContributors(
-        subjects = List(specificSubject1, specificSubject2, vagueSubject),
-        contributors = Nil
-      )
+      // They all had the same label before, so they are no longer unique having harmonised their types
+      subjects.length shouldBe 1
 
-    subjects.length shouldBe 3
-
-    forAll(subjects) { subject =>
-      subject
+      subjects.head
         .asInstanceOf[Subject[IdState.Identifiable]]
         .id
         .sourceIdentifier
         .ontologyType shouldBe "Person"
     }
-  }
 
+    it("chooses the first specific concept if there are more than one") {
+      val vagueSubject = new Subject(
+        label = "Maimonides, in his work on Logic",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Concept",
+            "sh00000000"
+          )
+        )
+      )
+      val specificSubject1 = new Subject(
+        label = "Maimonides",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Person",
+            "sh00000000"
+          )
+        )
+      )
+      val specificSubject2 = new Subject(
+        label = "Maimonides and his chums",
+        id = Identifiable(
+          SourceIdentifier(
+            IdentifierType.LCSubjects,
+            "Organisation",
+            "sh00000000"
+          )
+        )
+      )
+      val (subjects, _) =
+        SubjectsAndContributors(
+          subjects = List(specificSubject1, specificSubject2, vagueSubject),
+          contributors = Nil
+        )
+
+      subjects.length shouldBe 3
+
+      forAll(subjects) { subject =>
+        subject
+          .asInstanceOf[Subject[IdState.Identifiable]]
+          .id
+          .sourceIdentifier
+          .ontologyType shouldBe "Person"
+      }
+    }
+
+    it("corrects the type of the sole concept in the concepts list") {
+      val conceptId = Identifiable(
+        SourceIdentifier(
+          IdentifierType.LCSubjects,
+          "Concept",
+          "sh00000000"
+        )
+      )
+      val personId = Identifiable(
+        SourceIdentifier(
+          IdentifierType.LCSubjects,
+          "Person",
+          "sh00000000"
+        )
+      )
+
+      val vagueSubject = new Subject(
+        label = "The homunculus in the mind of Maimonides",
+        id = conceptId,
+        concepts =
+          List(new Concept(id = conceptId, label = "Maimonides' homunculus"))
+      )
+
+      val specificSubject = new Subject(
+        label = "Maimonides",
+        id = personId,
+        concepts = List(new Person(id = personId, label = "Maimonides"))
+      )
+      val (subjects, _) =
+        SubjectsAndContributors(
+          subjects = List(specificSubject, vagueSubject),
+          contributors = Nil
+        )
+      subjects.length shouldBe 2
+
+      forAll(subjects) { subject =>
+        subject.concepts.head
+          .asInstanceOf[Person[IdState.Identifiable]]
+          .id
+          .sourceIdentifier
+          .ontologyType shouldBe "Person"
+      }
+    }
+
+    it("leaves the label-derived concept list in a compound subject alone") {
+      val conceptId = Identifiable(
+        SourceIdentifier(
+          IdentifierType.LCSubjects,
+          "Concept",
+          "sh00000000"
+        )
+      )
+      val personId = Identifiable(
+        SourceIdentifier(
+          IdentifierType.LCSubjects,
+          "Person",
+          "sh00000000"
+        )
+      )
+
+      val vagueSubject = new Subject(
+        label = "The homunculus in the mind of Maimonides",
+        id = conceptId,
+        concepts = List(
+          new Concept(
+            id = IdState.Identifiable(
+              sourceIdentifier = SourceIdentifier(
+                identifierType = IdentifierType.LabelDerived,
+                value = "Maimonides",
+                ontologyType = "Concept"
+              )
+            ),
+            label = "Maimonides"
+          ),
+          new Concept(
+            id = IdState.Identifiable(
+              sourceIdentifier = SourceIdentifier(
+                identifierType = IdentifierType.LabelDerived,
+                value = "homunculus",
+                ontologyType = "Concept"
+              )
+            ),
+            label = "homunculus"
+          )
+        )
+      )
+
+      val specificSubject = new Subject(
+        label = "Maimonides of Cordoba",
+        id = personId,
+        concepts = List(Person("Maimonides"), Place("Cordoba"))
+      )
+
+      val (subjects, _) =
+        SubjectsAndContributors(
+          subjects = List(vagueSubject, specificSubject),
+          contributors = Nil
+        )
+
+      subjects.length shouldBe 2
+      // The vague subject should be changed to be Person
+      forAll(subjects) { subject =>
+        subject
+          .asInstanceOf[Subject[IdState.Identifiable]]
+          .id
+          .sourceIdentifier
+          .ontologyType shouldBe "Person"
+      }
+      // But the concepts list is left unchanged.
+      forAll(subjects.head.concepts) { concept =>
+        concept
+          .asInstanceOf[Concept[IdState.Identifiable]]
+          .id
+          .sourceIdentifier
+          .ontologyType shouldBe "Concept"
+      }
+
+    }
+  }
 }
