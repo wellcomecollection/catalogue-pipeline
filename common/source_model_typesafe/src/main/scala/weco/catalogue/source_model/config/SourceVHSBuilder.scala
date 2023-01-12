84257b19580e6e27e425ca8f652de54f3edf9b3b
Alex Chan <alex@alexwlchan.net>
2023-01-12 12:17:50 +0000

    remove some now-unneeded implicits

--- common/source_model_typesafe/src/main/scala/weco/catalogue/source_model/config/SourceVHSBuilder.scala
+++ common/source_model_typesafe/src/main/scala/weco/catalogue/source_model/config/SourceVHSBuilder.scala
@@ -4,7 +4,6 @@ import com.typesafe.config.Config
 import org.scanamo.generic.auto._
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
 import software.amazon.awssdk.services.s3.S3Client
-import software.amazon.awssdk.transfer.s3.S3TransferManager
 import weco.storage.s3.{S3ObjectLocation, S3ObjectLocationPrefix}
 import weco.storage.store.{HybridStoreWithMaxima, VersionedHybridStore}
 import weco.storage.store.dynamo.{
@@ -25,8 +24,6 @@ object SourceVHSBuilder {
   def build[T](config: Config, namespace: String = "vhs")(
     implicit codec: Codec[T]): SourceVHS[T] = {
     implicit val s3Client: S3Client = S3Client.builder().build()
-    implicit val s3TransferManager: S3TransferManager =
-      S3TransferManager.builder().build()
 
     implicit val dynamoClient: DynamoDbClient =
       DynamoBuilder.buildDynamoClient
