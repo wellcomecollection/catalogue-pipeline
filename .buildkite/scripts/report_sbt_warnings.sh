076aac0222553440e2528d5d93f0e45b0a5ed50e
Paul Butcher <paul.butcher@gmail.com>
2023-01-26 16:07:27 +0000

    soft fail on warnings

--- .buildkite/scripts/report_sbt_warnings.sh
+++ .buildkite/scripts/report_sbt_warnings.sh
@@ -6,5 +6,7 @@ echo "$WARNING_COUNT warning(s) found"
 
 if [ $WARNING_COUNT != 0 ]
 then
+  buildkite-agent step update "outcome" "soft_failed"
   buildkite-agent annotate "Completed with $WARNING_COUNT warning(s)" --style 'warning' --context 'ctx-warn'
+  exit 2
 fi
