107b005931b723ce949570d5fd40d2374d62114b
Paul Butcher <paul.butcher@gmail.com>
2023-01-27 10:11:57 +0000

    better output

--- builds/run_formatting.sh
+++ builds/run_formatting.sh
@@ -7,14 +7,14 @@ ECR_REGISTRY="760097843905.dkr.ecr.eu-west-1.amazonaws.com"
 
 ROOT=$(git rev-parse --show-toplevel)
 
-mkdir -p reports
+mkdir -p .reports
 
 docker run --tty --rm \
 	--volume "$ROOT:/repo" \
 	--workdir /repo \
 	"public.ecr.aws/hashicorp/terraform:light" fmt -recursive
 
-./builds/run_sbt_task_in_docker.sh "scalafmt" | tee reports/scalafmt.out
+./builds/run_sbt_task_in_docker.sh "scalafmt" | tee .reports/scalafmt.out
 
 docker run --tty --rm \
 	--volume "$ROOT:/repo" \
