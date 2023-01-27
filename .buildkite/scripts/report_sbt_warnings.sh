5940c472ccc7862fc0e0df8bfb84c6f97c7e0f3e
Paul Butcher <paul.butcher@gmail.com>
2023-01-27 09:55:04 +0000

    report unique warnings

--- .buildkite/scripts/report_sbt_warnings.sh
+++ .buildkite/scripts/report_sbt_warnings.sh
@@ -1,10 +1,12 @@
 echo "Scanning for warnings"
 WARNINGS=$(cat - | grep '\[warn\]')
-WARNING_COUNT=$(echo $WARNINGS | sed '/^$/d' | wc -l | sed 's/^[ \t]*//')
-UNIQUE_WARNINGS=$(echo $WARNINGS | sort | uniq)
-UNIQUE_WARNING_COUNT=$(echo UNIQUE_WARNINGS | sed '/^$/d' | wc -l| sed 's/^[ \t]*//')
+WARNING_COUNT=$(echo "$WARNINGS" | sed '/^$/d' | wc -l | sed 's/^[ \t]*//')
+UNIQUE_WARNINGS=$(echo "$WARNINGS" | sort | uniq)
+UNIQUE_WARNING_COUNT=$(echo "$UNIQUE_WARNINGS" | sed '/^$/d' | wc -l| sed 's/^[ \t]*//')
+MESSAGE="Completed with $WARNING_COUNT ($UNIQUE_WARNING_COUNT distinct) warning(s)\n\`\`\`term\n$UNIQUE_WARNINGS\n\`\`\`"
+echo "$MESSAGE"
 if [ $WARNING_COUNT != 0 ]
 then
-  echo -e "Completed with $WARNING_COUNT ($UNIQUE_WARNING_COUNT distinct) warning(s)\n\`\`\`term\n$UNIQUE_WARNINGS\n\`\`\`" | buildkite-agent annotate  --style 'warning' --context 'ctx-warn'
+  echo -e "$MESSAGE" | buildkite-agent annotate  --style 'warning' --context 'ctx-warn'
   exit 2
 fi
