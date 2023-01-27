25539eaa97e014c6a18c84904024c987ce848981
Paul Butcher <paul.butcher@gmail.com>
2023-01-27 08:56:38 +0000

    report unique warnings

--- .buildkite/scripts/report_sbt_warnings.sh
+++ .buildkite/scripts/report_sbt_warnings.sh
@@ -1,11 +1,9 @@
 echo "Scanning for warnings"
-WARNINGS=$(cat - | grep '\[warn\]')
+WARNINGS=$(cat - | grep '\[warn\]'| sort | uniq)
 WARNING_COUNT=$(echo $WARNINGS | sed '/^$/d' | wc -l)
-
-echo "$WARNING_COUNT warning(s) found"
 
 if [ $WARNING_COUNT != 0 ]
 then
-  echo -e "Completed with $WARNING_COUNT warning(s)\n\`\`\`term\n$WARNINGS\n\`\`\`" | buildkite-agent annotate  --style 'warning' --context 'ctx-warn'
+  echo -e "Completed with $WARNING_COUNT unique warning(s)\n\`\`\`term\n$WARNINGS\n\`\`\`" | buildkite-agent annotate  --style 'warning' --context 'ctx-warn'
   exit 2
 fi
