78ad1257c44115539118cbb70d04510581a0820b
Alex Chan <alex@alexwlchan.net>
2023-02-13 10:59:43 +0000

    remove more bits from Image

--- common/internal_model2/src/main/scala/weco/catalogue/internal_model/image/Image.scala
+++ common/internal_model2/src/main/scala/weco/catalogue/internal_model/image/Image.scala
@@ -23,16 +23,6 @@ case class Image[State <: ImageState](
 ) {
   def id: String = state.id
   def sourceIdentifier: SourceIdentifier = state.sourceIdentifier
-
-  def transition[OutState <: ImageState](args: OutState#TransitionArgs = ())(
-    implicit transition: ImageFsm.Transition[State, OutState]
-  ): Image[OutState] =
-    Image[OutState](
-      state = transition.state(this, args),
-      version = version,
-      locations = locations,
-      modifiedTime = modifiedTime
-    )
 }
 
 sealed trait ImageState {
@@ -44,73 +34,10 @@ sealed trait ImageState {
   def id: String = canonicalId.toString
 }
 
-/** ImageState represents the state of the image in the pipeline. Its stages are
-  * as follows:
-  *
-  * \| \| (merger) ▼ Initial \| \| (inferrer) ▼ Augmented \| \| (ingestor) ▼
-  * Indexed
-  */
 object ImageState {
 
   case class Initial(
     sourceIdentifier: SourceIdentifier,
     canonicalId: CanonicalId
-  ) extends ImageState {
-    type TransitionArgs = Unit
-  }
-
-  case class Augmented(
-    sourceIdentifier: SourceIdentifier,
-    canonicalId: CanonicalId,
-    inferredData: InferredData
-  ) extends ImageState {
-    type TransitionArgs = InferredData
-  }
-}
-
-// ImageFsm contains all of the possible transitions between image states
-object ImageFsm {
-  import ImageState._
-
-  sealed trait Transition[InState <: ImageState, OutState <: ImageState] {
-    def state(self: Image[InState], args: OutState#TransitionArgs): OutState
-  }
-
-  implicit val initialToAugmented = new Transition[Initial, Augmented] {
-    def state(
-      self: Image[Initial],
-      inferredData: InferredData
-    ): Augmented =
-      Augmented(
-        sourceIdentifier = self.state.sourceIdentifier,
-        canonicalId = self.state.canonicalId,
-        inferredData = inferredData
-      )
-  }
-}
-
-case class InferredData(
-  // We split the feature vector so that it can fit into
-  // ES's dense vector type (max length 2048)
-  features1: List[Float],
-  features2: List[Float],
-  reducedFeatures: List[Float],
-  palette: List[String],
-  averageColorHex: Option[String],
-  binSizes: List[List[Int]],
-  binMinima: List[Float],
-  aspectRatio: Option[Float]
-)
-
-object InferredData {
-  def empty: InferredData = InferredData(
-    features1 = Nil,
-    features2 = Nil,
-    reducedFeatures = Nil,
-    palette = Nil,
-    averageColorHex = None,
-    binSizes = Nil,
-    binMinima = Nil,
-    aspectRatio = None
-  )
+  ) extends ImageState
 }
