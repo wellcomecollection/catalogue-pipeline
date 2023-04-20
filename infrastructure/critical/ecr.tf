ef667885850d72e63569f89d719cbd50950535c6
Alex Chan <alex@alexwlchan.net>
2023-04-20 09:16:51 +0100

    better naming

--- infrastructure/critical/ecr.tf
+++ infrastructure/critical/ecr.tf
@@ -1,5 +1,5 @@
 locals {
-  images = [
+  pipeline_images = [
     "nginx_services",
     "transformer_miro",
     "transformer_sierra",
@@ -40,14 +40,14 @@ locals {
   })
 }
 
-resource "aws_ecr_repository" "service" {
-  for_each = toset(local.images)
+resource "aws_ecr_repository" "pipeline_services" {
+  for_each = toset(local.pipeline_images)
 
   name = "uk.ac.wellcome/${each.key}"
 }
 
-resource "aws_ecr_lifecycle_policy" "content_webapp" {
-  for_each = aws_ecr_repository.service
+resource "aws_ecr_lifecycle_policy" "pipeline_services" {
+  for_each = aws_ecr_repository.pipeline_services
 
   repository = each.value.name
   policy     = local.ecr_policy_only_keep_the_last_100_images
