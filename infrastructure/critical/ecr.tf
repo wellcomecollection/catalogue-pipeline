6ba68ac2dcb2e82257324afbaffa32ee41a5609b
Alex Chan <alex@alexwlchan.net>
2023-04-20 09:15:17 +0100

    Add an ECR repo policy for the pipeline services

--- infrastructure/critical/ecr.tf
+++ infrastructure/critical/ecr.tf
@@ -21,10 +21,34 @@ locals {
     "router",
     "batcher",
   ]
+
+  ecr_policy_only_keep_the_last_100_images = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Only keep the last 100 images in a repo"
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 100
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
 }
 
 resource "aws_ecr_repository" "service" {
   for_each = toset(local.images)
 
   name = "uk.ac.wellcome/${each.key}"
+}
+
+resource "aws_ecr_lifecycle_policy" "content_webapp" {
+  for_each = aws_ecr_repository.service
+
+  repository = each.value.name
+  policy     = local.ecr_policy_only_keep_the_last_100_images
 }
