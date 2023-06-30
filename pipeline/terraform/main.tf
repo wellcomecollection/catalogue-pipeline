fe7ec69349bf7ff866462f6f71b3e2db3adbb0d6
Alex Chan <alex@alexwlchan.net>
2023-06-30 12:10:19 +0100

    Combine the Terraform into a single file

--- pipeline/terraform/main.tf
+++ pipeline/terraform/main.tf
@@ -63,3 +63,47 @@ module "pipelines" {
     aws.catalogue = aws.catalogue
   }
 }
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::760097843905:role/platform-admin"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  alias = "catalogue"
+
+  assume_role {
+    role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
+  }
+}
+
+provider "ec" {}
+
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    ec = {
+      source  = "elastic/ec"
+      version = "0.2.1"
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+
+    bucket         = "wellcomecollection-platform-infra"
+    key            = "terraform/catalogue/pipeline.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
+  }
+}
