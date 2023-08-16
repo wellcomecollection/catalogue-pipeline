0789d560ff1facaee69c205aaf91b1e4dc4b7b25
Paul Butcher <paul.butcher@gmail.com>
2023-08-16 15:53:36 +0100

    Remove redundant code

--- pipeline/merger/src/main/scala/weco/pipeline/merger/models/MergerItem.scala
+++ pipeline/merger/src/main/scala/weco/pipeline/merger/models/MergerItem.scala
@@ -20,37 +20,6 @@ case class MergerItem[State](item: Item[State]) {
         .flatMap(_.locations)
     )
 
-  def copyLicenceFrom(sources: Seq[Work[Identified]]): Item[State] = {
-    val candidateLicences = sources
-      .flatMap(_.data.items)
-      .flatMap(_.locations)
-      .flatMap(_.license)
-      .distinct
-    candidateLicences match {
-      // Only copy the licence if there is no ambiguity.
-      // The purpose of this is to harmonise the licences on a work when the source overrides the target
-      // if there are multiple conflicting sources, then we might as well leave it as it is.
-      case Seq(licence) => copyLicence(licence)
-      //
-      case _ =>
-        item // Probably log that nothing has changed, particularly if there are more than one licence
-    }
-  }
-
-  def copyLicence[T](
-    newLicence: License
-  ): Item[State] = {
-
-    item.copy(
-      locations = item.locations map {
-        case digitalLocation: DigitalLocation =>
-          digitalLocation.copy(license = Some(newLicence))
-        case physicalLocation: PhysicalLocation =>
-          physicalLocation.copy(license = Some(newLicence))
-      }
-    )
-  }
-
   def isAvailableOnline: Boolean = item.locations.exists(
     _.locationType == LocationType.OnlineResource
   )
