8775a767e65c6149447c05309c839db53051c7d2
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2023-09-29 10:20:43 +0000

    Apply auto-formatting rules

--- scripts/suppress_miro_batch.py
+++ scripts/suppress_miro_batch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from miro_updates import suppress_image, update_miro_image_suppressions_doc
 
+
 def suppress_miro(miro_id):
     message = "Brought to Life/Science Museum images. Take-down request signed off by Wellcome Collection quarterly governance meeting, 24 August 2023"
     try:
@@ -11,10 +12,10 @@ def suppress_miro(miro_id):
         with open("suppress_miro_failed_ids.txt", "a") as failed_ids_file:
             failed_ids_file.write(f"{miro_id}\n")
 
+
 if __name__ == "__main__":
     with open("btl_miro_images_to_suppress.txt") as btl_miro_images_to_suppress:
-       for id in btl_miro_images_to_suppress:
+        for id in btl_miro_images_to_suppress:
             suppress_miro(id)
 
     update_miro_image_suppressions_doc()
-
