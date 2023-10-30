9b374ae86bf44027ce3f025a3aa3c8c28ce36693
jamie <j.parkinson@wellcome.org>
2023-10-30 11:25:43 +0000

    Need permissions for the secret

--- tei_adapter/terraform/tei_updater/iam_role_policy.tf
+++ tei_adapter/terraform/tei_updater/iam_role_policy.tf
@@ -8,3 +8,15 @@ resource "aws_iam_role_policy" "tei_updater_s3_read_write" {
   role   = module.tei_updater_lambda.role_name
   policy = data.aws_iam_policy_document.allow_s3_read_write.json
 }
+
+resource "aws_iam_role_policy" "tei_updater_read_github_token_secret" {
+  role   = module.tei_updater_lambda.role_name
+  policy = data.aws_iam_policy_document.allow_secret_read.json
+}
+
+data "aws_iam_policy_document" "allow_secret_read" {
+  statement {
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = ["${var.github_token_secret}*"]
+  }
+}
