62df030d5baef9ce24b831e820915d6a0cdc658b
Paul Butcher <paul.butcher@gmail.com>
2023-12-14 16:42:24 +0000

    METS: Test various file/flocat behaviours (#2497)
    * Remove Either from methods that always return Right
    
    * make the transforms a bit more SRP & DRY
    
    * clean up import
    
    * move normalisation tests to new ImageData suite
    
    * tests now expect non-normalised file references
    
    * remove last bit of premature normalisation
    
    * add multiple flocat test
    
    * test scenarios where files are ignored
    
    * test mimetype handline

--- pipeline/transformer/transformer_mets/src/test/scala/weco/pipeline/transformer/mets/transformer/GoobiMetsXmlTest.scala
+++ pipeline/transformer/transformer_mets/src/test/scala/weco/pipeline/transformer/mets/transformer/GoobiMetsXmlTest.scala
@@ -1,6 +1,6 @@
 package weco.pipeline.transformer.mets.transformer
 
-import org.scalatest.EitherValues
+import org.scalatest.{EitherValues, LoneElement}
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import weco.fixtures.LocalResources
@@ -12,7 +12,8 @@ class GoobiMetsXmlTest
     with Matchers
     with EitherValues
     with LocalResources
-    with MetsGenerators {
+    with MetsGenerators
+    with LoneElement {
 
   val xml = readResource("b30246039.xml")
 
@@ -38,6 +39,146 @@ class GoobiMetsXmlTest
     GoobiMetsXml(xmlMultipleDistictIds).recordIdentifier shouldBe a[
       Left[_, _]
     ]
+  }
+
+  it("only fetches one fileReference per file") {
+    info(
+      "according to the schema (https://www.loc.gov/standards/mets/mets.xsd)"
+    )
+    info(
+      "each FLocat underneath a single file element should identify/contain identical copies of a single file."
+    )
+    info("we normally only have one <FLocat> per <file>.")
+    MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+        <mets:file ID="FILE_0001_OBJECTS" MIMETYPE="image/jp2">
+          <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0001.jp2"/>
+          <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0002.jp2"/>
+        </mets:file>
+      </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences shouldBe List(
+      FileReference(
+        id = "FILE_0001_OBJECTS",
+        location = "objects/hello_0001.jp2",
+        listedMimeType = Some("image/jp2")
+      )
+    )
+  }
+
+  it("skips over unusable file definitions, but returns good ones") {
+    MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+        <mets:file ID="FILE_0001_OBJECTS" MIMETYPE="image/jp2">
+        </mets:file>
+        <mets:file ID="FILE_0002_OBJECTS" MIMETYPE="image/jp2">
+          <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0002.jp2"/>
+        </mets:file>
+      </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences shouldBe List(
+      FileReference(
+        id = "FILE_0002_OBJECTS",
+        location = "objects/hello_0002.jp2",
+        listedMimeType = Some("image/jp2")
+      )
+    )
+  }
+
+  it("guesses the mime type if none is provided") {
+    val reference = MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+          <mets:file ID="FILE_0001_OBJECTS">
+            <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0001.pdf"/>
+          </mets:file>
+        </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences.loneElement
+
+    reference shouldBe FileReference(
+      id = "FILE_0001_OBJECTS",
+      location = "objects/hello_0001.pdf",
+      listedMimeType = None
+    )
+    reference.mimeType shouldBe Some("application/pdf")
+  }
+
+  it("treats an empty mimetype the same as an absent mimetype") {
+    val reference = MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+          <mets:file ID="FILE_0001_OBJECTS">
+            <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0001.pdf" MIMETYPE=""/>
+          </mets:file>
+        </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences.loneElement
+
+    reference shouldBe FileReference(
+      id = "FILE_0001_OBJECTS",
+      location = "objects/hello_0001.pdf",
+      listedMimeType = None
+    )
+    reference.mimeType shouldBe Some("application/pdf")
+  }
+
+  it("returns no mime type if it cannot be guessed") {
+    val reference = MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+          <mets:file ID="FILE_0001_OBJECTS">
+            <mets:FLocat LOCTYPE="URL" xlink:href="objects/hello_0001.unknownextension"/>
+          </mets:file>
+        </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences.loneElement
+
+    reference shouldBe FileReference(
+      id = "FILE_0001_OBJECTS",
+      location = "objects/hello_0001.unknownextension",
+      listedMimeType = None
+    )
+    reference.mimeType shouldBe None
+  }
+
+  it("does not return a filereference if a file has no FLocat ") {
+    MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+        <mets:file ID="FILE_0001_OBJECTS" MIMETYPE="image/jp2">
+        </mets:file>
+      </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences shouldBe Nil
+  }
+
+  it("does not return a filereference if an FLocat has no href") {
+    MetsXml(
+      metsXmlWith(
+        recordIdentifier = "deadbeef",
+        fileSec = <mets:fileSec><mets:fileGrp USE="OBJECTS">
+          <mets:file ID="FILE_0001_OBJECTS" MIMETYPE="image/jp2">
+            <mets:FLocat LOCTYPE="URL"/>
+          </mets:file>
+        </mets:fileGrp></mets:fileSec>,
+        structMap = structMap
+      )
+    ).value.fileReferences shouldBe Nil
   }
 
   it("parses file references mapping from XML") {
