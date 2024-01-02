c596467238bb4585329f59bba978dbaa91c75960
Paul Butcher <paul.butcher@gmail.com>
2024-01-02 15:35:47 +0000

    METS: merge candidates could be CALM or Sierra (#2499)
    * merge candidates could be CALM or Sierra
    
    * add case test
    
    * rename MetsGenerators in anticipation of adding Archivematica Mets

---
+++ pipeline/transformer/transformer_mets/src/test/scala/weco/pipeline/transformer/mets/transformers/MetsMergeCandidateTest.scala
@@ -0,0 +1,47 @@
+package weco.pipeline.transformer.mets.transformers
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import weco.catalogue.internal_model.identifiers.IdentifierType
+
+import scala.util.{Failure, Try}
+
+class MetsMergeCandidateTest extends AnyFunSpec with Matchers {
+
+  describe("extracting the merge candidate") {
+
+    it("returns a Sierra merge candidate if the id is a b number") {
+      MetsMergeCandidate(
+        "b12345678"
+      ).id.sourceIdentifier.identifierType shouldBe IdentifierType.SierraSystemNumber
+    }
+    it(
+      "also works with uppercase B-Numbers"
+    ) {
+      MetsMergeCandidate(
+        "B12345678"
+      ).id.sourceIdentifier.identifierType shouldBe IdentifierType.SierraSystemNumber
+    }
+
+    it("returns a CALM merge candidate if the id is a CALM reference number") {
+      MetsMergeCandidate(
+        "A/BAAD/CAFE"
+      ).id.sourceIdentifier.identifierType shouldBe IdentifierType.CalmRefNo
+
+    }
+
+    it(
+      "can tell the difference between a b number and a b at the start of the id"
+    ) {
+      MetsMergeCandidate(
+        "b/a/nana"
+      ).id.sourceIdentifier.identifierType shouldBe IdentifierType.CalmRefNo
+    }
+
+    it("fails if the id type could not be determined") {
+      Try(MetsMergeCandidate("!@£$%^&*()")) shouldBe a[Failure[_]]
+    }
+
+  }
+
+}
