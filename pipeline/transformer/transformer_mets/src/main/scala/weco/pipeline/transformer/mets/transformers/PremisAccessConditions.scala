3ce562cdc5e759f50965b118c33859852e6ec6cf
Paul Butcher <paul.butcher@gmail.com>
2024-01-03 15:35:59 +0000

    add Premis conditions exmaple

--- pipeline/transformer/transformer_mets/src/main/scala/weco/pipeline/transformer/mets/transformers/PremisAccessConditions.scala
+++ pipeline/transformer/transformer_mets/src/main/scala/weco/pipeline/transformer/mets/transformers/PremisAccessConditions.scala
@@ -19,6 +19,36 @@ case class PremisAccessConditions(
 }
 
 object PremisAccessConditions {
+
+  /** The access conditions are encoded a premis elementin the METS. For
+    * example:
+    * {{{
+    *     <mets:rightsMD ID="rightsMD_1">
+    *         <mets:mdWrap MDTYPE="PREMIS:RIGHTS">
+    *             <mets:xmlData>
+    *                 <premis:rightsStatement xmlns:premis="http://www.loc.gov/premis/v3" xsi:schemaLocation="http://www.loc.gov/premis/v3 http://www.loc.gov/standards/premis/v3/premis.xsd">
+    *                     <premis:rightsBasis>Copyright</premis:rightsBasis>
+    *                     <premis:copyrightInformation>
+    *                         <premis:copyrightStatus>copyrighted</premis:copyrightStatus>
+    *                         <premis:copyrightJurisdiction>UK</premis:copyrightJurisdiction>
+    *                         <premis:copyrightStatusDeterminationDate />
+    *                         <premis:copyrightNote>In copyright</premis:copyrightNote>
+    *                     </premis:copyrightInformation>
+    *                     <premis:rightsGranted>
+    *                         <premis:act>use</premis:act>
+    *                         <premis:rightsGrantedNote>Open</premis:rightsGrantedNote>
+    *                     </premis:rightsGranted>
+    *                 </premis:rightsStatement>
+    *             </mets:xmlData>
+    *         </mets:mdWrap>
+    *     </mets:rightsMD>
+    * }}}
+    *
+    * In this example,
+    *   - The accessStatus is "Open"
+    *   - The licence is "In Copyright"
+    *     - There is no usage string
+    */
   def apply(rightsMd: Node): PremisAccessConditions = {
     val rightsStatement = rightsMd \ "mdWrap" \ "xmlData" \ "rightsStatement"
     val copyrightNoteElem =
