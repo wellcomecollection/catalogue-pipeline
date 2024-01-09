502a5c3ca887278a2e3cb7143c38ff8cfed87f14
jamie <j.parkinson@wellcome.org>
2024-01-09 16:00:59 +0000

    Fix variable

--- pipeline/terraform/modules/stack/elastic_pipeline.tf
+++ pipeline/terraform/modules/stack/elastic_pipeline.tf
@@ -9,7 +9,7 @@ resource "ec_deployment" "pipeline" {
   version = data.ec_stack.latest_patch.version
 
   region                 = data.ec_stack.latest_patch.region
-  deployment_template_id = local.es_deployment_template_id
+  deployment_template_id = var.es_cluster_deployment_template
 
   traffic_filter = local.network_config.traffic_filters
 
