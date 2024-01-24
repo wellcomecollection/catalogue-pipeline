bbdbc40aec0e78729ba85a7a98d032a6718563e2
Paul Butcher <paul.butcher@gmail.com>
2024-01-24 10:49:20 +0000

    Report sbt warnings (#2310)
    * report sbt warnings in buildkite
    
    * fix path typo
    
    * add execture permission to script
    
    * be a bit clearer in the logs
    
    * soft fail on warnings
    
    * try listing the warnings in the output
    
    * better warning output
    
    * report unique warnings
    
    * report unique warnings
    
    * report unique warnings
    
    * report unique warnings
    
    * remove soft failure
    
    * Apply auto-formatting rules
    
    * Apply auto-formatting rules
    
    * Apply auto-formatting rules
    
    * don't sort warnings
    
    * better output
    
    * use agent to set soft fail
    
    * set soft fail on warnings
    
    * set soft fail on warnings
    
    * do not set soft fail on warnings
    
    * soft fail
    
    ---------
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

---
+++ .buildkite/scripts/report_sbt_warnings.sh
@@ -0,0 +1,12 @@
+echo "Scanning for warnings"
+WARNINGS=$(cat - | grep '\[warn\]')
+WARNING_COUNT=$(echo "$WARNINGS" | sed '/^$/d' | wc -l | sed 's/^[ \t]*//')
+UNIQUE_WARNINGS=$(echo "$WARNINGS" | uniq)
+UNIQUE_WARNING_COUNT=$(echo "$UNIQUE_WARNINGS" | sed '/^$/d' | wc -l| sed 's/^[ \t]*//')
+MESSAGE="Completed with $WARNING_COUNT ($UNIQUE_WARNING_COUNT distinct) warning(s)\n\`\`\`term\n$UNIQUE_WARNINGS\n\`\`\`"
+echo "$MESSAGE"
+if [ $WARNING_COUNT != 0 ]
+then
+  echo -e "$MESSAGE" | buildkite-agent annotate  --style 'warning' --context 'ctx-warn'
+  exit 2
+fi
