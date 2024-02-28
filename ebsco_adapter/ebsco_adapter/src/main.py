442b6997001687536cb1364af43981e5d301714a
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-02-28 10:48:42 +0000

    Apply auto-formatting rules

--- ebsco_adapter/ebsco_adapter/src/main.py
+++ ebsco_adapter/ebsco_adapter/src/main.py
@@ -4,21 +4,24 @@ from types import SimpleNamespace
 import boto3
 import requests
 
+
 def one_plus(n):
     return n + 1
 
+
 def lambda_handler(event, context):
     print("Lambda function ARN:", context.invoked_function_arn)
-    print("Current identity: " + boto3.client('sts').get_caller_identity().get('Arn'))
+    print("Current identity: " + boto3.client("sts").get_caller_identity().get("Arn"))
 
-    r = requests.get('https://api.wellcomecollection.org/catalogue/v2/works/jr9vvkum?include=items,partOf,identifiers')
+    r = requests.get(
+        "https://api.wellcomecollection.org/catalogue/v2/works/jr9vvkum?include=items,partOf,identifiers"
+    )
     print(r.text)
 
-    return {
-        "message": "hi"
-    }
+    return {"message": "hi"}
+
 
 if __name__ == "__main__":
     event = []
-    context = SimpleNamespace(invoked_function_arn= None)
+    context = SimpleNamespace(invoked_function_arn=None)
     lambda_handler(event, context)
