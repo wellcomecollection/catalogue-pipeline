86e32cee1fd6745ac5ffe0e4db143391adab196f
Paul Butcher <paul.butcher@gmail.com>
2024-03-06 16:24:13 +0000

    dont need to think about materialType yet

--- pipeline/transformer/transformer_marc_common/src/main/scala/weco/pipeline/transformer/marc_common/models/MarcRecord.scala
+++ pipeline/transformer/transformer_marc_common/src/main/scala/weco/pipeline/transformer/marc_common/models/MarcRecord.scala
@@ -5,10 +5,6 @@ package weco.pipeline.transformer.marc_common.models
  * This provides an interface for retrieving fields, subfields, and values
  * */
 trait MarcRecord {
-  // This is derived from the MARC21 Leader.
-  // TODO: In the implementation(s), we will need to do something to harmonise the values from different sources
-  //   because a given code from EBSCO may not meant the same thing as in Sierra.
-  val materialTypeId: Option[String]
 
   val fields: Seq[MarcField]
   def fieldsWithTags(tags: String*): Seq[MarcField]
