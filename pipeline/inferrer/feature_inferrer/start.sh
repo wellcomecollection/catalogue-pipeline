55d498672c8302ea63897135d0a20f80c7ffcafb
jamie <j.parkinson@wellcome.org>
2024-03-25 12:52:19 +0000

    Remove unused feature training service and associated infra

--- pipeline/inferrer/feature_inferrer/start.sh
+++ pipeline/inferrer/feature_inferrer/start.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-python prestart.py
 exec gunicorn -c gunicorn.conf.py --reload app.main:app
--- pipeline/inferrer/terraform/modules/image_training/release_uris.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ecr_repository" "feature_training" {
-  name = "uk.ac.wellcome/feature_training"
-}
-
-locals {
-  feature_training_image = "${data.aws_ecr_repository.feature_training.repository_url}:env.${var.release_label}"
-}
