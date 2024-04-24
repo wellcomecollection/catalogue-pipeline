d3d237e6343248c746e23a4f42da47c276927f01
Robert Kenny <kenoir@users.noreply.github.com>
2024-04-24 10:54:31 +0100

    Wire up EBSCO transformer (#2617)
    * wire EBSCO transformer to MarcXMLRecordTransformer
    
    * move test and ebsco specific logic
    
    * Apply auto-formatting rules
    
    * unsubscribe ebsco transformer topic
    
    ---------
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- pipeline/terraform/modules/stack/service_transformers.tf
+++ pipeline/terraform/modules/stack/service_transformers.tf
@@ -51,7 +51,14 @@ locals {
   }
 
   transformer_output_topic_arns = [
-    for k, v in module.transformers : v.output_topic_arn
+    # Temporarily remove the Ebsco transformer output topic
+    # In order to experiment with the new transformer
+    module.transformers["mets"].output_topic_arn,
+    module.transformers["calm"].output_topic_arn,
+    module.transformers["sierra"].output_topic_arn,
+    module.transformers["tei"].output_topic_arn,
+    module.transformers["miro"].output_topic_arn,
+    # for k, v in module.transformers : v.output_topic_arn
   ]
 }
 
