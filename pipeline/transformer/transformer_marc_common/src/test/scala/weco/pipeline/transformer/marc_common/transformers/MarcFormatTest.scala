2710753b055847a962a2daac115dd3e2639f184c
Robert Kenny <r.kenny@wellcome.org>
2024-05-23 16:28:46 +0100

    Use table driven property tests for conciseness and clarity
    Co-Authored-By: Paul Butcher <paul.butcher+github@gmail.com>

--- pipeline/transformer/transformer_marc_common/src/test/scala/weco/pipeline/transformer/marc_common/transformers/MarcFormatTest.scala
+++ pipeline/transformer/transformer_marc_common/src/test/scala/weco/pipeline/transformer/marc_common/transformers/MarcFormatTest.scala
@@ -3,11 +3,12 @@ package weco.pipeline.transformer.marc_common.transformers
 import org.scalatest.OptionValues
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
+import org.scalatest.prop.TableDrivenPropertyChecks
 import weco.catalogue.internal_model.work.Format.{EBooks, EJournals}
 import weco.pipeline.transformer.marc_common.generators.MarcTestRecord
 import weco.pipeline.transformer.marc_common.models.MarcControlField
 
-class MarcFormatTest extends AnyFunSpec with Matchers with OptionValues {
+class MarcFormatTest extends AnyFunSpec with Matchers with OptionValues with TableDrivenPropertyChecks{
   describe(
     "Extracting format information from MARC Leader/07, 006/06 & 008 material specific details"
   ) {
@@ -15,42 +16,38 @@ class MarcFormatTest extends AnyFunSpec with Matchers with OptionValues {
     info("https://www.loc.gov/marc/bibliographic/bd006.html")
     info("https://www.loc.gov/marc/bibliographic/bd008s.html")
 
-    it("returns an e-journal format when metadata indicates it") {
-      val record = MarcTestRecord(
-        leader = "00000cas a22000003a 4500",
-        controlFields = Seq(
-          MarcControlField("006", "m\\\\\\\\\\o\\\\d\\\\||||||")
+
+    describe("with a MARC leader and 006 control field") {
+      forAll(
+        Table(
+          ("leader", "006", "expectedFormat"),
+          // 'as' in Leader/06 & 07  indicates 'language material' and 'serial'
+          // 'o' in 006/06 indicates 'online'
+          ("00000cas a22000003a 4500", "m\\\\\\\\\\o\\\\d\\\\||||||", Some(EJournals)),
+          // 'am' in Leader/06 & 07  indicates 'language material' and 'monograph'
+          // 'o' in 006/06 indicates 'online'
+          ("00000cam a22000003a 4500", "m\\\\\\\\\\o\\\\d\\\\||||||", Some(EBooks)),
+          // 'am' in Leader/06 & 07  indicates 'language material' and 'monograph'
+          // 'f' in 006/06 indicates 'braille'
+          ("00000cam a22000003a 4500", "m\\\\\\\\\\f\\\\d\\\\||||||", None),
+          // 'ac' in Leader/06 & 07  indicates 'language material' and 'collection'
+          // 'o' in 006/06 is non-sensical without a mapping to material type
+          ("00000cac a22000003a 4500", "m\\\\\\\\\\o\\\\d\\\\||||||", None),
+
         )
-      )
-
-      MarcFormat(record).value shouldBe EJournals
-    }
-
-    it("returns an e-book format when metadata indicates it") {
-      val record = MarcTestRecord(
-        leader = "00000cam a22000003a 4500",
-        controlFields = Seq(
-          MarcControlField("006", "m\\\\\\\\\\o\\\\d\\\\||||||")
-        )
-      )
-
-      MarcFormat(record).value shouldBe EBooks
-    }
-
-    // We only care about the format of e-journals and e-books at the moment
-    // so we don't need to test for other formats. As this transformer
-    // is only used in the context of the EBSCO transformer.
-    it(
-      "returns no format when metadata indicates neither e-journal nor e-book"
-    ) {
-      val record = MarcTestRecord(
-        leader = "00000 am  2200289 a 4500",
-        controlFields = Seq(
-          MarcControlField("006", "m\\\\\\\\\\f\\\\d\\\\||||||")
-        )
-      )
-
-      MarcFormat(record) shouldBe None
+      ) {
+        (leader, controlField006, expectedFormat) =>
+          it(
+            s"returns $expectedFormat when Leader/06 & 07 is '${leader.slice(6, 8)}' and 006/06 is '${controlField006.slice(6, 7)}'"
+          ) {
+            MarcFormat(MarcTestRecord(
+              leader = leader,
+              controlFields = Seq(
+                MarcControlField("006", controlField006)
+              )
+            )) shouldBe expectedFormat
+          }
+      }
     }
   }
 }
