29f17c6dac8dbbca5d02db731ab7e10ac9e1fc87
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-06-07 15:49:49 +0000

    Apply auto-formatting rules

--- ebsco_adapter/ebsco_indexer/src/test_main.py
+++ ebsco_adapter/ebsco_indexer/src/test_main.py
@@ -9,7 +9,9 @@ from .local_utils import construct_sns_event
 @mock.patch("elasticsearch.Elasticsearch", new=MockElasticsearchClient)
 @mock.patch("src.main.ES_INDEX_NAME", "test_ebsco_index")
 def test_lambda_handler_correctly_indexes_documents():
-    index_event = construct_sns_event("test_id_1", "test_bucket", "prod/test_id_1", False)
+    index_event = construct_sns_event(
+        "test_id_1", "test_bucket", "prod/test_id_1", False
+    )
     lambda_handler(index_event, None)
 
     indexed_documents = MockElasticsearchClient.indexed_documents["test_ebsco_index"]
@@ -22,46 +24,61 @@ def test_lambda_handler_correctly_indexes_documents():
 
     assert indexed_documents["test_id_1-001-1"]["data"] == "test_id_1"
     assert indexed_documents["test_id_1-003-2"]["data"] == "EBZ"
-    
+
     field_856 = indexed_documents["test_id_1-856-3"]
     assert field_856["ind1"] == "4"
     assert field_856["ind2"] == "0"
     assert field_856["position"] == 3
-    
+
     assert field_856["subfields.code"] == ["3", "z", "u"]
 
     assert field_856["subfields.content"][0] == "Full text available: 1995 to present."
-    assert field_856["subfields.content"][1] == "Available in ScienceDirect Subject Collections - Agricultural and Biological Sciences.\n            "
-    assert field_856["subfields.content"][2] == "https://resolver.ebscohost.com/Redirect/PRL?EPPackageLocationID=841.9579.0&epcustomerid=s7451719"
+    assert (
+        field_856["subfields.content"][1]
+        == "Available in ScienceDirect Subject Collections - Agricultural and Biological Sciences.\n            "
+    )
+    assert (
+        field_856["subfields.content"][2]
+        == "https://resolver.ebscohost.com/Redirect/PRL?EPPackageLocationID=841.9579.0&epcustomerid=s7451719"
+    )
 
 
 @mock.patch("boto3.Session", new=MockBoto3Session)
 @mock.patch("elasticsearch.Elasticsearch", new=MockElasticsearchClient)
 @mock.patch("src.main.ES_INDEX_NAME", "test_ebsco_index")
 def test_lambda_handler_deletes_indexed_documents():
-    index_event = construct_sns_event("test_id_1", "test_bucket", "prod/test_id_1", False)
+    index_event = construct_sns_event(
+        "test_id_1", "test_bucket", "prod/test_id_1", False
+    )
     lambda_handler(index_event, None)
 
     indexed_documents = MockElasticsearchClient.indexed_documents["test_ebsco_index"]
     assert len(indexed_documents.keys()) == 3
 
-    delete_event = construct_sns_event("test_id_1", "test_bucket", "prod/test_id_1", True)
+    delete_event = construct_sns_event(
+        "test_id_1", "test_bucket", "prod/test_id_1", True
+    )
     lambda_handler(delete_event, None)
 
     indexed_documents = MockElasticsearchClient.indexed_documents["test_ebsco_index"]
     assert len(indexed_documents.keys()) == 0
 
+
 @mock.patch("boto3.Session", new=MockBoto3Session)
 @mock.patch("elasticsearch.Elasticsearch", new=MockElasticsearchClient)
 @mock.patch("src.main.ES_INDEX_NAME", "test_ebsco_index")
 def test_lambda_handler_does_not_delete_incorrect_documents():
-    index_event = construct_sns_event("test_id_1", "test_bucket", "prod/test_id_1", False)
+    index_event = construct_sns_event(
+        "test_id_1", "test_bucket", "prod/test_id_1", False
+    )
     lambda_handler(index_event, None)
 
     indexed_documents = MockElasticsearchClient.indexed_documents["test_ebsco_index"]
     assert len(indexed_documents.keys()) == 3
 
-    delete_event = construct_sns_event("test_id_2", "test_bucket", "prod/test_id_2", True)
+    delete_event = construct_sns_event(
+        "test_id_2", "test_bucket", "prod/test_id_2", True
+    )
     lambda_handler(delete_event, None)
 
     indexed_documents = MockElasticsearchClient.indexed_documents["test_ebsco_index"]
