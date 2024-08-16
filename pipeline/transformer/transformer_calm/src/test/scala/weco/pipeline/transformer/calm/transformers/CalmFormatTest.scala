da5535faf7c5b3e323d3251d82bb928a21092a11
Robert Kenny <kenoir@users.noreply.github.com>
2024-08-16 10:51:22 +0100

    New ArchivesDigital format, transformed from CALM Material type (#2687)
    * new ArchivesDigital format, transformed from CALM Material type
    
    * Apply auto-formatting rules
    
    * update label to Archives - Digital
    
    * update works ingestor tests
    
    ---------
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

---
+++ pipeline/transformer/transformer_calm/src/test/scala/weco/pipeline/transformer/calm/transformers/CalmFormatTest.scala
@@ -0,0 +1,34 @@
+package weco.pipeline.transformer.calm.transformers
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import weco.catalogue.internal_model.work._
+import weco.catalogue.source_model.generators.CalmRecordGenerators
+import weco.fixtures.RandomGenerators
+
+class CalmFormatTest extends AnyFunSpec with Matchers with CalmRecordGenerators with RandomGenerators {
+  it("Correctly extracts the ArchivesDigital Format when the appropriate Material string is present") {
+    val digitalRecord = createCalmRecordWith(
+      ("Material", "Archives - Digital"),
+    )
+
+    val digitalFormat = CalmFormat(digitalRecord)
+
+    digitalFormat shouldBe Format.ArchivesDigital
+
+    // Check that the label is "Archives - Digital"
+    Format.ArchivesDigital.label shouldBe("Archives - Digital")
+  }
+
+  it("Defaults to ArchivesAndManuscripts for all other contents of Material") {
+    val nonDigitalRecords = (1 to 100)
+      .map(_ => randomAlphanumeric())
+      .map(materialName => createCalmRecordWith(
+        ("Material", materialName),
+      ))
+
+    val nonDigitalFormats = nonDigitalRecords.map(CalmFormat(_)).toSet
+
+    nonDigitalFormats shouldBe Set(Format.ArchivesAndManuscripts)
+  }
+}
