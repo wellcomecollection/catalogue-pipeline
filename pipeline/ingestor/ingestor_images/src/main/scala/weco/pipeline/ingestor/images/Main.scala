372e718fc400dc74bb8336a3256d6bfb037cdeaf
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-09-02 13:39:45 +0100

    formatting error + rename pipeline step

--- pipeline/ingestor/ingestor_images/src/main/scala/weco/pipeline/ingestor/images/Main.scala
+++ pipeline/ingestor/ingestor_images/src/main/scala/weco/pipeline/ingestor/images/Main.scala
@@ -8,7 +8,7 @@ import weco.typesafe.WellcomeTypesafeApp
 object Main extends WellcomeTypesafeApp {
   val ingestor = new IngestorMain(
     name = "images",
-    inputIndexField = "es.augmented-images.index",
+        inputIndexField = "es.augmented-images.index",
     outputIndexField = "es.indexed-images.index",
     transform = ImageTransformer.deriveData
   )
