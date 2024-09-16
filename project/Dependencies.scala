cbf5b141d7cf9ff1d19b18d1764436659a540d83
Paul Butcher <paul.butcher@gmail.com>
2024-09-16 10:23:06 +0100

    update apache-commons (#2703)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -94,7 +94,7 @@ object WellcomeDependencies {
 
 object ExternalDependencies {
   lazy val versions = new {
-    val apacheCommons = "1.9"
+    val apacheCommons = "1.11.0"
     val circe = "0.14.1"
     val diffJson = "4.1.1"
     val fastparse = "2.3.0"
