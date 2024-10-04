3e8e98b8a8f087780349540d751a17c182fb01dc
Štěpán Brychta <stepan.brychta@me.com>
2024-10-04 11:33:52 +0100

    Update TableProvisioner.scala

--- pipeline/id_minter/src/main/scala/weco/pipeline/id_minter/database/TableProvisioner.scala
+++ pipeline/id_minter/src/main/scala/weco/pipeline/id_minter/database/TableProvisioner.scala
@@ -8,15 +8,16 @@ import scala.collection.JavaConverters._
 class TableProvisioner(rdsClientConfig: RDSClientConfig) {
 
   def provision(database: String, tableName: String): Unit = {
-    val flyway = new Flyway()
-    flyway.setDataSource(
+    val flyway = Flyway.configure()
+    .dataSource(
       s"jdbc:mysql://${rdsClientConfig.primaryHost}:${rdsClientConfig.port}/$database",
       rdsClientConfig.username,
       rdsClientConfig.password
     )
-    flyway.setPlaceholders(
+    .placeholders(
       Map("database" -> database, "tableName" -> tableName).asJava
-    )
+    ).load()
+
     flyway.migrate()
   }
 
