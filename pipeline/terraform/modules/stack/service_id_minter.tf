feb6ff9b8147aef47e6068e2da906a04be118ebe
Robert Kenny <r.kenny@wellcome.org>
2024-10-14 15:43:59 +0100

    replace min max for id_minter

--- pipeline/terraform/modules/stack/service_id_minter.tf
+++ pipeline/terraform/modules/stack/service_id_minter.tf
@@ -34,39 +34,26 @@ module "id_minter" {
     ingest_flush_interval_seconds = 30
   }
 
-  // TODO: Swap these two blocks when we're ready to switch to the new RDS cluster
-  secret_env_vars = merge({
-    cluster_url          = "rds/identifiers-delta-cluster/endpoint"
-    cluster_url_readonly = "rds/identifiers-delta-cluster/reader_endpoint"
-    db_port              = "rds/identifiers-delta-cluster/port"
-    db_username          = "catalogue/id_minter/rds_user"
-    db_password          = "catalogue/id_minter/rds_password"
-  }, local.pipeline_storage_es_service_secrets["id_minter"])
-
-  #   secret_env_vars = merge({
-  #     cluster_url          = "rds/identifiers-serverless/endpoint"
-  #     cluster_url_readonly = "rds/identifiers-serverless/reader_endpoint"
-  #     db_port              = "rds/identifiers-serverless/port"
-  #     db_username          = "catalogue/id_minter/rds_user"
-  #     db_password          = "catalogue/id_minter/rds_password"
-  #   }, local.pipeline_storage_es_service_secrets["id_minter"])
+    secret_env_vars = merge({
+      cluster_url          = "rds/identifiers-serverless/endpoint"
+      cluster_url_readonly = "rds/identifiers-serverless/reader_endpoint"
+      db_port              = "rds/identifiers-serverless/port"
+      db_username          = "catalogue/id_minter/rds_user"
+      db_password          = "catalogue/id_minter/rds_password"
+    }, local.pipeline_storage_es_service_secrets["id_minter"])
 
   cpu    = 2048
   memory = 4096
 
   # The total number of connections to RDS across all tasks from all ID minter
   # services must not exceed the maximum supported by the RDS instance.
-  #   min_capacity = var.min_capacity
-  #   max_capacity = min(
-  #     floor(
-  #       local.id_minter_rds_max_connections / local.id_minter_task_max_connections
-  #     ),
-  #     local.max_capacity
-  #   )
-
-  # Manually setting min_capacity and max_capacity to 0 to prevent autoscaling
-  max_capacity = 0
-  min_capacity = 0
+  min_capacity = var.min_capacity
+  max_capacity = min(
+    floor(
+      local.id_minter_rds_max_connections / local.id_minter_task_max_connections
+    ),
+    local.max_capacity
+  )
 
   fargate_service_boilerplate = local.fargate_service_boilerplate
 }
