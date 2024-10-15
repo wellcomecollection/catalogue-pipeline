448005c6648729b0fe9cd6d30bdb6f3cc6db94fb
Robert Kenny <r.kenny@wellcome.org>
2024-10-15 09:45:33 +0100

    enable data api, deletion protection and downgrade provider perms

--- infrastructure/critical/modules/rds-serverless/main.tf
+++ infrastructure/critical/modules/rds-serverless/main.tf
@@ -14,8 +14,8 @@ resource "aws_rds_cluster" "serverless" {
   vpc_security_group_ids = [var.db_security_group_id]
 
   storage_encrypted    = false
-  enable_http_endpoint = false
-  deletion_protection  = false
+  enable_http_endpoint = true
+  deletion_protection  = true
 
   serverlessv2_scaling_configuration {
     max_capacity = var.max_scaling_capacity
--- tei_adapter/terraform/provider.tf
+++ tei_adapter/terraform/provider.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 
   assume_role {
-    role_arn = "arn:aws:iam::760097843905:role/platform-admin"
+    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
   }
 }
