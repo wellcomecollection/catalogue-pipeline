e770050a793b7390d55a603c5f6681ceb259d8ee
Robert Kenny <r.kenny@wellcome.org>
2024-10-15 08:57:49 +0100

    add serverless database

--- infrastructure/critical/modules/rds-serverless/main.tf
+++ infrastructure/critical/modules/rds-serverless/main.tf
@@ -13,15 +13,21 @@ resource "aws_rds_cluster" "serverless" {
   db_subnet_group_name   = var.aws_db_subnet_group_name
   vpc_security_group_ids = [var.db_security_group_id]
 
-  storage_encrypted = false
-  // This needs to be false until migration is complete
-  enable_http_endpoint = true
-  deletion_protection  = true
+  storage_encrypted    = false
+  enable_http_endpoint = false
+  deletion_protection  = false
 
   serverlessv2_scaling_configuration {
     max_capacity = var.max_scaling_capacity
     min_capacity = var.min_scaling_capacity
   }
+}
+
+resource "aws_rds_cluster_instance" "t3_instance" {
+  cluster_identifier = aws_rds_cluster.serverless.id
+  instance_class     = "db.t3.medium"
+  engine             = aws_rds_cluster.serverless.engine
+  engine_version     = aws_rds_cluster.serverless.engine_version
 }
 
 resource "aws_rds_cluster_instance" "serverless_instance" {
--- tei_adapter/terraform/rds_id_extractor.tf
+++ tei_adapter/terraform/rds_id_extractor.tf
@@ -64,6 +64,22 @@ module "tei_id_extractor_rds_cluster" {
   db_parameter_group_name = aws_db_parameter_group.default.id
 }
 
+module "tei_id_extractor_rds_serverless_cluster" {
+  source             = "../../infrastructure/critical/modules/rds-serverless"
+  cluster_identifier = "tei-adapter-cluster-serverless"
+  database_name      = "pathid"
+  master_username    = local.rds_username
+  master_password    = local.rds_password
+
+  db_security_group_id = aws_security_group.database_sg.id
+  aws_db_subnet_group_name = aws_db_subnet_group.default.name
+
+  engine_version = "8.0.mysql_aurora.3.07.1"
+
+  snapshot_identifier = "aurora-mysql-v3-tei-pre-migration-24-15-08"
+}
+
+
 resource "aws_security_group" "rds_ingress_security_group" {
   name        = "tei_adapter_rds_ingress_security_group"
   description = "Allow traffic to rds database"
