6e6419f07871f377c7f26917d8bdd649952b0523
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-11-01 15:03:49 +0000

    add batcher_lambda_output_topic

--- pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
+++ pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
@@ -5,42 +5,49 @@ import grizzled.slf4j.Logging
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
 import org.apache.pekko.actor.ActorSystem
+import weco.messaging.typesafe.SNSBuilder
+import weco.json.JsonUtil._
+import com.typesafe.config.ConfigFactory
+import weco.typesafe.config.builders.EnrichConfig.RichConfig
 
 import scala.collection.JavaConverters._
 import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 object LambdaMain extends RequestHandler[SQSEvent, String] with Logging {
+  private val config = ConfigFactory.load("application")
 
   override def handleRequest(
     event: SQSEvent,
     context: Context
   ): String = {
-    info(s"running batcher lambda, got event: $event")
+      info(s"running batcher lambda, got event: $event")
 
-    implicit val actorSystem: ActorSystem =
-      ActorSystem("main-actor-system")
-    implicit val ec: ExecutionContext =
-      actorSystem.dispatcher
+      implicit val actorSystem: ActorSystem =
+        ActorSystem("main-actor-system")
+      implicit val ec: ExecutionContext =
+        actorSystem.dispatcher
 
-    val recordList: List[SQSMessage] = event.getRecords.asScala.toList
-    val paths = recordList map {
-      message: SQSMessage =>
-        // This assumes that the input is message body is just the path
-        // not wrapped in any other JSON.
-        // This may need to be altered depending on how the message is formatted
-        // in real life
-        ujson.read(message.getBody).str
-    }
-    PathsProcessor(
-      40, // TODO: 40 is the number in the config used by Main, do this properly later
-      paths,
-      STDIODownstream
-    )
-    "Done"
+      val recordList: List[SQSMessage] = event.getRecords.asScala.toList
+      val paths = recordList map {
+        message: SQSMessage =>
+          // This assumes that the input is message body is just the path
+          // not wrapped in any other JSON.
+          // This may need to be altered depending on how the message is formatted
+          // in real life
+          ujson.read(message.getBody).str
+      }
+      PathsProcessor(
+        config.requireInt("batcher.max_batch_size"),
+        paths,
+        SNSDownstream
+      )
+      "Done"
   }
 
-  private object STDIODownstream extends Downstream {
-    override def notify(batch: Batch): Try[Unit] = Try(println(batch))
+  private object SNSDownstream extends Downstream {
+    private val msgSender = SNSBuilder
+      .buildSNSMessageSender(config, subject = "Sent from batcher")
+    override def notify(batch: Batch): Try[Unit] = msgSender.sendT(batch)
   }
 }
