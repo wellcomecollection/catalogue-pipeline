6644acbe95f52f964872980ab6d06cd8fde596d2
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-11-01 15:05:53 +0000

    Apply auto-formatting rules

--- pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
+++ pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
@@ -21,28 +21,28 @@ object LambdaMain extends RequestHandler[SQSEvent, String] with Logging {
     event: SQSEvent,
     context: Context
   ): String = {
-      info(s"running batcher lambda, got event: $event")
+    info(s"running batcher lambda, got event: $event")
 
-      implicit val actorSystem: ActorSystem =
-        ActorSystem("main-actor-system")
-      implicit val ec: ExecutionContext =
-        actorSystem.dispatcher
+    implicit val actorSystem: ActorSystem =
+      ActorSystem("main-actor-system")
+    implicit val ec: ExecutionContext =
+      actorSystem.dispatcher
 
-      val recordList: List[SQSMessage] = event.getRecords.asScala.toList
-      val paths = recordList map {
-        message: SQSMessage =>
-          // This assumes that the input is message body is just the path
-          // not wrapped in any other JSON.
-          // This may need to be altered depending on how the message is formatted
-          // in real life
-          ujson.read(message.getBody).str
-      }
-      PathsProcessor(
-        config.requireInt("batcher.max_batch_size"),
-        paths,
-        SNSDownstream
-      )
-      "Done"
+    val recordList: List[SQSMessage] = event.getRecords.asScala.toList
+    val paths = recordList map {
+      message: SQSMessage =>
+        // This assumes that the input is message body is just the path
+        // not wrapped in any other JSON.
+        // This may need to be altered depending on how the message is formatted
+        // in real life
+        ujson.read(message.getBody).str
+    }
+    PathsProcessor(
+      config.requireInt("batcher.max_batch_size"),
+      paths,
+      SNSDownstream
+    )
+    "Done"
   }
 
   private object SNSDownstream extends Downstream {
