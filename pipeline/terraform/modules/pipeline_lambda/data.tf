e4d15199f0c018007867a5105d99d7f1d067a4ff
Robert Kenny <r.kenny@wellcome.org>
2024-11-05 16:53:43 +0000

    add new pipeline 2024-11-05

--- pipeline/terraform/modules/pipeline_lambda/data.tf
+++ pipeline/terraform/modules/pipeline_lambda/data.tf
@@ -5,13 +5,9 @@ locals {
   image_tag = var.tag_override != "" ? var.tag_override : "env.${var.pipeline_date}"
 
   queue_name = var.queue_config.name == null ? trim("${local.name}_lambda_input", "_") : var.queue_config.name
+
 }
 
 data "aws_ecr_repository" "repository" {
   name = var.ecr_repository_name
-}
-
-data "aws_ecr_image" "lambda_image" {
-  repository_name = data.aws_ecr_repository.repository.name
-  image_tag       = local.image_tag
 }
