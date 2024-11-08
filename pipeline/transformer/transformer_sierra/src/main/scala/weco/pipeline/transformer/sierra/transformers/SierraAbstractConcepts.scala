fd6f8b7d841006fe02fe12bb5e1a10965a5d97cd
Štěpán Brychta <stepan.brychta@me.com>
2024-11-08 08:53:41 +0000

    Remove redundant prefixes from MeSH IDs #5819

--- pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/transformers/SierraAbstractConcepts.scala
+++ pipeline/transformer/transformer_sierra/src/main/scala/weco/pipeline/transformer/sierra/transformers/SierraAbstractConcepts.scala
@@ -1,9 +1,10 @@
 package weco.pipeline.transformer.sierra.transformers
 
 import grizzled.slf4j.Logging
+import weco.pipeline.transformer.marc_common.transformers.MarcHasRecordControlNumber
 import weco.pipeline.transformer.sierra.data.SierraMarcDataConversions
 
 trait SierraAbstractConcepts
     extends Logging
-    with SierraHasRecordControlNumber
+    with MarcHasRecordControlNumber
     with SierraMarcDataConversions
