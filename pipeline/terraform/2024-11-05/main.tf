00d935bf13d10100609182b443886c0c23408bc6
Robert Kenny <r.kenny@wellcome.org>
2024-11-12 09:19:00 +0000

    scale up for reindex

--- pipeline/terraform/2024-11-05/main.tf
+++ pipeline/terraform/2024-11-05/main.tf
@@ -3,10 +3,10 @@ module "pipeline" {
 
   reindexing_state = {
     listen_to_reindexer      = true
-    scale_up_tasks           = false
-    scale_up_elastic_cluster = false
-    scale_up_id_minter_db    = false
-    scale_up_matcher_db      = false
+    scale_up_tasks           = true
+    scale_up_elastic_cluster = true
+    scale_up_id_minter_db    = true
+    scale_up_matcher_db      = true
   }
 
   index_config = {
