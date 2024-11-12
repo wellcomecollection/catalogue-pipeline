8010a8694b899b339955c4a7d5cb979ad852f568
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-11-12 09:17:15 +0000

    Apply auto-formatting rules

--- pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/lib/SQSEventOps.scala
+++ pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/lib/SQSEventOps.scala
@@ -5,16 +5,17 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
 import scala.collection.JavaConverters._
 
 object SQSEventOps {
-  /** Messages consumed by Lambda from SQS are taken from a queue populated by an
-   * SNS topic. The actual message we are interested in is a String containing
-   * the path. However, the matryoshka-like nature of these things means the
-   * lambda receives
-   *   - an event containing
-   *   - a `Records` list, each Record containing
-   *   - an SQS Message with a JSON body containing
-   *   - an SNS notification containing
-   *   - a `Message`, which is the actual content we want
-   */
+
+  /** Messages consumed by Lambda from SQS are taken from a queue populated by
+    * an SNS topic. The actual message we are interested in is a String
+    * containing the path. However, the matryoshka-like nature of these things
+    * means the lambda receives
+    *   - an event containing
+    *   - a `Records` list, each Record containing
+    *   - an SQS Message with a JSON body containing
+    *   - an SNS notification containing
+    *   - a `Message`, which is the actual content we want
+    */
   implicit class ExtractPathFromSqsEvent(event: SQSEvent) {
     def extractPaths: List[String] =
       event.getRecords.asScala.toList.flatMap(extractPathFromMessage)
