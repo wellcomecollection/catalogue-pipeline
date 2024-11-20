07de45a513acf137fe591cd83dacfeb1bfabd5ed
Agnes Garoux <a.garoux@wellcome.ac.uk>
2024-11-20 13:24:18 +0000

    adds lambda with dumb handler, infra + build

---
+++ pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/LambdaMain.scala
@@ -0,0 +1,15 @@
+package weco.pipeline.relation_embedder
+
+import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
+import grizzled.slf4j.Logging
+import com.amazonaws.services.lambda.runtime.events.SQSEvent
+
+object LambdaMain extends RequestHandler[SQSEvent, String] with Logging {
+  override def handleRequest(
+    event: SQSEvent,
+    context: Context
+  ): String = {
+    info(s"running relation_ lambda, got event")
+    "Done"
+  }
+}
