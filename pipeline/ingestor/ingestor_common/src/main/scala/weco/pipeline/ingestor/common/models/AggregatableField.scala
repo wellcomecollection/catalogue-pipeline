411dbdcb4d9662733fb1400cccabdc8e84e64d1f
Štěpán Brychta <stepan.brychta@me.com>
2024-11-25 15:04:59 +0000

    Add comment explaining AggregatableField #5825

--- pipeline/ingestor/ingestor_common/src/main/scala/weco/pipeline/ingestor/common/models/AggregatableField.scala
+++ pipeline/ingestor/ingestor_common/src/main/scala/weco/pipeline/ingestor/common/models/AggregatableField.scala
@@ -7,10 +7,16 @@ case class AggregatableField(
   label: String
 )
 
+// Each AggregatableField has an ID and a label. The aggregation is based on the ID, and the label is displayed
+// to the user in the front-end. For label-based aggregatable fields which do not have an ID (e.g. production dates),
+// use the `fromLabel` method, which automatically sets the ID to the value of the label. The end result of this is
+// the same as aggregating based on labels, with the added benefit of providing a consistent interface -- the frontend
+// does not need to know whether a given aggregation is ID-based or label-based.
 case object AggregatableField {
   private def normaliseLabel(label: String) = {
     label.stripSuffix(".")
   }
+
   def apply(id: String, label: String): AggregatableField = {
     new AggregatableField(id = id, label = normaliseLabel(label))
   }
