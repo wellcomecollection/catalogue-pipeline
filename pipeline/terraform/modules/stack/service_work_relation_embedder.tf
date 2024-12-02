12bca4381dd88ad717ad9384d9f2fb7af470b59e
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-12-02 10:43:23 +0000

    Apply auto-formatting rules

--- pipeline/terraform/modules/stack/service_work_relation_embedder.tf
+++ pipeline/terraform/modules/stack/service_work_relation_embedder.tf
@@ -1,7 +1,7 @@
 module "relation_embedder_output_topic" {
   source = "../topic"
 
-  name = "${local.namespace}_relation_embedder_output"
+  name       = "${local.namespace}_relation_embedder_output"
   role_names = [module.relation_embedder.task_role_name]
 }
 
@@ -32,16 +32,16 @@ module "relation_embedder" {
     es_merged_index       = local.es_works_merged_index
     es_denormalised_index = local.es_works_denormalised_index
 
-    queue_parallelism = 3  // NOTE: limit to avoid memory errors
-    affected_works_scroll_size = 50 // NOTE: limit to avoid memory errors
+    queue_parallelism            = 3  // NOTE: limit to avoid memory errors
+    affected_works_scroll_size   = 50 // NOTE: limit to avoid memory errors
     complete_tree_scroll_size    = 800
-    index_batch_size = 100 // NOTE: too large results in 413 from ES
+    index_batch_size             = 100 // NOTE: too large results in 413 from ES
     index_flush_interval_seconds = 60
   }
 
   secret_env_vars = local.pipeline_storage_es_service_secrets["relation_embedder"]
 
-  cpu = 2048
+  cpu    = 2048
   memory = 4096
 
   # NOTE: limit to avoid >500 concurrent scroll contexts
