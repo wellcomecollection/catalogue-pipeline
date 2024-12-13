a631e3f790ccd64b19b951fe3e902d7e7fad2c94
Robert Kenny <r.kenny@wellcome.org>
2024-12-13 09:18:55 +0000

    fix tf typo

--- pipeline/terraform/modules/pipeline_lambda/secrets.tf
+++ pipeline/terraform/modules/pipeline_lambda/secrets.tf
@@ -21,17 +21,17 @@ data "aws_iam_policy_document" "secrets_policy" {
       : "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${secret_name}-*"
     ]
   }
-}[]
+}
 
 resource "aws_iam_policy" "secret_policy" {
-  count      = length(local.secret_names) > 0 ? 1 : 0
+  count = length(local.secret_names) > 0 ? 1 : 0
 
   name_prefix = "${local.name}-secrets-policy"
   policy      = data.aws_iam_policy_document.secrets_policy.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_secret_role_policy" {
-  count      = length(local.secret_names) > 0 ? 1 : 0
+  count = length(local.secret_names) > 0 ? 1 : 0
 
   role       = module.pipeline_step.lambda_role.name
   policy_arn = aws_iam_policy.secret_policy[0].arn
