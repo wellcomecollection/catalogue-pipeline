2622c78a5cd6329cdd1bc70dc13aab6b08ef6c6a
Paul Butcher <paul.butcher@gmail.com>
2024-12-16 13:49:03 +0000

    Better relation embedder cli (#2788)
    * start creating CLIMain
    
    * make a local CLIMain work
    
    * enable CLI to read from live database
    
    * make pipeline data an env var
    
    * make pipeline date an argument
    
    * Apply auto-formatting rules
    
    * remove debugging code
    
    * de-akkafy reading from stdin
    
    * better handling of dodgy input
    
    * try removing internal model from batcher
    
    * improve StdInNDJSON interface
    
    * improve commentary
    
    * report parse failure
    
    ---------
    
    Co-authored-by: Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/lib/StdInBatches.scala
+++ pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/lib/StdInBatches.scala
@@ -1,15 +1,41 @@
 package weco.pipeline.relation_embedder.lib
+import grizzled.slf4j.Logging
 import weco.json.JsonUtil._
 import weco.pipeline.relation_embedder.models.Batch
 
 import scala.io.Source.stdin
+import scala.util.{Failure, Success, Try}
 
-trait StdInBatches {
+/** Trait to deal with Newline Delimited JSON being provided on STDIN.
+  *
+  * Each JSON object in the input is transformed to an instance of T, according
+  * to jsonToInstance (provided by the extending class) and used to populate the
+  * instances Iterator.
+  */
+
+trait StdInNDJSON[T] extends Logging {
+  protected def jsonToInstance(str: String): Try[T]
   private val stdInStrings: Iterator[String] = stdin.getLines()
 
-  private def toBatch(jsonString: String) =
-    fromJson[Batch](jsonString).get
+  private def toInstance(jsonString: String): Option[T] =
+    jsonToInstance(jsonString) match {
+      case Failure(exception) =>
+        error(exception.getMessage)
+        None
+      case Success(value) => Some(value)
+    }
 
-  protected val batches: Iterator[Batch] =
-    stdInStrings.map(toBatch)
+  protected val instances: Iterator[T] =
+    stdInStrings
+      .flatMap(
+        toInstance
+      )
+
+}
+
+trait StdInBatches extends StdInNDJSON[Batch] {
+  def jsonToInstance(jsonString: String): Try[Batch] =
+    fromJson[Batch](jsonString)
+
+  protected val batches: Iterator[Batch] = instances
 }
