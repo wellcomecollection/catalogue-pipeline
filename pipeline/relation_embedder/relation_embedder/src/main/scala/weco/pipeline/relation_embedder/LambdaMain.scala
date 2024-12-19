14a47a44fb5c77c6a653797e98bc47aee191a636
Robert Kenny <r.kenny@wellcome.org>
2024-12-19 13:44:25 +0000

    add tests for lambdaapp

--- pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/LambdaMain.scala
+++ pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/LambdaMain.scala
@@ -5,10 +5,9 @@ import weco.pipeline.relation_embedder.lib._
 
 import scala.concurrent.Future
 
-
 object LambdaMain
-  extends LambdaApp[SQSEvent, String, RelationEmbedderConfig]
-  with RelationEmbedderConfigurable {
+    extends LambdaApp[SQSEvent, String, RelationEmbedderConfig]
+    with RelationEmbedderConfigurable {
 
   import SQSEventOps._
   private lazy val batchProcessor = BatchProcessor(config)
@@ -16,6 +15,8 @@ object LambdaMain
   def processEvent(event: SQSEvent): Future[String] = {
     info(s"running relation_embedder lambda, got event: $event")
 
-    Future.sequence(event.extractBatches.map(batchProcessor(_))).map(_ => "Done")
+    Future
+      .sequence(event.extractBatches.map(batchProcessor(_)))
+      .map(_ => "Done")
   }
 }
--- pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/lib/LambdaApp.scala
+++ pipeline/relation_embedder/relation_embedder/src/main/scala/weco/pipeline/relation_embedder/lib/LambdaApp.scala
@@ -8,12 +8,12 @@ import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 trait LambdaApp[In, Out, Config <: ApplicationConfig]
-  extends RequestHandler[In, Out]
+    extends RequestHandler[In, Out]
     with LambdaConfigurable[Config]
     with Logging {
 
   // 15 minutes is the maximum time allowed for a lambda to run, as of 2024-12-19
-  private val maximumExecutionTime: FiniteDuration = 15.minutes
+  protected val maximumExecutionTime: FiniteDuration = 15.minutes
 
   implicit val actorSystem: ActorSystem =
     ActorSystem("main-actor-system")
@@ -23,9 +23,9 @@ trait LambdaApp[In, Out, Config <: ApplicationConfig]
   def processEvent(in: In): Future[Out]
 
   override def handleRequest(
-                              event: In,
-                              context: Context
-                            ): Out = Await.result(
+    event: In,
+    context: Context
+  ): Out = Await.result(
     processEvent(event),
     maximumExecutionTime
   )
