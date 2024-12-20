ceb0d6f35a32ddb0ade30b793480401f1b6e5f0d
Robert Kenny <r.kenny@wellcome.org>
2024-12-20 14:53:50 +0000

    rejig lambda app to account for type erasure bug :(

--- pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
+++ pipeline/relation_embedder/batcher/src/main/scala/weco/pipeline/batcher/LambdaMain.scala
@@ -1,24 +1,19 @@
 package weco.pipeline.batcher
 
-import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import weco.lambda._
 import weco.pipeline.batcher.lib.{BatcherConfig, BatcherConfigurable}
 
 import scala.concurrent.Future
 
 object LambdaMain
-    extends LambdaApp[SQSEvent, String, BatcherConfig]
+  extends SQSLambdaApp[String, String, BatcherConfig]
     with BatcherConfigurable {
-
-  import weco.lambda.SQSEventOps._
 
   private val pathsProcessor = PathsProcessor(
     Downstream(config.downstreamTarget),
     config.maxBatchSize
   )
 
-  override def processEvent(in: SQSEvent): Future[String] = {
-    debug(s"Running batcher lambda, got event: $in")
-    pathsProcessor(in.extract[String]).map(_ => "Done")
-  }
+  override def processT(t: List[String]): Future[String] =
+    pathsProcessor(t).map(_ => "Done")
 }
