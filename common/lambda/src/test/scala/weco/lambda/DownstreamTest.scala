852180296c35b6ea7c69406625db3b8f1965d8f0
Robert Kenny <kenoir@users.noreply.github.com>
2025-01-09 13:51:32 +0000

    Add config builder for DownstreamTarget to avoid ambiguity (#2804)
    * Add config builder for DownstreamTarget to avoid ambiguity
    
    * Apply auto-formatting rules
    
    ---------
    
    Co-authored-by: Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

---
+++ common/lambda/src/test/scala/weco/lambda/DownstreamTest.scala
@@ -0,0 +1,55 @@
+package weco.lambda
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+import weco.lambda.helpers.ConfigurationTestHelpers
+import weco.messaging.sns.SNSConfig
+
+class DownstreamTest extends AnyFunSpec
+  with ConfigurationTestHelpers
+  with Matchers {
+
+  describe("DownstreamBuilder") {
+    it("builds a StdOut downstream target") {
+      val config =
+        """
+          |downstream.target = "stdio"
+          |""".asConfig
+
+        DownstreamBuilder.buildDownstreamTarget(config) shouldBe StdOut
+    }
+
+    it("builds an SNS downstream target") {
+      val config =
+        """
+          |downstream.target = "sns"
+          |aws.sns.topic.arn = "arn:aws:sns:eu-west-1:123456789012:my-topic"
+          |""".asConfig
+
+        DownstreamBuilder.buildDownstreamTarget(config) shouldBe SNS(
+          config = SNSConfig(
+            topicArn = "arn:aws:sns:eu-west-1:123456789012:my-topic"
+          )
+        )
+    }
+
+    it("builds a StdOut downstream target if no downstream target is specified") {
+      val config =
+        """
+          |""".asConfig
+
+      DownstreamBuilder.buildDownstreamTarget(config) shouldBe StdOut
+    }
+
+    it("throws an exception if the downstream target is not recognised") {
+      val config =
+        """
+          |downstream.target = "invalid"
+          |""".asConfig
+
+      intercept[IllegalArgumentException] {
+        DownstreamBuilder.buildDownstreamTarget(config)
+      }
+    }
+  }
+}
