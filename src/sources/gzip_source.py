d53de6cbbe01bbf657e2988f856afe0074a31728
Antonia Langfelder <a.langfelder@wellcome.org>
2025-01-10 20:49:26 +0000

    Refactor MeSH source

--- src/sources/gzip_source.py
+++ src/sources/gzip_source.py
@@ -1,41 +1,22 @@
 import gzip
 import json
 from collections.abc import Generator
-from typing import Literal
 
-import xml.etree.ElementTree as ET
 import requests
 
 from .base_source import BaseSource
 
 
 class GZipSource(BaseSource):
-    def __init__(self, url: str, ftype: Literal["json", "xml"] = "json"):
+    def __init__(self, url: str):
         self.url = url
-        self.ftype = ftype
 
-    def _stream_raw_json(self, response: requests.Response) -> Generator[dict]:
+    def stream_raw(self) -> Generator[dict]:
+        response = requests.get(self.url, stream=True)
+
         with gzip.GzipFile(fileobj=response.raw) as file:
             for line_bytes in file:
                 yield json.loads(line_bytes.decode("utf8"))
-    
-    def _stream_raw_xml(self, response: requests.Response) -> Generator[ET.Element]:
-        response.raw.decode_content = True
-        events = ET.iterparse(response.raw)
-        for _, elem in events:
-            yield elem
-
-    def stream_raw(self) -> Generator[dict | ET.Element]:
-        response = requests.get(self.url, stream=True)
-
-        if self.ftype == "json":
-            return self._stream_raw_json(response)
-        
-        elif self.ftype == "xml":
-            return self._stream_raw_xml(response)
-        
-        else:
-            raise ValueError("Unknown file type.")
 
 
 class MultiGZipSource(BaseSource):
---
+++ src/sources/mesh/concepts_source.py
@@ -0,0 +1,18 @@
+from collections.abc import Generator
+import requests
+import xml.etree.ElementTree as ET
+
+from sources.base_source import BaseSource
+
+class MeSHConceptsSource(BaseSource):
+    def __init__(self, url: str):
+        self.url = url
+
+    def stream_raw(self) -> Generator[ET.Element]:
+        response = requests.get(self.url, stream=True)
+        response.raw.decode_content = True
+
+        events = ET.iterparse(response.raw)
+        return (
+            elem for _, elem in events if elem.tag == "DescriptorRecord"
+        )
