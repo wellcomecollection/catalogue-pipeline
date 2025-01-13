901a37578da5c529a9b7594256c0862aee2a094e
Antonia Langfelder <a.langfelder@wellcome.org>
2025-01-13 14:28:52 +0000

    Add xml type to base source

--- src/sources/base_source.py
+++ src/sources/base_source.py
@@ -1,7 +1,8 @@
 from collections.abc import Generator
+import xml.etree.ElementTree as ET
 
 
 class BaseSource:
-    def stream_raw(self) -> Generator[dict]:
+    def stream_raw(self) -> Generator[dict | ET.Element]:
         """Returns a generator of dictionaries, each corresponding to a raw entity extracted from the source."""
         raise NotImplementedError("Each source must implement a `stream_raw` method.")
