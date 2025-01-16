4a03502f168675d5d0cd56297944b87d1865ab3a
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-01-16 11:22:44 +0000

    changes to satisfy  Terraform v1.10.1

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">= 0.11"
 
   backend "s3" {
-    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
-
+    assume_role = {
+      role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+    }
     bucket         = "wellcomecollection-platform-infra"
     key            = "terraform/catalogue/graph.tfstate"
     dynamodb_table = "terraform-locktable"
@@ -15,7 +16,9 @@ data "terraform_remote_state" "aws_account_infrastructure" {
   backend = "s3"
 
   config = {
+    assume_role = {
     role_arn = "arn:aws:iam::760097843905:role/platform-read_only"
+    }
     bucket   = "wellcomecollection-platform-infra"
     key      = "terraform/aws-account-infrastructure/platform.tfstate"
     region   = "eu-west-1"
