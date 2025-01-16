944cbc536111e3d1ad5b55ee0dfc00c30d8d05ba
Antonia Langfelder <a.langfelder@wellcome.org>
2025-01-16 11:11:49 +0000

    Add reverse RELATED_TO edge

--- src/transformers/mesh/concepts_transformer.py
+++ src/transformers/mesh/concepts_transformer.py
@@ -45,3 +45,6 @@ class MeSHConceptsTransformer(BaseTransformer):
             yield SourceConceptRelatedTo(
                 from_id=raw_concept.source_id, to_id=related_id
             )
+            yield SourceConceptRelatedTo(
+                from_id=related_id, to_id=raw_concept.source_id
+            )
