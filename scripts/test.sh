4e0070dee11a31df6bd0db5f4c91aea4e46a893a
Robert Kenny <r.kenny@wellcome.org>
2025-01-22 15:32:58 +0000

    add coverage report

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,4 +6,6 @@ set -o nounset
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT+="$(dirname "$DIR")"
 
-pytest --cov src ./src --cov-report term
+pytest --cov src ./src \
+    --cov-report term \
+    --cov-report xml:coverage.xml
