0e3869bb585633cc7ba6d76e7eac1e9a4def099e
Paul Butcher <paul.butcher@gmail.com>
2025-01-30 10:23:39 +0000

    reformat

--- tests/transformers/loc/test_common.py
+++ tests/transformers/loc/test_common.py
@@ -1,14 +1,14 @@
-from src.transformers.loc.common import remove_id_prefix
+from transformers.loc.common import remove_id_prefix
 
 
-def test_remove_prefix_noop():
+def test_remove_prefix_noop() -> None:
     """
     If there is no prefix to remove, remove_id_prefix will do nothing
     """
     assert remove_id_prefix("sh1234567890") == "sh1234567890"
 
 
-def test_remove_prefix_fully_qualified():
+def test_remove_prefix_fully_qualified() -> None:
     """
     remove_id_prefix removes fully-qualified URL-style prefixes
     """
@@ -22,7 +22,7 @@ def test_remove_prefix_fully_qualified():
     )
 
 
-def test_remove_prefix_relative():
+def test_remove_prefix_relative() -> None:
     """
     remove_id_prefix removes relative/local prefixes
     """
@@ -30,7 +30,7 @@ def test_remove_prefix_relative():
     assert remove_id_prefix("/authorities/names/sh0987654321") == "sh0987654321"
 
 
-def test_remove_prefix_lookalikes():
+def test_remove_prefix_lookalikes() -> None:
     """
     remove_id_prefix only removes specific known prefixes,
     not just things that look a bit like them
