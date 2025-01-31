7346c8152e488e8f9b59d32fb615a4191c016315
Štěpán Brychta <stepan.brychta@me.com>
2025-01-31 17:27:41 +0000

    Add Wikidata source testing for nodes #5898

--- tests/sources/test_wikidata_concepts_source.py
+++ tests/sources/test_wikidata_concepts_source.py
@@ -9,7 +9,7 @@ from test_utils import load_fixture
 
 def add_mock_wikidata_request(
     query_type: Literal[
-        "all_ids", "linked_ids", "parents_instance_of", "parents_subclass_of"
+        "all_ids", "linked_ids", "parents_instance_of", "parents_subclass_of", "items"
     ]
 ) -> None:
     params = json.loads(load_fixture(f"wikidata/{query_type}_query.json"))
@@ -19,7 +19,7 @@ def add_mock_wikidata_request(
     )
 
 
-def test_wikidata_concepts_source() -> None:
+def test_wikidata_concepts_source_edges() -> None:
     MockSmartOpen.mock_s3_file(
         "s3://bulk_load_test_bucket/loc_concepts__nodes.csv",
         load_fixture("loc_concepts_transformer_output.csv").decode(),
@@ -55,3 +55,36 @@ def test_wikidata_concepts_source() -> None:
     assert ("Q1", "Q4") in has_parent_edges
     assert ("Q2", "Q1") in has_parent_edges
     assert ("Q2", "Q3") in has_parent_edges
+
+
+def test_wikidata_concepts_source_nodes() -> None:
+    MockSmartOpen.mock_s3_file(
+        "s3://bulk_load_test_bucket/loc_concepts__nodes.csv",
+        load_fixture("loc_concepts_transformer_output.csv").decode(),
+    )
+    MockSmartOpen.mock_s3_file(
+        "s3://bulk_load_test_bucket/loc_locations__nodes.csv",
+        load_fixture("loc_concepts_transformer_output.csv").decode(),
+    )
+    MockSmartOpen.mock_s3_file(
+        "s3://bulk_load_test_bucket/loc_names__nodes.csv",
+        load_fixture("loc_concepts_transformer_output.csv").decode(),
+    )
+    add_mock_wikidata_request("all_ids")
+    add_mock_wikidata_request("linked_ids")
+    add_mock_wikidata_request("parents_instance_of")
+    add_mock_wikidata_request("parents_subclass_of")
+    add_mock_wikidata_request("items")
+
+    mesh_concepts_source = WikidataLinkedOntologySource(
+        node_type="concepts", linked_ontology="loc", entity_type="nodes"
+    )
+
+    stream_result = list(mesh_concepts_source.stream_raw())
+
+    assert len(stream_result) == 4
+
+    for raw_node in stream_result:
+        assert "item" in raw_node
+        assert "itemLabel" in raw_node
+        assert "itemDescription" in raw_node
