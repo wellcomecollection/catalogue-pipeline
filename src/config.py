45b238313552e70dd4404c23ce9055c42797b87c
Paul Butcher <paul.butcher@gmail.com>
2025-02-04 10:31:02 +0000

    use mads for subject headings

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ S3_BULK_LOAD_BUCKET_NAME = os.environ.get("S3_BULK_LOAD_BUCKET_NAME")
 GRAPH_QUERIES_SNS_TOPIC_ARN = os.environ.get("GRAPH_QUERIES_SNS_TOPIC_ARN")
 
 LOC_SUBJECT_HEADINGS_URL = (
-    "https://id.loc.gov/download/authorities/subjects.skosrdf.jsonld.gz"
+    "https://id.loc.gov/download/authorities/subjects.madsrdf.jsonld.gz"
 )
 LOC_NAMES_URL = "https://id.loc.gov/download/authorities/names.skosrdf.jsonld.gz"
 MESH_URL = "https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/desc2025.gz"
