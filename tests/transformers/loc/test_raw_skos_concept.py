10f50dc4dbcf8c5a15a4f38eb20e2d2250e6a8ea
Štěpán Brychta <stepan.brychta@me.com>
2025-02-06 11:06:05 +0000

    Organise test fixtures into folders

--- tests/transformers/loc/test_raw_skos_concept.py
+++ tests/transformers/loc/test_raw_skos_concept.py
@@ -10,13 +10,13 @@ def test_label() -> None:
     Label is extracted from skos:prefLabel
     """
     concept = RawLibraryOfCongressSKOSConcept(
-        json.loads(load_fixture("skos_concept.json"))
+        json.loads(load_fixture("loc/skos_concept.json"))
     )
     assert concept.label == "Pickling"
 
 
 def test_broader_concepts() -> None:
     concept = RawLibraryOfCongressSKOSConcept(
-        json.loads(load_fixture("skos_geographic_concept.json"))
+        json.loads(load_fixture("loc/skos_geographic_concept.json"))
     )
     assert concept.broader_concept_ids == ["sh85040229", "sh85053109", "sh92006359"]
