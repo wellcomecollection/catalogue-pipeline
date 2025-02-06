f3abb8f193e721da766c550e65fdcbf604b6d50b
Štěpán Brychta <stepan.brychta@me.com>
2025-02-06 14:27:44 +0000

    Expand Wikidata unit testing to cover names

---
+++ tests/sources/test_wikidata_names_source.py
@@ -0,0 +1,35 @@
+from sources.wikidata.linked_ontology_source import WikidataLinkedOntologySource
+from test_wikidata_concepts_source import (
+    _add_mock_wikidata_requests,
+    _add_mock_transformer_outputs,
+)
+
+
+def test_wikidata_names_source_edges() -> None:
+    _add_mock_transformer_outputs("wikidata_linked_loc")
+    _add_mock_transformer_outputs("loc")
+    _add_mock_wikidata_requests("edges", "names")
+
+    mesh_concepts_source = WikidataLinkedOntologySource(
+        node_type="names", linked_ontology="loc", entity_type="edges"
+    )
+    stream_result = list(mesh_concepts_source.stream_raw())
+
+    assert len(stream_result) == 3
+
+    same_as_edges = set()
+    has_field_of_work_edges = set()
+    for edge in stream_result:
+        if edge["type"] == "SAME_AS":
+            same_as_edges.add((edge["from_id"], edge["to_id"]))
+        elif edge["type"] == "HAS_FIELD_OF_WORK":
+            has_field_of_work_edges.add((edge["from_id"], edge["to_id"]))
+        else:
+            raise ValueError(f"Unknown edge type {edge['type']}")
+
+    assert len(same_as_edges) == 2
+    assert ("Q100", "n00000001") in same_as_edges
+    assert ("Q101", "n00000021") in same_as_edges
+
+    assert len(has_field_of_work_edges) == 1
+    assert ("Q100", "Q1") in has_field_of_work_edges
