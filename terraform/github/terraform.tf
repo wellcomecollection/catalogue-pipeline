405632595d40865ddc1609ed16f35259fe773189
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-02-11 15:36:37 +0000

    indent + correct repo name + backend for new tfstack

--- terraform/github/terraform.tf
+++ terraform/github/terraform.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_version = ">= 0.11"
+
+  backend "s3" {
+    assume_role = {
+      role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+    }
+    bucket         = "wellcomecollection-platform-infra"
+    key            = "terraform/catalogue/ci.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
+  }
+}
+
 data "terraform_remote_state" "aws_account_infrastructure" {
   backend = "s3"
 
