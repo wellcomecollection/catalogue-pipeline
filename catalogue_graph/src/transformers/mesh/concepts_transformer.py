c45fe0a44cbf4424306c6e8dfd323c617abdd238
Antonia Langfelder <a.langfelder@wellcome.org>
2025-02-12 21:01:19 +0000

    Remove duplicate edges

--- catalogue_graph/src/transformers/mesh/concepts_transformer.py
+++ catalogue_graph/src/transformers/mesh/concepts_transformer.py
@@ -42,6 +42,3 @@ class MeSHConceptsTransformer(BaseTransformer):
             yield SourceConceptRelatedTo(
                 from_id=raw_concept.source_id, to_id=related_id
             )
-            yield SourceConceptRelatedTo(
-                from_id=related_id, to_id=raw_concept.source_id
-            )
