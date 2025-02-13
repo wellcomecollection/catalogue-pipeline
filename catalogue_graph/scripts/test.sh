cc4fe4bed56d69059e2eba1ef86531b6417b7eb2
Štěpán Brychta <stepan.brychta@me.com>
2025-02-13 09:09:04 +0000

    Update github permissions

--- catalogue_graph/scripts/test.sh
+++ catalogue_graph/scripts/test.sh
@@ -6,6 +6,6 @@ set -o nounset
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT+="$(dirname "$DIR")"
 
-pytest --cov src \
+pytest catalogue_graph --cov src \
     --cov-report term \
     --cov-report xml:coverage.xml
