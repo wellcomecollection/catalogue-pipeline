baeec3047eed2755b518187260edea10277aa81e
Antonia Langfelder <a.langfelder@wellcome.org>
2025-02-17 14:27:26 +0000

    Add test for MeSH location

---
+++ catalogue_graph/tests/transformers/test_mesh_locations_transformer.py
@@ -0,0 +1,38 @@
+from test_mocks import MockRequest
+from test_utils import load_fixture
+
+from transformers.mesh.locations_transformer import MeSHLocationsTransformer
+from models.graph_node import SourceLocation
+
+
+def test_mesh_concepts_transformer() -> None:
+    test_url = "https://example.com"
+
+    MockRequest.mock_responses(
+        [
+            {
+                "method": "GET",
+                "url": test_url,
+                "status_code": 200,
+                "json_data": None,
+                "content_bytes": load_fixture("mesh/raw_descriptors.xml"),
+                "params": None,
+            }
+        ]
+    )
+    mesh_concepts_transformer = MeSHLocationsTransformer(test_url)
+
+    # test transform_node
+    nodes = list(mesh_concepts_transformer._stream_nodes())
+
+    assert len(list(nodes)) == 1
+    assert nodes[0] == SourceLocation(
+        id="D008131",
+        label="London",
+        source="nlm-mesh",
+        alternative_ids=["Z01"],
+        alternative_labels=[],
+        description="The capital of the United Kingdom. It is located in England.\n    ",
+        latitude=None,
+        longitude=None
+    )
