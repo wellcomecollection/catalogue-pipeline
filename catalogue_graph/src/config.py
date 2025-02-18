edc0fdfc388267020cf1e6510f6f26275e1c38fe
Antonia Langfelder <a.langfelder@wellcome.org>
2025-02-18 18:09:19 +0000

    Update lcnaf url

--- catalogue_graph/src/config.py
+++ catalogue_graph/src/config.py
@@ -6,7 +6,7 @@ GRAPH_QUERIES_SNS_TOPIC_ARN = os.environ.get("GRAPH_QUERIES_SNS_TOPIC_ARN")
 LOC_SUBJECT_HEADINGS_URL = (
     "https://id.loc.gov/download/authorities/subjects.madsrdf.jsonld.gz"
 )
-LOC_NAMES_URL = "https://id.loc.gov/download/authorities/names.skosrdf.jsonld.gz"
+LOC_NAMES_URL = "https://id.loc.gov/download/authorities/names.madsrdf.jsonld.gz"
 MESH_URL = "https://nlmpubs.nlm.nih.gov/projects/mesh/MESH_FILES/xmlmesh/desc2025.gz"
 WIKIDATA_SPARQL_URL = "https://query.wikidata.org/sparql"
 CATALOGUE_SNAPSHOT_URL = (
