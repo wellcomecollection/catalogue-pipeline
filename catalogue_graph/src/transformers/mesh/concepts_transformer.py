7becfbec6da3b8a89efc994e3a941332a3fadc7c
Antonia Langfelder <a.langfelder@wellcome.org>
2025-02-20 11:23:32 +0000

    Add comment about RELATED_TO edge directionality

--- catalogue_graph/src/transformers/mesh/concepts_transformer.py
+++ catalogue_graph/src/transformers/mesh/concepts_transformer.py
@@ -38,6 +38,8 @@ class MeSHConceptsTransformer(BaseTransformer):
         for parent_id in raw_concept.parent_concept_ids:
             yield SourceConceptHasParent(from_id=raw_concept.source_id, to_id=parent_id)
 
+        # RELATED_TO edges are undirected, but we are only creating one edge here to avoid
+        # duplicates due to both MeSH concepts referencing each other in the source data 
         for related_id in raw_concept.related_concept_ids:
             yield SourceConceptRelatedTo(
                 from_id=raw_concept.source_id, to_id=related_id
