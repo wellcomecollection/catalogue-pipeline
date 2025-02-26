b3d83a0dc584e2b6869aacb25f6578c6c79a43aa
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-02-26 09:52:15 +0000

    Apply auto-formatting rules

--- pipeline/terraform/modules/stack/elastic_pipeline.tf
+++ pipeline/terraform/modules/stack/elastic_pipeline.tf
@@ -119,7 +119,7 @@ module "pipeline_indices" {
   es_works_denormalised_index = local.es_works_denormalised_index
   es_works_index              = local.es_works_index
 
-  es_concepts_index           = local.es_concepts_index
+  es_concepts_index = local.es_concepts_index
 
   es_images_initial_index   = local.es_images_initial_index
   es_images_augmented_index = local.es_images_augmented_index
