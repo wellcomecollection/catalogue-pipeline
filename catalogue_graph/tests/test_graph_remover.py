ff4ba1dab9e0ef6726fd37fe3f2a5ac9635864e9
Štěpán Brychta <stepan.brychta@me.com>
2025-04-07 14:35:37 +0100

    Update test_graph_remover.py

--- catalogue_graph/tests/test_graph_remover.py
+++ catalogue_graph/tests/test_graph_remover.py
@@ -2,10 +2,9 @@ from datetime import datetime
 
 import polars as pl
 import pytest
+from graph_remover import lambda_handler
 from test_mocks import MockRequest, MockSmartOpen
 from test_utils import load_fixture
-
-from graph_remover import lambda_handler
 
 CATALOGUE_CONCEPTS_SNAPSHOT_URI = "s3://wellcomecollection-catalogue-graph/graph_remover/previous_ids_snapshot/catalogue_concepts__nodes.parquet"
 CATALOGUE_CONCEPTS_REMOVED_IDS_URI = "s3://wellcomecollection-catalogue-graph/graph_remover/deleted_ids/catalogue_concepts__nodes.parquet"
@@ -34,14 +33,14 @@ def test_graph_remover_first_run() -> None:
 
 
 def test_graph_remover_subsequent_run() -> None:
+    # Mock a previous concept IDs snapshot which purposefully omits some IDs from the bulk load file mock
+    # and includes some IDs not in the bulk load file mock.
     MockSmartOpen.mock_s3_file(
         CATALOGUE_CONCEPTS_SNAPSHOT_URI,
         load_fixture("catalogue/id_snapshot_catalogue_concepts__nodes.parquet"),
     )
-    # MockSmartOpen.mock_s3_file(
-    #     CATALOGUE_CONCEPTS_SNAPSHOT_URI,
-    #     load_fixture("catalogue/id_snapshot_catalogue_concepts__nodes.parquet"),
-    # )
+    
+    add_bulk_load_file_mocks()
 
     MockRequest.mock_responses(
         [
@@ -56,13 +55,10 @@ def test_graph_remover_subsequent_run() -> None:
         ]
     )
 
-    add_bulk_load_file_mocks()
-
     event = {"transformer_type": "catalogue_concepts", "entity_type": "nodes"}
     lambda_handler(event, None)
-
-    print(MockSmartOpen.file_lookup)
-
+    
+    # Verify that the correct IDs are listed in the 'added IDs' file
     with MockSmartOpen.open(CATALOGUE_CONCEPTS_ADDED_IDS_URI, "rb") as f:
         df = pl.read_parquet(f)
         ids = pl.Series(df.select(pl.col("id"))).to_list()
@@ -73,6 +69,7 @@ def test_graph_remover_subsequent_run() -> None:
 
         assert set(ids) == {"fqe7m83w", "pnpsyqp8", "drypfe3u"}
 
+    # Verify that the correct IDs are listed in the 'removed IDs' file
     with MockSmartOpen.open(CATALOGUE_CONCEPTS_REMOVED_IDS_URI, "rb") as f:
         df = pl.read_parquet(f)
         ids = pl.Series(df.select(pl.col("id"))).to_list()
@@ -82,12 +79,16 @@ def test_graph_remover_subsequent_run() -> None:
         assert timestamps[0] == datetime.today().date()
 
         assert set(ids) == {"byzuqyr5"}
-
+    
+    # Verify that the snapshot file was correctly updated
     with MockSmartOpen.open(CATALOGUE_CONCEPTS_SNAPSHOT_URI, "rb") as f:
         df = pl.read_parquet(f)
         ids = pl.Series(df.select(pl.first())).to_list()
-        assert len(set(ids)) == 21
+        assert len(set(ids)) == 19
         assert "vjfb76xy" in ids
+        assert "byzuqyr5" not in ids
+        assert "fqe7m83w" in ids
+        assert "drypfe3u" in ids
 
 
 def test_graph_remover_missing_bulk_load_file() -> None:
