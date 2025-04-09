24dec33588ae5ec883f722a123b55b7926b4005f
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-04-09 10:07:02 +0000

    Apply auto-formatting rules

--- catalogue_graph/src/index_remover.py
+++ catalogue_graph/src/index_remover.py
@@ -2,9 +2,10 @@ import argparse
 import typing
 from datetime import date, datetime
 
-import config
 import polars as pl
 import smart_open
+
+import config
 import utils.elasticsearch
 from graph_remover import DELETED_IDS_FOLDER
 from utils.aws import df_from_s3_parquet
@@ -28,10 +29,10 @@ def _get_concepts_index_name(pipeline_date: str | None) -> str:
 def get_current_id_count(pipeline_date: str | None, is_local: bool) -> int:
     """Return the number of documents currently stored in ES in the concepts index."""
     es = utils.elasticsearch.get_client(pipeline_date, is_local)
-    
+
     response = es.count(index=_get_concepts_index_name(pipeline_date))
     count: int = response.get("count", 0)
-    return count 
+    return count
 
 
 def delete_concepts_from_elasticsearch(
@@ -42,16 +43,9 @@ def delete_concepts_from_elasticsearch(
     index_name = _get_concepts_index_name(pipeline_date)
 
     response = es.delete_by_query(
-        index=index_name,
-        body={
-            "query": {
-                "ids": {
-                    "values": list(deleted_ids)
-                }
-            }
-        }
+        index=index_name, body={"query": {"ids": {"values": list(deleted_ids)}}}
     )
-    
+
     deleted_count = response["deleted"]
     print(f"Deleted {deleted_count} documents from the {index_name} index.")
 
@@ -62,7 +56,7 @@ def get_ids_to_delete(pipeline_date: str | None) -> set[str]:
 
     # Retrieve a log of concept IDs which were deleted from the graph (see `graph_remover.py`).
     df = df_from_s3_parquet(s3_file_uri)
-    
+
     try:
         # Filter for IDs which were added to the log file since the last run of the index_remover.
         cutoff_date = get_last_run_date(pipeline_date)
@@ -70,7 +64,7 @@ def get_ids_to_delete(pipeline_date: str | None) -> set[str]:
     except (OSError, KeyError):
         # The file might not exist on the first run, which implies that no filter should be applied.
         pass
-    
+
     ids = pl.Series(df.select(pl.col("id"))).to_list()
     return set(ids)
 
@@ -80,17 +74,17 @@ def get_last_run_date(pipeline_date: str | None) -> date:
     s3_uri = _get_last_index_remover_run_file_uri(pipeline_date)
     with smart_open.open(s3_uri, "r") as f:
         formatted_date = f.read()
-    
-    return datetime.strptime(formatted_date, '%Y-%m-%d').date()
+
+    return datetime.strptime(formatted_date, "%Y-%m-%d").date()
 
 
 def update_last_run_date(pipeline_date: str | None) -> None:
     """Update the S3 file storing the date of the last index_remover run with today's date."""
     s3_uri = _get_last_index_remover_run_file_uri(pipeline_date)
-    formatted_today = datetime.today().strftime('%Y-%m-%d')
+    formatted_today = datetime.today().strftime("%Y-%m-%d")
     with smart_open.open(s3_uri, "w") as f:
         f.write(formatted_today)
-    
+
 
 def handler(
     pipeline_date: str | None,
@@ -99,7 +93,7 @@ def handler(
 ) -> None:
     ids_to_delete = get_ids_to_delete(pipeline_date)
     current_id_count = get_current_id_count(pipeline_date, is_local)
-    
+
     if (
         current_id_count > 0
         and len(ids_to_delete) / current_id_count > ACCEPTABLE_DIFF_THRESHOLD
@@ -134,7 +128,7 @@ def local_handler() -> None:
     parser.add_argument(
         "--pipeline-date",
         type=str,
-        help='The pipeline date corresponding to the concepts index to remove from.',
+        help="The pipeline date corresponding to the concepts index to remove from.",
         required=False,
     )
 
