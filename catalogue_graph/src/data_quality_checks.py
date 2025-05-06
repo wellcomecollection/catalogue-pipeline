368fd716462da900264a987a4d5e940872163c8d
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-05-06 16:22:39 +0000

    Apply auto-formatting rules

--- catalogue_graph/src/data_quality_checks.py
+++ catalogue_graph/src/data_quality_checks.py
@@ -9,13 +9,17 @@ client = get_neptune_client(True)
 def are_concept_types_consistent(concept_types: list[ConceptType]) -> bool:
     # 'Concept' and 'Subject' types are consistent with all other types,
     # so we filter them out when determining consistency
-    filtered_types = [concept_type for concept_type in concept_types if concept_type not in ('Concept', 'Subject')]
+    filtered_types = [
+        concept_type
+        for concept_type in concept_types
+        if concept_type not in ("Concept", "Subject")
+    ]
 
     if len(filtered_types) <= 1:
         return True
 
     # Of all remaining types, only Agent/Org and Agent/Person are compatible. All other combinations are invalid.
-    compatible_combinations = [{'Agent', 'Organisation'}, {'Agent', 'Person'}]
+    compatible_combinations = [{"Agent", "Organisation"}, {"Agent", "Person"}]
     return set(filtered_types) in compatible_combinations
 
 
@@ -41,9 +45,13 @@ with open("invalid.txt", "w") as f:
         """
         print(start_offset)
 
-        concept_result = client.run_open_cypher_query(query, {"start_offset": start_offset, "limit": LIMIT})
+        concept_result = client.run_open_cypher_query(
+            query, {"start_offset": start_offset, "limit": LIMIT}
+        )
         for concept in concept_result:
             if not are_concept_types_consistent(concept["concept_types"]):
-                f.write(f"{concept['concept']['id']} {concept['concept']['label']} {concept['concept_types']}\n")
+                f.write(
+                    f"{concept['concept']['id']} {concept['concept']['label']} {concept['concept_types']}\n"
+                )
         f.flush()
         start_offset += LIMIT
