e0704b28a89a779d81bb21e1fa901ee1ff258126
Štěpán Brychta <stepan.brychta@me.com>
2025-05-07 13:07:13 +0100

    Update data_quality_checks.py

--- catalogue_graph/src/data_quality_checks.py
+++ catalogue_graph/src/data_quality_checks.py
@@ -1,14 +1,29 @@
 import csv
 
+import boto3
+import config
+import smart_open
 from models.graph_node import ConceptType
 from utils.aws import get_neptune_client
 
 LIMIT = 20000
 
-client = get_neptune_client(True)
+CONCEPT_TYPES_QUERY = """
+    MATCH (concept:Concept)
+    WITH concept ORDER BY concept.id
+    SKIP $start_offset LIMIT $limit
+    OPTIONAL MATCH (work)-[has_concept:HAS_CONCEPT]-(concept)
+    WITH 
+        concept,
+        collect(DISTINCT has_concept.referenced_type) AS concept_types
+    RETURN
+        concept,
+        concept_types        
+"""
 
 
 def are_concept_types_consistent(concept_types: list[ConceptType]) -> bool:
+    """Return `True` if all provided concept types are mutually compatible. Otherwise, return `False`."""
     # 'Concept' and 'Subject' types are consistent with all other types,
     # so we filter them out when determining consistency
     filtered_types = [
@@ -25,41 +40,40 @@ def are_concept_types_consistent(concept_types: list[ConceptType]) -> bool:
     return set(filtered_types) in compatible_combinations
 
 
-count_query = "MATCH (c:Concept) RETURN count(c) AS count"
-concepts_count = client.run_open_cypher_query(count_query)[0]["count"]
+def check_concept_type_consistency() -> None:
+    count_query = "MATCH (c:Concept) RETURN count(c) AS count"
+    client = get_neptune_client(True)
+    concepts_count = client.run_open_cypher_query(count_query)[0]["count"]
 
-with open("invalid.csv", "w") as f:
-    csv_writer = csv.DictWriter(
-        f, fieldnames=["concept_id", "concept_label", "concept_types"]
-    )
-    csv_writer.writeheader()
+    transport_params = {"client": boto3.client("s3")}
+    s3_uri = f"s3://{config.INGESTOR_S3_BUCKET}/data_quality_checks/inconsistent_concept_types.csv"
+    with smart_open.open(s3_uri, "w", transport_params=transport_params) as f:
+        csv_writer = None
 
-    start_offset = 0
-    while start_offset < concepts_count:
-        query = """
-            MATCH (concept:Concept)
-            WITH concept ORDER BY concept.id
-            SKIP $start_offset LIMIT $limit
-            OPTIONAL MATCH (work)-[has_concept:HAS_CONCEPT]-(concept)
-            WITH 
-                concept,
-                collect(DISTINCT has_concept.referenced_type) AS concept_types
-            RETURN
-                concept,
-                concept_types        
-        """
+        start_offset = 0
+        while start_offset < concepts_count:
+            params = {"start_offset": start_offset, "limit": LIMIT}
+            response = client.run_open_cypher_query(CONCEPT_TYPES_QUERY, params)
 
-        concept_result = client.run_open_cypher_query(
-            query, {"start_offset": start_offset, "limit": LIMIT}
-        )
-        for concept in concept_result:
-            if not are_concept_types_consistent(concept["concept_types"]):
-                csv_writer.writerow(
-                    {
-                        "concept_id": concept["concept"]["~properties"]["id"],
-                        "concept_label": concept["concept"]["~properties"]["label"],
-                        "concept_types": "||".join(concept["concept_types"]),
+            for concept in response:
+                concept_properties = concept["concept"]["~properties"]
+                concept_types = concept["concept_types"]
+
+                if not are_concept_types_consistent(concept_types):
+                    invalid_item = {
+                        "concept_id": concept_properties["id"],
+                        "concept_label": concept_properties["label"],
+                        "concept_types": "||".join(concept_types),
                     }
-                )
-        f.flush()
-        start_offset += LIMIT
+
+                    if csv_writer is None:
+                        csv_writer = csv.DictWriter(f, fieldnames=invalid_item.keys())
+                        csv_writer.writeheader()
+
+                    csv_writer.writerow(invalid_item)
+
+            start_offset += LIMIT
+
+
+if __name__ == "__main__":
+    check_concept_type_consistency()
