2b3d9ac6e3baef3ebfde96433ac0d1376e316c7a
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-06-10 14:17:45 +0100

    merger lambda config

---
+++ pipeline/matcher_merger/merger/src/main/scala/weco/pipeline/merger/config/MergerConfig.scala
@@ -0,0 +1,39 @@
+package weco.pipeline.merger.config
+
+import com.typesafe.config.Config
+import weco.elasticsearch.typesafe.ElasticBuilder.buildElasticClientConfig
+import weco.elasticsearch.typesafe.ElasticConfig
+import weco.lambda.DownstreamBuilder.buildDownstreamTarget
+import weco.lambda.{ApplicationConfig, DownstreamTarget, LambdaConfigurable}
+
+
+case class MergerConfig(
+  elasticConfig: ElasticConfig,
+  identifiedWorkIndex: String,
+  denormalisedWorkIndex: String,
+  initialImageIndex: String,
+  workDownstreamTarget: DownstreamTarget,
+  pathDownstreamTarget: DownstreamTarget,
+  pathConcatDownstreamTarget: DownstreamTarget,
+  imageDownstreamTarget: DownstreamTarget
+
+) extends ApplicationConfig
+
+trait MergerConfigurable
+  extends LambdaConfigurable[MergerConfig] {
+  import weco.typesafe.config.builders.EnrichConfig._
+
+  def build(rawConfig: Config): MergerConfig =
+    MergerConfig(
+      elasticConfig = buildElasticClientConfig(rawConfig),
+      identifiedWorkIndex = rawConfig.requireString(("es.identified-works.index")),
+      denormalisedWorkIndex =
+        rawConfig.requireString("es.denormalised-works.index"),
+      initialImageIndex = rawConfig.requireString("es.initial-images.index"),
+      workDownstreamTarget = buildDownstreamTarget(rawConfig),
+      pathDownstreamTarget = buildDownstreamTarget(rawConfig),
+      pathConcatDownstreamTarget = buildDownstreamTarget(rawConfig),
+      imageDownstreamTarget = buildDownstreamTarget(rawConfig)
+    )
+}
+
