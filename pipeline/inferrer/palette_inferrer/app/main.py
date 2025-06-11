c7e682d44de4449ac284e9193cc0c313cbf28370
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-06-11 13:35:11 +0000

    Apply auto-formatting rules

--- pipeline/inferrer/palette_inferrer/app/main.py
+++ pipeline/inferrer/palette_inferrer/app/main.py
@@ -29,7 +29,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     title="Palette extractor",
     description="extracts color palette vectors from images",
-    lifespan=lifespan
+    lifespan=lifespan,
 )
 logger.info("API started, awaiting requests")
 
