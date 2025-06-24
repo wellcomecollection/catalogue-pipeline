de518fd6cd73cfaed21b7d84ba5a72ec08bd136c
Paul Butcher <paul.butcher@gmail.com>
2025-06-24 14:58:21 +0100

    all Matcher lambda tests migrated

--- common/lambda/src/test/scala/weco/lambda/behaviours/LambdaBehaviours.scala
+++ common/lambda/src/test/scala/weco/lambda/behaviours/LambdaBehaviours.scala
@@ -1,6 +1,7 @@
 package weco.lambda.behaviours
 
 import io.circe.Decoder
+import org.scalatest.LoneElement
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
@@ -8,23 +9,31 @@ import weco.lambda.helpers.DownstreamHelper
 import weco.lambda._
 import weco.lambda.matchers.LambdaResultMatchers
 
-trait LambdaBehaviours[T, Config <: ApplicationConfig]
-    extends Matchers
+trait LambdaBehaviours[
+  T,
+  Config <: ApplicationConfig,
+  OutputMessageType,
+  ComparisonType
+] extends Matchers
     with LambdaResultMatchers[T]
     with DownstreamHelper
-    with ScalaFutures {
+    with ScalaFutures
+    with LoneElement {
   this: AnyFunSpec =>
   protected type LambdaApp = SQSBatchResponseLambdaApp[T, Config]
   protected type IncomingMessage = SQSLambdaMessage[T]
-  protected type OutputMessageType
   protected implicit val outputDecoder: Decoder[OutputMessageType]
 
-  def failingLambda(
-    sut: Downstream => LambdaApp,
+  protected def convertForComparison(
+    results: Seq[OutputMessageType]
+  ): Seq[ComparisonType]
+
+  def aFailingInvocation(
+    lambdaBuilder: Downstream => LambdaApp,
     messages: Seq[IncomingMessage]
-  ): Unit = describe("When all messages fail") {
+  ): Unit = {
     val downstream = new MemoryDownstream
-    whenReady(sut(downstream).processMessages(messages = messages)) {
+    whenReady(lambdaBuilder(downstream).processMessages(messages = messages)) {
       results: Seq[SQSLambdaMessageResult] =>
         it(
           "returns BatchItemFailure responses for each input message"
@@ -34,6 +43,54 @@ trait LambdaBehaviours[T, Config <: ApplicationConfig]
         }
         it("does not notify downstream") {
           downstream.msgSender.getMessages[OutputMessageType] shouldBe empty
+        }
+    }
+  }
+
+  def aPartialSuccess(
+    lambdaBuilder: Downstream => LambdaApp,
+    messages: Seq[IncomingMessage],
+    failingMessages: Seq[IncomingMessage],
+    outputs: Seq[ComparisonType]
+  ): Unit = {
+
+    val downstream = new MemoryDownstream
+
+    whenReady(
+      lambdaBuilder(downstream).processMessages(messages = messages)
+    ) {
+      results: Seq[SQSLambdaMessageResult] =>
+        it("returns BatchItemFailure responses only for failing ids") {
+          results should haveTheSameIdsAs(failingMessages)
+          all(results) shouldBe a[SQSLambdaMessageFailedRetryable]
+        }
+        it("notifies downstream only for successful ids") {
+          convertForComparison(
+            downstream.msgSender
+              .getMessages[OutputMessageType]
+          ) should contain theSameElementsAs outputs
+        }
+    }
+  }
+
+  def aTotalSuccess(
+    lambdaBuilder: Downstream => LambdaApp,
+    messages: Seq[IncomingMessage],
+    outputs: Seq[ComparisonType]
+  ): Unit = {
+    val downstream = new MemoryDownstream
+    whenReady(
+      lambdaBuilder(downstream).processMessages(messages = messages)
+    ) {
+      response =>
+        it("returns no results") {
+          response shouldBe empty
+        }
+        it("sends all the identifiers downstream") {
+          convertForComparison(
+            downstream.msgSender
+              .getMessages[OutputMessageType]
+          ) should contain theSameElementsAs outputs
         }
     }
   }
--- pipeline/matcher_merger/matcher/src/test/scala/weco/pipeline/matcher/MatcherFeatureTest.scala
+++ pipeline/matcher_merger/matcher/src/test/scala/weco/pipeline/matcher/MatcherFeatureTest.scala
@@ -6,12 +6,7 @@ import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 import weco.lambda.behaviours.LambdaBehaviours
-import weco.lambda.{
-  Downstream,
-  SQSLambdaMessage,
-  SQSLambdaMessageFailedRetryable,
-  SQSLambdaMessageResult
-}
+import weco.lambda.{Downstream, SQSLambdaMessage}
 import weco.pipeline.matcher.config.{MatcherConfig, MatcherConfigurable}
 import weco.pipeline.matcher.matcher.WorksMatcher
 import weco.lambda.helpers.DownstreamHelper
@@ -25,14 +20,23 @@ class MatcherFeatureTest
     with ScalaFutures
     with MatcherFixtures
     with DownstreamHelper
-    with LambdaBehaviours[String, MatcherConfig] {
+    with LambdaBehaviours[String, MatcherConfig, MatcherResult, Set[String]] {
   private object SQSTestLambdaMessage {
     def apply[T](message: T): SQSLambdaMessage[T] =
       SQSLambdaMessage(messageId = randomUUID.toString, message = message)
   }
-  protected type OutputMessageType = MatcherResult
-  protected implicit val outputDecoder: Decoder[OutputMessageType] =
+
+  protected implicit val outputDecoder: Decoder[MatcherResult] =
     MatcherResult.decoder
+
+  implicit class MatcherResultOps(result: MatcherResult) {
+    def toWorkSet: Set[String] =
+      result.works.flatMap(id => id.identifiers.map(_.identifier.toString()))
+  }
+
+  override protected def convertForComparison(
+    results: Seq[MatcherResult]
+  ): Seq[Set[String]] = results.map(_.toWorkSet)
 
   /** Stub class representing the Lambda interface, which can be connected to a
     * dummy matcher to test the behaviour of the lambda when faced with various
@@ -44,90 +48,63 @@ class MatcherFeatureTest
   ) extends MatcherSQSLambda[MatcherConfig]
       with MatcherConfigurable
 
-  val LambdaBuilder = StubLambda.curried
+  private val baadcafe = SQSTestLambdaMessage(message = "baadcafe")
+  private val baadd00d = SQSTestLambdaMessage(message = "baadd00d")
+  private val g00dcafe = SQSTestLambdaMessage(message = "g00dcafe")
+  private val g00dd00d = SQSTestLambdaMessage(message = "g00dd00d")
 
-  it should behave like failingLambda(
-    LambdaBuilder(MatcherStub(Nil)),
-    Seq(
-      SQSTestLambdaMessage(message = "baadcafe"),
-      SQSTestLambdaMessage(message = "baadd00d")
-    )
-  )
+  private val LambdaBuilder: WorksMatcher => Downstream => StubLambda =
+    StubLambda.curried
 
-  describe("when everything is successful") {
-    val matcher = MatcherStub(Seq(Set(Set("g00dcafe"), Set("g00dd00d"))))
-    val downstream = new MemoryDownstream
-    val sut = StubLambda(matcher, downstream)
-    whenReady(
-      sut.processMessages(messages =
-        Seq(
-          SQSTestLambdaMessage(message = "g00dcafe"),
-          SQSTestLambdaMessage(message = "g00dd00d")
-        )
+  describe("When all messages fail") {
+    it should behave like aFailingInvocation(
+      LambdaBuilder(MatcherStub(Nil)),
+      Seq(
+        baadcafe,
+        baadd00d
       )
-    ) {
-      response =>
-        it("returns no results") {
-          response shouldBe empty
-        }
-        it("sends all the identifiers downstream") {
-          downstream.msgSender
-            .getMessages[MatcherResult]
-            .map(
-              matcherResult =>
-                matcherResult.works
-                  .flatMap(id => id.identifiers.map(_.identifier.toString()))
-            ) should contain theSameElementsAs Seq(Set("g00dcafe", "g00dd00d"))
-        }
-    }
+    )
   }
 
-  describe("when some messages fail to process") {
-    val messages = Seq(
-      SQSTestLambdaMessage(message = "g00dcafe"),
-      SQSTestLambdaMessage(message = "baadd00d")
+  describe("When some messages fail") {
+    it should behave like aPartialSuccess(
+      LambdaBuilder(MatcherStub(Seq(Set(Set(g00dcafe.message))))),
+      messages = Seq(g00dcafe, baadd00d),
+      failingMessages = Seq(baadd00d),
+      outputs = Seq(Set(g00dcafe.message))
     )
-    val downstream = new MemoryDownstream
-    val sut = StubLambda(MatcherStub(Seq(Set(Set("g00dcafe")))), downstream)
-
-    whenReady(
-      sut
-        .processMessages(messages = messages)
-    ) {
-      results: Seq[SQSLambdaMessageResult] =>
-        it("returns BatchItemFailure responses only for failing ids") {
-          results.loneElement.messageId shouldBe messages(1).messageId
-          results.loneElement shouldBe a[SQSLambdaMessageFailedRetryable]
-        }
-        it("notifies downstream only for successful ids") {
-          downstream.msgSender
-            .getMessages[MatcherResult]
-            .map(
-              matcherResult =>
-                matcherResult.works
-                  .flatMap(
-                    id =>
-                      id.identifiers
-                        .map(_.identifier.toString())
-                  )
-            ) should contain theSameElementsAs Seq(Set("g00dcafe"))
-        }
-    }
+  }
+  describe("When everything is successful") {
+    it should behave like aTotalSuccess(
+      LambdaBuilder(MatcherStub(Seq(Set(Set("g00dcafe"), Set("g00dd00d"))))),
+      messages = Seq(g00dcafe, g00dd00d),
+      outputs = Seq(Set("g00dcafe", "g00dd00d"))
+    )
   }
 
-  describe("when matcher results contain other identifiers") {
-    val messages = Seq(
-      SQSTestLambdaMessage(message = "baadd00d"), // fails
-      SQSTestLambdaMessage(message = "g00dcafe"),
-      SQSTestLambdaMessage(message = "beefcafe"),
-      SQSTestLambdaMessage(message = "f00df00d"),
-      SQSTestLambdaMessage(message = "f00dfeed")
+  describe("When matcher results contain other identifiers") {
+    info(
+      "a message only counts as a failure if its body cannot be found in any matcher result"
     )
-    // Five messages went in, one fails and only two works come out.
+    info(
+      "*all* ids found in all matcher results are notified downstream, even if not mentioned in the messages from upstream"
+    )
+    val beefcafe = SQSTestLambdaMessage(message = "beefcafe")
+    val f00df00d = SQSTestLambdaMessage(message = "f00df00d")
+    val f00dfeed = SQSTestLambdaMessage(message = "f00dfeed")
+    // Five messages went in,
+    val messages = Seq(
+      baadd00d, // fails
+      g00dcafe,
+      beefcafe,
+      f00df00d,
+      f00dfeed
+    )
+    // one fails and only two works come out.
     // (perhaps something went wrong with the handling of those messages)
     // However, the matches returned by the two works cover
     // the two remaining works, so they are deemed to have succeeded
-    val matcher =
+    val matcherLambda = LambdaBuilder(
       MatcherStub(
         Seq(
           Set(
@@ -139,42 +116,20 @@ class MatcherFeatureTest
           )
         )
       )
-    val downstream = new MemoryDownstream
-    val sut = StubLambda(matcher, downstream)
+    )
 
-    whenReady(
-      sut.processMessages(messages = messages)
-    ) {
-      results: Seq[SQSLambdaMessageResult] =>
-        it(
-          "treats a message as a failure if its body cannot be found in any matcher result"
-        ) {
-          // baadd00d is first in the input list, and is absent from any output results
-          results.loneElement.messageId shouldBe messages.head.messageId
-          results.loneElement shouldBe a[SQSLambdaMessageFailedRetryable]
-        }
-        it(
-          "notifies downstream of *all* ids found in all matcher results, even if not mentioned in the messages from upstream"
-        ) {
-          downstream.msgSender
-            .getMessages[MatcherResult]
-            .map(
-              matcherResult =>
-                matcherResult.works
-                  .flatMap(
-                    id =>
-                      id.identifiers
-                        .map(_.identifier.toString())
-                  )
-            ) should contain theSameElementsAs Seq(
-            Set(
-              "g00dcafe",
-              "beefcafe",
-              "cafef00d"
-            ), // cafef00d was not one of the input messages, but it was found in the match for one of them
-            Set("f00df00d", "f00dfeed")
-          )
-        }
-    }
+    it should behave like aPartialSuccess(
+      matcherLambda,
+      messages = messages,
+      failingMessages = Seq(baadd00d),
+      outputs = Seq(
+        Set(
+          g00dcafe.message,
+          beefcafe.message,
+          "cafef00d"
+        ), // cafef00d was not one of the input messages, but it was found in the match for one of them
+        Set(f00df00d.message, f00dfeed.message)
+      )
+    )
   }
 }
