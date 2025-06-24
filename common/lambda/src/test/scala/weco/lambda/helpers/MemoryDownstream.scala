e7dde9a2bb5c2a788adb35b609371a2b096699d5
Agnes Garoux <a.garoux@wellcome.ac.uk>
2025-06-24 11:24:46 +0100

    make Batcher happy?

--- common/lambda/src/test/scala/weco/lambda/helpers/MemoryDownstream.scala
+++ common/lambda/src/test/scala/weco/lambda/helpers/MemoryDownstream.scala
@@ -16,6 +16,24 @@ trait MemoryDownstream {
     override def notify(workId: String): Try[Unit] =
       Try(msgSender.send(workId))
     override def notify[T](batch: T)(implicit encoder: Encoder[T]): Try[Unit] =
-      Try(msgSender.send(encoder.apply(batch).toString()))
+      sender.sendT(batch)
   }
 }
+
+
+
+//class MemoryDownstream(messageSender: MessageSender) extends Downstream {
+//  override def notify(workId: String): Try[Unit] = ???
+//  override def notify[T](batch: T)(implicit encoder: Encoder[T]): Try[Unit] = messageSender.sendT(batch)
+//}
+
+//class MemorySNSDownstream(sender: MemoryMessageSender = new MemoryMessageSender)
+//  extends Downstream {
+//
+//  val msgSender: MemoryMessageSender = sender
+//
+//  override def notify(workId: String): Try[Unit] =
+//    Try(msgSender.send(workId))
+//  override def notify[T](batch: T)(implicit encoder: Encoder[T]): Try[Unit] =
+//    sender.sendT(batch)
+//}
