11996c7c0bf76ac49a5c74277148e85ab542cdb3
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-07-10 12:37:48 +0000

    Apply auto-formatting rules

--- catalogue_graph/src/sources/catalogue/concepts_source.py
+++ catalogue_graph/src/sources/catalogue/concepts_source.py
@@ -1,9 +1,8 @@
 from collections.abc import Generator
 
-from utils.types import WorkConceptKey
-
 from sources.base_source import BaseSource
 from sources.gzip_source import GZipSource
+from utils.types import WorkConceptKey
 
 
 def extract_concepts_from_work(
@@ -11,9 +10,9 @@ def extract_concepts_from_work(
 ) -> Generator[tuple[dict, WorkConceptKey]]:
     """Returns all concepts associated with the given work. Does not deduplicate."""
     # Some subjects contain nested component concepts. For example, the subject 'Milk - Quality' consists
-    # of concepts 'Milk' and 'Quality' (each with its own Wellcome ID). For now, we are not interested in 
-    # extracting these component concepts, since the frontend does not make use of them and the resulting 
-    # theme pages would be empty. 
+    # of concepts 'Milk' and 'Quality' (each with its own Wellcome ID). For now, we are not interested in
+    # extracting these component concepts, since the frontend does not make use of them and the resulting
+    # theme pages would be empty.
     for subject in raw_work.get("subjects", []):
         yield subject, "subjects"
 
