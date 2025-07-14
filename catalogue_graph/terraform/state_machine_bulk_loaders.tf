e0afd03278815b44cee8b68eb8648bbef0f43e10
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-07-14 09:20:36 +0000

    Apply auto-formatting rules

--- catalogue_graph/terraform/state_machine_bulk_loaders.tf
+++ catalogue_graph/terraform/state_machine_bulk_loaders.tf
@@ -5,21 +5,21 @@ resource "aws_sfn_state_machine" "catalogue_graph_bulk_loaders" {
   definition = jsonencode({
     Comment = "Trigger the catalogue-graph-bulk-loader state machine in sequence for each combination of inputs."
     StartAt = "Load ${var.state_machine_inputs[0].label}"
-    States  = merge(tomap({
+    States = merge(tomap({
       for index, task_input in var.state_machine_inputs :
       "Load ${task_input.label}" => {
-        Type       = "Task"
-        Resource   = "arn:aws:states:::states:startExecution.sync:2",
+        Type     = "Task"
+        Resource = "arn:aws:states:::states:startExecution.sync:2",
         Parameters = {
           StateMachineArn = aws_sfn_state_machine.catalogue_graph_bulk_loader.arn
-          Input           = {
+          Input = {
             "transformer_type" = task_input.transformer_type,
             "entity_type"      = task_input.entity_type
           }
         }
         Next = index == length(var.state_machine_inputs) - 1 ? "Success" : "Load ${var.state_machine_inputs[index + 1].label}"
       }
-    }), {
+      }), {
       Success = {
         Type = "Succeed"
       }
@@ -34,18 +34,18 @@ resource "aws_sfn_state_machine" "catalogue_graph_bulk_loaders_monthly" {
   definition = jsonencode({
     Comment = "Trigger the catalogue-graph-bulk-loader state machine in sequence for each combination of inputs."
     StartAt = "Load ${local.concepts_pipeline_inputs_monthly[0].label}"
-    States  = merge(tomap({
+    States = merge(tomap({
       for index, task_input in local.concepts_pipeline_inputs_monthly :
       "Load ${task_input.label}" => {
-        Type       = "Task"
-        Resource   = "arn:aws:states:::states:startExecution.sync:2",
+        Type     = "Task"
+        Resource = "arn:aws:states:::states:startExecution.sync:2",
         Parameters = {
           StateMachineArn = aws_sfn_state_machine.catalogue_graph_bulk_loader.arn
           Input           = task_input
         }
         Next = index == length(local.concepts_pipeline_inputs_monthly) - 1 ? "Success" : "Load ${local.concepts_pipeline_inputs_monthly[index + 1].label}"
       }
-    }), {
+      }), {
       Success = {
         Type = "Succeed"
       }
@@ -60,18 +60,18 @@ resource "aws_sfn_state_machine" "catalogue_graph_bulk_loaders_daily" {
   definition = jsonencode({
     Comment = "Trigger the catalogue-graph-bulk-loader state machine in sequence for each combination of inputs."
     StartAt = "Load ${local.concepts_pipeline_inputs_daily[0].label}"
-    States  = merge(tomap({
+    States = merge(tomap({
       for index, task_input in local.concepts_pipeline_inputs_daily :
       "Load ${task_input.label}" => {
-        Type       = "Task"
-        Resource   = "arn:aws:states:::states:startExecution.sync:2",
+        Type     = "Task"
+        Resource = "arn:aws:states:::states:startExecution.sync:2",
         Parameters = {
           StateMachineArn = aws_sfn_state_machine.catalogue_graph_bulk_loader.arn
           Input           = task_input
         }
         Next = index == length(local.concepts_pipeline_inputs_daily) - 1 ? "Success" : "Load ${local.concepts_pipeline_inputs_daily[index + 1].label}"
       }
-    }), {
+      }), {
       Success = {
         Type = "Succeed"
       }
