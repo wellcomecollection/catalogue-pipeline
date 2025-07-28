b4d2fe66f5c09b1ee21721fc7bec57eb1b194170
Robert Kenny <r.kenny@wellcome.org>
2025-07-28 11:52:32 +0100

    correct name for namespace

--- ebsco_adapter/terraform/iceberg_adapter/main.tf
+++ ebsco_adapter/terraform/iceberg_adapter/main.tf
@@ -2,7 +2,7 @@ resource "aws_s3tables_table_bucket" "table_bucket" {
   name = "wellcomecollection-platform-ebsco-adapter"
 }
 
-resource "aws_s3tables_namespace" "example" {
+resource "aws_s3tables_namespace" "namespace" {
   namespace        = "wellcomecollection_catalogue"
   table_bucket_arn = aws_s3tables_table_bucket.table_bucket.arn
 }
