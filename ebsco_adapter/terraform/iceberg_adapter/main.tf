cfc0aed0771cca031acebf7123aad6a356d9a53a
Robert Kenny <r.kenny@wellcome.org>
2025-07-30 10:32:24 +0100

    correct namespace

--- ebsco_adapter/terraform/iceberg_adapter/main.tf
+++ ebsco_adapter/terraform/iceberg_adapter/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3tables_namespace" "namespace" {
 
 resource "aws_s3tables_table" "iceberg_table" {
   name             = "ebsco_adapter_table"
-  namespace        = aws_s3tables_namespace.example.namespace
+  namespace        = aws_s3tables_namespace.namespace.namespace
   table_bucket_arn = aws_s3tables_table_bucket.table_bucket.arn
   format           = "ICEBERG"
 }
