9aea5ed74d1936ac9c33af6f24f6909ed2565a21
Robert Kenny <kenoir@users.noreply.github.com>
2025-07-30 10:39:32 +0100

    Update ECR lifecycle policy to protect deployment tags (#2975)
    * update catalotgei-graph ECR policy
    
    * correct namespace
    
    * needs a tag ref, wildcard as this is all other tags

--- catalogue_graph/terraform/ecr.tf
+++ catalogue_graph/terraform/ecr.tf
@@ -11,11 +11,63 @@ resource "aws_ecr_lifecycle_policy" "expire_old_images" {
     rules = [
       {
         rulePriority = 1
-        description  = "Only keep the last 25 images in a repo"
+        description  = "Keep prod tagged image"
         selection = {
-          tagStatus   = "any"
+          tagStatus     = "tagged"
+          tagPrefixList = ["prod"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep latest tagged image"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["latest"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 3
+        description  = "Keep dev tagged image"
+        selection = {
+          tagStatus     = "tagged"
+          tagPrefixList = ["dev"]
+          countType     = "imageCountMoreThan"
+          countNumber   = 1
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 4
+        description  = "Expire other tagged images, keep only the last 50"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"]
+          countType      = "imageCountMoreThan"
+          countNumber    = 50
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 5
+        description  = "Expire untagged images, keep only the last 5"
+        selection = {
+          tagStatus   = "untagged"
           countType   = "imageCountMoreThan"
-          countNumber = 25
+          countNumber = 5
         }
         action = {
           type = "expire"
