92a483205d71c91ce8c8351d1ed18d293e586a7d
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-08-04 10:52:50 +0000

    Apply auto-formatting rules

--- ebsco_adapter/ebsco_adapter_iceberg/app/steps/trigger.py
+++ ebsco_adapter/ebsco_adapter_iceberg/app/steps/trigger.py
@@ -2,24 +2,32 @@ import argparse
 from typing import Any
 from pydantic import BaseModel
 
+
 class EbscoAdapterTriggerConfig(BaseModel):
     is_local: bool = False
+
 
 class EbscoAdapterTriggerEvent(BaseModel):
     job_id: str | None = None
 
-class EbscoAdapterLoaderEvent(BaseModel):
-    s3_location: str 
 
-def handler(event: EbscoAdapterTriggerEvent, config: EbscoAdapterTriggerConfig) -> EbscoAdapterLoaderEvent:
+class EbscoAdapterLoaderEvent(BaseModel):
+    s3_location: str
+
+
+def handler(
+    event: EbscoAdapterTriggerEvent, config: EbscoAdapterTriggerConfig
+) -> EbscoAdapterLoaderEvent:
     print(f"Running handler with config: {config}")
     print(f"Processing event: {event}")
     return EbscoAdapterLoaderEvent(s3_location="s3://bucket/path/to/file")
+
 
 def lambda_handler(event: EbscoAdapterTriggerEvent, context: Any) -> dict:
     return handler(
         EbscoAdapterTriggerEvent.model_validate(event), EbscoAdapterTriggerConfig()
     ).model_dump()
+
 
 def local_handler() -> None:
     parser = argparse.ArgumentParser(description="Process XML file with EBSCO adapter")
