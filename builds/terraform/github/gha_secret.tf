0af8a5f0d4c5e5ec81496b40e051910cb090d13c
Robert Kenny <kenoir@users.noreply.github.com>
2025-08-04 14:35:22 +0100

    Implement EBSCO Adapter trigger lambda skeleton (#2976)
    * start work on trigger lambda
    
    * update build and steps
    
    * Apply auto-formatting rules
    
    * trigger ci
    
    * rename app -> src
    
    * needs correct pythonpath
    
    * update build so pyarrow can install
    
    * prefer binary not supported
    
    * pin pyarrow so there are available wheels
    
    * downgrade python to 3.12 so wheels are hopefully avilable for pyarrow and pyiceberg
    
    * move ci infra, provision ebsco adapter ci role
    
    * attempt deployment of adapter
    
    * add missing workflow
    
    * correct lambda name
    
    * fix GHA stype
    
    ---------
    
    Co-authored-by: Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- builds/terraform/github/gha_secret.tf
+++ builds/terraform/github/gha_secret.tf
@@ -12,3 +12,9 @@ resource "github_actions_secret" "catalogue_graph_ci" {
   secret_name     = "CATALOGUE_GRAPH_CI_ROLE_ARN"
   plaintext_value = module.gha_catalogue_graph_ci_role.role_arn
 }
+
+resource "github_actions_secret" "ebsco_adapter_ci" {
+  repository      = "catalogue-pipeline"
+  secret_name     = "EBSCO_ADAPTER_CI_ROLE_ARN"
+  plaintext_value = module.gha_ebsco_adapter_ci_role.role_arn
+}
