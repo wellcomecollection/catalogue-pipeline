a2b4a95ea4da1fc5d119b00c7049407b4fec4342
Paul Butcher <paul.butcher@gmail.com>
2025-08-21 09:14:34 +0100

    formatting

--- catalogue_graph/tests/test_ingestor_indexer.py
+++ catalogue_graph/tests/test_ingestor_indexer.py
@@ -53,61 +53,63 @@ def test_ingestor_indexer_success(record_type: IngestorType) -> None:
 def build_failure_test_matrix() -> list[tuple]:
     concepts: Literal["works", "concepts"] = "concepts"
     works: Literal["works", "concepts"] = "works"
-    return list(chain.from_iterable(
-        [
+    return list(
+        chain.from_iterable(
             [
-                (
-                    "the file at s3_uri doesn't exist",
-                    IngestorIndexerLambdaEvent(
-                        ingestor_type=correct_type,
-                        pipeline_date="2021-07-01",
-                        index_date="2025-01-01",
-                        job_id="123",
-                        object_to_index=IngestorIndexerObject(
-                            s3_uri="s3://test-catalogue-graph/ghost-file"
+                [
+                    (
+                        "the file at s3_uri doesn't exist",
+                        IngestorIndexerLambdaEvent(
+                            ingestor_type=correct_type,
+                            pipeline_date="2021-07-01",
+                            index_date="2025-01-01",
+                            job_id="123",
+                            object_to_index=IngestorIndexerObject(
+                                s3_uri="s3://test-catalogue-graph/ghost-file"
+                            ),
                         ),
+                        None,
+                        KeyError,
+                        "Mock S3 file s3://test-catalogue-graph/ghost-file does not exist.",
                     ),
-                    None,
-                    KeyError,
-                    "Mock S3 file s3://test-catalogue-graph/ghost-file does not exist.",
-                ),
-                (
-                    "the S3 file is malformed",
-                    IngestorIndexerLambdaEvent(
-                        ingestor_type=correct_type,
-                        pipeline_date="2021-07-01",
-                        index_date="2025-01-01",
-                        job_id="123",
-                        object_to_index=IngestorIndexerObject(
-                            s3_uri="s3://test-catalogue-graph/catalogue/denormalised_works_example.jsonl"
+                    (
+                        "the S3 file is malformed",
+                        IngestorIndexerLambdaEvent(
+                            ingestor_type=correct_type,
+                            pipeline_date="2021-07-01",
+                            index_date="2025-01-01",
+                            job_id="123",
+                            object_to_index=IngestorIndexerObject(
+                                s3_uri="s3://test-catalogue-graph/catalogue/denormalised_works_example.jsonl"
+                            ),
                         ),
+                        "catalogue/denormalised_works_example.jsonl",
+                        polars.exceptions.ComputeError,
+                        "parquet: File out of specification: The file must end with PAR1",
                     ),
-                    "catalogue/denormalised_works_example.jsonl",
-                    polars.exceptions.ComputeError,
-                    "parquet: File out of specification: The file must end with PAR1",
-                ),
-                (
-                    "the S3 file contains invalid data",
-                    IngestorIndexerLambdaEvent(
-                        ingestor_type=correct_type,
-                        pipeline_date="2021-07-01",
-                        index_date="2025-01-01",
-                        job_id="123",
-                        object_to_index=IngestorIndexerObject(
-                            s3_uri="s3://test-catalogue-graph/catalogue/00000000-00000010.parquet"
+                    (
+                        "the S3 file contains invalid data",
+                        IngestorIndexerLambdaEvent(
+                            ingestor_type=correct_type,
+                            pipeline_date="2021-07-01",
+                            index_date="2025-01-01",
+                            job_id="123",
+                            object_to_index=IngestorIndexerObject(
+                                s3_uri="s3://test-catalogue-graph/catalogue/00000000-00000010.parquet"
+                            ),
                         ),
+                        f"ingestor/{wrong_type}/00000000-00000010.parquet",
+                        pydantic_core.ValidationError,
+                        "\\d+ validation errors for Indexable.*",
                     ),
-                    f"ingestor/{wrong_type}/00000000-00000010.parquet",
-                    pydantic_core.ValidationError,
-                    "\\d+ validation errors for Indexable.*",
-                ),
+                ]
+                for correct_type, wrong_type in [
+                    (concepts, works),
+                    (works, concepts),
+                ]
             ]
-            for correct_type, wrong_type in [
-            (concepts, works),
-            (works, concepts),
-        ]
-        ]
-    ))
+        )
+    )
 
 
 def get_test_id(argvalue: str) -> str:
@@ -120,11 +122,11 @@ def get_test_id(argvalue: str) -> str:
     ids=get_test_id,
 )
 def test_ingestor_indexer_failure(
-        description: str,
-        event: IngestorIndexerLambdaEvent,
-        fixture: str,
-        expected_error: Any | tuple,
-        error_message: str,
+    description: str,
+    event: IngestorIndexerLambdaEvent,
+    fixture: str,
+    expected_error: Any | tuple,
+    error_message: str,
 ) -> None:
     config = IngestorIndexerConfig()
 
