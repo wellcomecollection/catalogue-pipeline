ea98b91acd58a1a153dd533e58944fad31e95d15
Robert Kenny <r.kenny@wellcome.org>
2025-08-29 08:02:02 +0100

    Move setting up ES config into config file
    Co-Authored-By: Agnes Garoux <a.garoux@wellcome.ac.uk>

--- ebsco_adapter/ebsco_adapter_iceberg/src/config.py
+++ ebsco_adapter/ebsco_adapter_iceberg/src/config.py
@@ -16,10 +16,11 @@ LOCAL_TABLE_NAME = os.getenv("LOCAL_TABLE_NAME", "mytable")
 LOCAL_NAMESPACE = os.getenv("LOCAL_NAMESPACE", "default")
 LOCAL_DB_NAME = os.getenv("LOCAL_DB_NAME", "catalog")
 
+
 # Local Elasticsearch Configuration
-ES_LOCAL_HOST = os.environ.get("ES_LOCAL_HOST")
-ES_LOCAL_PORT = os.environ.get("ES_LOCAL_PORT")
-ES_LOCAL_SCHEME = os.environ.get("ES_LOCAL_SCHEME")
+ES_LOCAL_HOST = os.environ.get("ES_LOCAL_HOST", "localhost")
+ES_LOCAL_PORT = int(os.environ.get("ES_LOCAL_PORT", "9200"))
+ES_LOCAL_SCHEME = os.environ.get("ES_LOCAL_SCHEME", "http")
 ES_LOCAL_API_KEY = os.environ.get("ES_LOCAL_API_KEY")
 
 PIPELINE_DATE = os.getenv("PIPELINE_DATE", "dev")
