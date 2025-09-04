755f64877742b0abba471fbdcec22095aa19bdb8
Robert Kenny <r.kenny@wellcome.org>
2025-09-04 15:23:11 +0100

    set up for using new index date

--- ebsco_adapter/ebsco_adapter_iceberg/src/steps/loader.py
+++ ebsco_adapter/ebsco_adapter_iceberg/src/steps/loader.py
@@ -112,8 +112,7 @@ def handler(
 
 def lambda_handler(event: EbscoAdapterLoaderEvent, context: Any) -> dict[str, Any]:
     return handler(
-        EbscoAdapterLoaderEvent.model_validate(event),
-        EbscoAdapterLoaderConfig()
+        EbscoAdapterLoaderEvent.model_validate(event), EbscoAdapterLoaderConfig()
     ).model_dump()
 
 
