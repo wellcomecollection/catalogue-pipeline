b340b9992a50cf187f7f4ec1816b2e2b6f8a3ded
Štěpán Brychta <stepan.brychta@me.com>
2025-09-04 17:53:54 +0100

    Update relation.py

--- catalogue_graph/src/ingestor/models/display/relation.py
+++ catalogue_graph/src/ingestor/models/display/relation.py
@@ -1,5 +1,3 @@
-from typing import Self
-
 from pydantic import BaseModel
 
 from ingestor.models.neptune.node import WorkNode
@@ -9,7 +7,6 @@ class DisplayRelation(BaseModel):
     id: str | None
     title: str | None
     referenceNumber: str | None = None
-    partOf: list[Self] | None = None
     totalParts: int
     type: str = "Work"
 
@@ -22,15 +19,3 @@ class DisplayRelation(BaseModel):
             referenceNumber=node.properties.reference_number,
             totalParts=total_parts,
         )
-
-    @staticmethod
-    def from_flat_hierarchy(
-        hierarchy: list[WorkNode], total_parts: int
-    ) -> "DisplayRelation":
-        """Recursively build a hierarchy of ancestors from a flat list, starting from the parent (the first item
-        in the list) to the root ancestor work (the last item in the list)."""
-        relation = DisplayRelation.from_neptune_node(hierarchy[0], total_parts)
-        if len(hierarchy) > 1:
-            relation.partOf = [DisplayRelation.from_flat_hierarchy(hierarchy[1:], 1)]
-
-        return relation
