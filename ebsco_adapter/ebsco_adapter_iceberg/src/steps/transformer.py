f81c527ee61d10afa356129bf6649efb05a6d2e2
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-09-09 13:21:48 +0000

    Apply auto-formatting rules

--- ebsco_adapter/ebsco_adapter_iceberg/src/steps/transformer.py
+++ ebsco_adapter/ebsco_adapter_iceberg/src/steps/transformer.py
@@ -236,7 +236,9 @@ def handler(
         "works-source",
         (
             # Cascading choice for date in index name
-            event.index_date or config_obj.index_date or config_obj.pipeline_date
+            event.index_date
+            or config_obj.index_date
+            or config_obj.pipeline_date
         ),
     )
 
