659da13fe9e49a2d03923e56a52869bb9e95e493
Paul Butcher <paul.butcher@gmail.com>
2025-09-23 14:26:55 +0100

    Copilot translates test_designation to gherkin

--- ebsco_adapter/ebsco_adapter_iceberg/tests/transformers/test_designation.py
+++ ebsco_adapter/ebsco_adapter_iceberg/tests/transformers/test_designation.py
@@ -2,6 +2,7 @@ import pytest
 from pymarc.record import Field, Record, Subfield
 
 from transformers.ebsco_to_weco import transform_record
+from ..helpers import lone_element
 
 
 def test_no_designation(marc_record: Record) -> None:
@@ -12,10 +13,10 @@ def test_no_designation(marc_record: Record) -> None:
     "marc_record",
     [
         (
-            Field(
-                tag="362",
-                subfields=[Subfield(code="a", value="    hello, I'm in space!     ")],
-            ),
+                Field(
+                    tag="362",
+                    subfields=[Subfield(code="a", value="    hello, I'm in space!     ")],
+                ),
         )
     ],
     indirect=True,
@@ -28,17 +29,17 @@ def test_tidies_value(marc_record: Record) -> None:
     "marc_record",
     [
         (
-            Field(
-                tag="362",
-                subfields=[
-                    Subfield(code="a", value="Tertiary adjunct of unimatrix zero one")
-                ],
-            ),
+                Field(
+                    tag="362",
+                    subfields=[
+                        Subfield(code="a", value="Tertiary adjunct of unimatrix zero one")
+                    ],
+                ),
         )
     ],
     indirect=True,
 )
-def test_multiple_designations(marc_record: Record) -> None:
+def test_extract_designation_from_362(marc_record: Record) -> None:
     assert transform_record(marc_record).designation == [
         "Tertiary adjunct of unimatrix zero one"
     ]
@@ -48,21 +49,21 @@ def test_multiple_designations(marc_record: Record) -> None:
     "marc_record",
     [
         (
-            Field(
-                tag="362",
-                subfields=[Subfield(code="a", value="Seven of Nine")],
-            ),
-            Field(
-                tag="362",
-                subfields=[
-                    Subfield(code="a", value="Tertiary adjunct of unimatrix zero one")
-                ],
-            ),
+                Field(
+                    tag="362",
+                    subfields=[Subfield(code="a", value="Seven of Nine")],
+                ),
+                Field(
+                    tag="362",
+                    subfields=[
+                        Subfield(code="a", value="Tertiary adjunct of unimatrix zero one")
+                    ],
+                ),
         )
     ],
     indirect=True,
 )
-def test_extract_designation_from_362(marc_record: Record) -> None:
+def test_multiple_designations(marc_record: Record) -> None:
     assert transform_record(marc_record).designation == [
         "Seven of Nine",
         "Tertiary adjunct of unimatrix zero one",
@@ -73,13 +74,13 @@ def test_extract_designation_from_362(marc_record: Record) -> None:
     "marc_record",
     [
         (
-            Field(
-                tag="362",
-                subfields=[
-                    Subfield(code="a", value="Tertiary adjunct of unimatrix zero one"),
-                    Subfield(code="z", value="Memory Alpha"),
-                ],
-            ),
+                Field(
+                    tag="362",
+                    subfields=[
+                        Subfield(code="a", value="Tertiary adjunct of unimatrix zero one"),
+                        Subfield(code="z", value="Memory Alpha"),
+                    ],
+                ),
         )
     ],
     indirect=True,
@@ -94,25 +95,25 @@ def test_ignore_subfield_z(marc_record: Record) -> None:
     "marc_record",
     [
         (
-            Field(
-                tag="362",
-                subfields=[Subfield(code="a", value="")],
-            ),
-            Field(
-                tag="362",
-                subfields=[Subfield(code="a", value="             ")],
-            ),
-            Field(
-                tag="362",
-                subfields=[Subfield(code="z", value="Bloke down the pub")],
-            ),
-            Field(
-                tag="362",
-                subfields=[
-                    Subfield(code="a", value="This one"),
-                    Subfield(code="z", value="Bloke down the pub"),
-                ],
-            ),
+                Field(
+                    tag="362",
+                    subfields=[Subfield(code="a", value="")],
+                ),
+                Field(
+                    tag="362",
+                    subfields=[Subfield(code="a", value="             ")],
+                ),
+                Field(
+                    tag="362",
+                    subfields=[Subfield(code="z", value="Bloke down the pub")],
+                ),
+                Field(
+                    tag="362",
+                    subfields=[
+                        Subfield(code="a", value="This one"),
+                        Subfield(code="z", value="Bloke down the pub"),
+                    ],
+                ),
         )
     ],
     indirect=True,
