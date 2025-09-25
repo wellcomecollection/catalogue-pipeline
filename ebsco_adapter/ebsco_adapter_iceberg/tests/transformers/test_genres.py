fc0bf40aa683190c506566bc340159850b4a932f
Paul Butcher <paul.butcher@gmail.com>
2025-09-25 16:59:13 +0100

    remove tests that have now been implemented elsewhere

--- ebsco_adapter/ebsco_adapter_iceberg/tests/transformers/test_genres.py
+++
@@ -1,7 +0,0 @@
-from pymarc.record import Record
-
-from transformers.ebsco_to_weco import transform_record
-
-
-def test_no_genres(marc_record: Record) -> None:
-    assert transform_record(marc_record).genres == []
