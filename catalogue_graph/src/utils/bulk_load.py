a916f0a535e553f07aa1ddddd886699a10fe8315
Štěpán Brychta <stepan.brychta@me.com>
2025-10-01 12:59:29 +0100

    Update bulk_load.py

--- catalogue_graph/src/utils/bulk_load.py
+++ catalogue_graph/src/utils/bulk_load.py
@@ -1,8 +1,7 @@
 from datetime import datetime, timedelta
 from pathlib import PurePosixPath
 
-import dateutil.parser
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, model_validator, parse_obj_as
 
 import config
 from utils.types import (
@@ -22,9 +21,8 @@ class IncrementalWindow(BaseModel):
     def calculate_start_time(cls, data: dict) -> dict:
         # If no `start_time` is provided, calculate it by subtracting `DEFAULT_WINDOW_MINUTES` from `end_time`
         if data.get("start_time") is None:
-            end_time = dateutil.parser.parse(data["end_time"])
+            end_time = parse_obj_as(datetime, data["end_time"])
             data["start_time"] = end_time - timedelta(minutes=DEFAULT_WINDOW_MINUTES)
-            data["end_time"] = end_time
 
         return data
 
