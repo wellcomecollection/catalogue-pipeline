7d055634487c58d1ff78b6c411979f5e80baa688
Robert Kenny <r.kenny@wellcome.org>
2025-10-13 15:19:50 +0100

    avoid mutable default types

--- catalogue_graph/src/models/pipeline/work_data.py
+++ catalogue_graph/src/models/pipeline/work_data.py
@@ -1,5 +1,7 @@
 from typing import Literal
 
+from pydantic import Field
+
 from models.pipeline.collection_path import CollectionPath
 from models.pipeline.concept import Concept, Contributor, Genre, Subject
 from models.pipeline.holdings import Holdings
@@ -24,21 +26,21 @@ class WorkData(ElasticsearchModel):
     physical_description: str | None = None
     lettering: str | None = None
     created_date: Concept | None = None
-    subjects: list[Subject] = []
-    genres: list[Genre] = []
-    contributors: list[Contributor] = []
+    subjects: list[Subject] = Field(default_factory=list)
+    genres: list[Genre] = Field(default_factory=list)
+    contributors: list[Contributor] = Field(default_factory=list)
     thumbnail: DigitalLocation | None = None
-    production: list[ProductionEvent] = []
-    languages: list[IdLabel] = []
+    production: list[ProductionEvent] = Field(default_factory=list)
+    languages: list[IdLabel] = Field(default_factory=list)
     edition: str | None = None
-    notes: list[Note] = []
+    notes: list[Note] = Field(default_factory=list)
     duration: int | None = None
-    items: list[Item] = []
-    holdings: list[Holdings] = []
+    items: list[Item] = Field(default_factory=list)
+    holdings: list[Holdings] = Field(default_factory=list)
     collection_path: CollectionPath | None = None
     reference_number: str | None = None
-    image_data: list[ImageData] = []
+    image_data: list[ImageData] = Field(default_factory=list)
     work_type: WorkType = "Standard"
     current_frequency: str | None = None
-    former_frequency: list[str] = []
-    designation: list[str] = []
+    former_frequency: list[str] = Field(default_factory=list)
+    designation: list[str] = Field(default_factory=list)
