04244c41d03637d977b1213830e53ad897c4ca54
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-10-22 16:00:47 +0000

    Apply auto-formatting rules

--- catalogue_graph/infra/graph/neptune.tf
+++ catalogue_graph/infra/graph/neptune.tf
@@ -1,12 +1,12 @@
 module "catalogue_graph_neptune_cluster" {
   source = "./modules/catalogue_graph"
 
-  namespace = local.namespace
-  vpc_id = local.vpc_id
-  private_subnets = local.private_subnets
-  public_subnets = local.public_subnets
+  namespace                  = local.namespace
+  vpc_id                     = local.vpc_id
+  private_subnets            = local.private_subnets
+  public_subnets             = local.public_subnets
   bulk_loader_s3_bucket_name = aws_s3_bucket.catalogue_graph_bucket.bucket
-  public_url = "catalogue-graph.wellcomecollection.org"
+  public_url                 = "catalogue-graph.wellcomecollection.org"
 
   providers = {
     aws = aws
