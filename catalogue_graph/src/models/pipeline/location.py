8a8cd0b280732c5a4900ee503420f6194fbc8c67
Robert Kenny <r.kenny@wellcome.org>
2025-10-23 17:44:19 +0100

    Add type discriminator to Location

--- catalogue_graph/src/models/pipeline/location.py
+++ catalogue_graph/src/models/pipeline/location.py
@@ -1,3 +1,5 @@
+from typing import Literal
+
 from models.pipeline.access_condition import AccessCondition
 from models.pipeline.id_label import Id
 from models.pipeline.serialisable import SerialisableModel
@@ -9,19 +11,24 @@ class LocationType(Id):
 
 OnlineResource = LocationType(id="online-resource")
 
+LocationDiscriminator = Literal["DigitalLocation", "PhysicalLocation"]
 
 class Location(SerialisableModel):
+    # Required for Scala deserialiser
+    type: LocationDiscriminator
     location_type: LocationType
     license: Id | None = None
     access_conditions: list[AccessCondition]
 
 
 class DigitalLocation(Location):
+    type: LocationDiscriminator = "DigitalLocation"
     url: str
     credit: str | None = None
     link_text: str | None = None
 
 
 class PhysicalLocation(Location):
+    type: LocationDiscriminator = "PhysicalLocation"
     label: str
     shelfmark: str | None = None
