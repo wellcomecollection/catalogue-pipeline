6f120b0cf1f9e8cdc5ae6a59fdfd25150dbe1861
Paul Butcher <paul.butcher@gmail.com>
2025-10-24 16:10:29 +0100

    fix inappropriate hard coded genre

--- catalogue_graph/src/adapters/ebsco/transformers/genres.py
+++ catalogue_graph/src/adapters/ebsco/transformers/genres.py
@@ -48,8 +48,8 @@ def extract_genre(field: Field) -> Genre | None:
     genre_label = " ".join(chain(a_subfields, subdivision_subfields))
 
     concepts = [
-        extract_concept_from_subfield_value("a", a_subfields[0])
-    ] + subdivision_concepts(field, SUBDIVISION_SUBFIELDS)
+                   extract_concept_from_subfield_value("a", a_subfields[0], ontology_type="Genre")
+               ] + subdivision_concepts(field, SUBDIVISION_SUBFIELDS)
 
     return Genre(
         label=genre_label,
