563a57906b17cdbd1cff37ebd2912c0d8e9fcfa5
Robert Kenny <r.kenny@wellcome.org>
2025-10-24 16:45:31 +0100

    test only visible source work with fixture

--- catalogue_graph/src/models/pipeline/source/work.py
+++ catalogue_graph/src/models/pipeline/source/work.py
@@ -1,9 +1,15 @@
+from pydantic import Field
+
+from ingestor.models.shared.merge_candidate import MergeCandidate
 from models.pipeline.work import DeletedWork, VisibleWork, Work
 from models.pipeline.work_state import WorkState
 
 
 class SourceWorkState(WorkState):
-    pass
+    # We don't need the internal_work_stubs in python, but to allow
+    # deserialisation in the scala pipeline, we include them here.
+    internal_work_stubs: list[str] = Field(default_factory=list)
+    merge_candidates: list[MergeCandidate] = Field(default_factory=list)
 
     def id(self) -> str:
         return str(self.source_identifier)
