476789263c45df8208ac7e12b516345ef28ffbfe
Štěpán Brychta <stepan.brychta@me.com>
2025-10-28 10:24:54 +0000

    Add more tests and documentation

--- catalogue_graph/src/adapters/ebsco/transformers/parents.py
+++ catalogue_graph/src/adapters/ebsco/transformers/parents.py
@@ -1,6 +1,10 @@
+import logging
+
 from pymarc.record import Record
 
 from models.pipeline.work_state import WorkAncestor
+
+logger: logging.Logger = logging.getLogger(__name__)
 
 SUBFIELD_TAGS: dict[str, list[str]] = {
     "440": ["a"],
@@ -11,18 +15,35 @@ SUBFIELD_TAGS: dict[str, list[str]] = {
 
 
 def get_parents(record: Record) -> list[WorkAncestor]:
+    """
+    Convert the parent link MARC fields into WorkAncestor objects. Links to a parent are found in four different
+    MARC fields These are all to result in a Series Relation with the title taken from the MARC field:
+        - 440 - Series Statement/Added Entry-Title (https://www.loc.gov/marc/bibliographic/bd440.html)
+        - 490 - Series Statement (https://www.loc.gov/marc/bibliographic/bd490.html)
+        - 773 - Host Item Entry (https://www.loc.gov/marc/bibliographic/bd773.html)
+        - 830 - Series Added Entry-Uniform Title (https://www.loc.gov/marc/bibliographic/bd830.html
+
+    MARC fields are designed to be output by concatenating the subfields in document order. As such, they may contain
+    punctuation (and spacing) intended to be presented in that context specifically. In this usage, we are separating
+    the main content from the subfield so this punctuation is not wanted.
+    """
     seen_titles = set()
     titles: list[str] = []
     for field in record.get_fields(*SUBFIELD_TAGS.keys()):
         subfields = field.get_subfields(*SUBFIELD_TAGS[field.tag])
         if len(subfields) == 0:
+            logger.warning(f"No {field.tag} Series relationship found for {field}.")
             continue
-        if len(subfields) > 1:
-            pass
 
-        if subfields[0] not in seen_titles:
-            seen_titles.add(subfields[0])
-            titles.append(subfields[0])
+        if len(subfields) > 1:
+            logger.warning(
+                f"Ambiguous {field.tag} Series relationship found for {field}."
+            )
+
+        title = subfields[0].removesuffix(";").removesuffix(",").strip()
+        if title and title not in seen_titles:
+            seen_titles.add(title)
+            titles.append(title)
 
     return [
         WorkAncestor(
--- catalogue_graph/tests/adapters/ebsco/transformers/test_parents.py
+++ catalogue_graph/tests/adapters/ebsco/transformers/test_parents.py
@@ -1,7 +1,9 @@
 import pytest
 from pymarc.record import Field, Record, Subfield
 
+from adapters.ebsco.transformers.ebsco_to_weco import transform_record
 from adapters.ebsco.transformers.parents import get_parents
+from models.pipeline.work_state import WorkAncestor, WorkRelations
 
 test_cases = [
     (
@@ -115,3 +117,60 @@ test_cases = [
 def test_get_parents(fields: list[Field], expected: list[str]) -> None:
     parents = get_parents(Record(fields=fields))
     assert [p.title for p in parents] == expected
+
+
+def test_remove_suffix() -> None:
+    fields = [
+        Field(
+            tag="440",
+            subfields=[Subfield(code="a", value="Some title;")],
+        )
+    ]
+
+    parents = get_parents(Record(fields=fields))
+    assert [p.title for p in parents] == ["Some title"]
+
+
+def test_remove_duplicates() -> None:
+    fields = [
+        Field(
+            tag="440",
+            subfields=[Subfield(code="a", value="   Some duplicate title   ;")],
+        ),
+        Field(
+            tag="830",
+            subfields=[Subfield(code="a", value="Some duplicate title,")],
+        ),
+    ]
+
+    parents = get_parents(Record(fields=fields))
+    assert [p.title for p in parents] == ["Some duplicate title"]
+
+
+@pytest.mark.parametrize(
+    "marc_record",
+    [
+        pytest.param(
+            [
+                Field(
+                    tag="440",
+                    subfields=[Subfield(code="a", value="A title from 440ǂa")],
+                ),
+            ]
+        )
+    ],
+    indirect=["marc_record"],
+)
+def test_multiple_holdings(marc_record: Record) -> None:
+    work = transform_record(marc_record)
+    assert work.state.relations == WorkRelations(
+        ancestors=[
+            WorkAncestor(
+                title="A title from 440ǂa",
+                work_type="Series",
+                depth=0,
+                num_children=0,
+                num_descendents=0,
+            )
+        ]
+    )
