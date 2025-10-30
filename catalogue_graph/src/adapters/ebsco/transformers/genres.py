346dd8c60e857c38bfb919e0134dab4b0f6725bb
Paul Butcher <paul.butcher@gmail.com>
2025-10-30 15:28:03 +0000

    tidy

--- catalogue_graph/src/adapters/ebsco/transformers/genres.py
+++ catalogue_graph/src/adapters/ebsco/transformers/genres.py
@@ -31,7 +31,9 @@ def extract_genres(record: Record) -> list[Genre]:
     """
     Build a list of Genre objects from MARC 655 fields.
     """
-    return distinct(non_empty(extract_genre(field) for field in record.get_fields("655")))
+    return distinct(
+        non_empty(extract_genre(field) for field in record.get_fields("655"))
+    )
 
 
 def distinct(genres: list[Genre]) -> list[Genre]:
