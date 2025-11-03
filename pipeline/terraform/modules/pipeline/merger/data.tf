a50860e4b3fb9d98dae783413a81ab5761053989
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2025-11-03 08:54:43 +0000

    Apply auto-formatting rules

--- pipeline/terraform/modules/pipeline/merger/data.tf
+++ pipeline/terraform/modules/pipeline/merger/data.tf
@@ -1,18 +1,18 @@
 locals {
   namespace_suffix = var.index_date != null ? "-${var.index_date}" : ""
-  namespace = "catalogue-${var.pipeline_date}${local.namespace_suffix}"
+  namespace        = "catalogue-${var.pipeline_date}${local.namespace_suffix}"
 
   es_upstream_config = {
-      es_upstream_host     = var.es_config.es_host
-      es_upstream_port     = var.es_config.es_port
-      es_upstream_protocol = var.es_config.es_protocol
-      es_upstream_apikey   = var.es_config.es_apikey
+    es_upstream_host     = var.es_config.es_host
+    es_upstream_port     = var.es_config.es_port
+    es_upstream_protocol = var.es_config.es_protocol
+    es_upstream_apikey   = var.es_config.es_apikey
   }
   es_downstream_config = {
-      es_downstream_host     = var.es_config.es_host
-      es_downstream_port     = var.es_config.es_port
-      es_downstream_protocol = var.es_config.es_protocol
-      es_downstream_apikey   = var.es_config.es_apikey
+    es_downstream_host     = var.es_config.es_host
+    es_downstream_port     = var.es_config.es_port
+    es_downstream_protocol = var.es_config.es_protocol
+    es_downstream_apikey   = var.es_config.es_apikey
   }
 
   index_date = var.index_date != null ? var.index_date : var.pipeline_date
