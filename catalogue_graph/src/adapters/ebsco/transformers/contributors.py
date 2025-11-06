d1ec6f2394b2f6ddddf7f64f39b789335490c56a
Paul Butcher <paul.butcher@gmail.com>
2025-11-06 17:26:45 +0000

    include $t in organisation contributors

--- catalogue_graph/src/adapters/ebsco/transformers/contributors.py
+++ catalogue_graph/src/adapters/ebsco/transformers/contributors.py
@@ -64,7 +64,7 @@ type_of_contributor: dict[str, RawConceptType] = {
 
 label_subfields: dict[str, list[str]] = {
     "00": ["a", "b", "c", "d", "q"],
-    "10": ["a", "b", "c", "d", "q"],
+    "10": ["a", "b", "c", "d", "q", "t"],
     "11": ["a", "c", "d", "n"],
 }
 
