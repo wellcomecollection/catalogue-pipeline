4bb8e8c703c0c57321177afeed5fdf3755d3a820
Štěpán Brychta <stepan.brychta@me.com>
2025-11-18 14:51:49 +0000

    Update test_query_transformer.py

--- catalogue_graph/tests/ingestor/test_query_transformer.py
+++ catalogue_graph/tests/ingestor/test_query_transformer.py
@@ -6,27 +6,22 @@ from ingestor.models.neptune.node import WorkNode
 from ingestor.models.neptune.query_result import WorkHierarchy, WorkHierarchyItem
 from ingestor.transformers.work_query_transformer import QueryWorkTransformer
 from models.graph_node import Work
+from models.pipeline.collection_path import CollectionPath
+from models.pipeline.work_state import WorkAncestor, WorkRelations
 from tests.test_utils import load_json_fixture
 
 
-def get_ancestor_node(collection_path: str | None) -> WorkNode:
-    return WorkNode.model_validate(
+def get_work_with_ancestor() -> VisibleExtractedWork:
+    fixture = load_json_fixture("ingestor/single_merged.json")
+    work = VisibleMergedWork.model_validate(fixture)
+
+    ancestor_node = WorkNode.model_validate(
         {
             "~id": "123",
             "~labels": ["Work"],
-            "~properties": Work(
-                id="123", label="123", type="Work", collection_path=collection_path
-            ),
+            "~properties": Work(id="123", label="123", type="Work"),
         }
     )
-
-
-def get_work_with_ancestor(
-    collection_path: str | None, ancestor_collection_path: str | None
-) -> VisibleExtractedWork:
-    fixture = load_json_fixture("ingestor/single_merged.json")
-    fixture |= {"data": {"collectionPath": {"path": collection_path}}}
-    work = VisibleMergedWork.model_validate(fixture)
 
     return VisibleExtractedWork(
         work=work,
@@ -34,7 +29,7 @@ def get_work_with_ancestor(
             id="some_id",
             ancestors=[
                 WorkHierarchyItem(
-                    work=get_ancestor_node(collection_path=ancestor_collection_path),
+                    work=ancestor_node,
                     parts=1,
                 )
             ],
@@ -44,19 +39,59 @@ def get_work_with_ancestor(
 
 
 def test_collection_path_expansion() -> None:
-    extracted = get_work_with_ancestor(
-        collection_path="456/789", ancestor_collection_path="123/456"
-    )
+    extracted = get_work_with_ancestor()
+    extracted.work.data.collection_path = CollectionPath(path="456/789")
+    extracted.hierarchy.ancestors[0].work.properties.collection_path = "123/456"
     assert QueryWorkTransformer(extracted).collection_path == "123/456/789"
 
-    extracted = get_work_with_ancestor(
-        collection_path="456/789", ancestor_collection_path="789"
-    )
+    extracted.work.data.collection_path = CollectionPath(path="456/789")
+    extracted.hierarchy.ancestors[0].work.properties.collection_path = "789"
     assert QueryWorkTransformer(extracted).collection_path == "456/789"
 
 
 def test_collection_path_no_expansion() -> None:
-    extracted = get_work_with_ancestor(
-        collection_path="123/456/789", ancestor_collection_path="456/789"
-    )
+    extracted = get_work_with_ancestor()
+    extracted.work.data.collection_path = CollectionPath(path="123/456/789")
+    extracted.hierarchy.ancestors[0].work.properties.collection_path = "456/789"
     assert QueryWorkTransformer(extracted).collection_path == "123/456/789"
+
+
+def test_series_ancestor_deduplication() -> None:
+    extracted = get_work_with_ancestor()
+    extracted.work.state.relations = WorkRelations(
+        ancestors=[
+            WorkAncestor(
+                title="Some ancestor title",
+                work_type="Series",
+                depth=0,
+                num_children=0,
+                num_descendents=0,
+            )
+        ]
+    )
+
+    extracted.hierarchy.ancestors[0].work.properties.label = "Some ancestor title."
+    assert list(QueryWorkTransformer(extracted).part_of_titles) == [
+        "Some ancestor title."
+    ]
+
+
+def test_series_ancestor_no_deduplication() -> None:
+    extracted = get_work_with_ancestor()
+    extracted.work.state.relations = WorkRelations(
+        ancestors=[
+            WorkAncestor(
+                title="Some series title",
+                work_type="Series",
+                depth=0,
+                num_children=0,
+                num_descendents=0,
+            )
+        ]
+    )
+
+    extracted.hierarchy.ancestors[0].work.properties.label = "Some ancestor title."
+    assert list(QueryWorkTransformer(extracted).part_of_titles) == [
+        "Some series title",
+        "Some ancestor title.",
+    ]
