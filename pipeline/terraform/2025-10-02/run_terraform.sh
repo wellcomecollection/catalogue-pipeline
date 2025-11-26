5428972015a74185e2fabe33b0fc5ef7dde038c6
Štěpán Brychta <stepan.brychta@me.com>
2025-11-26 16:49:26 +0000

    Add ES mappings dot notation checker

--- pipeline/terraform/2025-10-02/run_terraform.sh
+++ pipeline/terraform/2025-10-02/run_terraform.sh
@@ -3,6 +3,8 @@
 set -o errexit
 set -o nounset
 
+ROOT=$(git rev-parse --show-toplevel)
+
 # Get the path to the current directory, which we can use to find the
 # 'scripts' folder and the date of the current pipeline.
 #
@@ -17,6 +19,12 @@ export PIPELINE_DATE="$(basename "$THIS_DIR")"
 
 # Get the Elastic Cloud API key, which we need for the EC provider
 EC_API_KEY=$(. "$SCRIPT_DIR/get_elastic_cloud_api_key.sh")
+
+# Check that we do not use dot notation in ES mapping files. See `index_config/README.md` for more info.
+if ! python3 "$SCRIPT_DIR"/dot_notation_checker.py "$ROOT"/index_config; then
+  echo >&2
+  exit 1
+fi
 
 # Now run Terraform itself, passing any arguments directly to the underlying
 # Terraform binary.
