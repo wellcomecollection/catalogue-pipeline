dc69516d47d7082264ad38322aade75bcf1ab9bb
Štěpán Brychta <stepan.brychta@me.com>
2026-01-12 11:53:57 +0000

    Refactoring

--- pipeline/terraform/modules/pipeline/state_machine_transformers.tf
+++ pipeline/terraform/modules/pipeline/state_machine_transformers.tf
@@ -19,7 +19,7 @@ module "transformer_lambda" {
   timeout     = 600
 
   vpc_config = {
-    subnet_ids = local.network_config.subnets
+    subnet_ids         = local.network_config.subnets
     security_group_ids = [
       aws_security_group.egress.id,
       local.network_config.ec_privatelink_security_group_id,
@@ -34,40 +34,28 @@ module "transformer_lambda" {
   }
 }
 
-# Attach read-only Iceberg access policy to transformer lambda
+# Attach read-only Iceberg access policies to transformer lambda
 resource "aws_iam_role_policy" "transformer_lambda_iceberg_read" {
+  for_each = local.transformer_types
+
   role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_s3tables_read["ebsco"].json
+  policy = data.aws_iam_policy_document.adapter_s3tables_read[each.key].json
 }
 
-# Attach read-only Iceberg access policy to transformer lambda for Axiell
-resource "aws_iam_role_policy" "transformer_lambda_iceberg_read_axiell" {
-  role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_s3tables_read["axiell"].json
-}
-
-# Attach S3 read policy to transformer lambda
+# Attach S3 read policies to transformer lambda
 resource "aws_iam_role_policy" "transformer_lambda_s3_read" {
+  for_each = local.transformer_types
+
   role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_bucket_read["ebsco"].json
+  policy = data.aws_iam_policy_document.adapter_bucket_read[each.key].json
 }
 
-# Attach Axiell S3 read policy to transformer lambda
-resource "aws_iam_role_policy" "transformer_lambda_axiell_s3_read" {
-  role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_bucket_read["axiell"].json
-}
-
-# Attach S3 write policy to transformer lambda
+# Attach S3 write policies to transformer lambda
 resource "aws_iam_role_policy" "transformer_lambda_s3_write" {
-  role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_bucket_write["ebsco"].json
-}
+  for_each = local.transformer_types
 
-# Attach Axiell S3 write policy to transformer lambda
-resource "aws_iam_role_policy" "transformer_lambda_axiell_s3_write" {
   role   = module.transformer_lambda.lambda_role.name
-  policy = data.aws_iam_policy_document.adapter_bucket_write["axiell"].json
+  policy = data.aws_iam_policy_document.adapter_bucket_write[each.key].json
 }
 
 # Allow transformer to read pipeline storage secrets
@@ -81,13 +69,13 @@ resource "aws_iam_role_policy" "transformer_lambda_pipeline_storage_secret_read"
 locals {
   transformer_state_machine_definition = jsonencode({
     StartAt = "TransformerStep"
-    States = {
+    States  = {
       TransformerStep = {
         Type      = "Task"
         Resource  = module.transformer_lambda.lambda.arn
         InputPath = "$.detail"
         Next      = "ShouldRunIdMinter"
-        Retry = [
+        Retry     = [
           {
             ErrorEquals     = ["Lambda.ServiceException", "Lambda.AWSLambdaException", "Lambda.SdkClientException"]
             IntervalSeconds = 2
@@ -97,7 +85,7 @@ locals {
         ]
       }
       "ShouldRunIdMinter" = {
-        Type = "Choice"
+        Type    = "Choice"
         Choices = [
           {
             Variable     = "$$.Execution.Input.detail.transformer_type"
@@ -111,8 +99,8 @@ locals {
         Type                  = "Map"
         MaxConcurrency        = 2
         ToleratedFailureCount = 0
-        ItemReader = {
-          Resource = "arn:aws:states:::s3:getObject"
+        ItemReader            = {
+          Resource     = "arn:aws:states:::s3:getObject"
           ReaderConfig = {
             InputType = "JSONL"
           }
@@ -133,10 +121,10 @@ locals {
             ExecutionType = "STANDARD"
           }
           StartAt = "IdMinterStep"
-          States = {
+          States  = {
             IdMinterStep = {
-              Type     = "Task"
-              Resource = module.id_minter_lambda_step_function.lambda_arn
+              Type           = "Task"
+              Resource       = module.id_minter_lambda_step_function.lambda_arn
               ResultSelector = {
                 "failures.$" = "$.failures"
                 "jobId.$"    = "$.jobId"
@@ -183,7 +171,7 @@ module "transformer_state_machine" {
 
   name                     = "transformer-${var.pipeline_date}"
   state_machine_definition = local.transformer_state_machine_definition
-  invokable_lambda_arns = [
+  invokable_lambda_arns    = [
     module.transformer_lambda.lambda.arn,
     module.id_minter_lambda_step_function.lambda_arn
   ]
@@ -241,7 +229,7 @@ module "reindex_transformer_trigger" {
   event_pattern = {
     source        = ["weco.pipeline.reindex"],
     "detail-type" = ["weco.pipeline.reindex.requested"],
-    detail = {
+    detail        = {
       reindex_targets = [each.value.reindex_target_value]
     }
   }
