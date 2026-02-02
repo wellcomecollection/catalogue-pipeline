29d3bee7e5946289bf92c2549d4d25f35a4dd5f7
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2026-02-02 15:53:25 +0000

    Apply auto-formatting rules

--- catalogue_graph/infra/adapters/s3_tables.tf
+++ catalogue_graph/infra/adapters/s3_tables.tf
@@ -2,7 +2,7 @@ resource "aws_s3tables_table_bucket" "table_bucket" {
   name = "wellcomecollection-platform-ebsco-adapter"
   encryption_configuration = {
     sse_algorithm = "AES256"
-    kms_key_arn = null
+    kms_key_arn   = null
   }
 }
 
@@ -15,7 +15,7 @@ resource "aws_s3tables_table_bucket" "axiell_table_bucket" {
   name = "wellcomecollection-platform-axiell-adapter"
   encryption_configuration = {
     sse_algorithm = "AES256"
-    kms_key_arn = null
+    kms_key_arn   = null
   }
 }
 
