4cf748daa08821b803d3ca924a8bd70d98ad4aa1
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2026-02-06 15:00:03 +0000

    Apply auto-formatting rules

--- pipeline/terraform/modules/pipeline/service_transformers.tf
+++ pipeline/terraform/modules/pipeline/service_transformers.tf
@@ -97,7 +97,7 @@ module "sqs_id_minter_bridge" {
   # machine driven instead of being triggered directly by the transformer output topic.
   #
   # sns_topic_arns  = local.transformer_output_topic_arns
-  lambda_arn     = module.id_minter_lambda_step_function.lambda_arn
+  lambda_arn = module.id_minter_lambda_step_function.lambda_arn
 
   batch_size                         = 75
   maximum_batching_window_in_seconds = 60
