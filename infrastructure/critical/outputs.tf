959709c53dcb32e948952edd743b85edbdd20b41
Robert Kenny <kenoir@users.noreply.github.com>
2026-02-10 08:51:53 +0000

    Provision identifiers-v2 RDS cluster for migration testing (#3223)
    * Add Step Functions export of id-minter RDS backups to S3 as Parquet
    
    * Provision identifiers-v2 RDS cluster for migration testing

--- infrastructure/critical/outputs.tf
+++ infrastructure/critical/outputs.tf
@@ -4,6 +4,18 @@ output "rds_serverless_cluster_id" {
   value = module.identifiers_serverless_rds_cluster.rds_cluster_id
 }
 
+output "rds_v2_serverless_cluster_id" {
+  value = module.identifiers_v2_serverless_rds_cluster.rds_cluster_id
+}
+
+output "rds_v2_serverless_cluster_arn" {
+  value = module.identifiers_v2_serverless_rds_cluster.rds_cluster_arn
+}
+
+output "rds_v2_access_security_group_id" {
+  value = aws_security_group.rds_v2_ingress_security_group.id
+}
+
 output "rds_subnet_group_name" {
   value = aws_db_subnet_group.default.name
 }
--- infrastructure/critical/rds_id_minter.tf
+++ infrastructure/critical/rds_id_minter.tf
@@ -64,6 +64,70 @@ module "identifiers_serverless_rds_cluster" {
   engine_version = "8.0.mysql_aurora.3.08.2"
 }
 
+resource "aws_security_group" "database_v2_sg" {
+  description = "Allows connection to identifiers-v2 RDS instance via TCP and egress to the world"
+  vpc_id      = local.vpc_id_new
+  name        = "identifiers_v2_database_sg"
+
+  ingress {
+    protocol  = "tcp"
+    from_port = 3306
+    to_port   = 3306
+
+    cidr_blocks = [
+      local.admin_cidr_ingress,
+    ]
+  }
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+
+    security_groups = [aws_security_group.rds_v2_ingress_security_group.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+module "identifiers_v2_serverless_rds_cluster" {
+  source = "./modules/rds-serverless"
+
+  cluster_identifier = "identifiers-v2-serverless"
+  database_name      = "identifiers"
+  master_username    = local.rds_username
+  master_password    = local.rds_password
+
+  db_security_group_id     = aws_security_group.database_v2_sg.id
+  aws_db_subnet_group_name = aws_db_subnet_group.default.name
+
+  max_scaling_capacity = 16
+
+  engine_version = "8.0.mysql_aurora.3.08.2"
+}
+
+resource "aws_security_group" "rds_v2_ingress_security_group" {
+  name        = "pipeline_rds_v2_ingress_security_group"
+  description = "Allow traffic to identifiers-v2 rds database"
+  vpc_id      = local.vpc_id_new
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  tags = {
+    Name = "pipeline-rds-v2-access"
+  }
+}
+
 
 resource "aws_security_group" "rds_ingress_security_group" {
   name        = "pipeline_rds_ingress_security_group"
