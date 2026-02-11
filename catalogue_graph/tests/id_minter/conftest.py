2753b37cd70e00c326a1b5de5514c915e036b9c8
Paul Butcher <paul.butcher@gmail.com>
2026-02-11 12:08:14 +0000

    DRY out the tests

--- catalogue_graph/tests/id_minter/conftest.py
+++ catalogue_graph/tests/id_minter/conftest.py
@@ -1,11 +1,12 @@
 from typing import Any
 
-import pytest
 import sqlite3
+import pytest
+from unittest import mock
 
 
 @pytest.fixture(scope="function")
-def sqlite_canonical_ids_db(tmp_path) -> sqlite3.Connection:
+def ids_db(tmp_path) -> sqlite3.Connection:
     db_path = tmp_path / "test.db"
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -22,3 +23,23 @@ def sqlite_canonical_ids_db(tmp_path) -> sqlite3.Connection:
     conn.commit()
     yield conn
     conn.close()
+
+
+@pytest.fixture(scope="function")
+def mock_generate_ids(request) -> None:
+    with mock.patch("id_minter.identifiers.generate_ids", side_effect=lambda count: request.param[count]):
+        yield
+
+
+@pytest.fixture(scope="function")
+def free_ids() -> list[str]:
+    return [
+        f"aaaaaaa{i}" for i in range(2, 5)
+    ]
+
+
+@pytest.fixture(scope="function")
+def assigned_ids() -> list[str]:
+    return [
+        f"bbbbbbb{i}" for i in range(2, 5)
+    ]
--- catalogue_graph/tests/id_minter/test_pregenerate.py
+++ catalogue_graph/tests/id_minter/test_pregenerate.py
@@ -4,11 +4,11 @@ from id_minter.pregenerate import top_up_ids, get_free_id_count
 from id_minter import identifiers
 
 
-def preload_ids(conn, free_ids, assigned_ids):
+def preload_ids(ids_db, free_ids, assigned_ids):
     """
     Set up the database with a given set of free and assigned ids.
     """
-    cursor = conn.cursor()
+    cursor = ids_db.cursor()
     cursor.executemany(
         """
         INSERT INTO canonical_ids (CanonicalId, Status) VALUES (?, ?)
@@ -16,14 +16,14 @@ def preload_ids(conn, free_ids, assigned_ids):
         [(id, "free") for id in free_ids] +
         [(id, "assigned") for id in assigned_ids]
     )
-    conn.commit()
+    ids_db.commit()
 
 
-def assertTableLooksLike(conn, rows: list[tuple[str, str]]):
-    cursor = conn.cursor()
+def assert_table_looks_like(ids_db, rows: list[tuple[str, str]], where_clause: str = ""):
+    cursor = ids_db.cursor()
     cursor.execute(
-        """
-        SELECT CanonicalId, Status FROM canonical_ids ORDER BY CanonicalId
+        f"""
+        SELECT CanonicalId, Status FROM canonical_ids ORDER BY CanonicalId {where_clause}
         """
     )
     actual_rows = cursor.fetchall()
@@ -34,28 +34,19 @@ def assertTableLooksLike(conn, rows: list[tuple[str, str]]):
     assert actual_rows == sorted(rows)
 
 
-def test_adequate_free_ids_does_nothing(sqlite_canonical_ids_db):
-    conn = sqlite_canonical_ids_db
-    # GIVEN
-    free_ids = [
-        f"aaaaaaa{i}" for i in range(2, 7)
-    ]
-    assigned_ids = [
-        f"bbbbbbb{i}" for i in range(2, 5)
-    ]
-    preload_ids(conn, free_ids, assigned_ids)
+def test_adequate_free_ids_does_nothing(ids_db, free_ids, assigned_ids):
+    preload_ids(ids_db, free_ids, assigned_ids)
     # WHEN
-    top_up_ids(conn, 5)
+    top_up_ids(ids_db, 3)
     # THEN
-    assertTableLooksLike(
-        conn,
+    assert_table_looks_like(
+        ids_db,
         [(id, "free") for id in free_ids] +
         [(id, "assigned") for id in assigned_ids]
     )
 
 
-def test_inadequate_free_ids_generates_more(sqlite_canonical_ids_db):
-    conn = sqlite_canonical_ids_db
+def test_inadequate_free_ids_generates_more(ids_db):
     # GIVEN there are only three free ids
     free_ids = [
         f"aaaaaaa{i}" for i in range(2, 5)
@@ -64,62 +55,53 @@ def test_inadequate_free_ids_generates_more(sqlite_canonical_ids_db):
         f"bbbbbbb{i}" for i in range(2, 5)
     ]
 
-    preload_ids(conn, free_ids, assigned_ids)
-    assert get_free_id_count(conn) < 5
+    preload_ids(ids_db, free_ids, assigned_ids)
+    assert get_free_id_count(ids_db) < 5
     # WHEN
-    new_ids = top_up_ids(conn, 5)
-    assert get_free_id_count(conn) == 5
+    new_ids = top_up_ids(ids_db, 5)
+    assert get_free_id_count(ids_db) == 5
     # THEN
-    assertTableLooksLike(
-        conn,
+    assert_table_looks_like(
+        ids_db,
         [(id, "free") for id in free_ids + new_ids] +
         [(id, "assigned") for id in assigned_ids]
     )
 
 
-def test_clashes(sqlite_canonical_ids_db):
-    conn = sqlite_canonical_ids_db
-    # GIVEN there are only three free ids
-    free_ids = [
-        f"aaaaaaa{i}" for i in range(2, 5)
-    ]
-    assigned_ids = [
-        f"bbbbbbb{i}" for i in range(2, 5)
-    ]
+@pytest.mark.parametrize('mock_generate_ids', [{
+    5: [
+        "aaaaaaa3",  # this id clashes with an existing free id
+        "aaaaaaa4",  # this id clashes with an existing free id
+        "bbbbbbb4",  # this id clashes with an existing assigned id
+        "aaaaaaa6",  # this id is new and should be added to the database
+        "aaaaaaa7",  # this id is new and should be added to the database
+    ],
+    3: ["njn9f485", "vssjb422", "yp8psp82"]  # All new ids
+}], indirect=["mock_generate_ids"])
+def test_clashes(ids_db, mock_generate_ids, free_ids, assigned_ids):
+    preload_ids(ids_db, free_ids, assigned_ids)
 
-    preload_ids(conn, free_ids, assigned_ids)
-    print(get_free_id_count(conn))
-    assert get_free_id_count(conn) < 8
-
-    def mock_generate_ids(count: int):
-        # At first, there are three free ids, so topping up to 8 should generate 5 new ids.
-        if count == 5:
-            yield from [
-                "aaaaaaa3",  # this id clashes with an existing free id
-                "aaaaaaa4",  # this id clashes with an existing free id
-                "bbbbbbb4",  # this id clashes with an existing assigned id
-                "aaaaaaa6",  # this id is new and should be added to the database
-                "aaaaaaa7",  # this id is new and should be added to the database
-            ]
-        # After the first attempt, where 3 of the 5 clashed,
-        # there should now be 5 free ids (the original 3, plus the 2 new ones),
-        # so the second attempt to top up to 8 should generate 3 new ids.
-        elif count == 3:
-            yield from ["njn9f485", "vssjb422", "yp8psp82"]
-        else:
-            raise ValueError(f"Unexpected count: {count}")
-
-    with mock.patch("id_minter.identifiers.generate_ids", new=mock_generate_ids):
-        top_up_ids(conn, 8)
-    assert get_free_id_count(conn) == 8
-    assertTableLooksLike(
-        conn,
+    assert get_free_id_count(ids_db) < 8
+    top_up_ids(ids_db, 8)
+    assert get_free_id_count(ids_db) == 8
+    assert_table_looks_like(
+        ids_db,
         [(id, "free") for id in free_ids + ["aaaaaaa6", "aaaaaaa7", "njn9f485", "vssjb422", "yp8psp82"]] +
         [(id, "assigned") for id in assigned_ids]
     )
 
 
-def test_persistent_clashes(sqlite_canonical_ids_db):
+@pytest.mark.parametrize('mock_generate_ids', [{
+    5: [
+        "aaaaaaa3",  # this id clashes with an existing free id
+        "aaaaaaa4",  # this id clashes with an existing free id
+        "bbbbbbb4",  # this id clashes with an existing assigned id
+        "aaaaaaa6",  # this id is new and should be added to the database
+        "aaaaaaa7",  # this id is new and should be added to the database
+    ],
+    3: ["aaaaaaa3", "vssjb422", "yp8psp82"]  # Only two of these are new
+}], indirect=["mock_generate_ids"])
+def test_persistent_clashes(ids_db, mock_generate_ids, free_ids, assigned_ids):
     """
     top_up_ids makes two attempts to top up to the desired count of free ids.
     It is possible that the second attempt could also generate clashing ids.
@@ -134,42 +116,12 @@ def test_persistent_clashes(sqlite_canonical_ids_db):
     and do not keep the pool topped up to the desired count, we should still have enough free ids
     for normal operations.
     """
-    conn = sqlite_canonical_ids_db
-    free_ids = [
-        f"aaaaaaa{i}" for i in range(2, 5)
-    ]
-    assigned_ids = [
-        f"bbbbbbb{i}" for i in range(2, 5)
-    ]
-
-    preload_ids(conn, free_ids, assigned_ids)
-    print(get_free_id_count(conn))
-    assert get_free_id_count(conn) < 8
-
-    def mock_generate_ids(count: int):
-        # At first, there are three free ids, so topping up to 8 should generate 5 new ids.
-        if count == 5:
-            yield from [
-                "aaaaaaa3",  # this id clashes with an existing free id
-                "aaaaaaa4",  # this id clashes with an existing free id
-                "bbbbbbb4",  # this id clashes with an existing assigned id
-                "aaaaaaa6",  # this id is new and should be added to the database
-                "aaaaaaa7",  # this id is new and should be added to the database
-            ]
-        # After the first attempt, where 3 of the 5 clashed,
-        # there should now be 5 free ids (the original 3, plus the 2 new ones),
-        # so the second attempt to top up to 8 should generate 3 new ids.
-        elif count == 3:
-            yield from ["aaaaaaa3", "vssjb422", "yp8psp82"]
-        else:
-            raise ValueError(f"Unexpected count: {count}")
-
-    with mock.patch("id_minter.identifiers.generate_ids", new=mock_generate_ids):
-        top_up_ids(conn, 8)
-
-    assert get_free_id_count(conn) == 7
-    assertTableLooksLike(
-        conn,
+    preload_ids(ids_db, free_ids, assigned_ids)
+    assert get_free_id_count(ids_db) < 8
+    top_up_ids(ids_db, 8)
+    assert get_free_id_count(ids_db) == 7
+    assert_table_looks_like(
+        ids_db,
         [(id, "free") for id in free_ids + ["aaaaaaa6", "aaaaaaa7", "vssjb422", "yp8psp82"]] +
         [(id, "assigned") for id in assigned_ids]
     )
