fe40b365e219b64b51103a6cc83be45c91d7867d
Štěpán Brychta <stepan.brychta@me.com>
2026-02-23 15:04:02 +0000

    Restrict cluster ingress to TCP 8182

--- catalogue_graph/infra/graph/modules/catalogue_graph/security_groups.tf
+++ catalogue_graph/infra/graph/modules/catalogue_graph/security_groups.tf
@@ -3,11 +3,13 @@ resource "aws_security_group" "neptune_security_group" {
   vpc_id = data.aws_vpc.vpc.id
 }
 
-# Allow any ingress traffic so that we can reach the cluster via its public endpoint.
+# Allow any ingress traffic on port 8182 so that we can reach the cluster via its public endpoint.
 resource "aws_vpc_security_group_ingress_rule" "neptune_ingress" {
   security_group_id = aws_security_group.neptune_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
+  ip_protocol       = "tcp"
+  from_port         = 8182
+  to_port           = 8182
 }
 
 # Allow any egress traffic. The Neptune cluster needs to be able to reach the bulk loader S3 bucket.
