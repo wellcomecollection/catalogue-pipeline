e84ab796ae09162a7226336e9b949919ea48d530
Agnes Garoux <a.garoux@wellcome.ac.uk>
2026-03-16 12:25:38 +0000

    switch lambdas and wire it up downstream

--- pipeline/terraform/modules/pipeline/id_minter/outputs.tf
+++ pipeline/terraform/modules/pipeline/id_minter/outputs.tf
@@ -2,6 +2,10 @@ output "id_generator_lambda_arn" {
   value = module.id_generator_lambda.lambda_arn
 }
 
+output "id_minter_lambda_arn" {
+  value = module.id_minter_lambda.lambda_arn
+}
+
 output "id_minter_lambda_role_name" {
   value = module.id_minter_lambda.lambda_role_name
 }
