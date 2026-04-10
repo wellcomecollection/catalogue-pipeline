ef4113705a467f8968cd96d7cfeaa6596cc290cf
Agnes Garoux <a.garoux@wellcome.ac.uk>
2026-04-10 15:53:49 +0100

    reverting: this was due to EC outage

--- pipeline/terraform/modules/pipeline/elastic/provider.tf
+++ pipeline/terraform/modules/pipeline/elastic/provider.tf
@@ -27,6 +27,6 @@ provider "elasticstack" {
   elasticsearch {
     username  = ec_deployment.pipeline.elasticsearch_username
     password  = ec_deployment.pipeline.elasticsearch_password
-    endpoints = ["https://${ec_deployment.pipeline.elasticsearch[0].resource_id}.es.${ec_deployment.pipeline.elasticsearch[0].region}.aws.elastic-cloud.com:443"]
+    endpoints = [ec_deployment.pipeline.elasticsearch[0].https_endpoint]
   }
 }
