2b7032f0d122b8aa5bba44e1da539c34504b56ec
Štěpán Brychta <stepan.brychta@me.com>
2026-05-07 15:28:50 +0100

    Remove graph pipeline shims (#3361)

--- catalogue_graph/src/graph_remover_incremental.py
+++
@@ -1,5 +0,0 @@
-# Shim — real code is in graph/steps/graph_remover_incremental.py
-from graph.steps.graph_remover_incremental import *  # noqa: F401, F403
-
-if __name__ == "__main__":
-    local_handler()  # noqa: F405
