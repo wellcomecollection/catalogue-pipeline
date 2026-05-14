4f9e1dc891feff02457d4a20ebf06a0ff065b606
Agnes Garoux <a.garoux@wellcome.ac.uk>
2026-05-14 17:25:14 +0100

    explicit index names + enabled namespacing

--- pipeline/terraform/modules/pipeline/merger/data.tf
+++ pipeline/terraform/modules/pipeline/merger/data.tf
@@ -1,6 +1,5 @@
 locals {
-  namespace_suffix = var.index_date != null ? "-${var.index_date}" : ""
-  namespace        = "catalogue-${var.pipeline_date}${local.namespace_suffix}"
+  namespace = "catalogue-${var.pipeline_date}_${var.service_name}"
 
   es_upstream_config = {
     es_upstream_host     = var.es_config.es_host
@@ -14,10 +13,4 @@ locals {
     es_downstream_protocol = var.es_config.es_protocol
     es_downstream_apikey   = var.es_config.es_apikey
   }
-
-  index_date = var.index_date != null ? var.index_date : var.pipeline_date
-
-  es_works_identified_index   = var.es_index_config.es_works_identified_index != null ? var.es_index_config.es_works_identified_index : "works-identified-${local.index_date}"
-  es_works_denormalised_index = var.es_index_config.es_works_denormalised_index != null ? var.es_index_config.es_works_denormalised_index : "works-denormalised-${local.index_date}"
-  es_images_initial_index     = var.es_index_config.es_images_initial_index != null ? var.es_index_config.es_images_initial_index : "images-initial-${local.index_date}"
 }
--- pipeline/terraform/modules/pipeline/merger/main.tf
+++ pipeline/terraform/modules/pipeline/merger/main.tf
@@ -1,47 +1,23 @@
-module "merger_works_output_topic" {
-  source = "../../topic"
-
-  name       = "${local.namespace}_merger_works_output"
-  role_names = [module.merger_lambda.lambda_role_name]
-}
-
-module "merger_works_path_output_topic" {
-  source = "../../topic"
-
-  name       = "${local.namespace}_merger_works_with_path_output"
-  role_names = [module.merger_lambda.lambda_role_name]
-}
-
-module "merger_works_incomplete_path_output_topic" {
-  source = "../../topic"
-
-  name       = "${local.namespace}_merger_works_incomplete_path_output"
-  role_names = [module.merger_lambda.lambda_role_name]
-}
-
 module "merger_images_output_topic" {
   source = "../../topic"
 
-  name       = "${local.namespace}_merger_images_output"
+  name       = "${local.namespace}_images_output"
   role_names = [module.merger_lambda.lambda_role_name]
 }
 
 module "merger_lambda" {
   source = "../../pipeline_lambda"
 
-  service_name  = "merger${local.namespace_suffix}"
+  service_name  = var.service_name
   pipeline_date = var.pipeline_date
   vpc_config    = var.vpc_config
 
   environment_variables = {
-    merger_works_topic_arn             = module.merger_works_output_topic.arn
-    merger_paths_topic_arn             = module.merger_works_path_output_topic.arn
-    merger_path_concatenator_topic_arn = module.merger_works_incomplete_path_output_topic.arn
     merger_images_topic_arn            = module.merger_images_output_topic.arn
 
-    es_identified_works_index   = local.es_works_identified_index
-    es_denormalised_works_index = local.es_works_denormalised_index
-    es_initial_images_index     = local.es_images_initial_index
+    es_identified_works_index   = var.es_works_identified_index
+    es_denormalised_works_index = var.es_works_denormalised_index
+    es_initial_images_index     = var.es_images_initial_index
   }
 
   secret_env_vars = merge(
