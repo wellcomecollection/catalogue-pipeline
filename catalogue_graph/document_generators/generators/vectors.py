e0ef02b232ed5d651e9c69d1f399d491de213955
Štěpán Brychta <stepan.brychta@me.com>
2026-06-02 13:06:37 +0100

    Round image vectors

--- catalogue_graph/document_generators/generators/vectors.py
+++ catalogue_graph/document_generators/generators/vectors.py
@@ -6,11 +6,11 @@ from .random import rng
 def random_vector(d: int, max_r: float = 1.0) -> list[float]:
     rand = _normalize(_random_normal(d))
     r = max_r * math.pow(rng.random(), 1.0 / d)
-    return _scalar_multiply(r, rand)
+    return _round_vector(_scalar_multiply(r, rand))
 
 
 def random_unit_length_vector(d: int) -> list[float]:
-    return _normalize(_random_normal(d))
+    return _round_vector(_normalize(_random_normal(d)))
 
 
 def _random_normal(d: int) -> list[float]:
@@ -24,3 +24,8 @@ def _normalize(vec: list[float]) -> list[float]:
 
 def _scalar_multiply(a: float, vec: list[float]) -> list[float]:
     return [a * x for x in vec]
+
+
+def _round_vector(vec: list[float]) -> list[float]:
+    """Round to 12 significant digits to eliminate platform-dependent ULP differences."""
+    return [round(x, 12) for x in vec]
