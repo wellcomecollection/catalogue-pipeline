012993f7ef54c974d6a414c9a360f3e34567baa9
Štěpán Brychta <stepan.brychta@me.com>
2026-06-15 15:14:10 +0100

    Add bdd tests for ownership notes

--- catalogue_graph/tests/adapters/transformers/axiell/bdd/test_notes.py
+++ catalogue_graph/tests/adapters/transformers/axiell/bdd/test_notes.py
@@ -2,3 +2,4 @@ from pytest_bdd import scenarios
 
 scenarios("../../marc/features/exhibitions_note.feature")
 scenarios("../../marc/features/location_of_note.feature")
+scenarios("../../marc/features/ownership_note.feature")
