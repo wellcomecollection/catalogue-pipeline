c8a14d0d46c3b26cf534b025c8cbd03bc0ef8804
Štěpán Brychta <stepan.brychta@me.com>
2026-06-16 09:45:27 +0100

    Reuse link formatting code

--- catalogue_graph/src/adapters/transformers/utils/html.py
+++ catalogue_graph/src/adapters/transformers/utils/html.py
@@ -1,5 +1,9 @@
 from urllib.parse import urlparse
 
+import structlog
+
+logger = structlog.get_logger(__name__)
+
 
 def is_url(maybe_url: str) -> bool:
     """
@@ -17,3 +21,13 @@ def is_url(maybe_url: str) -> bool:
     # So we need to do a bit of extra checking to see if it really
     # looks like a URL.
     return bool(url.scheme in ["http", "https"] and url.netloc)
+
+
+def format_as_html_link(maybe_url: str) -> str:
+    """Format $u subfield contents as an HTML link if valid URL, otherwise return contents unchanged."""
+    trimmed = maybe_url.strip()
+    if is_url(trimmed):
+        return f'<a href="{trimmed}">{trimmed}</a>'
+
+    logger.warning("$u subfield doesn't look like a URL", value=maybe_url)
+    return maybe_url
