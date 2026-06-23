4329532cebc57da5e21f510a58b29a82264b4b73
Štěpán Brychta <stepan.brychta@me.com>
2026-06-23 17:03:28 +0100

    Update notes.py

--- catalogue_graph/src/adapters/transformers/axiell/notes.py
+++ catalogue_graph/src/adapters/transformers/axiell/notes.py
@@ -1,6 +1,9 @@
 from pymarc.record import Record
 
-from adapters.transformers.marc.notes import _create_note
+from adapters.transformers.axiell.organisation_and_arrangement import (
+    extract_arrangement,
+)
+from adapters.transformers.marc.notes import ARRANGEMENT_NOTE, _create_note
 from adapters.transformers.marc.notes import extract_notes as base_extract_notes
 from models.pipeline.id_label import IdLabel
 from models.pipeline.note import Note
@@ -12,6 +15,12 @@ def extract_notes(record: Record) -> list[Note]:
     notes = base_extract_notes(record, exclude_fields=["506", "540"])
 
     for field in record.get_fields("590"):
-        _create_note(field, note_type=FINDING_AIDS)
+        finding_aids_note = _create_note(field, note_type=FINDING_AIDS)
+        if finding_aids_note:
+            notes.append(finding_aids_note)
+
+    arrangement = extract_arrangement(record)
+    if arrangement:
+        notes.append(Note(contents=arrangement, note_type=ARRANGEMENT_NOTE))
 
     return notes
