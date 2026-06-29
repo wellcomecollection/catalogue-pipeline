91080badd8be3232654649f45ce2fbb84d840b4d
Štěpán Brychta <stepan.brychta@me.com>
2026-06-29 17:42:28 +0100

    Update production.py

--- catalogue_graph/src/adapters/transformers/axiell/production.py
+++ catalogue_graph/src/adapters/transformers/axiell/production.py
@@ -26,19 +26,6 @@ def _day_end(d: date) -> date:
     return datetime.combine(d, time.max, tzinfo=UTC)
 
 
-def _get_range_label(start_date: date, end_date: date) -> str:
-    if start_date == end_date:
-        return f"{start_date.day} {start_date.strftime('%B %Y')}"
-
-    if start_date.year == end_date.year:
-        if start_date.month == end_date.month:
-            return f"{start_date.strftime('%B %Y')}"
-
-        return f"{start_date.year}"
-
-    return f"{start_date.year}-{end_date.year}"
-
-
 def extract_production(record: Record) -> list[ProductionEvent]:
     production_label = first_non_empty_subfield("264", "c", record)
     start_date = extract_production_start_date(record)
@@ -47,15 +34,13 @@ def extract_production(record: Record) -> list[ProductionEvent]:
     if production_label is None or start_date is None or end_date is None:
         return []
 
-    time_range_label = _get_range_label(start_date, end_date)
-
     period = Period(
         label=production_label,
-        range=DateTimeRange(
-            **{
+        range=DateTimeRange.model_validate(
+            {
                 "from": _day_start(start_date).strftime("%Y-%m-%dT%H:%M:%SZ"),
                 "to": _day_end(end_date).strftime("%Y-%m-%dT%H:%M:%S.%f") + "999Z",
-                "label": time_range_label,
+                "label": production_label,
             }
         ),
         id=Unidentifiable(),
