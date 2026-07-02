92551cb553fe4f0bd36108cbaf0cfbce7c775bce
Štěpán Brychta <stepan.brychta@me.com>
2026-07-02 09:44:08 +0100

    Add comments

--- catalogue_graph/src/adapters/transformers/axiell/production.py
+++ catalogue_graph/src/adapters/transformers/axiell/production.py
@@ -27,35 +27,37 @@ def _day_end(d: date) -> date:
     return datetime.combine(d, time.max, tzinfo=UTC)
 
 
-def _extract_period_from_dates(record: Record, production_label: str) -> Period:
-    start_date = extract_production_start_date(record)
-    end_date = extract_production_end_date(record)
+def _period_from_dates(
+    production_label: str, start_date: date, end_date: date
+) -> Period:
+    formatted_start = _day_start(start_date).strftime("%Y-%m-%dT%H:%M:%SZ")
 
-    date_range = None
-    if start_date is not None and end_date is not None:
-        formatted_start = _day_start(start_date).strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        # Reproduce the nanosecond precision of the Scala pipeline.
-        formatted_end = _day_end(end_date).strftime("%Y-%m-%dT%H:%M:%S.%f") + "999Z"
-        date_range = DateTimeRange.model_validate(
-            {
-                "from": formatted_start,
-                "to": formatted_end,
-                "label": production_label,
-            }
-        )
+    # Reproduce the nanosecond precision of the Scala pipeline.
+    formatted_end = _day_end(end_date).strftime("%Y-%m-%dT%H:%M:%S.%f") + "999Z"
+    date_range = DateTimeRange.model_validate(
+        {
+            "from": formatted_start,
+            "to": formatted_end,
+            "label": production_label,
+        }
+    )
 
     return Period(label=production_label, range=date_range, id=Unidentifiable())
 
 
 def extract_production(record: Record) -> list[ProductionEvent]:
     production_labels = non_empty_subfields("264", "c", record)
+    start_date = extract_production_start_date(record)
+    end_date = extract_production_end_date(record)
 
     if not production_labels:
         return []
 
-    if len(production_labels) == 1:
-        periods = [_extract_period_from_dates(record, production_labels[0])]
+    # Unlike CALM, Axiell usually returns parsed start and end production dates (stored in MARC 046).
+    # If available, and if there is exactly one production label, use the provided dates instead
+    # of trying to parse the period ourselves from the production label.
+    if len(production_labels) == 1 and start_date is not None and end_date is not None:
+        periods = [_period_from_dates(production_labels[0], start_date, end_date)]
     else:
         periods = [parse_period(label) for label in production_labels]
 
