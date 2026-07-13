1c26721d6306205298b41650ce2e1702cfcaf21f
Agnes Garoux <a.garoux@wellcome.ac.uk>
2026-07-13 11:54:15 +0100

    shared create_config script needs the variable

--- pipeline/terraform/2025-10-02/run_terraform.sh
+++ pipeline/terraform/2025-10-02/run_terraform.sh
@@ -15,6 +15,8 @@ SCRIPT_DIR="$(dirname "$THIS_DIR")/scripts"
 # Create the config file that tells Terraform which pipeline we're running
 # in and where to store the remote state.
 export PIPELINE_DATE="$(basename "$THIS_DIR")"
+# needed for pipeline/terraform/scripts/create_terraform_config_file.sh but unused in pipeline/terraform/2025-10-02
+export ES_CLUSTER_DATE=""
 . $SCRIPT_DIR/create_terraform_config_file.sh
 
 # Get the Elastic Cloud API key, which we need for the EC provider
