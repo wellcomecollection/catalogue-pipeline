9b39c79d992b59cefb78db034da9a4ebc3aeda9a
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2026-07-21 08:21:57 +0000

    Apply auto-formatting rules

--- catalogue_graph/infra/graph/outputs.tf
+++ catalogue_graph/infra/graph/outputs.tf
@@ -2,17 +2,17 @@
 
 # Note: the legacy production cluster uses an empty string ("") graph_date, so prod is keyed by "" in these maps.
 output "neptune_cluster_arns" {
-  value = {for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_arn}
+  value = { for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_arn }
 }
 
 output "neptune_cluster_resource_ids" {
-  value = {for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_resource_id}
+  value = { for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_resource_id }
 }
 
 output "neptune_cluster_endpoint_secret_arns" {
-  value = {for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_endpoint_secret_arn}
+  value = { for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_endpoint_secret_arn }
 }
 
 output "neptune_cluster_data_access_arns" {
-  value = {for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_data_access_arn}
+  value = { for cluster in local.neptune_clusters : cluster.graph_date => cluster.neptune_cluster_data_access_arn }
 }
