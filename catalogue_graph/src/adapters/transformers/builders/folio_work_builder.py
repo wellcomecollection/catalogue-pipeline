28e8680174e61205acb540dd3e99f03785787043
Štěpán Brychta <stepan.brychta@me.com>
2026-09-08 15:42:24 +0100

    Formatting

--- catalogue_graph/src/adapters/transformers/builders/folio_work_builder.py
+++ catalogue_graph/src/adapters/transformers/builders/folio_work_builder.py
@@ -1,8 +1,8 @@
 from adapters.extractors.oai_pmh.folio.enrichment.models import FolioEnrichedInstance
 from adapters.transformers.builders.marc_xml_work_builder import MarcXmlWorkBuilder
 from adapters.transformers.folio.identifier import extract_hrid, extract_instance_uuid
-from adapters.transformers.marc.former_frequency import extract_former_frequency
 from adapters.transformers.marc.edition import extract_edition
+from adapters.transformers.marc.former_frequency import extract_former_frequency
 from adapters.transformers.marc.physical_description import (
     extract_physical_description,
 )
