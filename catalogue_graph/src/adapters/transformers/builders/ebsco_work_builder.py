8396a781d76cb2c41160824b37df8404a4ebdf2e
Štěpán Brychta <stepan.brychta@me.com>
2026-09-08 14:21:24 +0100

    Folio transformer: add designation field

--- catalogue_graph/src/adapters/transformers/builders/ebsco_work_builder.py
+++ catalogue_graph/src/adapters/transformers/builders/ebsco_work_builder.py
@@ -1,7 +1,6 @@
 from adapters.transformers.builders.marc_xml_work_builder import MarcXmlWorkBuilder
 from adapters.transformers.ebsco.contributors import extract_contributors
 from adapters.transformers.ebsco.current_frequency import extract_current_frequency
-from adapters.transformers.ebsco.designation import extract_designation
 from adapters.transformers.ebsco.format import extract_format
 from adapters.transformers.ebsco.genres import extract_genres
 from adapters.transformers.ebsco.holdings import extract_holdings
@@ -9,6 +8,7 @@ from adapters.transformers.ebsco.language import extract_languages
 from adapters.transformers.ebsco.other_identifiers import extract_other_identifiers
 from adapters.transformers.ebsco.production import extract_production
 from adapters.transformers.ebsco.subjects import extract_subjects
+from adapters.transformers.marc.designation import extract_designation
 from adapters.transformers.marc.edition import extract_edition
 from models.pipeline.concept import Contributor, Genre, Subject
 from models.pipeline.format import Format
--- catalogue_graph/src/adapters/transformers/builders/folio_work_builder.py
+++ catalogue_graph/src/adapters/transformers/builders/folio_work_builder.py
@@ -1,6 +1,7 @@
 from adapters.extractors.oai_pmh.folio.enrichment.models import FolioEnrichedInstance
 from adapters.transformers.builders.marc_xml_work_builder import MarcXmlWorkBuilder
 from adapters.transformers.folio.identifier import extract_hrid, extract_instance_uuid
+from adapters.transformers.marc.designation import extract_designation
 from adapters.transformers.marc.edition import extract_edition
 from adapters.transformers.marc.physical_description import (
     extract_physical_description,
@@ -67,6 +68,10 @@ class FolioWorkBuilder(MarcXmlWorkBuilder):
             )
 
         return None
+
+    @property
+    def designation(self) -> list[str]:
+        return extract_designation(self.record)
 
     @property
     def items(self) -> list[Item]:
