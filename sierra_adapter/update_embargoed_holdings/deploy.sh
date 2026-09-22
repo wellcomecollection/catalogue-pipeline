a660cc7178f89e9520e9ecd8f23b6b3194718353
Robert Kenny <r.kenny@wellcome.org>
2026-09-22 16:15:51 +0100

    Move update_embargoed_holdings to Python 3.12 as well
    It set no runtime and inherited python3.9 from the shared Lambda module
    default. Dependabot's anyio bump for it (#3674) needs Python 3.10, so
    this PR is the prerequisite for that one as well as #3673.

--- sierra_adapter/update_embargoed_holdings/deploy.sh
+++ sierra_adapter/update_embargoed_holdings/deploy.sh
@@ -11,6 +11,7 @@ pushd "$ROOT/sierra_adapter/update_embargoed_holdings"
   pip3 install \
     --target . \
     --platform manylinux2014_x86_64 \
+    --python-version 3.12 \
     --only-binary=:all: \
     -r requirements.txt
   zip -r ../update_embargoed_holdings.zip *
