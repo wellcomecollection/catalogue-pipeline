71a4b3be195c2aa44b73d996526dd3e94fa81a2b
Robert Kenny <r.kenny@wellcome.org>
2026-09-23 15:51:13 +0100

    Note the re-enable-then-reharvest order for the bibs pause switch

--- sierra_adapter/terraform/variables.tf
+++ sierra_adapter/terraform/variables.tf
@@ -6,7 +6,8 @@ variable "namespace" {
   default = "sierra-adapter"
 }
 
-# Set false to hold bib changes out of the catalogue while items keep flowing; catch up by reharvesting the missed bib windows.
+# Set false to hold bib changes out of the catalogue while items keep flowing; the progress reporter will alert on the bibs gap.
+# To catch up, re-enable first: build_missing_windows.py only sees a gap once a newer bib window closes it.
 variable "sierra_bibs_updates_enabled" {
   type    = bool
   default = true
